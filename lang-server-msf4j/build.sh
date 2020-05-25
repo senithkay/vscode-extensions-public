c6bdceb26d9f6549ce7575eae66291ac61ce68b9
Kavith Lokuhewage <kaviththiranga@gmail.com>
2020-05-25 14:49:31 +0530

    Change build script to fetch ballerina from product dist

--- lang-server-msf4j/build.sh
+++ lang-server-msf4j/build.sh
@@ -1,5 +1,6 @@
+wget https://dist.ballerina.io/downloads/1.2.4/ballerina-1.2.4.zip
 docker build \
-    --build-arg balDist=jballerina-tools-1.3.0-SNAPSHOT.zip \
-    --build-arg balVersion=1.3.0-SNAPSHOT \
+    --build-arg balDist=ballerina-1.2.4.zip \
+    --build-arg balVersion=1.2.4 \
     -t workspace-langserver:0.0.1 \
     . \
