1e869fcc38356d92fd286dffb6db12510816e2d2
Kavith Lokuhewage <kaviththiranga@gmail.com>
2020-06-26 06:44:30 +0530

    Fix lang-server root path

--- lang-server-msf4j/src/main/java/org/wso2/choreo/workspace/langserver/LangServerEndpoint.java
+++ lang-server-msf4j/src/main/java/org/wso2/choreo/workspace/langserver/LangServerEndpoint.java
@@ -23,7 +23,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
-@ServerEndpoint("/lang-server")
+@ServerEndpoint("/orgs/{org}/apps/{app}/workspace/lang-server")
 public class LangServerEndpoint {
 
     private final static Logger LOGGER = Logger.getLogger(LangServerEndpoint.class.getName());
