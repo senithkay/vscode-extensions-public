306ac310fe51c2a1684b31c615734a9b62c53e09
anjanash <anjanash@wso2.com>
2020-12-02 02:26:12 +0530

    Wired Editor with errors

--- workspace-server-mock/src/index.js
+++ workspace-server-mock/src/index.js
@@ -17,7 +17,54 @@ app.options('*', cors());
 
 const port = 3000;
 
-const defaultContent = "";
+const defaultContent = `
+import ballerina/io;
+import ballerina/test;
+
+function beforeFunc() {
+    // This is the function, which will be executed before the Test functions.
+}
+
+function afterFunc() {
+    // This is the function, which will be executed after the Test functions.
+}
+
+// This test function will not be executed.
+@test:Config {
+    enable: false
+}
+function testFunction1() {
+    io:println("I'm in test function 1!");
+    test:assertTrue(true, msg = "Failed!");
+}
+
+// This test function depends on the "testFunction3".
+@test:Config{  
+    before: "beforeFunc",
+    after: "afterFunc",
+    dependsOn: ["testFunction3"],
+    dataProvider:"dataGen",
+    groups: ["g1"]
+}
+function testFunction2 (int value) returns error? {
+    test:assertEquals(value, 1, msg = "value is not correct");
+}
+
+
+function dataGen() returns (int[][]) {
+    return [[1]];
+}
+
+// This is a random test function. This will randomly execute without depending on the other functions.
+// However, note that "testFunction3" depends on this.
+@test:Config {
+    groups: ["g1", "g2"]
+}
+function testFunction3() {
+    io:println("I'm in test function 3!");
+    test:assertTrue(true, msg = "Failed!");
+}
+`;
 
 const repoPath = path.resolve(__dirname, "..", "repo");
 const dumpPath = path.resolve(__dirname, "..", "dump");
