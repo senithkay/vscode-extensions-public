b164608cac17a33f32193abdf3c8c36417ca8c3a
CharukaK <charukakarunanayake@gmail.com>
2021-03-10 07:48:27 +0530

    Implement parameter and returntype rendering for data-mapper

---
+++ low-code-editor/src/Diagram/components/DataMapper/components/ParameterTypes/RecordType/index.tsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ * associated services.
+ */
+
+import React from 'react';
+import {DataPointViewstate} from "../../../viewstate";
+import {getDataMapperComponent} from "../../../util";
+
+interface RecordTypeProps {
+    viewState: DataPointViewstate;
+    isMain?: boolean;
+}
+
+export function RecordType(props: RecordTypeProps) {
+    const { viewState, isMain } = props;
+
+    const childComponents: any = [];
+
+    viewState.fields.forEach(field => {
+        childComponents.push(getDataMapperComponent(field.type, { viewState: field }))
+    })
+
+    return (
+        <g>
+            <text
+                x={viewState.bBox.x}
+                y={viewState.bBox.y}
+                fontFamily="Verdana"
+                fontSize="15"
+                fontWeight={isMain ? 'bold' : null}
+                fill="blue"
+            >
+                {`${viewState.name ? `${viewState.name}: ` : ''}${viewState.displayType}`}
+            </text>
+            {childComponents}
+        </g>
+    )
+}
---
+++ low-code-editor/src/Diagram/components/DataMapper/components/ParameterTypes/index.ts
@@ -0,0 +1,11 @@
+import { RecordType } from './RecordType';
+import { ValueType } from './ValueType';
+
+export { RecordType as record }
+export { ValueType as string }
+export { ValueType as int }
+export { ValueType as float }
+export { ValueType as xml }
+export { ValueType as json }
+export { ValueType as collection }
+
