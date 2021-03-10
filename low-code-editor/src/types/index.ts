f43ceffa72e6578d7668027aeb5b0b4e1f91a9bf
axewilledge <anjanash@wso2.com>
2021-03-10 09:11:01 +0530

    Diagram with custom ST

--- low-code-editor/src/types/index.ts
+++ low-code-editor/src/types/index.ts
@@ -18,7 +18,7 @@ import { ModulePart, STNode } from "@ballerina/syntax-tree";
 import { Diagnostic } from "monaco-languageclient/lib/monaco-language-client";
 
 import { AppInfo, ApplicationFile } from "../api/models";
-import { ConfigPanelStatus, DiagramState, ExpressionEditorState, Gcalendar, GithubRepo, LowCodeLangClient } from "../Definitions";
+import { ConfigPanelStatus, DiagramState, ExpressionEditorState, LowCodeLangClient } from "../Definitions";
 import { BallerinaConnectorsInfo } from "../Definitions/lang-client-extended";
 
 import {
@@ -64,27 +64,27 @@ export interface AppSplitViews{
 export interface LowCodeEditorProps {
     appInfo?: AppViewState;
     zoomStatus?: any;
-    isLoadingAST: boolean;
-    isMutationProgress: boolean;
-    isWaitingOnWorkspace: boolean;
+    isLoadingAST?: boolean;
+    isMutationProgress?: boolean;
+    isWaitingOnWorkspace?: boolean;
     error?: Error;
-    langServerURL: string;
+    langServerURL?: string;
     langClient?: LowCodeLangClient;
-    workingFile: string;
+    workingFile?: string;
     syntaxTree: ModulePart;
-    stSymbolInfo: STSymbolInfo;
-    isCodeEditorActive: boolean;
-    isConfigPanelOpen: boolean;
-    currentApp: AppInfo;
+    stSymbolInfo?: STSymbolInfo;
+    isCodeEditorActive?: boolean;
+    isConfigPanelOpen?: boolean;
+    currentApp?: AppInfo;
     currentFile?: ApplicationFile;
     exprEditorState?: ExpressionEditorState;
     diagnostics?: Diagnostic[];
     targetPosition?: any;
     isReadOnly?: boolean;
     dispatch?: (fn: any) => void;
-    configPanelStatus: ConfigPanelStatus;
-    connectors: BallerinaConnectorsInfo[];
-    isLoadingSuccess: boolean;
+    configPanelStatus?: ConfigPanelStatus;
+    connectors?: BallerinaConnectorsInfo[];
+    isLoadingSuccess?: boolean;
     connectionData?: ConnectionData;
     userInfo?: UserState;
     onZoomIn?: any;
@@ -100,8 +100,7 @@ export interface LowCodeEditorProps {
     insertComponentStart?: any;
     getAiSuggestions?: any;
     getGsheetList?: any;
-    getGcalendarList?: (orgHandle: string, handler: string) => Promise<Gcalendar[]>;
-    getGithubRepoList?: (orgHandle: string, handler: string, username: string) => Promise<GithubRepo[]>;
+    getGcalendarList?: any;
     getLangClientForCurrentApp?: any;
     oauthSessions?: OauthSessionState;
 }
