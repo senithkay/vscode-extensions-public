54590537015389082909815e49f5c1d12b99cade
YashodhaSamarawickrama <yasho.samarawickrama@gmail.com>
2021-03-16 17:33:53 +0530

    Fix lint issues

--- low-code-editor/src/ErrorBoundrary/Error/style.ts
+++ low-code-editor/src/ErrorBoundrary/Error/style.ts
@@ -4,7 +4,7 @@ export const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             flexGrow: 1,
-            marginTop:'16rem'
+            marginTop: '16rem'
         },
         errorContainer: {
             display: "flex",
