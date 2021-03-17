356e1ad07f36fc12f007f9413afb90e099f47b49
“kajendran” <a.kajendran@gmail.com>
2021-03-17 16:56:56 +0530

    Modify expression editor story initial state

--- low-code-editor/src/Diagram/components/Portals/ConfigForm/Elements/ExpressionEditor/index.stories.tsx
+++ low-code-editor/src/Diagram/components/Portals/ConfigForm/Elements/ExpressionEditor/index.stories.tsx
@@ -43,9 +43,37 @@ const args: FormElementProps = {
     },
     defaultValue: initialVal
 };
+export const defaultArgs = args;
 
-const contextState = {
-    diagnostics:  [],
+// Hiding & disabling unused props
+const argTypes = {
+    defaultValue: { table: { disable: true } },
+    placeholder: { table: { disable: true } },
+    index: { table: { disable: true } },
+    label	: { table: { disable: true } },
+    rowsMax: { table: { disable: true } },
+    errorMessage: { table: { disable: true } },
+    validateEmptyField: { table: { disable: true } },
+    onFieldValueChange: { table: { disable: true } },
+    size: { table: { disable: true } },
+    type: { table: { disable: true } },
+    currentFile: { table: { disable: true } },
+    currentApp: { table: { disable: true } },
+    editorDiagnostics: { table: { disable: true } },
+    mainDiagnostics: { table: { disable: true } },
+    targetPositionDraft: { table: { disable: true } },
+    disabled: { table: { disable: true } },
+    dataTestId: { table: { disable: true } },
+    currentValue: { table: { disable: true } },
+    tooltip: { table: { disable: true } },
+    onClick: { table: { disable: true } },
+    dispatchExprEditorStart: { table: { disable: true } },
+    dispatchExprEditorContentChange: { table: { disable: true } },
+    dispatchExprEditorClose: { table: { disable: true } },
+}
+
+export const initialContext = {
+    diagnostics:  [] as any,
     targetPosition: {
         endColumn: 22,
         endLine: 10,
@@ -53,39 +81,64 @@ const contextState = {
         startLine: 10,
     },
     currentFile: {
-        content: "",
+        content: "CnB1YmxpYyBmdW5jdGlvbiBtYWluKCkgcmV0dXJucyBlcnJvcj8gewoKfQo=",
     },
     currentApp: {
-        workingFile: "/apps/kajendranalagaratnam/ab22/project/choreo.bal",
+        workingFile: "/apps/username/apName/project/choreo.bal",
     },
-    langClient: {
+    langServerURL: '',
+    getExpressionEditorLangClient:async ()=>({
         getCompletion: async (_completionParams: CompletionParams): Promise<CompletionResponse[]> => {
             const completions: CompletionResponse[] = [];
             return completions;
         },
-        // tslint:disable-next-line:no-empty
-        didChange: async (_content): Promise<void> => {},
-        diagnostics: async (_content): Promise<any> => {
-            return [];
+        didChange: async (): Promise<void> => {},
+        diagnostics: async (): Promise<any> => ([])
+    }),
+    syntaxTree: {
+        functionBody:{
+            position:{
+                endColumn: 1,
+                endLine: 3,
+                startColumn: 38,
+                startLine: 1
+            }
         }
     },
-    syntaxTree: {},
 }
 
 
 const Template: Story<FormElementProps> = (formArgs: FormElementProps) =>  (
-    <DiagramProvider initialState={contextState} >
+    <DiagramProvider initialState={initialContext} >
         <ExpressionEditor {...formArgs}/>
     </DiagramProvider>
 );
 export const ExpressionEditorStandard = Template.bind({});
+ExpressionEditorStandard.argTypes = argTypes;
 ExpressionEditorStandard.args = args;
 
 
 const TemplateWithError: Story<FormElementProps> = (formArgs: FormElementProps) =>  (
-    <DiagramProvider initialState={{...contextState, diagnostics: errorDiagnostic}} >
+    <DiagramProvider initialState={{...initialContext, diagnostics: errorDiagnostic}} >
         <ExpressionEditor {...formArgs}/>
     </DiagramProvider>
 );
 export const ExpressionEditorWithError = TemplateWithError.bind({});
+ExpressionEditorWithError.argTypes = argTypes;
 ExpressionEditorWithError.args = args;
+
+const TemplateWithDefaultValue: Story<FormElementProps> = (formArgs: FormElementProps) =>  (
+    <DiagramProvider initialState={initialContext} >
+        <ExpressionEditor {...formArgs} />
+    </DiagramProvider>
+);
+export const ExpressionEditorWithDefaultValue = TemplateWithDefaultValue.bind({});
+ExpressionEditorWithDefaultValue.argTypes = argTypes;
+ExpressionEditorWithDefaultValue.args = {
+    ...args,
+    model: {
+        ...args.model,
+        value: 'Default Value'
+    }
+};
+
