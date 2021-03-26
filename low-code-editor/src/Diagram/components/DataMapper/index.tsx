8cbcb7b1a393d80adc337483867ea925fc8424b4
CharukaK <charukakarunanayake@gmail.com>
2021-03-26 07:41:26 +0530

    Remove debugger keyword from the datamapper

--- low-code-editor/src/Diagram/components/DataMapper/index.tsx
+++ low-code-editor/src/Diagram/components/DataMapper/index.tsx
@@ -104,7 +104,7 @@ export function DataMapper(props: DataMapperProps) {
             dataPoints.push(<DataPoint dataPointViewState={dataPoint}/>);
         });
     }
-    debugger;
+
     return (
         <>
             <g>
