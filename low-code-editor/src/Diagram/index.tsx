a2ca759ae9322e10b2a8db20969f4902cc6e4669
CharukaK <charukakarunanayake@gmail.com>
2021-03-30 09:28:14 +0530

    Fix commented out property

--- low-code-editor/src/Diagram/index.tsx
+++ low-code-editor/src/Diagram/index.tsx
@@ -57,7 +57,7 @@ export function Diagram(props: DiagramProps) {
         isWaitingOnWorkspace,
         isMutationInProgress,
         isCodeEditorActive,
-        // isConfigPanelOpen,
+        isConfigPanelOpen,
         triggerType
     } = props;
     const { state: { diagnostics } } = useContext(DiagramContext);
