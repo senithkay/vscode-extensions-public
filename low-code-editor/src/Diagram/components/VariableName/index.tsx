679fd3f3462cc48fb7f16c03fb00d4dbe9045d60
Sharon David <srsharondavid@gmail.com>
2021-05-04 16:48:33 +0530

    Update process componant alignment

--- low-code-editor/src/Diagram/components/VariableName/index.tsx
+++ low-code-editor/src/Diagram/components/VariableName/index.tsx
@@ -11,57 +11,48 @@
  * associated services.
  */
 // tslint:disable: jsx-no-multiline-js align  jsx-wrap-multiline
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { CustomStatementIcon, LogIcon, PropertyIcon } from "../../../../src/assets/icons";
 import { DefaultConfig } from "../../visitors/default";
 
 import "./style.scss";
 
-export let VARIABLE_NAME_WIDTH = 150;
+export let VARIABLE_NAME_WIDTH = 125;
+export const ICON_SVG_WRAPPER_WIDTH = 25;
 
-export function VariableName(props: { x: number, y: number, variableName: string, processType: string, }) {
-    const { processType, variableName, ...xyProps } = props;
-    const ICON_SVG_WRAPPER_WIDTH = 30;
-    const textWidth = variableName.length * 8;
-    let textX = 0;
+export function VariableName(props: { x: number, y: number, variableName: string, processType: string, key_id: number }) {
+    const { processType, variableName, key_id, ...xyProps } = props;
+    const [textWidth, setTextWidth] = useState(VARIABLE_NAME_WIDTH);
 
-    // tslint:disable-next-line: prefer-conditional-expression
-    if (variableName.length >= 15) {
-        textX = 0 + ICON_SVG_WRAPPER_WIDTH;
-    } else {
-        textX = (VARIABLE_NAME_WIDTH - textWidth);
-    }
+    useEffect(() => {
+        setTextWidth(document.getElementById("variableLegnth_" + key_id).getBoundingClientRect().width);
+    }, []);
+
     const variableMaxWidth = variableName.length >= 15;
+    const variableWidth = textWidth
+    let variableX = 0;
 
-    const iconX = variableMaxWidth ? 1 : VARIABLE_NAME_WIDTH - (textWidth + ICON_SVG_WRAPPER_WIDTH);
+    variableX = (variableWidth > VARIABLE_NAME_WIDTH) ? variableWidth - ICON_SVG_WRAPPER_WIDTH : variableX = (VARIABLE_NAME_WIDTH - variableWidth - (DefaultConfig.dotGap * 2));
+
+    const iconX = (variableWidth > 125) ? VARIABLE_NAME_WIDTH : VARIABLE_NAME_WIDTH - (variableWidth + ICON_SVG_WRAPPER_WIDTH);
 
     return (
-        <svg {...xyProps}>
-            <defs>
-                <filter id="VariableName_Filter" x="0" y="0" width="150" height="33" filterUnits="userSpaceOnUse">
-                    <feOffset dy="1" in="SourceAlpha" />
-                    <feGaussianBlur stdDeviation="1.5" result="blur" />
-                    <feFlood floodColor="#aaacb8" floodOpacity="0.302" />
-                    <feComposite operator="in" in2="blur" />
-                    <feComposite in="SourceGraphic" />
-                </filter>
-            </defs>
-            <g id="Variable_Name">
-                <text id="getResponse" className="variable-name" >
-                    <tspan id="getResponseName" width="150" textAnchor={variableMaxWidth ? "start" : ""} x={textX} y="15">{variableMaxWidth ? variableName.slice(0, 14) + "..." : variableName}</tspan>
-                </text>
-                <g filter="url(#VariableName_Filter)">
-                    <g id="Reactangle_wrapper" fill="#fff" stroke="#ccd1f2" strokeMiterlimit="10" strokeWidth="1" >
-                        <rect width="24" x={iconX} height="24" rx="4" stroke="none" />
-                        <rect x={iconX} y="0.5" width="23" height="23" rx="3.5" fill="none" />
+        <svg {...xyProps} width="150" height="24" >
+            <text id="getResponse" transform="translate(40 1)" className="variable-name">
+                <tspan id={"variableLegnth_" + key_id} x={variableX} y="15">{variableMaxWidth ? variableName.slice(0, 14) + "..." : variableName}</tspan>
+            </text>
+            {processType === "Variable" &&
+                (
+                    <g id="Reactangle_wrapper" fill="#fff" stroke="#ccd1f2" strokeMiterlimit="10" strokeWidth="1" transform="translate(20 0)">
+                        <rect x={iconX} width="24" height="24" rx="4" stroke="none" />
+                        <rect x={iconX + 0.5} y="0.5" width="23" height="23" rx="3.5" fill="none" />
                     </g>
-                </g>
-                <g id="Variable_icon">
-                    {(processType === "Variable" || processType === "Action") && <PropertyIcon height={16} width={16} x={iconX + DefaultConfig.dotGap / 2} y={4} />}
-                    {processType === "Custom" && <CustomStatementIcon height={16} width={15.251} x={iconX + DefaultConfig.dotGap / 2} y={4} />}
-                </g>
+                )
+            }
+            < g id="Variable_icon" transform="translate(4 4)">
+                {(processType === "Variable" || processType === "Action") && <PropertyIcon height={16} width={16} x={iconX + 20} y={0} transform="translate(0 0)" />}
             </g>
-        </svg>
+        </svg >
     );
 }
