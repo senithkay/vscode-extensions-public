40af4cb12cb78b0782b64f7aadd0acd43ae711fe
Kanushka Gayan <kanushkanet@gmail.com>
2021-05-13 14:24:02 +0530

    Update tools/low-code-editor/src/Diagram/components/FormAccordion/style.ts
    Co-authored-by: Sharon David <srsharon@users.noreply.github.com>

--- low-code-editor/src/Diagram/components/FormAccordion/style.ts
+++ low-code-editor/src/Diagram/components/FormAccordion/style.ts
@@ -183,7 +183,7 @@ export const useStyles = makeStyles((theme: Theme) =>
         accordionSecondaryHeading: {
             color: '#CBCEDB',
             fontSize: "13px",
-            paddingLeft: "8px",
+            paddingLeft: "8",
             fontWeight: 500,
             textTransform: 'capitalize',
         },
