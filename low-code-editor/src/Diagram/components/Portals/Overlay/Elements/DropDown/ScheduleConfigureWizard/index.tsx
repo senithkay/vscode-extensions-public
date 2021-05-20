3ba3ce8462377a437c5621f1f081912c278e8f89
janithrs <janithravindu@gmail.com>
2021-05-20 21:53:58 +0530

    Fix incorrectly showing current hour value in PM

--- low-code-editor/src/Diagram/components/Portals/Overlay/Elements/DropDown/ScheduleConfigureWizard/index.tsx
+++ low-code-editor/src/Diagram/components/Portals/Overlay/Elements/DropDown/ScheduleConfigureWizard/index.tsx
@@ -84,7 +84,7 @@ export function ScheduleConfigureWizard(props: ScheduleConfigureWizardProps) {
   const cronSplit = currentCron?.split(" ", 5);
 
   const [cronMinuteValue, setCronMinuteValue] = useState(cron ? cronSplit[0] : format(new Date(), 'm'));
-  const [cronHourValue, setCronHourValue] = useState(cron ? cronSplit[1] : format(new Date(), 'h'));
+  const [cronHourValue, setCronHourValue] = useState(cron ? cronSplit[1] : (new Date().getHours().toString()));
   const [cronDayValue, setCronDayValue] = useState(cron ? cronSplit[2] : format(new Date(), 'd'));
   const [cronMonthValue, setCronMonthValue] = useState(cron ? cronSplit[3] : format(new Date(), 'M'));
   const [cronWeekValue, setCronWeekValue] = useState(cron ? cronSplit[4] : weekOptions[0]);
