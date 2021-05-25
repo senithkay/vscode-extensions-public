6a3335d344613267e5d4ac43ebfd398210f8ab67
CharukaK <charukakarunanayake@gmail.com>
2021-05-25 10:51:30 +0530

    Add Input variable selector forms for DataMapper

---
+++ low-code-editor/src/Diagram/components/DataMapper/components/forms/VariablePicker/index.tsx
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ * associated services.
+ */
+// tslint:disable: jsx-no-lambda
+// tslint:disable: jsx-no-multiline-js
+import React, { useContext } from 'react';
+
+import { CaptureBindingPattern, LocalVarDecl, STNode } from '@ballerina/syntax-tree';
+import { Box, FormControl, Typography } from '@material-ui/core';
+import { CloseRounded } from '@material-ui/icons';
+import classNames from 'classnames';
+
+import { Context as DiagramContext } from '../../../../../../Contexts/Diagram';
+import { wizardStyles } from "../../../../ConfigForms/style";
+import { FormAutocomplete } from '../../../../Portals/ConfigForm/Elements/Autocomplete';
+import { ButtonWithIcon } from '../../../../Portals/ConfigForm/Elements/Button/ButtonWithIcon';
+import { PrimaryButton } from '../../../../Portals/ConfigForm/Elements/Button/PrimaryButton';
+import { SecondaryButton } from '../../../../Portals/ConfigForm/Elements/Button/SecondaryButton';
+import { useStyles as useFormStyles } from "../../../../Portals/ConfigForm/forms/style";
+import { DataMapperConfig, DataMapperInputTypeInfo } from '../../../../Portals/ConfigForm/types';
+
+export interface VariablePickerProps {
+    dataMapperConfig: DataMapperConfig
+    toggleVariablePicker: () => void;
+}
+
+export function VariablePicker(props: VariablePickerProps) {
+    const { state: { stSymbolInfo }, updateDataMapperConfig } = useContext(DiagramContext)
+    const { toggleVariablePicker, dataMapperConfig } = props;
+    const formClasses = useFormStyles();
+    const overlayClasses = wizardStyles();
+
+    const selectedVariables = dataMapperConfig.inputTypes.map(input => input.name);
+    const variables: DataMapperInputTypeInfo[] = [];
+
+    const onSelectVariable = (evt: any, typeEntry: DataMapperInputTypeInfo) => {
+        dataMapperConfig.inputTypes.push(typeEntry);
+    };
+
+    const onClickAdd = () => {
+        updateDataMapperConfig(dataMapperConfig);
+        toggleVariablePicker();
+    }
+
+    stSymbolInfo.variables.forEach((definedVars: STNode[], type: string) => {
+        definedVars
+            .filter((el: LocalVarDecl) => selectedVariables.indexOf((el.typedBindingPattern.bindingPattern as CaptureBindingPattern).variableName.value) === -1)
+            .forEach((el: LocalVarDecl) => {
+                variables.push({
+                    name: (el.typedBindingPattern.bindingPattern as CaptureBindingPattern).variableName.value,
+                    type,
+                    node: el
+                })
+            });
+    });
+
+    return (
+        <>
+            <FormControl data-testid="data-mapper-form" className={formClasses.wizardFormControl}>
+                <div className={overlayClasses.configWizardContainer}>
+                    <ButtonWithIcon
+                        className={formClasses.overlayDeleteBtn}
+                        onClick={toggleVariablePicker}
+                        icon={<CloseRounded fontSize="small" />}
+                    />
+                    <div className={formClasses.formTitleWrapper}>
+                        <div className={formClasses.mainTitleWrapper}>
+                            <Typography variant="h4">
+                                <Box paddingTop={2} paddingBottom={2}>Select Input Variables</Box>
+                            </Typography>
+                        </div>
+                    </div>
+                    <FormAutocomplete
+                        itemList={variables}
+                        onChange={onSelectVariable}
+                        label={'Select Input type'}
+                        getItemLabel={(option) => option.name}
+                        renderItem={(option) => (
+                            <React.Fragment>
+                                {option.name} {option.type}
+                            </React.Fragment>
+                        )}
+                    />
+                    <div className={overlayClasses.buttonWrapper}>
+                        <SecondaryButton text="Cancel" fullWidth={false} onClick={toggleVariablePicker} />
+                        <PrimaryButton
+                            disabled={false}
+                            dataTestId={"datamapper-save-btn"}
+                            text={"Add"}
+                            fullWidth={false}
+                            onClick={onClickAdd}
+                        />
+                    </div>
+                </div>
+            </FormControl>
+        </>
+    );
+}
