3f1ff5a5fa04e7cf63613e287c8676d9e9b90503
Nipuna Marcus <nipunac@wso2.com>
2021-06-02 01:09:30 +0530

    Add support for external endpoints

--- low-code-editor/src/Diagram/components/Connector/index.tsx
+++ low-code-editor/src/Diagram/components/Connector/index.tsx
@@ -15,13 +15,13 @@ import * as React from "react";
 import { STNode } from "@ballerina/syntax-tree";
 import cn from "classnames";
 
-import { StatementViewState } from "../../view-state";
+import { EndpointViewState, StatementViewState } from "../../view-state";
 import { DefaultConfig } from "../../visitors/default";
 import { ActionInvoLine } from "../ActionInvocation/ActionInvoLine";
 import { ARROW_WIDTH } from "../ArrowHead";
 
 import { ConnectorHeader } from "./ConnectorHeader";
-import { CLIENT_RADIUS, CLIENT_SHADOW_OFFSET, CLIENT_SVG_WIDTH_WITH_SHADOW, ConnectorSVG } from "./ConnectorHeader/ConnectorClientSVG";
+import { CLIENT_RADIUS, CLIENT_SHADOW_OFFSET, CLIENT_SVG_HEIGHT, CLIENT_SVG_WIDTH_WITH_SHADOW, ConnectorSVG } from "./ConnectorHeader/ConnectorClientSVG";
 import { ConnectorProcess } from "./ConnectorProcess";
 import { ConnectorProcessSVG, CONNECTOR_PROCESS_SVG_WIDTH } from "./ConnectorProcess/ConnectorProcessSVG";
 import "./style.scss";
@@ -41,47 +41,72 @@ export function Connector(props: ConnectorProps) {
         setSelected(!selected);
     };
 
-    const viewState: StatementViewState = model.viewState as StatementViewState;
-
     const classes = cn("connector", { selected });
-    const arrowClasses = cn("action-invocation");
-    const leftline = "leftline";
-    const actionLineStartX = viewState.bBox.cx + (CONNECTOR_PROCESS_SVG_WIDTH / 2) + DefaultConfig.actionArrowPadding;
-    const actionLineEndX = x - (CLIENT_RADIUS) - DefaultConfig.actionArrowPadding;
-    const actionRightLineY = viewState.bBox.cy + (CONNECTOR_PROCESS_SVG_WIDTH / 2);
+    let component: any;
 
-    const connectorHeadX = x - (CLIENT_SVG_WIDTH_WITH_SHADOW / 2);
-    const connectorHeadY = viewState.bBox.cy - (CLIENT_SHADOW_OFFSET / 2);
-    const component = (
-        <g>
-            <ConnectorProcess model={model} />
-            <g className={classes} onClick={toggleSelection}>
-                <line x1={x} y1={viewState.bBox.cy + CLIENT_RADIUS} x2={x} y2={viewState.bBox.cy + h} />
-                <text
-                    x={x}
-                    y={viewState.bBox.cy - DefaultConfig.textLine.height - (DefaultConfig.dotGap * 2)}
-                    textAnchor="middle"
-                    dominantBaseline="central"
-                    className='endpont-name'
-                >
-                    {connectorName}
-                </text>
-            </g>
+    const viewState: StatementViewState = model.viewState as StatementViewState;
+    const epViewState: EndpointViewState = viewState.endpoint;
+    if (epViewState.isExternal) {
+        component = (
             <g>
-                <ConnectorHeader model={model} />
+
+                <g className={classes} onClick={toggleSelection}>
+                    <line x1={x} y1={y} x2={x} y2={y + h} />
+                    <text
+                        x={x}
+                        y={y - CLIENT_SVG_HEIGHT - DefaultConfig.textLine.height - (DefaultConfig.dotGap * 2)}
+                        textAnchor="middle"
+                        dominantBaseline="central"
+                        className='endpont-name'
+                    >
+                        {connectorName}
+                    </text>
+                </g>
+                <g>
+                    <ConnectorHeader model={model} />
+                </g>
             </g>
-            <g className={arrowClasses}>
-                <ActionInvoLine
-                    clientInvoX={actionLineStartX}
-                    clientInvoY={actionRightLineY}
-                    actionX={actionLineEndX}
-                    actionY={actionRightLineY}
-                    direction={"right"}
-                    className={leftline}
-                />
+        );
+    } else {
+        const arrowClasses = cn("action-invocation");
+        const leftline = "leftline";
+        const actionLineStartX = viewState.bBox.cx + (CONNECTOR_PROCESS_SVG_WIDTH / 2) + DefaultConfig.actionArrowPadding;
+        const actionLineEndX = x - (CLIENT_RADIUS) - DefaultConfig.actionArrowPadding;
+        const actionRightLineY = viewState.bBox.cy + (CONNECTOR_PROCESS_SVG_WIDTH / 2);
+
+        const connectorHeadX = x - (CLIENT_SVG_WIDTH_WITH_SHADOW / 2);
+        const connectorHeadY = viewState.bBox.cy - (CLIENT_SHADOW_OFFSET / 2);
+        component = (
+            <g>
+                <ConnectorProcess model={model} />
+                <g className={classes} onClick={toggleSelection}>
+                    <line x1={x} y1={viewState.bBox.cy + CLIENT_RADIUS} x2={x} y2={viewState.bBox.cy + h} />
+                    <text
+                        x={x}
+                        y={viewState.bBox.cy - DefaultConfig.textLine.height - (DefaultConfig.dotGap * 2)}
+                        textAnchor="middle"
+                        dominantBaseline="central"
+                        className='endpont-name'
+                    >
+                        {connectorName}
+                    </text>
+                </g>
+                <g>
+                    <ConnectorHeader model={model} />
+                </g>
+                <g className={arrowClasses}>
+                    <ActionInvoLine
+                        clientInvoX={actionLineStartX}
+                        clientInvoY={actionRightLineY}
+                        actionX={actionLineEndX}
+                        actionY={actionRightLineY}
+                        direction={"right"}
+                        className={leftline}
+                    />
+                </g>
             </g>
-        </g>
-    );
+        );
+    }
     return (
         !viewState.endpoint.collapsed && component
     );
