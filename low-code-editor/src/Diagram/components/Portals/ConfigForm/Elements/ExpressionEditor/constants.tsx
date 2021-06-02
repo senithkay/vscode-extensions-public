2d1a00bd465d5d7624b93e9d805d7ee69a3414fc
sajeerAhamed15 <ahamedsajeer.15@cse.mrt.ac.lk>
2021-06-02 10:13:04 +0530

    Remove Incompitable types from suggesting double quotes

--- low-code-editor/src/Diagram/components/Portals/ConfigForm/Elements/ExpressionEditor/constants.tsx
+++ low-code-editor/src/Diagram/components/Portals/ConfigForm/Elements/ExpressionEditor/constants.tsx
@@ -20,5 +20,5 @@ export const acceptedKind: CompletionItemKind[] = [
 export const EXPAND_WIDGET_ID = "expand-widget";
 export const COLLAPSE_WIDGET_ID = "collapse-widget";
 
-// 2066 = Incompatible types, 0411 = Missing quotes, 2010 = Undefined symbol, 0012 = Missing plus token
-export const INCORRECT_STR_DIAGNOSTICS = ["BCE2066", "BCE0411", "BCE2010", "BCE0012"]
+// 0411 = Missing quotes, 2010 = Undefined symbol, 0012 = Missing plus token
+export const INCORRECT_STR_DIAGNOSTICS = ["BCE0411", "BCE2010", "BCE0012"]
