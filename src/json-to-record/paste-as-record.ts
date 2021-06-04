b8298c14e0f120dbfd58d173c41fd5aeecb5f9d3
Tharindu Weerasinghe <tharindu.weerasinghe.16@cse.mrt.ac.lk>
2021-06-04 17:22:04 +0530

    address comments on code formatting

--- src/json-to-record/paste-as-record.ts
+++ src/json-to-record/paste-as-record.ts
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 import { read } from "clipboardy";
 import { BallerinaExtension } from "../core";
 import { window } from "vscode";
@@ -7,13 +26,13 @@ export function pasteAsRecord(balExtension: BallerinaExtension) {
     read().then(clipboardText => {
         balExtension.langClient!.getRecordsForJson(clipboardText)
         .then(response => {
-            if(!response || response.codeBlock === "") {
+            if (!response || response.codeBlock === "") {
                 window.showErrorMessage("Invalid JSON string");
                 return;
             }
             const editor = window.activeTextEditor;
             editor?.edit(editBuilder => {
-                if(editor.selection.isEmpty) {
+                if (editor.selection.isEmpty) {
                     const startPosition = editor.selection.active;
                     editBuilder.insert(startPosition, response.codeBlock);
                 } else {
