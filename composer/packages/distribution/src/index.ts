c00f01bdddf80d4ce6d9649f7559399d4ad70cfc
Prabushi Samarakoon <prabushisamarakoon@gmail.com>
2021-06-09 13:53:50 +0530

    Remove unused packages

--- composer/packages/distribution/src/index.ts
+++ composer/packages/distribution/src/index.ts
@@ -1,3 +1,2 @@
 export * from "@ballerina/ballerina-by-examples";
-export * from "@ballerina/documentation";
 export * from "@ballerina/low-code-editor";
--- composer/packages/documentation/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DocPreview from './components/DocPreview';
-
-export function renderDocPreview(html, nodeType, el) {
-    ReactDOM.render(<DocPreview html={html} nodeType={nodeType} />, el);
-}
