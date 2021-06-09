b0eda4ba6130715a1f2e58395c3c7c9b9f2cadd1
thinkholic <mailtokasun@gmail.com>
2021-06-09 12:30:00 +0530

    Add type declaration for configs

--- low-code-editor/src/config.ts
+++ low-code-editor/src/config.ts
@@ -1,2 +1,2 @@
-export const CHOREO_DOCS = 'https://docs.dv.choreo.dev';
-export const BALLERINA_EXPRESSION_SYNTAX_PATH = `${CHOREO_DOCS}/references/ballerina-expression-syntax/`;
+export const CHOREO_DOCS: string = 'https://docs.dv.choreo.dev';
+export const BALLERINA_EXPRESSION_SYNTAX_PATH: string = `${CHOREO_DOCS}/references/ballerina-expression-syntax/`;
