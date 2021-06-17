17ad9c8ccf486c1fc5c4a7bb94ed827ffebe7350
tharindulak <tharindu.jayathilake1@gmail.com>
2021-06-17 16:13:54 +0530

    Handle record field display name

--- low-code-editor/src/Diagram/visitors/form-field-extraction-visitor.ts
+++ low-code-editor/src/Diagram/visitors/form-field-extraction-visitor.ts
@@ -424,6 +424,17 @@ class FieldVisitor implements Visitor {
             viewState.fields = [];
             if (node.fields) {
                 node.fields.forEach(field => {
+                    const annotations = (field as any).metadata?.annotations;
+                    if ((field as any).metadata?.annotations?.length > 0) {
+                        const annotateField = annotations.find((annotation: any) =>
+                            (annotation.annotReference as SimpleNameReference).name.value === "display")?.
+                        annotValue?.fields.find((field: any) => STKindChecker.isSpecificField(field)) as SpecificField;
+                        if (annotateField?.fieldName.value === "label"){
+                            const labelField = annotateField.valueExpr as StringLiteral;
+                            field.viewState.label = labelField?.literalToken.value
+                                .replace(/\"/gi, '');
+                        }
+                    }
                     viewState.fields.push(field.viewState);
                 });
             }
