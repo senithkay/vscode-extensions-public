1728f113757a59bdada2b46a9139841f685e38a1
Prabushi Samarakoon <prabushisamarakoon@gmail.com>
2021-06-21 10:29:12 +0530

    Fix 'pasteAsRecord' command not found error in Beta1
    Fix 'ballerina.pasteAsRecord' command not found error in SL Beta1.

--- src/project/cmds/json-to-record.ts
+++ src/project/cmds/json-to-record.ts
@@ -17,81 +17,74 @@
  *
  */
 
- import {
-    sendTelemetryEvent,
-    sendTelemetryException,
-    TM_EVENT_PASTE_AS_RECORD,
-    CMP_JSON_TO_RECORD,
+import {
+    sendTelemetryEvent, sendTelemetryException, TM_EVENT_PASTE_AS_RECORD, CMP_JSON_TO_RECORD,
 } from "../../telemetry";
 import { commands, window, env } from "vscode";
 import { ballerinaExtInstance } from "../../core";
 import { PALETTE_COMMANDS, MESSAGES } from "./cmd-runner";
 
+const MSG_NOT_SUPPORT = "Paste JSON as a Ballerina record feature is not supported";
+
 export function activatePasteJsonAsRecord() {
-    sendTelemetryEvent(
-        ballerinaExtInstance,
-        TM_EVENT_PASTE_AS_RECORD,
-        CMP_JSON_TO_RECORD
-    );
+    sendTelemetryEvent(ballerinaExtInstance, TM_EVENT_PASTE_AS_RECORD, CMP_JSON_TO_RECORD);
 
     if (!ballerinaExtInstance.langClient) {
         return;
     }
 
-    // This command is only available since Swan Lake Beta 2
-    // Check the version before registering the command
-    if (
-        ballerinaExtInstance.isSwanLake()
-        && !ballerinaExtInstance.ballerinaVersion.includes("Alpha")
-        && !ballerinaExtInstance.ballerinaVersion.includes("Preview")
-    ) {
-        // check if SL Beta version >= 2
-        const digits = ballerinaExtInstance.ballerinaVersion.replace(/[^0-9]/g, "");
-        const versionNumber = +digits;
-        if (versionNumber < 2) {
+    commands.registerCommand(PALETTE_COMMANDS.PASTE_JSON_AS_RECORD, () => {
+        // This command is only available since Swan Lake Beta 2
+        // Check the version before registering the command
+        const balVersion = ballerinaExtInstance.ballerinaVersion.toLowerCase();
+        if (ballerinaExtInstance.isSwanLake() && !balVersion.includes("alpha") && !balVersion.includes("preview")) {
+            if (balVersion.includes("beta")) {
+                // check if SL Beta version >= 2
+                const digits = ballerinaExtInstance.ballerinaVersion.replace(/[^0-9]/g, "");
+                const versionNumber = +digits;
+                if (versionNumber < 2) {
+                    window.showErrorMessage(`${MSG_NOT_SUPPORT} in ${ballerinaExtInstance.ballerinaVersion}`);
+                    return;
+                }
+            }
+        } else {
+            window.showErrorMessage(`${MSG_NOT_SUPPORT} in ${ballerinaExtInstance.ballerinaVersion}`);
             return;
         }
-    } else {
-        return;
-    }
-
-    
-
-    commands.registerCommand(PALETTE_COMMANDS.PASTE_JSON_AS_RECORD, () => {
         if (!window.activeTextEditor || !window.activeTextEditor?.document.fileName.endsWith('.bal')) {
             window.showErrorMessage("Target is not a Ballerina file!");
             return;
         }
         env.clipboard.readText()
-        .then(clipboardText => {
-            if (!ballerinaExtInstance.langClient) {
-                window.showErrorMessage("Ballerina language client not found.");
-                return;
-            }
-            ballerinaExtInstance.langClient!.getRecordsForJson(clipboardText)
-            .then(response => {
-                if (!response || response.codeBlock === "") {
-                    window.showErrorMessage(MESSAGES.INVALID_JSON);
+            .then(clipboardText => {
+                if (!ballerinaExtInstance.langClient) {
+                    window.showErrorMessage("Ballerina language client not found.");
                     return;
                 }
-                const editor = window.activeTextEditor;
-                editor?.edit(editBuilder => {
-                    if (editor.selection.isEmpty) {
-                        const startPosition = editor.selection.active;
-                        editBuilder.insert(startPosition, response.codeBlock);
-                    } else {
-                        editBuilder.replace(editor.selection, response.codeBlock);
-                    }
-                });
+                ballerinaExtInstance.langClient!.getRecordsForJson(clipboardText)
+                    .then(response => {
+                        if (!response || response.codeBlock === "") {
+                            window.showErrorMessage(MESSAGES.INVALID_JSON);
+                            return;
+                        }
+                        const editor = window.activeTextEditor;
+                        editor?.edit(editBuilder => {
+                            if (editor.selection.isEmpty) {
+                                const startPosition = editor.selection.active;
+                                editBuilder.insert(startPosition, response.codeBlock);
+                            } else {
+                                editBuilder.replace(editor.selection, response.codeBlock);
+                            }
+                        });
+                    },
+                        error => {
+                            window.showErrorMessage(error.message);
+                            sendTelemetryException(ballerinaExtInstance, error, CMP_JSON_TO_RECORD);
+                        });
             },
-            error => {
-                window.showErrorMessage(error.message);
-                sendTelemetryException(ballerinaExtInstance, error, CMP_JSON_TO_RECORD);
-            });
-        },
-        error => {
-            window.showErrorMessage(error.message);
-            sendTelemetryException(ballerinaExtInstance, error, CMP_JSON_TO_RECORD);
-        }); 
+                error => {
+                    window.showErrorMessage(error.message);
+                    sendTelemetryException(ballerinaExtInstance, error, CMP_JSON_TO_RECORD);
+                });
     });
 }
