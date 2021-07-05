63aeb81bb033942dd45499b339e5f905f1463c6c
chamilaadhi <cka.adhi@gmail.com>
2021-07-05 16:54:41 +0530

    Rename additionalProperties

--- low-code-editor/src/api/models.ts
+++ low-code-editor/src/api/models.ts
@@ -350,7 +350,7 @@ export interface APIManagerAPIObj {
     transport?: string[],
     securityScheme?: string[],
     corsConfiguration?: CorsConfiguration,
-    additionalProperties?: AdditionalProperties,
+    additionalPropertiesMap?: AdditionalProperties,
     version?: string,
     lifeCycleStatus?: ApiState,
     status?: ApiState,
