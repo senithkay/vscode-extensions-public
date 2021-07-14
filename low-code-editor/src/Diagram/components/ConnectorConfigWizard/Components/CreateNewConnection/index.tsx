496e5722ced8b99fe4025305bf1ba1df8f46e467
kaumini <kauminikg@gmail.com>
2021-07-14 10:07:57 +0530

    Fix check issues

--- low-code-editor/src/Diagram/components/ConnectorConfigWizard/Components/CreateNewConnection/index.tsx
+++ low-code-editor/src/Diagram/components/ConnectorConfigWizard/Components/CreateNewConnection/index.tsx
@@ -191,7 +191,7 @@ export function CreateConnectorForm(props: CreateConnectorFormProps) {
           <FormattedMessage id="lowcode.develop.connectorForms.createConnection.endPointName.tooltip.bulletPoint2" defaultMessage="A numerical character at the beginning"/>
         </li>
         <li>
-          <FormattedMessage id="lowcode.develop.connectorForms.createConnection.endPointName.tooltip.bulletPoint3" defaultMessage="Keywords such as Return, Foreach, Resource, and Object"/>
+          <FormattedMessage id="lowcode.develop.connectorForms.createConnection.endPointName.tooltip.bulletPoint3" defaultMessage="Keywords such as <code>Return</code>, <code>Foreach</code>, <code>Resource</code>, <code>Object</code>, etc." values={{ code: (chunks: string) => <code>{chunks}</code>}} />
         </li>
       </ul>
     </div>
