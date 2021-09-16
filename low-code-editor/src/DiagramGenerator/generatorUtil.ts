d2760266ea3171e6089700e583d74b3fd32badb0
CharukaK <charukakarunanayake@gmail.com>
2021-09-16 10:47:15 +0530

    Stop filtering of the components

--- low-code-editor/src/DiagramGenerator/generatorUtil.ts
+++ low-code-editor/src/DiagramGenerator/generatorUtil.ts
@@ -19,61 +19,66 @@ export function getLowcodeST(payload: any, startLine: string, startColumn: strin
     const members: STNode[] = modulePart?.members || [];
     let responseST: STNode;
     // FunctionDefinition
-    const fnMembers = members.filter((m: any) => (m.kind === "FunctionDefinition"));
-    for (const node of fnMembers) {
-        if (STKindChecker.isFunctionDefinition(node) && node.functionName.position.startLine.toString() === startLine
-             && node.functionName.position.startColumn.toString() === startColumn) {
-            if (STKindChecker.isExternalFunctionBody(node.functionBody)) {
-                responseST = node;
-                break;
-            }
-            const fnDef = getLowCodeSTFnSelected(payload, node, true);
-            const st: STNode = sizingAndPositioningST(fnDef);
-            cleanLocalSymbols();
-            traversNode(st, SymbolVisitor);
-            responseST = st;
-            break;
-        }
-    }
-    const serviceMembers = members.filter((m: any) => (STKindChecker.isServiceDeclaration(m)));
-    for (const node of serviceMembers) {
-        if (STKindChecker.isServiceDeclaration(node)) {
-            const resourceMembers: STNode[] = node.members;
-            for (const resource of resourceMembers) {
-                const functionDef = resource as FunctionDefinition;
-                if (functionDef.functionName.position.startLine.toString() === startLine
-                    && functionDef.functionName.position.startColumn.toString() === startColumn) {
-                    const fnDef = getLowCodeSTFnSelected(payload, resource, false);
-                    const st: STNode = sizingAndPositioningST(fnDef);
-                    cleanLocalSymbols();
-                    traversNode(st, SymbolVisitor);
-                    responseST = st;
-                    break;
-                }
-            }
-        }
-    }
+    // const fnMembers = members.filter((m: any) => (m.kind === "FunctionDefinition"));
+    // for (const node of fnMembers) {
+    //     if (STKindChecker.isFunctionDefinition(node) && node.functionName.position.startLine.toString() === startLine
+    //          && node.functionName.position.startColumn.toString() === startColumn) {
+    //         if (STKindChecker.isExternalFunctionBody(node.functionBody)) {
+    //             responseST = node;
+    //             break;
+    //         }
+    //         const fnDef = getLowCodeSTFnSelected(payload, node, true);
+    //         const st: STNode = sizingAndPositioningST(fnDef);
+    //         cleanLocalSymbols();
+    //         traversNode(st, SymbolVisitor);
+    //         responseST = st;
+    //         break;
+    //     }
+    // }
+    // const serviceMembers = members.filter((m: any) => (STKindChecker.isServiceDeclaration(m)));
+    // for (const node of serviceMembers) {
+    //     if (STKindChecker.isServiceDeclaration(node)) {
+    //         const resourceMembers: STNode[] = node.members;
+    //         for (const resource of resourceMembers) {
+    //             const functionDef = resource as FunctionDefinition;
+    //             if (functionDef.functionName.position.startLine.toString() === startLine
+    //                 && functionDef.functionName.position.startColumn.toString() === startColumn) {
+    //                 const fnDef = getLowCodeSTFnSelected(payload, resource, false);
+    //                 const st: STNode = sizingAndPositioningST(fnDef);
+    //                 cleanLocalSymbols();
+    //                 traversNode(st, SymbolVisitor);
+    //                 responseST = st;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
 
-    // TODO falling back to showing first fn available
-    // Now that editing is enabled in vscode, start positions are going to change
-    // We need a proper way to persist lastly shown construct not from position
-    // Or we have to show all the constructs in a given file
-    if (!responseST) {
-        if (fnMembers && fnMembers.length > 0) {
-            const fnDef = getLowCodeSTFnSelected(payload, fnMembers[0], true);
-            const st: STNode = sizingAndPositioningST(fnDef);
-            cleanLocalSymbols();
-            traversNode(st, SymbolVisitor);
-            responseST = st;
-        } else if (serviceMembers && serviceMembers.length > 0) {
-            const fnDef = getLowCodeSTFnSelected(payload, serviceMembers[0], true);
-            const st: STNode = sizingAndPositioningST(fnDef);
-            cleanLocalSymbols();
-            traversNode(st, SymbolVisitor);
-            responseST = st;
-        }
-    }
-    return responseST;
+    // // TODO falling back to showing first fn available
+    // // Now that editing is enabled in vscode, start positions are going to change
+    // // We need a proper way to persist lastly shown construct not from position
+    // // Or we have to show all the constructs in a given file
+    // if (!responseST) {
+    //     if (fnMembers && fnMembers.length > 0) {
+    //         const fnDef = getLowCodeSTFnSelected(payload, fnMembers[0], true);
+    //         const st: STNode = sizingAndPositioningST(fnDef);
+    //         cleanLocalSymbols();
+    //         traversNode(st, SymbolVisitor);
+    //         responseST = st;
+    //     } else if (serviceMembers && serviceMembers.length > 0) {
+    //         const fnDef = getLowCodeSTFnSelected(payload, serviceMembers[0], true);
+    //         const st: STNode = sizingAndPositioningST(fnDef);
+    //         cleanLocalSymbols();
+    //         traversNode(st, SymbolVisitor);
+    //         responseST = st;
+    //     }
+    // }
+    // ToDo: Commented out to stop the component filtering check if this functionality is required. 
+    const st = sizingAndPositioningST(payload);
+    cleanLocalSymbols();
+    cleanModuleLevelSymbols();
+    traversNode(st, SymbolVisitor);
+    return st;
 }
 
 export function sizingAndPositioningST(st: STNode): STNode {
