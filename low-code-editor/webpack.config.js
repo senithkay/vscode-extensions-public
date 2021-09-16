32b5c26f15f666a18f2ba442e5562cb5e02c171c
Kavith Lokuhewage <kaviththiranga@gmail.com>
2021-09-16 23:04:28 +0530

    Add glimer font & fix MUI theme loading

--- low-code-editor/webpack.config.js
+++ low-code-editor/webpack.config.js
@@ -47,15 +47,7 @@ module.exports = (env, argv) => ({
             {
                 test: /\.css$/,
                 include: APP_DIR,
-                use: [{
-                    loader: 'style-loader',
-                }, {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true,
-                        namedExport: true,
-                    },
-                }],
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.css$/,
@@ -63,8 +55,8 @@ module.exports = (env, argv) => ({
                 use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.ttf$/,
-                include: [MONACO_DIR],
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                include: [APP_DIR, MONACO_DIR],
                 use: ['file-loader']
             },
             {
