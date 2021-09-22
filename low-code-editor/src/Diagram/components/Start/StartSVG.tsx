404c23f654c067deb8ae62d09d6a6231cb76605a
Sharon David <srsharondavid@gmail.com>
2021-09-22 16:14:10 +0530

    Update start Button shadow

--- low-code-editor/src/Diagram/components/Start/StartSVG.tsx
+++ low-code-editor/src/Diagram/components/Start/StartSVG.tsx
@@ -22,7 +22,7 @@ export const START_SVG_WIDTH = 82;
 export const START_SVG_HEIGHT = 40;
 export const START_SVG_SHADOW_OFFSET = START_SVG_HEIGHT_WITH_SHADOW - START_SVG_HEIGHT;
 
-export function StartSVG(props: { x: number, y: number, text: string}) {
+export function StartSVG(props: { x: number, y: number, text: string }) {
     const { text, ...xyProps } = props;
 
     return (
@@ -31,18 +31,24 @@ export function StartSVG(props: { x: number, y: number, text: string}) {
                 <filter id="StartFilterDefault" x="0" y="0" width="94" height="52" filterUnits="userSpaceOnUse">
                     <feOffset dy="1" in="SourceAlpha" />
                     <feGaussianBlur stdDeviation="2" result="blur" />
-                    <feFlood floodColor="#a9acb6" floodOpacity="0.388" />
+                    <feFlood flood-color="#a9acb6" flood-opacity="0.388" />
                     <feComposite operator="in" in2="blur" />
                     <feComposite in="SourceGraphic" />
                 </filter>
                 <linearGradient id="linear-gradient" x1="0.5" x2="0.5" y2="1" gradientUnits="objectBoundingBox">
-                    <stop offset="0" stopColor="#fcfcfd" />
-                    <stop offset="1" stopColor="#f7f8fb" />
+                    <stop offset="0" stop-color="#fcfcfd" />
+                    <stop offset="1" stop-color="#f7f8fb" />
                 </linearGradient>
             </defs>
             <g id="Start" className="start-button" >
-                <g className="start-button-rect">
-                    <rect id="StartRectangle" width={START_SVG_WIDTH} height={START_SVG_HEIGHT} rx="24.5" />
+                <g>
+                    <g className="start-button-rect" transform="matrix(1, 0, 0, 1, -6, -5)" filter="url(#StartFilterDefault)">
+                        <rect id="StartRectangle" width={START_SVG_WIDTH} height={START_SVG_HEIGHT} rx="20" transform="translate(6 5)" fill="#dce1ff" />
+                    </g>
+                    <g id="Rectangle-4" className="start-rect-fill" >
+                        <rect width="82" height="40" rx="20" stroke="none" fill="#dce1ff" />
+                        <rect x="0.5" y="0.5" width="81" height="39" rx="19.5" fill="none" />
+                    </g>
                 </g>
                 <text id="StartText" x="44" y="24" >
                     <tspan className="start-text"> {text}  </tspan>
