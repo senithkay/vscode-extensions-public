f47094b5ea6e93305fa482de20bcd0e370662bed
kaumini <kauminikg@gmail.com>
2021-09-29 16:05:31 +0530

    Add default value on variable form edit

--- low-code-editor/src/Diagram/components/ConfigForms/ProcessConfigForms/ProcessForm/AddVariableConfig/index.tsx
+++ low-code-editor/src/Diagram/components/ConfigForms/ProcessConfigForms/ProcessForm/AddVariableConfig/index.tsx
@@ -60,6 +60,7 @@ export function AddVariableConfig(props: AddVariableConfigProps) {
     let initialModelType: string = 'json';
     let modelType;
     let variableName: string = '';
+    let varExpression;
 
     const existingProperty = config && config.model;
     if (existingProperty && STKindChecker.isLocalVarDecl(config.model)) {
@@ -80,8 +81,10 @@ export function AddVariableConfig(props: AddVariableConfigProps) {
             modelType = typeDescriptor.source.trim();
         }
         variableName = localVarDec.typedBindingPattern.bindingPattern.source.trim();
+        varExpression = localVarDec.initializer.source;
     } else {
         variableName = stSymbolInfo ? null : "";
+        varExpression = stSymbolInfo ? null : "";
     }
 
     const [selectedType, setSelectedType] = useState(initialModelType);
@@ -91,7 +94,7 @@ export function AddVariableConfig(props: AddVariableConfigProps) {
     const [varNameError, setVarNameError] = useState("");
     const [isValidVarName, setIsValidVarName] = useState(false);
     const [validExpresssionValue, setValidExpresssionValue] = useState(config.config !== "");
-    const [variableExpression, setVariableExpression] = useState<string>(config.config.toString());
+    const [variableExpression, setVariableExpression] = useState<string>(varExpression);
     const [editorFocus, setEditorFocus] = useState<boolean>(false);
 
     if (defaultVarName === undefined) {
@@ -304,6 +307,7 @@ export function AddVariableConfig(props: AddVariableConfigProps) {
                                         revertFocus: revertEditorFocus
                                     }}
                                     onChange={onPropertyChange}
+                                    defaultValue={variableExpression}
                                 />
                             </div>
                         </div>
