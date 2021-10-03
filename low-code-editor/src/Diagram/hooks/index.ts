2a202136c58c8c6bde5c7c11b54318afcb82c5a4
Kavith Lokuhewage <Kavith Lokuhewage>
2021-10-03 22:40:06 +0530

    Expand service when a member is selected

--- low-code-editor/src/Diagram/hooks/index.ts
+++ low-code-editor/src/Diagram/hooks/index.ts
@@ -5,7 +5,7 @@ import { STKindChecker, STNode } from "@ballerina/syntax-tree";
 import { Context } from "../../Contexts/Diagram";
 import { SelectedPosition } from "../../types";
 
-export function useSelectedStatus(node: STNode, containerRef: React.MutableRefObject<any>): boolean {
+export function useSelectedStatus(node: STNode, containerRef?: React.MutableRefObject<any>): boolean {
     const {
         props: { selectedPosition }
     } = useContext(Context)
@@ -14,17 +14,21 @@ export function useSelectedStatus(node: STNode, containerRef: React.MutableRefOb
     React.useEffect(() => {
         const selected = isNodeSelected(selectedPosition, node);
         setSelected(selected);
-        if (selected) {
+        if (selected && containerRef) {
             containerRef.current.scrollIntoView();
         }
     }, [selectedPosition, node])
     return isSelected;
 }
 
-export function isNodeSelected(selectedPosition: SelectedPosition, node: STNode) {
-    if (STKindChecker.isFunctionDefinition(node)) {
+export function isNodeSelected(selectedPosition: SelectedPosition, node: STNode): boolean {
+    if (STKindChecker.isFunctionDefinition(node) || STKindChecker.isResourceAccessorDefinition(node)) {
         return selectedPosition.startLine === node.functionName.position?.startLine
         && selectedPosition.startColumn === node.functionName.position?.startColumn;
+    } else if (STKindChecker.isServiceDeclaration(node)) {
+        // check if the selected node is a member of the service
+        const selectedMembers = node.members.filter((memberNode) => isNodeSelected(selectedPosition, memberNode));
+        return selectedMembers && selectedMembers.length > 0;
     } else {
         return selectedPosition.startLine === node.position?.startLine
         && selectedPosition.startColumn === node.position?.startColumn;
