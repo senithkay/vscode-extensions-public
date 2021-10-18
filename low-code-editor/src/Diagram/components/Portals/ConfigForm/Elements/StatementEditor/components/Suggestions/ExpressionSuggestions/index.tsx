1e58fca5a7d4bff4ff25f7faec7948d553e7f2ac
sachiniSam <sachiniprof@gmail.com>
2021-10-18 13:05:16 +0530

    Add story for ExpressionSuggestions component

--- low-code-editor/src/Diagram/components/Portals/ConfigForm/Elements/StatementEditor/components/Suggestions/ExpressionSuggestions/index.tsx
+++ low-code-editor/src/Diagram/components/Portals/ConfigForm/Elements/StatementEditor/components/Suggestions/ExpressionSuggestions/index.tsx
@@ -20,7 +20,7 @@ import { SuggestionItem } from "../../../models/definitions";
 import { addExpression, addOperator } from "../../../utils/utils";
 import { statementEditorStyles } from "../../ViewContainer/styles";
 
-interface ExpressionSuggestionsProps {
+export interface ExpressionSuggestionsProps {
     model: STNode
     suggestions?: SuggestionItem[],
     operator: boolean,
