70bd6d3594e37d3c7fb9d952d5f7f5503d574ed5
YashodhaSamarawickrama <yasho.samarawickrama@gmail.com>
2021-11-09 13:28:51 +0530

    Resolve comments and fix module variable getting undefined issue

--- low-code-editor/src/Diagram/components/IfElse/index.tsx
+++ low-code-editor/src/Diagram/components/IfElse/index.tsx
@@ -151,7 +151,10 @@ export function IfElse(props: IfElseProps) {
     };
 
     const isDraftStatement: boolean = viewState instanceof DraftStatementViewState;
-    const conditionWrapper = isDraftStatement ? (diagnostics?.length !== 0 ? cn("main-condition-wrapper active-condition-error") : cn("main-condition-wrapper active-condition")) : (diagnostics?.length !== 0 ? cn("main-condition-wrapper if-condition-error-wrapper") : cn("main-condition-wrapper if-condition-wrapper"));
+    const conditionWrapper = isDraftStatement ? (diagnostics?.length !== 0 ? 
+        cn("main-condition-wrapper active-condition-error") : cn("main-condition-wrapper active-condition")) : 
+        (diagnostics?.length !== 0 ? 
+        cn("main-condition-wrapper if-condition-error-wrapper") : cn("main-condition-wrapper if-condition-wrapper"));
 
     let assignmentText: any = (!isDraftStatement && STKindChecker?.isIfElseStatement(model));
     assignmentText = (model as IfElseStatement)?.condition.source;
