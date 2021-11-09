7628b6cb97c0f761b59f76263c47718be0ab73c6
ajanthasingam <ajanthasingam.16@cse.mrt.ac.lk>
2021-11-09 16:35:42 +0530

    add incrmeent logic

--- low-code-editor/src/Diagram/components/Portals/ConfigForm/Elements/StatementEditor/components/InputEditor/index.tsx
+++ low-code-editor/src/Diagram/components/Portals/ConfigForm/Elements/StatementEditor/components/InputEditor/index.tsx
@@ -323,7 +323,11 @@ export function InputEditor(props: InputEditorProps) {
     };
 
     const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newLine = addToTargetPosition(stmtCtx.modelCtx.statementModel.source, model.position.startColumn, event.target.value ? " " + event.target.value : "", model.position.endColumn + 1);
+        let increment = 0;
+        if (stmtCtx.modelCtx.statementModel.source.charAt(0) === "\n"){
+            increment = 1;
+        }
+        const newLine = addToTargetPosition(stmtCtx.modelCtx.statementModel.source, model.position.startColumn + increment, event.target.value ? event.target.value : "", model.position.endColumn + increment);
         debouncedContentChange(newLine, "");
         getContextBasedCompletions(event.target.value);
         setUserInput(event.target.value);
