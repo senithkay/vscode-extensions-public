2ff97a33a134fd6fddd18e08d25ab8fac27d87a9
Sharon David <srsharondavid@gmail.com>
2021-11-12 09:54:04 +0530

    Remove statement type component from variable name

--- low-code-editor/src/Diagram/components/VariableName/index.tsx
+++ low-code-editor/src/Diagram/components/VariableName/index.tsx
@@ -13,7 +13,6 @@
 // tslint:disable: jsx-no-multiline-js align  jsx-wrap-multiline
 import React, { ReactElement, useEffect, useState } from "react";
 
-import { PropertyIcon } from "../../../assets/icons";
 import Tooltip from '../../../components/Tooltip';
 import { DefaultConfig } from "../../visitors/default";
 
@@ -22,31 +21,20 @@ import "./style.scss";
 export let VARIABLE_NAME_WIDTH = 125;
 export const ICON_SVG_WRAPPER_WIDTH = 25;
 
-export function VariableName(props: { x: number, y: number, variableName: string, processType: string, key_id: number, statementType?: string }) {
-    const { processType, variableName, key_id, statementType, ...xyProps } = props;
+export function VariableName(props: { x: number, y: number, variableName: string, processType: string, key_id: number }) {
+    const { processType, variableName, key_id, ...xyProps } = props;
     const [textWidth, setTextWidth] = useState(VARIABLE_NAME_WIDTH);
-    const [statementTextWidth, setStatementTextWidth] = useState(VARIABLE_NAME_WIDTH);
 
     useEffect(() => {
         setTextWidth(document.getElementById("variableLegnth_" + key_id).getBoundingClientRect().width);
-        setStatementTextWidth(document.getElementById("statementLegnth_" + key_id).getBoundingClientRect().width);
     }, []);
 
     const variableMaxWidth = variableName.length >= 15;
     const variableWidth = textWidth
     let variableX = 0;
 
-    const statementTypeMaxWidth = 132;
-    const statementWidth = statementTextWidth;
-    const statmentTypeX = statementTypeMaxWidth - statementWidth;
-
-    const statementRectPadding = 20;
-    const statementRectwidth = statementWidth + statementRectPadding;
-    const statementRectX = statementTypeMaxWidth - (statementWidth + (statementRectPadding / 3));
 
     variableX = (variableWidth > VARIABLE_NAME_WIDTH) ? variableWidth - ICON_SVG_WRAPPER_WIDTH : variableX = (VARIABLE_NAME_WIDTH - variableWidth - (DefaultConfig.dotGap * 2));
-
-    const iconX = (variableWidth > 125) ? VARIABLE_NAME_WIDTH : VARIABLE_NAME_WIDTH - (variableWidth + ICON_SVG_WRAPPER_WIDTH);
 
     const variableTextComp: ReactElement = (
         <text id="getResponse" transform="translate(36 1)" className="variable-name">
@@ -60,20 +48,6 @@ export function VariableName(props: { x: number, y: number, variableName: string
         </text>
     );
 
-    const statementTextComp: ReactElement = (
-        <g>
-            <text className="statement-name">
-                <tspan x={statmentTypeX} id={"statementLegnth_" + key_id} y="10">
-                    {statementType}
-                </tspan>
-            </text>
-            <g className="statement-text-wrapper">
-                <rect width={statementWidth} height="14" rx="4" stroke="none" />
-                <rect x={statementRectX} y="0" width={statementRectwidth} height="13.25" rx="3.625" fill="none" />
-            </g>
-        </g>
-    );
-
     return (
         <svg {...xyProps} width="150" height="24" className="variable-wrapper">
             {variableMaxWidth ?
@@ -83,7 +57,6 @@ export function VariableName(props: { x: number, y: number, variableName: string
                 :
                 variableTextComp
             }
-            {statementTextComp}
         </svg >
     );
 }
