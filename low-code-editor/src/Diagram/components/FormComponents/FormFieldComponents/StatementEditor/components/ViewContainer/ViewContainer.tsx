94fed3e35eb47ee79f0ae0f5e5e3c5b0f8c6788b
sachiniSam <sachiniprof@gmail.com>
2021-11-17 11:03:27 +0530

    Update targetposition to get from formArgs

--- low-code-editor/src/Diagram/components/FormComponents/FormFieldComponents/StatementEditor/components/ViewContainer/ViewContainer.tsx
+++ low-code-editor/src/Diagram/components/FormComponents/FormFieldComponents/StatementEditor/components/ViewContainer/ViewContainer.tsx
@@ -152,7 +152,7 @@ export function ViewContainer(props: ViewProps) {
                         model={model}
                         onCancelClicked={onCancelClicked}
                         updateModel={updateModel}
-                        formModel={formArgs.formArgs.model}
+                        formArgs={formArgs}
                         validateStatement={validateStatement}
                     >
                         <LeftPane
