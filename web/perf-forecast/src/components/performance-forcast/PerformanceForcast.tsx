5b0b4215b250f494f99018b77aff64cb0b924c9c
gigara <info@gigara.info>
2021-11-25 14:51:55 +0530

    Improve the clicks on the perf analyzer plots

--- web/perf-forecast/src/components/performance-forcast/PerformanceForcast.tsx
+++ web/perf-forecast/src/components/performance-forcast/PerformanceForcast.tsx
@@ -18,7 +18,7 @@
  */
 
 import React from 'react';
-import { AreaChart, Area, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LineChart } from 'recharts';
 import "./graph-styles.css";
 
 export interface DataPoint {
@@ -38,7 +38,8 @@ interface vscode {
 }
 
 declare const vscode: vscode;
-
+let currentConcurrency = 1;
+let hoverConcurrency = 1;
 export const PerformanceForcast = ({ name, data }: PerformanceForcastProps) => {
     return (
         <div className="performance-forcast">
@@ -48,11 +49,13 @@ export const PerformanceForcast = ({ name, data }: PerformanceForcastProps) => {
 
                 </div>
                 <ResponsiveContainer height={250}>
-                    <AreaChart
+                    <LineChart
                         width={500}
                         height={300}
                         data={data}
                         syncId="performance"
+                        onMouseMove={handleHover}
+                        onClick={handleClick}
                         margin={{
                             top: 5,
                             right: 30,
@@ -71,9 +74,9 @@ export const PerformanceForcast = ({ name, data }: PerformanceForcastProps) => {
                         <YAxis />
                         <Tooltip cursor={{ strokeWidth: 2 }} />
                         <CartesianGrid strokeDasharray="0" />
-                        <Area type="monotone" dataKey="tps" activeDot={{ onClick: handleClick }} baseLine={8}
+                        <Line type="monotone" dataKey="tps" activeDot={{ onClick: handleClick }} baseLine={8}
                             stroke="#5567D5" strokeWidth="2" fillOpacity={1} fill="url(#colorThroughput)" />
-                    </AreaChart>
+                    </LineChart>
                 </ResponsiveContainer>
                 <div className="x-label">
                     User Count
@@ -82,11 +85,13 @@ export const PerformanceForcast = ({ name, data }: PerformanceForcastProps) => {
 
             <div className="diagram latency">
                 <ResponsiveContainer width="100%" height={250}>
-                    <AreaChart
+                    <LineChart
                         width={500}
                         height={300}
                         data={data}
                         syncId="performance"
+                        onMouseMove={handleHover}
+                        onClick={handleClick}
                         margin={{
                             top: 5,
                             right: 30,
@@ -106,9 +111,9 @@ export const PerformanceForcast = ({ name, data }: PerformanceForcastProps) => {
                         <YAxis />
                         <Tooltip cursor={{ strokeWidth: 2 }} />
                         <CartesianGrid strokeDasharray="0" />
-                        <Area type="monotone" dataKey="latency" activeDot={{ onClick: handleClick }}
+                        <Line type="monotone" dataKey="latency" activeDot={{ onClick: handleClick }}
                             stroke="#EA4C4D" strokeWidth="2" fillOpacity={1} fill="url(#colorLatency)" />
-                    </AreaChart>
+                    </LineChart>
                 </ResponsiveContainer>
                 <div className="x-label">
                     User Count
@@ -122,16 +127,62 @@ export const PerformanceForcast = ({ name, data }: PerformanceForcastProps) => {
 };
 
 /**
- * Send clicked data point to update codelenses,
- * @param index Point index
- * @param data Point data
+ * Handle mouse click event.
+ * 
+ * @param index clicked index
+ * @param data event data
  */
 const handleClick = (index: any, data: any) => {
-    const payload: DataPoint = data.payload;
+    if (index.chartX && index.activeCoordinate) {
+
+        // if click on line
+        const activeCoordinateX = index.activeCoordinate.x;
+        if (index.chartX > activeCoordinateX - 1 && index.chartX < activeCoordinateX + 1) {
+
+            const concurrenct = index.activeLabel;
+            currentConcurrency = concurrenct;
+            updateConcurrency(concurrenct);
+        }
+
+    } else {
+
+        // if click on dot
+        const payload: DataPoint = data.payload;
+        currentConcurrency = payload.concurrency;
+        updateConcurrency(payload.concurrency);
+    }
+
+}
+
+/**
+ * Handle mouse hover event.
+ * 
+ * @param data event data
+ */
+const handleHover = (data: any) => {
+    if (!data.activeLabel) {
+
+        updateConcurrency(currentConcurrency);
+    }
+
+    if (data.activeLabel && data.activeLabel != hoverConcurrency) {
+
+        hoverConcurrency = data.activeLabel;
+        updateConcurrency(data.activeLabel);
+
+    }
+}
+
+/**
+ * Send concurrency to update codelenses.
+ * 
+ * @param concurrency Concurrency
+ */
+function updateConcurrency(concurrency: number) {
     vscode.postMessage({
         command: 'updateCodeLenses',
-        text: payload.concurrency
-    })
+        text: concurrency
+    });
 }
 
 const getXAxis = () =>
