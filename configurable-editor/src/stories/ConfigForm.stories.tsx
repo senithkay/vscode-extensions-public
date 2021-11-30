b97b9af458cc3e6b17612e809a768c2c682b8ea8
IrushiL <irushi@wso2.com>
2021-11-30 20:44:28 +0530

    Fix lint issues

--- configurable-editor/src/stories/ConfigForm.stories.tsx
+++ configurable-editor/src/stories/ConfigForm.stories.tsx
@@ -2,50 +2,11 @@ import React from "react";
 
 import ConfigForm from "../components/ConfigForm";
 
+import configSchema from "./data/config-schema.json";
+
 export default {
   component: ConfigForm,
-  title: 'ConfigForm',
+  title: "ConfigForm",
 };
 
-const data = {
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "type": "object",
-  "properties": {
-    "dilhashanazeer": {
-      "type": "object",
-      "properties": {
-        "simpleconfigs": {
-          "type": "object",
-          "properties": {
-            "itemCode": {
-              "type": "string",
-              "description": "description"
-            },
-            "discount": {
-              "type": "number",
-              "description": "description"
-            },
-            "testMode": {
-              "type": "boolean",
-              "description": "description"
-            },
-            "mod1": {
-              "type": "object",
-              "properties": {
-                "nameVal": {
-                  "type": "string",
-                  "description": "description"
-                }
-              }
-            }
-          },
-          "required": [
-            "testMode"
-          ]
-        }
-      }
-    }
-  }
-};
-
-export const BasicForm = () => <ConfigForm configSchema={data}/>;
+export const BasicForm = () => <ConfigForm configSchema={configSchema}/>;
