017ab5d5580bab44428fa40d829f98afd69d782b
CharukaK <charukakarunanayake@gmail.com>
2021-12-06 14:13:41 +0530

    Fix expansion of newly added components

--- low-code-editor/src/Diagram/hooks/index.ts
+++ low-code-editor/src/Diagram/hooks/index.ts
@@ -35,17 +35,8 @@ export function useSelectedStatus(node: STNode, containerRef?: React.MutableRefO
 }
 
 export function isNodeSelected(selectedPosition: SelectedPosition, node: STNode): boolean {
-    if (STKindChecker.isFunctionDefinition(node) || STKindChecker.isResourceAccessorDefinition(node)) {
-        return selectedPosition.startLine === node.functionName.position?.startLine
-            && selectedPosition.startColumn === node.functionName.position?.startColumn;
-    } else if (STKindChecker.isServiceDeclaration(node)) {
-        // check if the selected node is a member of the service
-        const selectedMembers = node.members.filter((memberNode) => isNodeSelected(selectedPosition, memberNode));
-        return selectedMembers && selectedMembers.length > 0;
-    } else {
-        return selectedPosition.startLine === node.position?.startLine
-            && selectedPosition.startColumn === node.position?.startColumn;
-    }
+    return selectedPosition.startLine >= node.position?.startLine
+        && selectedPosition.startLine <= node.position?.endLine;
 }
 
 export function useOverlayRef(): [
