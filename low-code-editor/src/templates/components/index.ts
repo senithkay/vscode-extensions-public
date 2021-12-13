a9db7ab68d0fdf35507d166855de65cc6c9f8a55
LahiruRajapaksha <theekshanabreezing@hotmail.com>
2021-12-13 12:32:27 +0530

    Update configurable param value

--- low-code-editor/src/templates/components/index.ts
+++ low-code-editor/src/templates/components/index.ts
@@ -90,10 +90,10 @@ service /{{{ BASE_PATH }}} on {{{ LISTENER_NAME }}}`,
     TYPE_DEFINITION: `
 {{#if ACCESS_MODIFIER }}{{{ ACCESS_MODIFIER }}} {{/if}}type {{{ TYPE_NAME }}} {{{ TYPE_DESCRIPTOR }}}`,
     TRIGGER: `
-    configurable {{triggerType}}:ListenerConfig userInput = ?;
+    configurable {{triggerType}}:ListenerConfig config = ?;
 
         {{#if httpBased }}listener http:Listener httpListener = new(8090);{{/if}}
-        listener {{triggerType}}:Listener webhookListener = new(userInput{{#if httpBased }}, httpListener{{/if}});
+        listener {{triggerType}}:Listener webhookListener = new(config{{#if httpBased }}, httpListener{{/if}});
 
         {{#each serviceTypes}}
         service {{../triggerType}}:{{ this.name }} on webhookListener {
