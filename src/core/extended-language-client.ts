c8a5ddc52add9f765fc747b0efdac8db3577d81b
tharindulak <tharindu.jayathilake1@gmail.com>
2021-12-17 15:44:50 +0530

    Add HTTP connectors cache

--- src/core/extended-language-client.ts
+++ src/core/extended-language-client.ts
@@ -38,6 +38,7 @@ import { Values } from "../forecaster/model";
 import { showChoreoSigninMessage } from "../forecaster";
 
 export const CONNECTOR_LIST_CACHE = "CONNECTOR_LIST_CACHE";
+export const HTTP_CONNECTOR_LIST_CACHE = "HTTP_CONNECTOR_LIST_CACHE";
 export const BALLERINA_LANG_ID = "ballerina";
 const NOT_SUPPORTED = {};
 
@@ -368,6 +369,11 @@ export class ExtendedLangClient extends LanguageClient {
         }
         if (!reset && params.query === "" && !params.keyword && !params.organization && !params.offset) {
             let connectorList = this.ballerinaExtInstance?.context?.globalState.get(CONNECTOR_LIST_CACHE) as BallerinaConnectorsResponse;
+            if (connectorList && connectorList.central?.length > 0) {
+                return Promise.resolve().then(() => connectorList);
+            }
+        } else if (!reset && params.query === "http" && !params.keyword && !params.organization && !params.offset) {
+            const connectorList = this.ballerinaExtInstance?.context?.globalState.get(HTTP_CONNECTOR_LIST_CACHE) as BallerinaConnectorsResponse;
             if (connectorList && connectorList.central?.length > 0) {
                 return Promise.resolve().then(() => connectorList);
             }
