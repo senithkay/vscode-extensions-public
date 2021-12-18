c30893c78caa4923b57791ace1415f01f893c9c0
Kanushka Gayan <kanushkanet@gmail.com>
2021-12-18 12:12:51 +0530

    Fix default icon missplace issue

--- low-code-editor/src/Diagram/components/LowCodeDiagram/Components/RenderingComponents/Connector/ConnectorHeader/ModuleIcon.tsx
+++ low-code-editor/src/Diagram/components/LowCodeDiagram/Components/RenderingComponents/Connector/ConnectorHeader/ModuleIcon.tsx
@@ -37,9 +37,9 @@ export function ModuleIcon(props: ModuleIconProps) {
 
     const [showDefaultIcon, setShowDefaultIcon] = useState(false);
 
-    if (node && node.typeData?.typeSymbol?.moduleID) {
-        const moduleInfo = node.typeData.typeSymbol.moduleID;
-        iconUrl = `${balCentralCdn}/${moduleInfo.orgName}_${moduleInfo.moduleName}_${moduleInfo.version}.png`;
+    if (node) {
+        const moduleInfo = node.typeData?.typeSymbol?.moduleID;
+        iconUrl = `${balCentralCdn}/${moduleInfo?.orgName}_${moduleInfo?.moduleName}_${moduleInfo?.version}.png`;
         defaultProps = {
             cx: cx - marginError,
             cy: cy - marginError,
