6eb7ccd1f2a9a863963382d4525c25198622e971
Niron Rasanjana <snironr@gmail.com>
2022-01-25 09:39:14 +0530

    Change project structure

--- code-server-tests/cypress/integration/code-server-smoke.ts
+++ code-server-tests/cypress/integration/code-server-smoke.ts
@@ -15,79 +15,80 @@
 /// <reference types="cypress-xpath" />
 
 describe("Code server smoke test", () => {
-  const packageName = "test_package_01";
-  const serviceName = "test_service_01";
-
   it("Validate vs code is opened without errors and Ballerina extension is present", () => {
     cy.visit(Cypress.env("workspaceUrl"));
-    cy.get(".category-description-container > .category-title", {
-      timeout: 60000,
-    })
-      .should("be.visible")
-      .contains("Get Started with VS Code");
+    // cy.get(".category-description-container > .category-title", {
+    //   timeout: 60000,
+    // })
+    //   .should("be.visible")
+    //   .contains("Get Started with VS Code");
     cy.get("body").should("not.contain", "error");
     cy.get("h2", {
       timeout: 60000,
     }).should("be.visible");
     //Verify extension
-    // cy.get("div[id='wso2.ballerina']", { timeout: 60000 }).contains(
-    //   "Ballerina SDK: Swan Lake"
-    // );
+    cy.get("div[id='wso2.ballerina']", { timeout: 60000 }).contains(
+      "Ballerina SDK: Swan Lake"
+    );
     cy.get('a[class="action-label codicon codicon-extensions-view-icon"]', {
       timeout: 60000,
     }).click();
     cy.get('span[class="name"]').contains("Ballerina");
 
-    // Go to package and open bal
-    cy.get(
-      'a[class="action-label codicon codicon-explorer-view-icon"]'
-    ).click();
-    cy.get("body").then(($body) => {
-      if ($body.find("div[aria-label='main.bal']").length == 0) {
-        cy.get('a[class="label-name"]').contains(packageName);
-        cy.xpath("//span[text()='test_package_01']").click();
-      }
-    });
-    cy.wait(5000);
-    cy.xpath("//span[text()='main.bal']").dblclick();
-
-    cy.wait(6000);
-    //Verify the lines in opened bal
-    cy.get(".view-lines", { timeout: 60000 }).contains(
-      "public function main() {"
-    );
+    //Verify service.bal diagram tab
+    cy.get("div[title='service.bal Diagram']").contains("service.bal Diagram");
   });
 
-  it.skip("Run the main", () => {
-    cy.get("a[title='Run']").click();
-    cy.wait(38000);
-    cy.get(
-      '[style=""] > .monaco-split-view2 > .monaco-scrollable-element > .split-view-container > .split-view-view > .terminal-split-pane > .terminal-wrapper > :nth-child(1) > .terminal > .xterm-screen > .xterm-cursor-layer'
-    ).should("contain", "Hello, World!");
+  it("Open a service bal source and verify the code rendered", () => {
+    cy.get("a[title='Show Source']").click();
+    cy.wait(1000);
+    // Close the diagram
+    cy.xpath(
+      "//div[@aria-label='service.bal Diagram']/div[@class='tab-actions']//a[@role='button']"
+    ).click();
+    // Verify code rendering
+    cy.get(".view-lines", {
+      timeout: 60000,
+    }).contains("service /hello on new http:Listener(9090)");
   });
 
   it("Verify issues are displayed in the status bar when code contains errors", () => {
     cy.get("a[aria-label='No Problems']").should("be.visible");
-    cy.get(".view-lines", { timeout: 60000 }).click();
+    cy.get(
+      ".editor-instance > .no-user-select > .overflow-guard > .monaco-scrollable-element > .lines-content > .view-lines",
+      {
+        timeout: 60000,
+      }
+    ).click();
     //Type some random string in to code
-    cy.get(".view-lines").type("Some Random text");
+    cy.get(
+      ".editor-instance > .no-user-select > .overflow-guard > .monaco-scrollable-element > .lines-content > .view-lines"
+    ).type("Some Random text");
     //Verify there is an increase in problem status bar
     cy.get("a[aria-label='No Problems']", { timeout: 60000 }).should(
       "not.exist"
     );
+    // Undo the added code
+    cy.get(
+      ".editor-instance > .no-user-select > .overflow-guard > .monaco-scrollable-element > .lines-content > .view-lines"
+    ).type("{cmd}z{cmd}z{cmd}z");
   });
 
-  it("Open a service bal and verify the code rendered", () => {
-    cy.get("body").then(($body) => {
-      if ($body.find("div[aria-label='service.bal']").length == 0) {
-        cy.get('a[class="label-name"]').contains(serviceName);
-        cy.xpath("//span[text()='test_service_01']").click();
-      }
+  it("Run the Service and invoke api", () => {
+    cy.get("a[title='Run']").click();
+    cy.wait(38000);
+    cy.get("div[class='outline-element monaco-breadcrumb-item']", {
+      timeout: 38000,
+    }).contains("service /hello ");
+    cy.request({
+      method: "GET",
+      url: "http://localhost:9090/hello/sayHello?name=Test user",
+      headers: {
+        accept: "text/plain",
+      },
+    }).then((resp) => {
+      expect(resp.status).to.eq(200);
+      expect(resp.body).to.eq("Hello, Test user");
     });
-    cy.xpath("//span[text()='service.bal']").click();
-    // Verify code rendering
-    cy.get(".view-lines", { timeout: 60000 }).contains(
-      "service /hello on new http:Listener(9090)"
-    );
   });
 });
