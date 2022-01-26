43313ff8444ab71e3d7ffa21e5375d1685637c50
Niron Rasanjana <snironr@gmail.com>
2022-01-26 16:45:24 +0530

    update snapshot

--- code-server-tests/cypress/support/commands.js
+++ code-server-tests/cypress/support/commands.js
@@ -23,10 +23,4 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
 
-addMatchImageSnapshotCommand({
-  failureThreshold: 0.05, // threshold for entire image
-  failureThresholdType: "percent", // percent of image or number of pixels
-  capture: "viewport", // capture viewport in screenshot
-});
--- code-server-tests/cypress/support/index.js
+++ code-server-tests/cypress/support/index.js
@@ -18,5 +18,10 @@ import './commands'
 require('cypress-xpath')
 import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
 
+addMatchImageSnapshotCommand({
+  failureThreshold: 0.05, // threshold for entire image
+  failureThresholdType: "percent", // percent of image or number of pixels
+  capture: "viewport", // capture viewport in screenshot
+});
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
