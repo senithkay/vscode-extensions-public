4409af13e5f0f5c1ef40abe0622bd21ab663bcf0
CharukaK <charukakarunanayake@gmail.com>
2022-01-31 11:41:01 +0530

    Add rendering component for worker declaration

---
+++ low-code-editor/src/Diagram/components/LowCodeDiagram/Components/RenderingComponents/WorkerDeclaration/WorkerHead/index.tsx
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ * associated services.
+ */
+
+import React from "react";
+
+import { NamedWorkerDeclaration } from "@wso2-enterprise/syntax-tree";
+
+import { FunctionViewState } from "../../../../ViewState";
+import { StartSVG, START_SVG_WIDTH } from "../../Start/StartSVG";
+
+interface WorkerHeadProps {
+    model: NamedWorkerDeclaration
+}
+
+export function WorkerHead(props: WorkerHeadProps) {
+    const { model } = props;
+    const viewState: FunctionViewState = model.viewState;
+
+    return (
+        <StartSVG
+            x={viewState.bBox.cx - (START_SVG_WIDTH / 2)}
+            y={viewState.bBox.cy - (START_SVG_WIDTH / 2)}
+            text={model.workerName.value}
+        />
+    )
+}
