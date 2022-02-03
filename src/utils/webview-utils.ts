871c1f90f68ad337aefd4419af55f07cd81e7e32
gigara <info@gigara.info>
2022-02-03 15:32:59 +0530

    Add Hot jar

--- src/utils/webview-utils.ts
+++ src/utils/webview-utils.ts
@@ -122,6 +122,17 @@ export function getLibraryWebViewContent(options: WebViewOptions) {
                     }
                     ${styles}
                 </style>
+                <!-- Hotjar Tracking Code for https://*.workspace.choreo.dev -->
+                <script>
+                    (function(h,o,t,j,a,r){
+                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                        h._hjSettings={hjid:2811498,hjsv:6};
+                        a=o.getElementsByTagName('head')[0];
+                        r=o.createElement('script');r.async=1;
+                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                        a.appendChild(r);
+                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                </script>
             </head>
             
             <body class="${bodyCss}">
