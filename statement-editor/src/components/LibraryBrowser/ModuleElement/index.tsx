735d46587b578b1624a1e5c3ab5f219b966b433d
Madusha Gunasekara <madushajg@gmail.com>
2022-02-07 12:25:28 +0530

    Send didChange from the library browser

--- statement-editor/src/components/LibraryBrowser/ModuleElement/index.tsx
+++ statement-editor/src/components/LibraryBrowser/ModuleElement/index.tsx
@@ -18,8 +18,11 @@ import {
     LibraryFunction,
     ModuleProperty
 } from "@wso2-enterprise/ballerina-low-code-edtior-commons";
+import * as monaco from "monaco-editor";
 
 import { StatementEditorContext } from "../../../store/statement-editor-context";
+import { addStatementToTargetLine, sendDidChange } from "../../../utils/ls-utils";
+import { EXPR_SCHEME, FILE_SCHEME } from "../../InputEditor/constants";
 import { useStatementEditorStyles } from "../../styles";
 
 interface ModuleElementProps {
@@ -39,8 +42,12 @@ export function ModuleElement(props: ModuleElementProps) {
             updateModel,
             addModuleImport
         },
+        currentFile,
+        getLangClient,
         getLibraryData
     } = useContext(StatementEditorContext);
+
+    const fileURI = monaco.Uri.file(currentFile.path).toString().replace(FILE_SCHEME, EXPR_SCHEME);
 
     const onClickOnModuleElement = async () => {
         const response: LibraryDataResponse = await getLibraryData(moduleOrgName, moduleId, moduleVersion);
@@ -68,8 +75,22 @@ export function ModuleElement(props: ModuleElementProps) {
                 content += `(${parameters.join(',')})`;
             }
         }
+
+        const updatedContent: string = await addStatementToTargetLine(
+            currentFile.content, {startLine: 0, startColumn: 0, endLine: 0, endColumn: 0}, `import ${moduleOrgName}/${moduleId};`, getLangClient);
+        const langClient = await getLangClient();
+        langClient.didOpen({
+            textDocument: {
+                uri: fileURI,
+                languageId: "ballerina",
+                text: currentFile.content,
+                version: 1
+            }
+        });
+        sendDidChange(fileURI, updatedContent, getLangClient).then();
         updateModel(content, currentModel.model.position);
-        addModuleImport(moduleOrgName, moduleId);
+
+        // addModuleImport(moduleOrgName, moduleId);
     }
 
     return (
