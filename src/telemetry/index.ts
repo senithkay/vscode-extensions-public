87b27620b4dc47bdbe72f1172d58731195c1a528
Prabushi Samarakoon <prabushisamarakoon@gmail.com>
2022-02-09 07:29:22 +0530

    Add correlationId for insight events
    Related issue https://github.com/wso2-enterprise/choreo/issues/11121

--- src/telemetry/index.ts
+++ src/telemetry/index.ts
@@ -25,6 +25,7 @@ const DEFAULT_KEY = "3a82b093-5b7b-440c-9aa2-3b8e8e5704e7";
 const INSTRUMENTATION_KEY = process.env.CODE_SERVER_ENV && process.env.VSCODE_CHOREO_INSTRUMENTATION_KEY ? process.env.VSCODE_CHOREO_INSTRUMENTATION_KEY : DEFAULT_KEY;
 const isWSO2User = process.env.VSCODE_CHOREO_USER_EMAIL ? process.env.VSCODE_CHOREO_USER_EMAIL.endsWith('@wso2.com') : false;
 const isAnonymous = process.env.VSCODE_CHOREO_USER_EMAIL ? 'false' : 'true';
+const CORRELATION_ID = process.env.VSCODE_CHOREO_CORRELATION_ID ? process.env.VSCODE_CHOREO_CORRELATION_ID : '';
 
 export function createTelemetryReporter(ext: BallerinaExtension): TelemetryReporter {
     const reporter = new TelemetryReporter(ext.getID(), ext.getVersion(), INSTRUMENTATION_KEY);
@@ -66,9 +67,10 @@ export function getTelemetryProperties(extension: BallerinaExtension, component:
         'ballerina.message': message,
         'scope': component,
         'idpId': process.env.VSCODE_CHOREO_USER_IDP_ID ? process.env.VSCODE_CHOREO_USER_IDP_ID : '',
-        'isWSO2User' : isWSO2User ? 'true' : 'false',
+        'isWSO2User': isWSO2User ? 'true' : 'false',
         'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone,
-        'AnonymousUser': isAnonymous
+        'AnonymousUser': isAnonymous,
+        'correlationId': CORRELATION_ID,
     };
 }
 
