6aff731bb8c19b87c94f9d1401f9330fa5e6dd46
Kavith Lokuhewage <kaviththiranga@gmail.com>
2022-02-18 08:16:46 +0530

    Move bundling logic to a new distribution module

---
+++ distribution/src/index.tsx
@@ -0,0 +1,7 @@
+import * as BLC from "@wso2-enterprise/ballerina-low-code-editor";
+
+const { renderDiagramEditor } = BLC;
+
+export default {
+    renderDiagramEditor
+}
---
+++ distribution/webpack.config.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const webpack = require('webpack');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+const LOW_CODE_DIR = path.resolve(__dirname, '../low-code-editor');
+const MONACO_DIR = path.resolve(__dirname, '../node_modules/monaco-editor');
+
+module.exports = (env, argv) => ({
+    mode: 'none',
+    entry: {
+        BLCEditor: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: 'web',
+    devtool: argv.mode === "production" ? undefined : "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".mjs"],
+        alias: {
+            handlebars: 'handlebars/dist/handlebars.min.js',
+            "vscode": require.resolve('monaco-languageclient/lib/vscode-compatibility'),
+            "crypto": false,
+            "net": false,
+            "os": false,
+            "path": false
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|mjs|jsx|ts|tsx)$/,
+                enforce: "pre",
+                use: ["source-map-loader"],
+            },
+            {
+                test: /\.ts(x?)$/,
+                use: [
+                    // "@jsdevtools/coverage-istanbul-loader", TODO: Enable this only when we are using final build for cypress
+                    'ts-loader'
+                ],
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.css$/,
+                include: LOW_CODE_DIR,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.css$/,
+                include: [MONACO_DIR],
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                include: [LOW_CODE_DIR, MONACO_DIR],
+                type: 'asset/inline'
+            },
+            {
+                test: /\.svg/,
+                use: {
+                    loader: 'svg-url-loader',
+                    options: {
+
+                    }
+                }
+            },
+        ],
+    },
+    ignoreWarnings: [/Failed to parse source map/],
+    output: {
+        filename: 'BLCEditor.js',
+        path: path.resolve(__dirname, 'build'),
+        library: {
+            name: "BLCEditor",
+            type: "umd"
+        }
+    },
+    devServer: {
+        allowedHosts: 'all',
+        port: 9000,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        devMiddleware: {
+            mimeTypes: { 'text/css': ['css'] },
+        },
+    },
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1
+        }),
+        new ForkTsCheckerWebpackPlugin(), // run TSC on a separate thread,
+        new MonacoWebpackPlugin({
+            languages: ['ballerina', 'yaml', 'json']
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(argv.mode)
+        })
+    ]
+});
+
