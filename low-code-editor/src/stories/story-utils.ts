2eb775564fcb688fbfeb26e89b10253a63bf6a60
Anjana S Porawagama <anjanash@wso2.com>
2022-03-01 10:41:02 +0530

    Added deleted function

--- low-code-editor/src/stories/story-utils.ts
+++ low-code-editor/src/stories/story-utils.ts
@@ -38,6 +38,35 @@ export async function updateFileContent(filePath: string, text: string): Promise
     }).then(result => result.success);
 }
 
+export function getDiagramGeneratorProps(filePath: string, enableSave: boolean = false): DiagramGeneratorProps {
+  return {
+    langClientPromise,
+    scale: "1",
+    panX: "0",
+    panY: "0",
+    filePath,
+    startColumn: 0,
+    startLine: 0,
+    getFileContent,
+    getPFSession: () => Promise.resolve(undefined),
+    getPerfDataFromChoreo: () =>  Promise.resolve(undefined),
+    gotoSource: () => Promise.resolve(false),
+    lastUpdatedAt: (new Date()).toISOString(),
+    resolveMissingDependency: () => Promise.resolve(false),
+    resolveMissingDependencyByCodeAction: () => Promise.resolve(false),
+    runCommand: () => Promise.resolve(false),
+    sendTelemetryEvent: () => Promise.resolve(undefined),
+    showMessage: () => Promise.resolve(false),
+    showPerformanceGraph: () => Promise.resolve(false),
+    updateFileContent: enableSave ? updateFileContent : () => Promise.resolve(undefined),
+    getLibrariesList: () => Promise.resolve(undefined),
+    getLibrariesData: () => Promise.resolve(undefined),
+    getLibraryData: () => Promise.resolve(undefined),
+    getSentryConfig: () => Promise.resolve(undefined)
+  }
+}
+
+
 export function renderStandaloneMockedEditor(container: string) {
     const element = createElement(StandaloneDiagramApp);
     render(element, document.getElementById(container));
