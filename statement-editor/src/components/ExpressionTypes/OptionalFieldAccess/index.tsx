2535986eec95540bac8c0e67badc6bd359bbbf6f
Madusha Gunasekara <madushajg@gmail.com>
2022-03-01 20:20:48 +0530

    Add optionalFieldAccess expression support

---
+++ statement-editor/src/components/ExpressionTypes/OptionalFieldAccess/index.tsx
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ * associated services.
+ */
+// tslint:disable: jsx-no-multiline-js
+import React, { ReactNode, useContext } from "react";
+
+import { OptionalFieldAccess } from "@wso2-enterprise/syntax-tree";
+import classNames from "classnames";
+
+import { DEFAULT_EXPRESSIONS } from "../../../constants";
+import { SuggestionItem, VariableUserInputs } from "../../../models/definitions";
+import { StatementEditorContext } from "../../../store/statement-editor-context";
+import { SuggestionsContext } from "../../../store/suggestions-context";
+import { getSuggestionsBasedOnExpressionKind, isPositionsEquals } from "../../../utils";
+import { addStatementToTargetLine, getContextBasedCompletions } from "../../../utils/ls-utils";
+import { ExpressionComponent } from "../../Expression";
+import { useStatementEditorStyles } from "../../styles";
+
+interface OptionalFieldAccessProps {
+    model: OptionalFieldAccess;
+    userInputs: VariableUserInputs;
+    isElseIfMember: boolean;
+    diagnosticHandler: (diagnostics: string) => void;
+}
+
+export function OptionalFieldAccessComponent(props: OptionalFieldAccessProps) {
+    const { model, userInputs, isElseIfMember, diagnosticHandler } = props;
+    const stmtCtx = useContext(StatementEditorContext);
+    const { modelCtx } = stmtCtx;
+    const { currentModel } = modelCtx;
+    const hasFieldAccessExprSelected = currentModel.model &&
+        isPositionsEquals(currentModel.model.position, model.position);
+
+    const statementEditorClasses = useStatementEditorStyles();
+    const { expressionHandler } = useContext(SuggestionsContext);
+    const { currentFile, getLangClient } = stmtCtx;
+    const targetPosition = stmtCtx.formCtx.formModelPosition;
+    const fileURI = `expr://${currentFile.path}`;
+
+    const onClickOnFieldAccessExpr = async (event: any) => {
+        event.stopPropagation();
+
+        const content: string = await addStatementToTargetLine(
+            currentFile.content, targetPosition, stmtCtx.modelCtx.statementModel.source, getLangClient);
+
+        const completions: SuggestionItem[] = await getContextBasedCompletions(
+            fileURI, content, targetPosition, model.position,
+            false, isElseIfMember, model.source, getLangClient);
+
+        expressionHandler(model, false, false, {
+            expressionSuggestions: getSuggestionsBasedOnExpressionKind(DEFAULT_EXPRESSIONS),
+            typeSuggestions: [],
+            variableSuggestions: completions
+        });
+    }
+
+    const onClickOnExpr = (event: any) => {
+        event.stopPropagation()
+        expressionHandler(model.expression, true, false,
+            { expressionSuggestions: [], typeSuggestions: [], variableSuggestions: [] })
+    }
+
+    const onClickOnFieldName = (event: any) => {
+        event.stopPropagation()
+        expressionHandler(model.fieldName, true, false,
+            { expressionSuggestions: [], typeSuggestions: [], variableSuggestions: [] })
+    }
+
+    const expression: ReactNode = (
+        <ExpressionComponent
+            model={model.expression}
+            userInputs={userInputs}
+            isElseIfMember={isElseIfMember}
+            diagnosticHandler={diagnosticHandler}
+            isTypeDescriptor={false}
+            onSelect={onClickOnExpr}
+        >
+            <span
+                className={classNames(
+                    statementEditorClasses.expressionBlock,
+                    statementEditorClasses.expressionBlockDisabled
+                )}
+            >
+                {model.optionalChainingToken.value}
+            </span>
+            <ExpressionComponent
+                model={model.fieldName}
+                userInputs={userInputs}
+                isElseIfMember={isElseIfMember}
+                diagnosticHandler={diagnosticHandler}
+                isTypeDescriptor={false}
+                onSelect={onClickOnFieldName}
+            />
+        </ExpressionComponent>
+    );
+
+    return (
+        <span>
+            {expression}
+        </span>
+    );
+}
