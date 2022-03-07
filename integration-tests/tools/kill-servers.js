f49f3b77c7e4ab8b06a3c9b64a72ff2f21098d38
Kavith Lokuhewage <kaviththiranga@gmail.com>
2022-03-07 08:29:48 +0530

    Fix background server kill script

--- integration-tests/tools/kill-servers.js
+++ integration-tests/tools/kill-servers.js
@@ -1,10 +1,14 @@
 const find = require('find-process');
 const kill = require('tree-kill');
 
-find('name', 'npm', true)
+find('name', 'node', true)
   .then(function (list) {
     console.log('there are %s server process(es)', list.length);
     list.forEach(element => {
-        kill(element.pid);
+        console.log(JSON.stringify(element));
+        if (element.cmd.endsWith("npm-run-all --parallel start-ls start-vscode-mock")) {
+          kill(element.pid);
+          console.log("Killing " + element.cmd)
+        }
     });
   });
