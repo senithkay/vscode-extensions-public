3ece6a79ab8a2d20b9836cf7b423847c9d942b98
CharukaK <charukakarunanayake@gmail.com>
2022-03-07 16:14:31 +0530

    Move worker rendering Component

---
+++ low-code-diagram/src/Components/RenderingComponents/WorkerDeclaration/index.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ * associated services.
+ */
+import React from 'react';
+
+import { NamedWorkerDeclaration } from '@wso2-enterprise/syntax-tree';
+
+
+import './style.scss';
+import { WorkerHead } from './WorkerHead';
+import { BlockViewState } from '../../../ViewState';
+import { WorkerDeclarationViewState } from '../../../ViewState/worker-declaration';
+import { End } from '../End';
+import { WorkerBody } from '../WorkerBody';
+import { WorkerLine } from '../WorkerLine';
+
+interface WorkerProps {
+    model: NamedWorkerDeclaration;
+}
+
+export function Worker(props: WorkerProps) {
+    const { model } = props;
+    const viewState: WorkerDeclarationViewState = model.viewState as WorkerDeclarationViewState;
+    const workerBodyVS: BlockViewState = model.workerBody.viewState as BlockViewState;
+
+    return (
+        <>
+            <WorkerLine viewState={model.viewState} />
+            <WorkerHead model={model} />
+            <WorkerBody model={model.workerBody} viewState={workerBodyVS} />
+            {!workerBodyVS.isEndComponentAvailable && <End viewState={model.viewState.end} />}
+        </>
+    )
+}
