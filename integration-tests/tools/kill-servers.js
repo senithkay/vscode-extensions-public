22c8791cbda7f05726ef1c00251857a1fdec38ee
Kavith Lokuhewage <kaviththiranga@gmail.com>
2022-03-09 09:46:39 +0530

    Kill npm process instead of node process to avoid pipeline failure

--- integration-tests/tools/kill-servers.js
+++ integration-tests/tools/kill-servers.js
@@ -1,14 +1,14 @@
 const find = require('find-process');
 const kill = require('tree-kill');
 
-find('name', 'node', true)
+find('name', 'npm', true)
   .then(function (list) {
     console.log('there are %s server process(es)', list.length);
     list.forEach(element => {
-        console.log(JSON.stringify(element));
-        if (element.cmd.endsWith("npm-run-all --parallel start-ls start-vscode-mock")) {
-          kill(element.pid);
+        if (element.cmd === "npm run start-servers") {
+          kill(element.pid, "SIGKILL");
           console.log("Killing " + element.cmd)
         }
+        console.log(JSON.stringify(element));
     });
   });
