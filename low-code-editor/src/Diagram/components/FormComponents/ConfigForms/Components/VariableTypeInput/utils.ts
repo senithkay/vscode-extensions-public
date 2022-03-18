7fe1001bf2cb0751a1aaa5ef3dcdea716cfadb26
Anjana S Porawagama <anjanash@wso2.com>
2022-03-18 12:59:27 +0530

    Update filtering logic for import modules

--- low-code-editor/src/Diagram/components/FormComponents/ConfigForms/Components/VariableTypeInput/utils.ts
+++ low-code-editor/src/Diagram/components/FormComponents/ConfigForms/Components/VariableTypeInput/utils.ts
@@ -12,14 +12,27 @@ export const getVarTypeCompletions = (ignoredCompletions: string[] = [], additio
     const langClient: ExpressionEditorLangClientInterface = await getExpressionEditorLangClient(langServerURL);
     const values: CompletionResponse[] = await langClient.getCompletion(completionParams);
 
-    //Supported Types: Class:7, Interface:8, Module:9, Unit:11, Enum:13, EnumMember:20, Struct:22   
+    // Supported Types: Class:7, Interface:8, Module:9, Unit:11, Enum:13, EnumMember:20, Struct:22
     const acceptedKind: CompletionItemKind[] = [7, 8, 9, 11, 13, 20, 22];
+
+    const validModules = (res: CompletionResponse) => {
+        if (res.additionalTextEdits && res.additionalTextEdits.length > 0) {
+            if (
+                res.additionalTextEdits[0].newText.includes("import") &&
+                res.additionalTextEdits[0].range.start.line === 0 &&
+                res.additionalTextEdits[0].range.end.line === 0) {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
 
     const filteredCompletionItem: CompletionResponse[] = values.filter(
         (completionResponse: CompletionResponse) =>
           (acceptedKind.includes(completionResponse.kind as CompletionItemKind) || completionResponse.label === 'var') &&
-          !ignoredCompletions.includes(completionResponse.label) && !completionResponse.label.includes("/")
-      );
+          !ignoredCompletions.includes(completionResponse.label) && validModules(completionResponse)
+    );
 
     const sortText: string[] = [];
     const completionItems: monaco.languages.CompletionItem[] = filteredCompletionItem.map((completionResponse: CompletionResponse, order: number) => {
