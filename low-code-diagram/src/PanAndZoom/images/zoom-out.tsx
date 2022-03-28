7a85ebaaa40bb32414a546c69efbb046c95cf275
YashodhaSamarawickrama <yasho.samarawickrama@gmail.com>
2022-03-28 17:26:32 +0530

    Fix lint issues and remove unused imports

--- low-code-diagram/src/PanAndZoom/images/zoom-out.tsx
+++ low-code-diagram/src/PanAndZoom/images/zoom-out.tsx
@@ -15,10 +15,10 @@ import * as React from "react";
 
 export default function ZoomOutSVG() {
   return (
-      <svg width="14" height="2">
-        <path id="ZoomOut" d="M151.312,276.1h0a1,1,0,0,1-1,1h-12a1,1,0,0,1-1-1h0a1,1,0,0,1,1-1h12A1,1,0,0,1,151.312,276.1Z" transform="translate(-137.312 -275.104)" fill="#8d91a3" opacity="0.769"/>
-        <title>Zoom Out</title>
-      </svg>
+    <svg width="14" height="2">
+      <path id="ZoomOut" d="M151.312,276.1h0a1,1,0,0,1-1,1h-12a1,1,0,0,1-1-1h0a1,1,0,0,1,1-1h12A1,1,0,0,1,151.312,276.1Z" transform="translate(-137.312 -275.104)" fill="#8d91a3" opacity="0.769" />
+      <title>Zoom out</title>
+    </svg>
   );
 }
 
