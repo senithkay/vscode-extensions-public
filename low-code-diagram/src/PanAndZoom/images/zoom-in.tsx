a66703083f933ff739ad61193e166448e9908f18
YashodhaSamarawickrama <yasho.samarawickrama@gmail.com>
2022-03-28 17:19:38 +0530

    Add tooltips to pan and zoom buttons

--- low-code-diagram/src/PanAndZoom/images/zoom-in.tsx
+++ low-code-diagram/src/PanAndZoom/images/zoom-in.tsx
@@ -12,11 +12,13 @@
  */
 // tslint:disable: jsx-no-multiline-js
 import * as React from "react";
+import { DefaultSVGTooltip } from "../../Components/RenderingComponents/DefaultSVGTooltip";
 
 export default function ZoomInSVG() {
   return (
     <svg width="12" height="12" >
-    <path id="ZoomIn" d="M145.521,260.555v-4h-4a1,1,0,0,1,0-2h4v-4a1,1,0,0,1,2,0v4h4a1,1,0,0,1,0,2h-4v4a1,1,0,0,1-2,0Z" transform="translate(-140.521 -249.555)" fill="#8d91a3" opacity="0.769"/>
-  </svg>
+        <path id="ZoomIn" d="M145.521,260.555v-4h-4a1,1,0,0,1,0-2h4v-4a1,1,0,0,1,2,0v4h4a1,1,0,0,1,0,2h-4v4a1,1,0,0,1-2,0Z" transform="translate(-140.521 -249.555)" fill="#8d91a3" opacity="0.769" />
+        <title>Zoom In</title>
+    </svg>
   );
 }
--- low-code-diagram/src/PanAndZoom/images/zoom-out.tsx
+++ low-code-diagram/src/PanAndZoom/images/zoom-out.tsx
@@ -17,6 +17,7 @@ export default function ZoomOutSVG() {
   return (
       <svg width="14" height="2">
         <path id="ZoomOut" d="M151.312,276.1h0a1,1,0,0,1-1,1h-12a1,1,0,0,1-1-1h0a1,1,0,0,1,1-1h12A1,1,0,0,1,151.312,276.1Z" transform="translate(-137.312 -275.104)" fill="#8d91a3" opacity="0.769"/>
+        <title>Zoom Out</title>
       </svg>
   );
 }
