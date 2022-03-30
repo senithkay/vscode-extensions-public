e6f50f584160a3a1db3b0c06007d44f9a87b9a71
HirumalPriyashan <hirupriyashanrc@gmail.com>
2022-03-30 16:14:09 +0530

    Add xml to notebook-renderer

---
+++ web/notebook-renderer/src/components/renderer/xml/xml.stories.tsx
@@ -0,0 +1,43 @@
+import { Attributes, ComponentChildren, Ref } from 'preact';
+import { Meta } from '@storybook/react';
+import { Xml } from './xml';
+import { NotebookCellResult } from '../types';
+
+export default {
+    component: Xml,
+    title: 'Components/Xml',
+} as Meta;
+
+const Template = (args: JSX.IntrinsicAttributes & { notebookCellOutput: Readonly<NotebookCellResult>; } 
+    & Readonly<Attributes & { children?: ComponentChildren; ref?: Ref<any>; }>) => <Xml {...args} />;
+
+export const SimpleXml1 = {
+    args: {
+        notebookCellOutput:{
+            shellValue: {
+                value: `<hello>World</hello>`,
+                mimeType: 'xml',
+                type: 'xml'
+            }
+        }
+    },
+};
+
+
+export const SimpleXml2 = {
+    args: {
+        notebookCellOutput:{
+            shellValue: {
+                value: `<note>
+                    <to>Jane</to>
+                    <from>Jon</from>
+                    <heading>Reminder</heading>
+                    <body>Hello World!</body>
+                </note>
+                `,
+                mimeType: 'xml',
+                type: 'xml'
+            }
+        }
+    },
+};
