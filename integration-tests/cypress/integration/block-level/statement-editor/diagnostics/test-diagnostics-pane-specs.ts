f03577fd9f7922d50bb497f8667ecc355f9bce51
sachiniSam <sachiniprof@gmail.com>
2022-05-23 12:43:43 +0530

    Add license headers

--- integration-tests/cypress/integration/block-level/statement-editor/diagnostics/test-diagnostics-pane-specs.ts
+++ integration-tests/cypress/integration/block-level/statement-editor/diagnostics/test-diagnostics-pane-specs.ts
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ * associated services.
+ */
 import { getIntegrationTestPageURL } from "../../../../utils/story-url-utils";
 import { Canvas } from "../../../../utils/components/canvas";
 import { VariableFormBlockLevel } from "../../../../utils/forms/variable-form-block-level";
@@ -55,6 +67,7 @@ describe('Test statement editor diagnostics', () => {
             .typeInput("int")
 
         EditorPane
+            .validateNewExpression("TypedBindingPattern","int")
             .validateDiagnosticMessage("incompatible types: expected 'int', found 'string'")
 
         StatementEditor
@@ -136,6 +149,7 @@ describe('Test statement editor diagnostics', () => {
             .typeInput("var1")
 
         EditorPane
+            .validateNewExpression("SimpleNameReference","var1")
             .checkForMultipleDiagnostics()
 
         StatementEditor
