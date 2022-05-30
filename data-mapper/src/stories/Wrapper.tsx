725017d9bb5765b7f68209be5cc58b8629faeb97
Kavith Lokuhewage <kaviththiranga@gmail.com>
2022-05-30 11:43:26 +0530

    Wire up bal project & code editor to stories

---
+++ data-mapper/src/stories/Wrapper.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect } from 'react';
+
+import { DiagramEditorLangClientInterface } from "@wso2-enterprise/ballerina-low-code-edtior-commons";
+
+import { CodeEditor } from './CodeEditor/CodeEditor';
+
+export interface DataMapperWrapperProps {
+    getFileContent: (url: string) => Promise<string>;
+    updateFileContent: (filePath: string, content: string) => Promise<boolean>;
+    filePath: string;
+    langClientPromise: Promise<DiagramEditorLangClientInterface>;
+    lastUpdatedAt: string;
+}
+
+export function DataMapperWrapper(props: DataMapperWrapperProps) {
+    const { langClientPromise, getFileContent, filePath, updateFileContent, lastUpdatedAt } = props;
+    const [didOpen, setDidOpen ] = React.useState(false);
+    const [fileContent, setFileContent ] = React.useState("");
+    const [lastUpdated, setLastUpdated ] = React.useState(lastUpdatedAt);
+
+    const updateFileContentOverride = (fPath: string, newContent: string) => {
+        setFileContent(newContent);
+        return updateFileContent(fPath, newContent);
+    }
+
+    const newProps = {
+        ...props,
+        lastUpdatedAt: lastUpdated,
+        updateFileContent: updateFileContentOverride
+    }
+
+    useEffect(() => {
+        async function openFileInLS() {
+            const text = await getFileContent(filePath);
+            const langClient = await langClientPromise;
+            langClient.didOpen({
+                textDocument: {
+                  languageId: "ballerina",
+                  text,
+                  uri: `file://${filePath}`,
+                  version: 1
+                }
+            });
+            setDidOpen(true);
+            setFileContent(text)
+        }
+
+        async function closeFileInLS() {
+            const langClient = await langClientPromise;
+            langClient.didClose({
+                textDocument: {
+                  uri: `file://${filePath}`,
+                }
+            });
+            setDidOpen(true);
+        }
+        openFileInLS();
+        return () => {
+            closeFileInLS();
+        }
+    }, []);
+
+    return !didOpen ? <>Opening the document...</>
+        :
+        // tslint:disable-next-line: jsx-wrap-multiline
+        <>
+            {/* Add component here */}
+            <hr/>
+            <CodeEditor
+                content={fileContent}
+                filePath={filePath}
+                // tslint:disable-next-line: jsx-no-multiline-js
+                onChange={
+                    // tslint:disable-next-line: jsx-no-lambda
+                    (fPath, newContent) => {
+                        updateFileContentOverride(fPath, newContent);
+                        langClientPromise.then((langClient) => {
+                            langClient.didChange({
+                                textDocument: {
+                                    uri: `file://${filePath}`,
+                                    version: 1
+                                },
+                                contentChanges: [
+                                    {
+                                        text: newContent
+                                    }
+                                ]
+                            });
+                            setLastUpdated((new Date()).toISOString());
+                        })
+                    }
+                }
+            />
+            <code id='file-content-holder' style={{ display: "none" }}>
+                {fileContent}
+            </code>
+        </>;
+}
---
+++ data-mapper/src/stories/utils.ts
@@ -0,0 +1,42 @@
+import { BalleriaLanguageClient, WSConnection } from "@wso2-enterprise/ballerina-languageclient";
+
+import { DataMapperWrapperProps } from "./Wrapper";
+
+export const MOCK_SERVER_URL = "http://localhost:3000"
+export const LANG_SERVER_URL = "ws://localhost:9095"
+
+export const langClientPromise = WSConnection.initialize(LANG_SERVER_URL).then((wsConnection: WSConnection) => {
+  return new BalleriaLanguageClient(wsConnection);
+});
+
+export async function getFileContent(filePath: string): Promise<string> {
+    return fetch(MOCK_SERVER_URL + "/file/" + encodeURIComponent(filePath))
+      .then(response => {
+        return response.text()
+      })
+  }
+
+export async function updateFileContent(filePath: string, text: string): Promise<boolean> {
+    return fetch(MOCK_SERVER_URL + "/file/" + encodeURIComponent(filePath),
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({ text })
+      })
+      .then(response => {
+        return response.json()
+      }).then(result => result.success);
+  }
+
+export function getDataMapperWrapperProps(filePath: string): DataMapperWrapperProps {
+    return {
+        getFileContent,
+        updateFileContent,
+        langClientPromise,
+        lastUpdatedAt: Date.now().toLocaleString(),
+        filePath
+    }
+}
