bbc48aedf0678a4f44b1ab9c2cf3fadc425596fa
gigara <info@gigara.info>
2022-06-20 17:12:35 +0530

    Add UI test for side bar

--- test/ui-test/button-test.ts
+++ test/ui-test/button-test.ts
@@ -1,4 +1,4 @@
-import { EditorView, VSBrowser } from 'vscode-extension-tester';
+import { ActivityBar, CustomTreeSection, EditorView, SideBarView, VSBrowser } from 'vscode-extension-tester';
 import { join } from 'path';
 import { expect } from 'chai';
 import { wait } from './util';
@@ -6,11 +6,13 @@ import { wait } from './util';
 describe('Hello World Example UI Tests', () => {
     const PROJECT_ROOT = join(__dirname, '..', '..', '..', 'test', 'data');
 
+    before(async () => {
+        await VSBrowser.instance.openResources(PROJECT_ROOT, `${PROJECT_ROOT}/hello_world.bal`);
+        await wait(2000);
+    });
 
     it('Test Title bar', async () => {
-        await VSBrowser.instance.openResources(PROJECT_ROOT, `${PROJECT_ROOT}/hello_world.bal`);
         const editorView = new EditorView();
-        await wait(2000);
         expect(await editorView.getAction("Run")).is.not.undefined;
         expect(await editorView.getAction("Debug")).is.not.undefined;
 
@@ -20,6 +22,39 @@ describe('Hello World Example UI Tests', () => {
 
         await wait(2000);
         expect(await editorView.getAction("Show Source", 1)).is.not.undefined;
+
+    });
+
+    it('Test Side bar', async () => {
+        const activityBar = new ActivityBar();
+
+        // test side bar low code activity
+        const lowCodeActivity = await activityBar.getViewControl('Ballerina Low-Code');
+        expect(lowCodeActivity).is.not.undefined;
+        lowCodeActivity!.openView();
+
+        const sideBar = new SideBarView();
+        // test tree views
+        expect(await sideBar.getTitlePart().getTitle()).is.equal("BALLERINA LOW-CODE");
+        const diagramExplorer = await sideBar.getContent().getSection('Diagram Explorer') as CustomTreeSection;
+        const choreoLogin = await sideBar.getContent().getSection('Choreo') as CustomTreeSection;
+        expect(diagramExplorer).is.not.undefined;
+        expect(choreoLogin).is.not.undefined;
+
+        // test root buttons
+        expect(diagramExplorer.getAction("Add Module")).is.not.undefined;
+        expect(diagramExplorer.getAction("Refresh")).is.not.undefined;
+
+        // test diagram explorer tree view
+        await wait(5000);
+        const rootFolder = (await diagramExplorer.getVisibleItems())[0];
+        expect(rootFolder).is.not.undefined;
+        expect(await rootFolder.getLabel()).is.equal("data");
+        expect(await rootFolder.getActionButton("New File")).is.not.undefined;
+        expect(await rootFolder.getActionButton("New Folder")).is.not.undefined;
+        expect(await rootFolder.getActionButton("Delete")).is.not.undefined;
+        await rootFolder.expand();
+        expect(await rootFolder.findChildItem("hello_world.bal")).is.not.undefined;
 
     });
 });
