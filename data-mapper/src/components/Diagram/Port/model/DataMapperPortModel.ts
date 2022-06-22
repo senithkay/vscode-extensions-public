e076d1946be6feca7133506257e93f2210aa1861
Kavith Lokuhewage <kaviththiranga@gmail.com>
2022-06-22 23:02:00 +0530

    WIP source gen for links

--- data-mapper/src/components/Diagram/Port/model/DataMapperPortModel.ts
+++ data-mapper/src/components/Diagram/Port/model/DataMapperPortModel.ts
@@ -1,9 +1,10 @@
 import { LinkModel, PortModel, PortModelGenerics } from '@projectstorm/react-diagrams';
-import { RecordField, RecordTypeDesc } from '@wso2-enterprise/syntax-tree';
+import { MappingConstructor, RecordField, RecordTypeDesc, STKindChecker } from '@wso2-enterprise/syntax-tree';
 
 import md5 from "blueimp-md5";
 
 import { DataMapperLinkModel } from '../../Link/model/DataMapperLink';
+import { DataMapperNodeModel } from '../../Node/model/DataMapperNode';
 export interface DataMapperNodeModelGenerics {
 	PORT: DataMapperPortModel;
 }
@@ -30,9 +31,49 @@ export class DataMapperPortModel extends PortModel<PortModelGenerics & DataMappe
 			},
 			targetPortChanged: (evt) => {
 
-				lm.addLabel(evt.port.getName() + " = " + lm.getSourcePort().getName());
+				lm.addLabel(createSpecificFieldSource(lm));
 			}
 		});
 		return lm;
 	}
+}
+
+function createSpecificFieldSource(link: DataMapperLinkModel) {
+	let lhs = "";
+	let rhs = "";
+	if (link.getTargetPort()) {
+		const targetPort = link.getTargetPort() as DataMapperPortModel;
+		const targetNode = targetPort.getNode() as DataMapperNodeModel;
+		if (STKindChecker.isExpressionFunctionBody(targetNode.value)) {
+			const mappingConstruct = targetNode.value.expression as MappingConstructor;
+			console.log(mappingConstruct.fields);
+		}
+		lhs = (STKindChecker.isRecordField(targetPort.typeNode)
+			? targetPort.typeNode.fieldName.value : "");
+		let parent = targetPort.parentModel;
+		while (parent != null) {
+			lhs = (STKindChecker.isRecordField(parent.typeNode)
+			? parent.typeNode.fieldName.value + "." : "") + lhs;
+			if (!parent.parentModel) {
+
+			}
+			parent = parent.parentModel;
+		}
+	}
+
+	if (link.getSourcePort()) {
+		const sourcePort = link.getSourcePort() as DataMapperPortModel;
+		rhs = (STKindChecker.isRecordField(sourcePort.typeNode)
+			? sourcePort.typeNode.fieldName.value : "");
+		let parent = sourcePort.parentModel;
+		while (parent != null) {
+			rhs = (STKindChecker.isRecordField(parent.typeNode)
+			? parent.typeNode.fieldName.value + "." : "")  + rhs;
+			if (!parent.parentModel) {
+
+			}
+			parent = parent.parentModel;
+		}
+	}
+	return `${lhs}: ${rhs}`;
 }
