e8f1e95e63634f1a940a02e6483c3e7634445748
Niron Perera <snironr@gmail.com>
2022-06-22 19:01:45 +0530

    updating screenshots

--- code-server-tests/cypress/integration/code-server-smoke.ts
+++ code-server-tests/cypress/integration/code-server-smoke.ts
@@ -81,9 +81,9 @@ describe("Code server smoke test", () => {
       "//div[@aria-label='main.bal Diagram']/div[@class='tab-actions']//a[@role='button']"
     ).click();
     //Close welcome tab
-    cy.xpath(
-      "//div[@title='Welcome']/div[@class='tab-actions']//a[@role='button']"
-    ).click();
+    // cy.xpath(
+    //   "//div[@title='Welcome']/div[@class='tab-actions']//a[@role='button']"
+    // ).click();
     // Verify code rendering
     cy.get(".view-lines", {
       timeout: 60000,
