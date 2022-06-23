87cdb8c90f8f854ceeb44ad78c89ccf84d94adae
IrushiL <irushiliyanage123@gmail.com>
2022-06-23 06:44:51 +0530

    Refactor the test cases

--- configurable-editor/src/tests/SimpleType.test.tsx
+++ configurable-editor/src/tests/SimpleType.test.tsx
@@ -1,22 +1,29 @@
+/**
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 import * as React from "react";
 
+import { composeStories } from "@storybook/testing-react";
 import { fireEvent, render, screen } from "@testing-library/react";
-import { composeStories } from '@storybook/testing-react';
 
-import { ConfigElementProps } from "../components/ConfigElement";
-import * as stories from '../stories/simple/SimpleType.stories';
+import * as stories from "../stories/simple/SimpleType.stories";
 
 const { SimpleTypes } = composeStories(stories);
-
-const onClickDefaultButton = () => {
-    // tslint:disable-next-line: no-console
-    console.log("Default Button clicked");
-};
-
-const onClickPrimaryButton = (configProperties: ConfigElementProps) => {
-    // tslint:disable-next-line: no-console
-    expect(configProperties).toBe("jk");
-};
 
 test("Renders the ConfigForm", () => {
     render(<SimpleTypes />);
