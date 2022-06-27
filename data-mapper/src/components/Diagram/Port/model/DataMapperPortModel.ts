14d4de7ad751786b9bbc14ed5d196b885df42d70
Kavith Lokuhewage <kaviththiranga@gmail.com>
2022-06-27 08:52:09 +0530

    Prevent linking to same type port
    Furthermore, move util source gen
    fn out of port model file.

--- data-mapper/src/components/Diagram/Port/model/DataMapperPortModel.ts
+++ data-mapper/src/components/Diagram/Port/model/DataMapperPortModel.ts
@@ -1,10 +1,10 @@
 import { LinkModel, PortModel, PortModelGenerics } from '@projectstorm/react-diagrams';
-import { MappingConstructor, NodePosition, RecordField, RecordTypeDesc, SpecificField, STKindChecker } from '@wso2-enterprise/syntax-tree';
+import { RecordField, RecordTypeDesc } from '@wso2-enterprise/syntax-tree';
 
 import md5 from "blueimp-md5";
 
 import { DataMapperLinkModel } from '../../Link/model/DataMapperLink';
-import { DataMapperNodeModel } from '../../Node/model/DataMapperNode';
+import { createSpecificFieldSource } from '../../utils';
 export interface DataMapperNodeModelGenerics {
 	PORT: DataMapperPortModel;
 }
@@ -35,105 +35,9 @@ export class DataMapperPortModel extends PortModel<PortModelGenerics & DataMappe
 		});
 		return lm;
 	}
+
+	canLinkToPort(port: DataMapperPortModel): boolean {
+		return this.portType !== port.portType;
+	}
 }
 
-async function createSpecificFieldSource(link: DataMapperLinkModel) {
-	let source = "";
-	let lhs = "";
-	let rhs = "";
-	if (link.getSourcePort()) {
-		const sourcePort = link.getSourcePort() as DataMapperPortModel;
-
-		rhs = (STKindChecker.isRecordField(sourcePort.typeNode)
-			? sourcePort.typeNode.fieldName.value : "");
-		let parent = sourcePort.parentModel;
-		while (parent != null) {
-			rhs = (STKindChecker.isRecordField(parent.typeNode)
-				? parent.typeNode.fieldName.value + "." : "") + rhs;
-			parent = parent.parentModel;
-		}
-		const sourceNode = sourcePort.getNode() as DataMapperNodeModel;
-		if (STKindChecker.isRequiredParam(sourceNode.value)) {
-			rhs = sourceNode.value.paramName.value + "." + rhs;
-		}
-	}
-
-	if (link.getTargetPort()) {
-		const targetPort = link.getTargetPort() as DataMapperPortModel;
-		lhs = STKindChecker.isRecordField(targetPort.typeNode)
-			? targetPort.typeNode.fieldName.value : "";
-
-		let parentFieldNames: string[] = [];
-		let parent = targetPort.parentModel;
-		while (parent != null) {
-			if (STKindChecker.isRecordField(parent.typeNode)) {
-				parentFieldNames.push(parent.typeNode.fieldName.value);
-			};
-			parent = parent.parentModel;
-		}
-		const targetNode = targetPort.getNode() as DataMapperNodeModel;
-		if (STKindChecker.isExpressionFunctionBody(targetNode.value)) {
-			let mappingConstruct = targetNode.value.expression as MappingConstructor;
-			let targetPos: NodePosition = undefined;
-			let targetMappingConstruct = mappingConstruct;
-			let fromFieldIdx = -1;
-			if (parentFieldNames.length > 0) {
-				const fieldNames = parentFieldNames.reverse();
-				for (let i = 0; i < fieldNames.length; i++) {
-					const fieldName = fieldNames[i];
-					if (mappingConstruct) {
-						const specificField = mappingConstruct.fields.find((val) => STKindChecker.isSpecificField(val) && val.fieldName.value === fieldName) as SpecificField;
-						if (specificField && specificField.valueExpr) {
-							mappingConstruct = specificField.valueExpr as MappingConstructor;
-						} else {
-							fromFieldIdx = i;
-							targetMappingConstruct = mappingConstruct;
-							break;
-						}
-					}
-				}
-				const createSpeficField = (missingFields: string[]) => {
-					let source = "";
-					if (missingFields.length > 0) {
-						source = `\t${missingFields[0]}: {\n${createSpeficField(missingFields.slice(1))}}`;
-					} else {
-						source = `\t${lhs}: ${rhs}`;
-					}
-					return source;
-				}
-				if (fromFieldIdx >= 0 && fromFieldIdx <= fieldNames.length) {
-					const missingFields = fieldNames.slice(fromFieldIdx);
-					source = createSpeficField(missingFields);
-				}
-			} else {
-				source = `${lhs}: ${rhs}`;
-			}
-			targetPos = targetMappingConstruct.openBrace.position as NodePosition;
-			if (targetMappingConstruct.fields.length > 0) {
-				source += ",\n";
-			}
-			const langClient = await targetNode.langClientPromise;
-			const updateFileContent = targetNode.updateFileContent;
-			const stModifyResp = await langClient.stModify({
-				documentIdentifier: {
-					uri: `file://${targetNode.filePath}`
-				},
-				astModifications: [
-					{ 
-						type: "INSERT",
-						config: {
-							"STATEMENT": source,
-						},
-						endColumn: targetPos.endColumn,
-						endLine: targetPos.endLine,
-						startColumn: targetPos.endColumn,
-						startLine: targetPos.endLine
-					}
-				]
-			});
-			updateFileContent(targetNode.filePath, stModifyResp.source);
-			console.log(stModifyResp);
-		}
-	}
-	return `${lhs} = ${rhs}`;
-}
--- data-mapper/src/components/Diagram/utils.ts
+++ data-mapper/src/components/Diagram/utils.ts
@@ -1,4 +1,6 @@
-import { ExpressionFunctionBody, FieldAccess, RecordField, RecordTypeDesc, RequiredParam, SimpleNameReference, STKindChecker, STNode } from "@wso2-enterprise/syntax-tree";
+import { ExpressionFunctionBody, FieldAccess, MappingConstructor, NodePosition, RecordField, RecordTypeDesc, RequiredParam, SimpleNameReference, SpecificField, STKindChecker, STNode } from "@wso2-enterprise/syntax-tree";
+import { DataMapperLinkModel } from "./Link/model/DataMapperLink";
+import { DataMapperNodeModel } from "./Node/model/DataMapperNode";
 import { DataMapperPortModel } from "./Port/model/DataMapperPortModel";
 
 export function generatePortId(typeNode: RecordField|RecordTypeDesc, type: "IN" | "OUT", nodeValue: ExpressionFunctionBody|RequiredParam,
@@ -35,4 +37,105 @@ export function getFieldNames(expr: FieldAccess) {
         nextExp = STKindChecker.isFieldAccess(nextExp.expression) ? nextExp.expression : undefined;
     } 
     return fieldNames.reverse();
+}
+
+export async function createSpecificFieldSource(link: DataMapperLinkModel) {
+	let source = "";
+	let lhs = "";
+	let rhs = "";
+	if (link.getSourcePort()) {
+		const sourcePort = link.getSourcePort() as DataMapperPortModel;
+
+		rhs = (STKindChecker.isRecordField(sourcePort.typeNode)
+			? sourcePort.typeNode.fieldName.value : "");
+		let parent = sourcePort.parentModel;
+		while (parent != null) {
+			rhs = (STKindChecker.isRecordField(parent.typeNode)
+				? parent.typeNode.fieldName.value + "." : "") + rhs;
+			parent = parent.parentModel;
+		}
+		const sourceNode = sourcePort.getNode() as DataMapperNodeModel;
+		if (STKindChecker.isRequiredParam(sourceNode.value)) {
+			rhs = sourceNode.value.paramName.value + "." + rhs;
+		}
+	}
+
+	if (link.getTargetPort()) {
+		const targetPort = link.getTargetPort() as DataMapperPortModel;
+		lhs = STKindChecker.isRecordField(targetPort.typeNode)
+			? targetPort.typeNode.fieldName.value : "";
+
+		let parentFieldNames: string[] = [];
+		let parent = targetPort.parentModel;
+		while (parent != null) {
+			if (STKindChecker.isRecordField(parent.typeNode)) {
+				parentFieldNames.push(parent.typeNode.fieldName.value);
+			};
+			parent = parent.parentModel;
+		}
+		const targetNode = targetPort.getNode() as DataMapperNodeModel;
+		if (STKindChecker.isExpressionFunctionBody(targetNode.value)) {
+			let mappingConstruct = targetNode.value.expression as MappingConstructor;
+			let targetPos: NodePosition = undefined;
+			let targetMappingConstruct = mappingConstruct;
+			let fromFieldIdx = -1;
+			if (parentFieldNames.length > 0) {
+				const fieldNames = parentFieldNames.reverse();
+				for (let i = 0; i < fieldNames.length; i++) {
+					const fieldName = fieldNames[i];
+					if (mappingConstruct) {
+						const specificField = mappingConstruct.fields.find((val) => STKindChecker.isSpecificField(val) && val.fieldName.value === fieldName) as SpecificField;
+						if (specificField && specificField.valueExpr) {
+							mappingConstruct = specificField.valueExpr as MappingConstructor;
+						} else {
+							fromFieldIdx = i;
+							targetMappingConstruct = mappingConstruct;
+							break;
+						}
+					}
+				}
+				const createSpeficField = (missingFields: string[]) => {
+					let source = "";
+					if (missingFields.length > 0) {
+						source = `\t${missingFields[0]}: {\n${createSpeficField(missingFields.slice(1))}}`;
+					} else {
+						source = `\t${lhs}: ${rhs}`;
+					}
+					return source;
+				}
+				if (fromFieldIdx >= 0 && fromFieldIdx <= fieldNames.length) {
+					const missingFields = fieldNames.slice(fromFieldIdx);
+					source = createSpeficField(missingFields);
+				}
+			} else {
+				source = `${lhs}: ${rhs}`;
+			}
+			targetPos = targetMappingConstruct.openBrace.position as NodePosition;
+			if (targetMappingConstruct.fields.length > 0) {
+				source += ",\n";
+			}
+			const langClient = await targetNode.langClientPromise;
+			const updateFileContent = targetNode.updateFileContent;
+			const stModifyResp = await langClient.stModify({
+				documentIdentifier: {
+					uri: `file://${targetNode.filePath}`
+				},
+				astModifications: [
+					{ 
+						type: "INSERT",
+						config: {
+							"STATEMENT": source,
+						},
+						endColumn: targetPos.endColumn,
+						endLine: targetPos.endLine,
+						startColumn: targetPos.endColumn,
+						startLine: targetPos.endLine
+					}
+				]
+			});
+			updateFileContent(targetNode.filePath, stModifyResp.source);
+			console.log(stModifyResp);
+		}
+	}
+	return `${lhs} = ${rhs}`;
 }
