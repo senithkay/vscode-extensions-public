b0a81b9f4e4ff6dfe8ca25d80f9dccf93a7ca9aa
Madusha Gunasekara <madushajg@gmail.com>
2022-07-01 09:52:37 +0530

    Apply formatting

--- integration-tests/cypress/integration/module-level/add-module-level-statements.spec.ts
+++ integration-tests/cypress/integration/module-level/add-module-level-statements.spec.ts
@@ -14,166 +14,166 @@ import { getCurrentSpecFolder } from "../../utils/file-utils";
 const BAL_FILE_PATH = "default/empty-file.bal";
 
 describe("Add module-level statements via Low Code", () => {
-  beforeEach(() => {
-    cy.visit(getIntegrationTestPageURL(BAL_FILE_PATH));
-  });
+    beforeEach(() => {
+        cy.visit(getIntegrationTestPageURL(BAL_FILE_PATH));
+    });
 
-  it("Add a variable to empty file", () => {
-    Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
-    TopLevelPlusWidget.clickOption("Variable");
+    it("Add a variable to empty file", () => {
+        Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
+        TopLevelPlusWidget.clickOption("Variable");
 
-    StatementEditor
-        .shouldBeVisible()
-        .getEditorPane();
+        StatementEditor
+            .shouldBeVisible()
+            .getEditorPane();
 
-    EditorPane
-        .getStatementRenderer()
-        .getExpression("SimpleNameReference")
-        .doubleClickExpressionContent(`<add-expression>`);
+        EditorPane
+            .getStatementRenderer()
+            .getExpression("SimpleNameReference")
+            .doubleClickExpressionContent(`<add-expression>`);
 
-    InputEditor
-        .typeInput('"Hello World"');
+        InputEditor
+            .typeInput('"Hello World"');
 
-    EditorPane
-        .validateNewExpression("StringLiteral", "Hello World")
-        .getExpression("IntTypeDesc")
-        .clickExpressionContent('int');
+        EditorPane
+            .validateNewExpression("StringLiteral", "Hello World")
+            .getExpression("IntTypeDesc")
+            .clickExpressionContent('int');
 
-    SuggestionsPane
-        .clickSuggestionsTab("Suggestions")
-        .clickLsSuggestion('string');
+        SuggestionsPane
+            .clickSuggestionsTab("Suggestions")
+            .clickLsSuggestion('string');
 
-    EditorPane
-        .validateNewExpression("StringTypeDesc", "string")
-        .getExpression("CaptureBindingPattern")
-        .doubleClickExpressionContent('variable');
+        EditorPane
+            .validateNewExpression("StringTypeDesc", "string")
+            .getExpression("CaptureBindingPattern")
+            .doubleClickExpressionContent('variable');
 
-    InputEditor
-        .typeInput("foo");
+        InputEditor
+            .typeInput("foo");
 
-    EditorPane
-        .validateNewExpression("CaptureBindingPattern", "foo");
+        EditorPane
+            .validateNewExpression("CaptureBindingPattern", "foo");
 
-    StatementEditor
-        .save();
-  });
+        StatementEditor
+            .save();
+    });
 
-  it("Add a record to empty file", () => {
-    Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
-    TopLevelPlusWidget.clickOption("Record");
+    it("Add a record to empty file", () => {
+        Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
+        TopLevelPlusWidget.clickOption("Record");
 
-    RecordForm.shouldBeVisible()
-      .typeRecordName("Animal")
-      .clickWhiteSpace()
-      .haveRecordName("Animal")
-      .editRecord()
-      .typeRecordName("Foo")
-      .clickWhiteSpace()
-      .haveRecordName("Foo")
-      .makePublicRecord()
-      .toggleClosedRecord()
-      .addNewField("int", "hello") //Have an issue here when adding default value
-      .clickWhiteSpace()
-      .addNewField("int", "world")
-      .deleteFirstField("hello")
-      .save()
-  });
+        RecordForm.shouldBeVisible()
+            .typeRecordName("Animal")
+            .clickWhiteSpace()
+            .haveRecordName("Animal")
+            .editRecord()
+            .typeRecordName("Foo")
+            .clickWhiteSpace()
+            .haveRecordName("Foo")
+            .makePublicRecord()
+            .toggleClosedRecord()
+            .addNewField("int", "hello") //Have an issue here when adding default value
+            .clickWhiteSpace()
+            .addNewField("int", "world")
+            .deleteFirstField("hello")
+            .save()
+    });
 
-  it("Add a configurable to empty file", () => {
-    Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
-    TopLevelPlusWidget.clickOption("Configurable");
+    it("Add a configurable to empty file", () => {
+        Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
+        TopLevelPlusWidget.clickOption("Configurable");
 
-    StatementEditor
-        .shouldBeVisible()
-        .getEditorPane();
+        StatementEditor
+            .shouldBeVisible()
+            .getEditorPane();
 
-    EditorPane
-        .getStatementRenderer()
-        .getExpression("CaptureBindingPattern")
-        .doubleClickExpressionContent('conf');
+        EditorPane
+            .getStatementRenderer()
+            .getExpression("CaptureBindingPattern")
+            .doubleClickExpressionContent('conf');
 
-    InputEditor
-        .typeInput("foo");
+        InputEditor
+            .typeInput("foo");
 
-    EditorPane
-        .validateNewExpression("CaptureBindingPattern", "foo")
-        .getExpression("RequiredExpression")
-        .doubleClickExpressionContent('?');
+        EditorPane
+            .validateNewExpression("CaptureBindingPattern", "foo")
+            .getExpression("RequiredExpression")
+            .doubleClickExpressionContent('?');
 
-    InputEditor
-        .typeInput('"Hello World"');
+        InputEditor
+            .typeInput('"Hello World"');
 
-    EditorPane
-        .validateNewExpression("StringLiteral", "Hello World")
-        .getExpression("IntTypeDesc")
-        .doubleClickExpressionContent('int');
+        EditorPane
+            .validateNewExpression("StringLiteral", "Hello World")
+            .getExpression("IntTypeDesc")
+            .doubleClickExpressionContent('int');
 
-    InputEditor
-        .typeInput('string');
+        InputEditor
+            .typeInput('string');
 
-    EditorPane
-        .validateNewExpression("StringTypeDesc", "string");
+        EditorPane
+            .validateNewExpression("StringTypeDesc", "string");
 
-    StatementEditor
-      .save();
-  });
+        StatementEditor
+            .save();
+    });
 
-  it("Add a constant to empty file", () => {
-    Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
-    TopLevelPlusWidget.clickOption("Constant");
+    it("Add a constant to empty file", () => {
+        Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
+        TopLevelPlusWidget.clickOption("Constant");
 
-    StatementEditor
-        .shouldBeVisible()
-        .getEditorPane();
+        StatementEditor
+            .shouldBeVisible()
+            .getEditorPane();
 
-    EditorPane
-        .getStatementRenderer()
-        .getExpression("IdentifierToken")
-        .doubleClickExpressionContent('CONST_NAME');
+        EditorPane
+            .getStatementRenderer()
+            .getExpression("IdentifierToken")
+            .doubleClickExpressionContent('CONST_NAME');
 
-    InputEditor
-        .typeInput("FOO");
+        InputEditor
+            .typeInput("FOO");
 
-    EditorPane
-        .validateNewExpression("IdentifierToken", "FOO")
-        .getExpression("SimpleNameReference")
-        .doubleClickExpressionContent(`<add-expression>`);
+        EditorPane
+            .validateNewExpression("IdentifierToken", "FOO")
+            .getExpression("SimpleNameReference")
+            .doubleClickExpressionContent(`<add-expression>`);
 
-    InputEditor
-        .typeInput('"Hello World"');
+        InputEditor
+            .typeInput('"Hello World"');
 
-    EditorPane
-        .validateNewExpression("StringLiteral", "Hello World");
+        EditorPane
+            .validateNewExpression("StringLiteral", "Hello World");
 
-    StatementEditor
-        .save();
-  });
+        StatementEditor
+            .save();
+    });
 
-  it("Add a listener to empty file", () => {
-    Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
-    TopLevelPlusWidget.clickOption("Listener");
+    it("Add a listener to empty file", () => {
+        Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
+        TopLevelPlusWidget.clickOption("Listener");
 
-    ListenerForm.shouldBeVisible()
-      .typeListenerName("hello")
-      .typeListenerPortValue(9090)
-      .save();
-  });
+        ListenerForm.shouldBeVisible()
+            .typeListenerName("hello")
+            .typeListenerPortValue(9090)
+            .save();
+    });
 
-  it("Add a enum to empty file", () => {
-    Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
-    TopLevelPlusWidget.clickOption("Enum");
+    it("Add a enum to empty file", () => {
+        Canvas.welcomeMessageShouldBeVisible().clickTopLevelPlusButton();
+        TopLevelPlusWidget.clickOption("Enum");
 
-    EnumerationForm.shouldBeVisible()
-      .typeEnumName("Foo")
-      .clickWhiteSpace()
-      .haveEnumName("Foo")
-      .addNewMember("RED")
-      .addNewMember("GREEN")
-      .addNewMember("BLUE")
-      .save();
+        EnumerationForm.shouldBeVisible()
+            .typeEnumName("Foo")
+            .clickWhiteSpace()
+            .haveEnumName("Foo")
+            .addNewMember("RED")
+            .addNewMember("GREEN")
+            .addNewMember("BLUE")
+            .save();
 
-    SourceCode.shouldBeEqualTo(
-      getCurrentSpecFolder() + "add-enum.expected.bal"
-    );
-  });
+        SourceCode.shouldBeEqualTo(
+            getCurrentSpecFolder() + "add-enum.expected.bal"
+        );
+    });
 });
