daf72a3e46ce661aa60799c15762ede065d9d66a
kaumini <kauminikg@gmail.com>
2022-07-04 09:26:13 +0530

    Save suggestions of lssuggestions and expresission suggestions

--- statement-editor/src/components/Suggestions/LangServerSuggestions/index.tsx
+++ statement-editor/src/components/Suggestions/LangServerSuggestions/index.tsx
@@ -98,7 +98,7 @@ export function LSSuggestions() {
             : targetPosition;
         updateModel(value, nodePosition);
         inputEditorCtx.onInputChange('');
-        inputEditorCtx.onSuggestionSelection(suggestion.value);
+        inputEditorCtx.onSuggestionSelection(value);
     }
 
     return (
