bd8c7ace4fea299427459726654fd8d5302d16fb
kaumini <kauminikg@gmail.com>
2022-07-06 17:14:38 +0530

    Reuse update statement in updateModel

--- statement-editor/src/components/StatementEditor/index.tsx
+++ statement-editor/src/components/StatementEditor/index.tsx
@@ -284,9 +284,9 @@ export function StatementEditor(props: StatementEditorProps) {
                 : await getPartialSTForStatement({ codeSnippet }, getLangClient);
         }
 
+        const updatedStatement = addToTargetPosition(model.source, currentModel.model.position, codeSnippet);
         if (!partialST.syntaxDiagnostics.length || config.type === CUSTOM_CONFIG_TYPE) {
             const updatedContent = getUpdatedSource(partialST.source, currentFile.content, targetPosition, moduleList);
-            const updatedStatement = addToTargetPosition(model.source, currentModel.model.position, codeSnippet);
             if (updatedStatement.trim() === partialST.source.trim()) {
                 sendDidChange(fileURI, updatedContent, getLangClient).then();
                 const diagnostics = await handleDiagnostics(partialST.source);
@@ -307,7 +307,6 @@ export function StatementEditor(props: StatementEditorProps) {
                 setHasSyntaxDiagnostics(false);
             }
         } else if (partialST.syntaxDiagnostics.length){
-            const updatedStatement = addToTargetPosition(model.source, currentModel.model.position, codeSnippet);
             const updatedContent = getUpdatedSource(updatedStatement, currentFile.content, targetPosition, moduleList);
 
             sendDidChange(fileURI, updatedContent, getLangClient).then();
