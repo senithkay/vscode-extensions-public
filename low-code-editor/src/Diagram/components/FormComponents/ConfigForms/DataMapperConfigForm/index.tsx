122bf19949b8f3d7105fb37630550e5e16842631
Madusha Gunasekara <madushajg@gmail.com>
2022-07-07 19:04:10 +0530

    Get the functionST from an existing content

--- low-code-editor/src/Diagram/components/FormComponents/ConfigForms/DataMapperConfigForm/index.tsx
+++ low-code-editor/src/Diagram/components/FormComponents/ConfigForms/DataMapperConfigForm/index.tsx
@@ -11,54 +11,115 @@
  * associated services.
  */
 // tslint:disable: jsx-no-multiline-js
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
+import { FormControl } from "@material-ui/core";
 import {
     DataMapper
 } from "@wso2-enterprise/ballerina-data-mapper";
 import {
-    FunctionDefinition,
-    NodePosition,
-    RecordTypeDesc,
-    TypeDefinition
+    ConfigOverlayFormStatus, DiagramEditorLangClientInterface,
+    ExpressionEditorLangClientInterface
+} from "@wso2-enterprise/ballerina-low-code-edtior-commons";
+import { FormHeaderSection } from "@wso2-enterprise/ballerina-low-code-edtior-ui-components";
+import {
+    getPartialSTForModuleMembers
+// tslint:disable-next-line:no-submodule-imports
+} from "@wso2-enterprise/ballerina-statement-editor/lib/utils/ls-utils";
+import {
+    FunctionDefinition, ModulePart,
+    NodePosition, STKindChecker,
+    STNode
 } from "@wso2-enterprise/syntax-tree";
 
 import { Context } from "../../../../../Contexts/Diagram";
+import {wizardStyles} from "../style";
 
 export interface DataMapperProps {
-    name: string;
-    existingModel?: FunctionDefinition;
-    model?: RecordTypeDesc | TypeDefinition;
+    model?: STNode;
     targetPosition?: NodePosition;
-    isTypeDefinition?: boolean;
     onCancel: () => void;
     onSave: (typeDesc: string, recModel: any) => void;
+    configOverlayFormStatus: ConfigOverlayFormStatus;
 }
 
 export function DataMapperConfigForm(props: DataMapperProps) {
-    const { existingModel } = props;
+    const { model, onCancel } = props;
 
     const {
         props: {
             currentFile
         },
         api: {
-            ls: { getExpressionEditorLangClient }
+            ls: {
+                getExpressionEditorLangClient,
+                getDiagramEditorLangClient
+            }
         }
     } = useContext(Context);
 
-    const filePath = `file://${currentFile.path}`;
+    const overlayClasses = wizardStyles();
+
+    const [functionST, setFunctionST] = React.useState<FunctionDefinition>(undefined);
+
+    // useEffect(() => {
+    //     async function getFunctionST() {
+    //         // const partialST = await langClient.getSTForSingleStatement({codeSnippet: ""});
+    //         const partialST = await getPartialSTForModuleMembers({codeSnippet: `function transform(InputMessage input) returns TransformedMessage => {};`}, getExpressionEditorLangClient);
+    //         setFunctionST(partialST as FunctionDefinition);
+    //         // const langClient: DiagramEditorLangClientInterface = await getDiagramEditorLangClient();
+    //
+    //         // const defReply = await langClient.definition({
+    //         //     position: {
+    //         //         line: 0,
+    //         //         character: 0
+    //         //     },
+    //         //     textDocument: {
+    //         //         uri: filePath
+    //         //     }
+    //         // });
+    //     }
+    //     getFunctionST();
+    // }, []);
+
+    useEffect(() => {
+        async function getSyntaxTree() {
+            const langClient: DiagramEditorLangClientInterface = await getDiagramEditorLangClient();
+            const { parseSuccess, syntaxTree } = await langClient.getSyntaxTree({
+                documentIdentifier: {
+                    uri: `file://${currentFile.path}`
+                }
+            });
+            if (parseSuccess) {
+                const modPart = syntaxTree as ModulePart;
+                const fns = modPart.members.filter((mem) => STKindChecker.isFunctionDefinition(mem)) as FunctionDefinition[];
+                setFunctionST(fns.find((mem) => mem.functionName.value === "transform"));
+                return;
+            }
+            setFunctionST(undefined);
+        }
+        getSyntaxTree();
+    }, [])
 
     const updateFileContentOverride = (fPath: string, newContent: string) => {
         return Promise.resolve(true);
     }
 
+
     return (
-        <DataMapper
-            fnST={existingModel}
-            getLangClient={getExpressionEditorLangClient}
-            filePath={filePath}
-            updateFileContent={updateFileContentOverride}
-        />
+        <FormControl data-testid="record-form" className={overlayClasses.wizardFormControlExtended}>
+            <FormHeaderSection
+                formTitle={"lowcode.develop.configForms.DataMapper.title"}
+                defaultMessage={"Data Mapper"}
+                onCancel={onCancel}
+            />
+            <DataMapper
+                fnST={functionST}
+                langClientPromise={getDiagramEditorLangClient}
+                getLangClient={getDiagramEditorLangClient}
+                filePath={currentFile.path}
+                updateFileContent={updateFileContentOverride}
+            />
+        </FormControl>
     );
 }
