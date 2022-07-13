88a65f3cb79917a884a86f393767041cb87e85f2
Kavith Lokuhewage <kaviththiranga@gmail.com>
2022-07-13 21:00:16 +0530

    Use a custom SVG path for the link

--- data-mapper/src/components/Diagram/Link/model/DataMapperLink.ts
+++ data-mapper/src/components/Diagram/Link/model/DataMapperLink.ts
@@ -1,4 +1,6 @@
 import { DefaultLinkModel } from "@projectstorm/react-diagrams";
+import { BezierCurve } from "@projectstorm/geometry";
+
 
 export const LINK_TYPE_ID = "datamapper-link";
 
@@ -11,5 +13,24 @@ export class DataMapperLinkModel extends DefaultLinkModel {
 			locked: true,
 			color: "#5567D5"
 		});
+	}
+
+	getSVGPath(): string {
+		if (this.points.length == 2) {
+			const curve = new BezierCurve();
+			curve.setSource(this.getFirstPoint().getPosition());
+			curve.setTarget(this.getLastPoint().getPosition());
+			curve.setSourceControl(this.getFirstPoint().getPosition().clone());
+			curve.setTargetControl(this.getLastPoint().getPosition().clone());
+
+			if (this.sourcePort) {
+				curve.getSourceControl().translate(...this.calculateControlOffset(this.getSourcePort()));
+			}
+
+			if (this.targetPort) {
+				curve.getTargetControl().translate(...this.calculateControlOffset(this.getTargetPort()));
+			}
+			return curve.getSVGCurve();
+		}
 	}
 }
