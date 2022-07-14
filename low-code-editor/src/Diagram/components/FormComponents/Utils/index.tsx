7fc36e814259858188d7792c8c86c7acf73cabf0
tharindulak <tharindu.jayathilake1@gmail.com>
2022-07-14 11:37:32 +0530

    Add correct SE compatible version

--- low-code-editor/src/Diagram/components/FormComponents/Utils/index.tsx
+++ low-code-editor/src/Diagram/components/FormComponents/Utils/index.tsx
@@ -204,5 +204,9 @@ export function isStatementEditorSupported(version: string): boolean {
     const versionRegex = new RegExp("^[0-9]{4}.[0-9].[0-9]");
     const versionStr = version.match(versionRegex);
     const splittedVersions = versionStr[0]?.split(".");
-    return (parseInt(splittedVersions[1], 10) > 0 || parseInt(splittedVersions[2], 10) >= 5);
+    if (parseInt(splittedVersions[1], 10) === 1) {
+        return parseInt(splittedVersions[2], 10) >= 1;
+    } else {
+        return parseInt(splittedVersions[1], 10) > 1;
+    }
 }
