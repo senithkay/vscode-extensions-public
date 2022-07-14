db0a1ee87a3fb0a12ec893677907aeb86ed4d7f8
kaumini <kauminikg@gmail.com>
2022-07-14 15:02:29 +0530

    Handle syntax issues on other statements

--- statement-editor/src/components/StatementEditor/index.tsx
+++ statement-editor/src/components/StatementEditor/index.tsx
@@ -287,7 +287,11 @@ export function StatementEditor(props: StatementEditorProps) {
                 : await getPartialSTForStatement({ codeSnippet }, getLangClient);
         }
 
-        if (!partialST.syntaxDiagnostics.length || config.type === CUSTOM_CONFIG_TYPE) {
+        const isAllowedCustomSyntaxIssue = config.type === CUSTOM_CONFIG_TYPE 
+            && partialST.syntaxDiagnostics.length && partialST.syntaxDiagnostics[0].message === "missing semicolon token" 
+            && stmtDiagnostics.length === 0;
+
+        if (!partialST.syntaxDiagnostics.length || isAllowedCustomSyntaxIssue) {
             const updatedContent = getUpdatedSource(partialST.source, currentFile.content, targetPosition, moduleList);
             sendDidChange(fileURI, updatedContent, getLangClient).then();
             const diagnostics = await handleDiagnostics(partialST.source);
@@ -313,7 +317,7 @@ export function StatementEditor(props: StatementEditorProps) {
 
             sendDidChange(fileURI, updatedContent, getLangClient).then();
             handleDiagnostics(updatedStatement).then();
-            setHasSyntaxDiagnostics(true);
+            !(config.type === CUSTOM_CONFIG_TYPE) && setHasSyntaxDiagnostics(true);
         }
     }
 
