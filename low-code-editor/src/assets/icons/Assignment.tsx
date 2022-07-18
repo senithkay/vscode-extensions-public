5a844a9dc8be630d14933914658acdc866844726
kaumini <kauminikg@gmail.com>
2022-07-18 12:09:05 +0530

    Change plus form icon colours

--- low-code-editor/src/assets/icons/Assignment.tsx
+++ low-code-editor/src/assets/icons/Assignment.tsx
@@ -19,10 +19,10 @@ export default function AssignmentIcon(props: any) {
             <g id="adding-new-Statements-updtd" transform="translate(-86.000000, -402.000000)" stroke-width="2">
                 <g id="Dropdown/Select/Default-Copy-2" transform="translate(76.000000, 392.000000)">
                     <g id="Logo/Circle" transform="translate(10.000000, 10.000000)">
-                        <path d="M6.5,14.5 L9.90616569,5.25469313 C9.92525847,5.20286987 9.98274728,5.17633658 10.0345705,5.19542936 C10.0620498,5.20555331 10.0837104,5.22721384 10.0938343,5.25469313 L13.5,14.5 L13.5,14.5" id="Path-12" stroke="#CCD1F2" stroke-linecap="round"/>
-                        <line x1="7.5" y1="11.0714286" x2="12.5" y2="11.0714286" id="Path-13" stroke="#CCD1F2"/>
-                        <path d="M10,1 C5.30557963,1 1.5,5.02943725 1.5,10 C1.5,14.9705627 5.30557963,19 10,19 C14.6944204,19 18.5,14.9705627 18.5,10" id="Path" stroke="#CCD1F2" stroke-linecap="round"/>
-                        <polyline id="Path-14" stroke="#5567D5" stroke-linecap="round" points="12 3 15 6 19 1"/>
+                        <path d="M6.5,14.5 L9.90616569,5.25469313 C9.92525847,5.20286987 9.98274728,5.17633658 10.0345705,5.19542936 C10.0620498,5.20555331 10.0837104,5.22721384 10.0938343,5.25469313 L13.5,14.5 L13.5,14.5" id="Path-12" stroke="#40404B" stroke-linecap="round"/>
+                        <line x1="7.5" y1="11.0714286" x2="12.5" y2="11.0714286" id="Path-13" stroke="#40404B"/>
+                        <path d="M10,1 C5.30557963,1 1.5,5.02943725 1.5,10 C1.5,14.9705627 5.30557963,19 10,19 C14.6944204,19 18.5,14.9705627 18.5,10" id="Path" stroke="#40404B" stroke-linecap="round"/>
+                        <polyline id="Path-14" stroke="#40404B" stroke-linecap="round" points="12 3 15 6 19 1"/>
                     </g>
                 </g>
             </g>
