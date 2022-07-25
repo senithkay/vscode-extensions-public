462004e4af506d501a900914f7c2d6853c0a00dc
kaumini <kauminikg@gmail.com>
2022-07-25 12:32:23 +0530

    Enable syntax issues on other form

--- statement-editor/src/components/StatementEditor/index.tsx
+++ statement-editor/src/components/StatementEditor/index.tsx
@@ -282,11 +282,7 @@ export function StatementEditor(props: StatementEditorProps) {
                 : await getPartialSTForStatement({ codeSnippet }, getLangClient);
         }
 
-        const isAllowedCustomSyntaxIssue = config.type === CUSTOM_CONFIG_TYPE 
-            && partialST.syntaxDiagnostics.length && partialST.syntaxDiagnostics[0].message === "missing semicolon token" 
-            && stmtDiagnostics.length === 0;
-
-        if (!partialST.syntaxDiagnostics.length || isAllowedCustomSyntaxIssue) {
+        if (!partialST.syntaxDiagnostics.length || config.type === CUSTOM_CONFIG_TYPE) {
             const updatedContent = getUpdatedSource(partialST.source, currentFile.content, targetPosition, moduleList);
             sendDidChange(fileURI, updatedContent, getLangClient).then();
             const diagnostics = await handleDiagnostics(partialST.source);
@@ -312,7 +308,7 @@ export function StatementEditor(props: StatementEditorProps) {
 
             sendDidChange(fileURI, updatedContent, getLangClient).then();
             handleDiagnostics(updatedStatement).then();
-            !(config.type === CUSTOM_CONFIG_TYPE) && setHasSyntaxDiagnostics(true);
+            setHasSyntaxDiagnostics(true);
         }
     }
 
