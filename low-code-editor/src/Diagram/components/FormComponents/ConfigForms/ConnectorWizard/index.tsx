44f98e02ffcd85cda931e94ceba43776c9282736
Kanushka Gayan <kanushkanet@gmail.com>
2022-07-27 14:54:23 +0530

    Fix existing endpoint and action cannot edit issue

--- low-code-editor/src/Diagram/components/FormComponents/ConfigForms/ConnectorWizard/index.tsx
+++ low-code-editor/src/Diagram/components/FormComponents/ConfigForms/ConnectorWizard/index.tsx
@@ -331,6 +331,7 @@ export function ConnectorWizard(props: ConnectorWizardProps) {
                     }}
                     targetPosition={targetPosition}
                     model={model}
+                    showLoader={true}
                 />
             )}
         </>
