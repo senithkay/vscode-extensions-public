5c2c4460e1ea92af70884785d733a5888da4f01c
Madusha Gunasekara <madushajg@gmail.com>
2022-08-19 17:37:20 +0530

    Add data manipulation widgets

---
+++ data-mapper/src/components/Diagram/Node/commons/DataManipulationWidget/DataManipulationWidget.tsx
@@ -0,0 +1,68 @@
+/*
+* Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+*
+* This software is the property of WSO2 Inc. and its suppliers, if any.
+* Dissemination of any information or reproduction of any material contained
+* herein is strictly forbidden, unless permitted by WSO2 in accordance with
+* the WSO2 Commercial License available at http://wso2.com/licenses.
+* For specific language governing the permissions and limitations under
+* this license, please see the license as well as any agreement you’ve
+* entered into with WSO2 governing the purchase of this software and any
+* associated services.
+*/
+// tslint:disable: jsx-no-multiline-js
+import * as React from 'react';
+
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { DiagramEngine } from '@projectstorm/react-diagrams';
+import { MappingConstructor } from '@wso2-enterprise/syntax-tree';
+
+import { TypeWithValue } from "../../../Mappings/TypeWithValue";
+import { RecordFieldPortModel, SpecificFieldPortModel } from '../../../Port';
+
+import { TypeWithValueItemWidget } from "./TypeWithValueItemWidget";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			flexGrow: 1,
+			maxWidth: 400,
+			color: "white",
+			position: "relative",
+			backgroundColor: " #FFFFFF",
+			padding: "20px"
+		}
+	}),
+);
+
+export interface DataManipulationWidgetProps {
+	id: string; // this will be the root ID used to prepend for UUIDs of nested fields
+	typeWithValue?: TypeWithValue;
+	value: MappingConstructor;
+	engine: DiagramEngine;
+	getPort: (portId: string) => SpecificFieldPortModel | RecordFieldPortModel;
+}
+
+
+export function DataManipulationWidget(props: DataManipulationWidgetProps) {
+	const { id, typeWithValue, value, engine, getPort } = props;
+	const classes = useStyles();
+
+	return (
+		<div className={classes.root}>
+			{
+				typeWithValue.childrenTypes.map((item) => {
+					return (
+						<TypeWithValueItemWidget
+							key={id}
+							engine={engine}
+							field={item}
+							getPort={getPort}
+							parentId={id}
+						/>
+					);
+				})
+			}
+		</div>
+	);
+}
---
+++ data-mapper/src/components/Diagram/Node/commons/DataManipulationWidget/TypeWithValueItemWidget.tsx
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ * associated services.
+ */
+// tslint:disable: jsx-no-multiline-js
+import React, { useState } from "react";
+
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { DiagramEngine } from "@projectstorm/react-diagrams-core";
+
+import { TypeWithValue } from "../../../Mappings/TypeWithValue";
+import { DataMapperPortWidget, RecordFieldPortModel, SpecificFieldPortModel } from "../../../Port";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        treeLabel: {
+            verticalAlign: "middle",
+            padding: "5px",
+            color: "#222228",
+            fontFamily: "GilmerMedium",
+            fontSize: "13px",
+            minWidth: "100px",
+            backgroundColor: "#FFFFFF",
+            border: "1px solid #DEE0E7",
+            display: "flex",
+            minHeight: "24px",
+            boxShadow: '0 2px 40px 0 rgba(102,103,133,0.15)',
+        },
+        treeLabelOutPort: {
+            float: "right",
+            width: 'fit-content',
+            marginLeft: "auto",
+        },
+        treeLabelInPort: {
+            float: "left",
+            marginRight: "5px",
+            width: 'fit-content',
+        },
+        typeLabel: {
+            marginLeft: "3px",
+            verticalAlign: "middle",
+            padding: "5px",
+            color: "#222228",
+            fontFamily: "GilmerRegular",
+            fontSize: "13px",
+            minWidth: "100px",
+            backgroundColor: "#FFFFFF",
+            marginRight: "24px"
+        },
+        valueLabel: {
+            verticalAlign: "middle",
+            padding: "5px",
+            color: "#222228",
+            fontFamily: "GilmerMedium",
+            fontSize: "13px",
+            backgroundColor: "#FFFFFF",
+        },
+        group: {
+            marginLeft: "0px",
+            paddingLeft: "0px",
+            paddingBottom: "5px"
+        },
+        content: {
+            borderTopRightRadius: theme.spacing(2),
+            borderBottomRightRadius: theme.spacing(2),
+            paddingRight: theme.spacing(1),
+        }
+
+    }),
+);
+
+export interface TypeWithValueItemWidgetProps {
+    parentId: string;
+    field: TypeWithValue;
+    engine: DiagramEngine;
+    getPort: (portId: string) => SpecificFieldPortModel | RecordFieldPortModel;
+    treeDepth?: number;
+}
+
+export function TypeWithValueItemWidget(props: TypeWithValueItemWidgetProps) {
+    const { parentId, field, getPort, engine, treeDepth = 0 } = props;
+    const classes = useStyles();
+
+    const fieldId = `${parentId}.${field.type.name}`;
+    const portIn = getPort(fieldId + ".IN");
+    const portOut = getPort(fieldId + ".OUT");
+    let fields: TypeWithValue[];
+
+    if (field.type.typeName === 'record') {
+        fields = field.childrenTypes;
+    }
+
+    const [expanded, setExpanded] = useState<boolean>(true)
+
+    const typeName = field.type.typeName
+        ? field.type.typeName
+        : "record";
+
+    const indentation = !!fields ? 0 : ((treeDepth + 1) * 16) + 8;
+
+    const label = (
+        <span style={{marginRight: "auto"}}>
+            <span className={classes.valueLabel} style={{marginLeft: indentation}}>
+                {field.type.name}
+                {typeName && ":"}
+            </span>
+            {typeName && (
+                <span className={classes.typeLabel}>
+                    {typeName}
+                </span>
+            )}
+
+        </span>
+    );
+
+    const handleExpand = () => {
+        // TODO Enable expand collapse functionality
+        // setExpanded(!expanded)
+    }
+
+    return (
+        <>
+            <div className={classes.treeLabel}>
+                <span className={classes.treeLabelInPort}>
+                    {portIn &&
+                        <DataMapperPortWidget engine={engine} port={portIn}/>
+                    }
+                </span>
+                {fields &&
+                    (expanded ? (
+                            <ExpandMoreIcon style={{color: "black", marginLeft: treeDepth * 16}} onClick={handleExpand}/>
+                        ) :
+                        (
+                            <ChevronRightIcon style={{color: "black", marginLeft: treeDepth * 16}} onClick={handleExpand}/>
+                        ))
+                }
+
+                <span> {label}</span>
+                <span className={classes.treeLabelOutPort}>
+                    {portOut &&
+                        <DataMapperPortWidget engine={engine} port={portOut}/>
+                    }
+                </span>
+            </div>
+            {fields &&
+                fields.map((subField) => {
+                    return (
+                        <TypeWithValueItemWidget
+                            key={fieldId}
+                            engine={engine}
+                            field={subField}
+                            getPort={getPort}
+                            parentId={fieldId}
+                            treeDepth={treeDepth + 1}
+                        />
+                    );
+                })
+            }
+        </>
+    );
+}
