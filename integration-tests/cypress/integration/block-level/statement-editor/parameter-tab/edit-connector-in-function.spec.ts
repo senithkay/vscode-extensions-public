997c7eeafaddccd0dd4bc4ac99e8c259d760f3f9
Kanushka Gayan <kanushkanet@gmail.com>
2022-08-26 12:49:00 +0530

    Skip edit connector parameter test until fix the issue

--- integration-tests/cypress/integration/block-level/statement-editor/parameter-tab/edit-connector-in-function.spec.ts
+++ integration-tests/cypress/integration/block-level/statement-editor/parameter-tab/edit-connector-in-function.spec.ts
@@ -12,7 +12,8 @@ describe('Edit connector with custom configurations via Low Code', () => {
         cy.visit(getIntegrationTestPageURL(BAL_FILE_PATH));
     });
 
-    it('Edit http endpoint with custom configurations', () => {
+    // TODO: Skip this test until fixing https://github.com/wso2-enterprise/choreo/issues/14988 issue
+    it.skip('Edit http endpoint with custom configurations', () => {
         Canvas.getFunction("myfunction")
             .nameShouldBe("myfunction")
             .shouldBeExpanded()
@@ -33,7 +34,7 @@ describe('Edit connector with custom configurations via Low Code', () => {
             .shouldHaveCustomArg("enabled")
             .shouldHaveUnionArg("compression")
             .toggleInclusionArg("config");
-cy.wait(500);
+
         StatementEditor
             .save();
 
