ccf70330f427eeb0e40ca3e108f75be2c386174c
kaje94 <a.kajendran@gmail.com>
2022-09-02 13:38:38 +0530

    Fix formatting in diagram link elements

--- data-mapper/src/components/Diagram/Link/model/DataMapperLinkFactory.tsx
+++ data-mapper/src/components/Diagram/Link/model/DataMapperLinkFactory.tsx
@@ -1,22 +1,24 @@
 import { DefaultLinkFactory } from "@projectstorm/react-diagrams";
 import React from "react";
 import { DataMapperLinkModel, LINK_TYPE_ID } from "./DataMapperLink";
-import { container, injectable, singleton } from 'tsyringe';
+import { container, injectable, singleton } from "tsyringe";
 import { DefaultLinkWidget } from "./DefaultLinkWidget";
 
 @injectable()
 @singleton()
 export class DataMapperLinkFactory extends DefaultLinkFactory {
-	constructor() {
-		super(LINK_TYPE_ID);
-	}
+    constructor() {
+        super(LINK_TYPE_ID);
+    }
 
-	generateModel(): DataMapperLinkModel {
-		return new DataMapperLinkModel();
-	}
+    generateModel(): DataMapperLinkModel {
+        return new DataMapperLinkModel();
+    }
 
-	generateReactWidget(event: { model: any; }): JSX.Element {
-		return <DefaultLinkWidget link={event.model} diagramEngine={this.engine} />;
-	}
+    generateReactWidget(event: { model: any }): JSX.Element {
+        return (
+            <DefaultLinkWidget link={event.model} diagramEngine={this.engine} />
+        );
+    }
 }
-container.register("LinkFactory", {useClass: DataMapperLinkFactory});
+container.register("LinkFactory", { useClass: DataMapperLinkFactory });
--- data-mapper/src/components/Diagram/Link/model/DefaultLinkSegmentWidget.tsx
+++ data-mapper/src/components/Diagram/Link/model/DefaultLinkSegmentWidget.tsx
@@ -1,46 +1,46 @@
-import * as React from 'react';
-import { DefaultLinkSegmentWidgetProps } from '@projectstorm/react-diagrams';
+import * as React from "react";
+import { DefaultLinkSegmentWidgetProps } from "@projectstorm/react-diagrams";
 
 export class DefaultLinkSegmentWidget extends React.Component<DefaultLinkSegmentWidgetProps> {
-	render() {
-		const Bottom = React.cloneElement(
-			this.props.factory.generateLinkSegment(
-				this.props.link,
-				this.props.selected || this.props.link.isSelected(),
-				this.props.path
-			),
-			{
-				ref: this.props.forwardRef
-			}
-		);
+    render() {
+        const Bottom = React.cloneElement(
+            this.props.factory.generateLinkSegment(
+                this.props.link,
+                this.props.selected || this.props.link.isSelected(),
+                this.props.path
+            ),
+            {
+                ref: this.props.forwardRef,
+            }
+        );
 
-		const Top = React.cloneElement(Bottom, {
-			strokeLinecap: 'round',
-			onMouseLeave: () => {
-				this.props.onSelection(false);
-			},
-			onMouseEnter: () => {
-				this.props.onSelection(true);
-			},
-			...this.props.extras,
-			ref: null,
-			'data-linkid': this.props.link.getID(),
-			strokeOpacity: this.props.selected ? 0.1 : 0,
-			strokeWidth: 10, // Note: Original strokeWidth was 20
-			fill: 'none',
-			onContextMenu: () => {
-				if (!this.props.link.isLocked()) {
-					event.preventDefault();
-					this.props.link.remove();
-				}
-			},
-		});
+        const Top = React.cloneElement(Bottom, {
+            strokeLinecap: "round",
+            onMouseLeave: () => {
+                this.props.onSelection(false);
+            },
+            onMouseEnter: () => {
+                this.props.onSelection(true);
+            },
+            ...this.props.extras,
+            ref: null,
+            "data-linkid": this.props.link.getID(),
+            strokeOpacity: this.props.selected ? 0.1 : 0,
+            strokeWidth: 10, // Note: Original strokeWidth was 20
+            fill: "none",
+            onContextMenu: () => {
+                if (!this.props.link.isLocked()) {
+                    event.preventDefault();
+                    this.props.link.remove();
+                }
+            },
+        });
 
-		return (
-			<g>
-				{Bottom}
-				{Top}
-			</g>
-		);
-	}
+        return (
+            <g>
+                {Bottom}
+                {Top}
+            </g>
+        );
+    }
 }
