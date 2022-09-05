40412080f815602b8b3c7c08fd170a2b69fdb2b1
Kavith Lokuhewage <kaviththiranga@gmail.com>
2022-09-05 11:29:05 +0530

    Generate an expr bodied fn for DM

--- data-mapper/src/components/DataMapper/ConfigPanel/DataMapperConfigPanel.tsx
+++ data-mapper/src/components/DataMapper/ConfigPanel/DataMapperConfigPanel.tsx
@@ -48,7 +48,9 @@ export function DataMapperConfigPanel(props: DataMapperProps) {
                     fnName,
                     parametersStr,
                     returnTypeStr,
-                    targetPosition
+                    targetPosition,
+                    false,
+                    true
                 )
             );
         }
