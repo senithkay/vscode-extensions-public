0de88515e505ee61400aa20a5be9d9f8855dbdc4
gigara <info@gigara.info>
2022-10-11 17:16:23 +0530

    Update plugin version to 3.3.0-beta

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@
 const SentryPlugin = require("@sentry/webpack-plugin");
 const path = require('path');
 const MergeIntoSingleFile = require('webpack-merge-and-include-globally');
-const APP_VERSION = "20220923-0937";
+const APP_VERSION = "20221011-1118";
 const optionalPlugins = [];
-const BALLERINA_VS_CODE_PATH = "~/config/extensions/wso2.ballerina-3.2.0/resources/jslibs";
+const BALLERINA_VS_CODE_PATH = "~/config/extensions/wso2.ballerina-3.3.0-beta/resources/jslibs";
 const logging = require('webpack/lib/logging/runtime');
 
 logging.getLogger("plugin-vscode").info('IS_RELEASE: ' + process.env.IS_RELEASE);
