9e609f0d33bcd1023a3b35286322b7ad665d9137
CharukaK <charukakarunanayake@gmail.com>
2022-10-11 16:22:41 +0530

    Fix lint issues

--- low-code-diagram/src/Visitors/sizing-visitor.ts
+++ low-code-diagram/src/Visitors/sizing-visitor.ts
@@ -1446,7 +1446,7 @@ export class SizingVisitor implements Visitor {
     }
 
     private endSizingBlock(node: BlockStatement, lastStatementIndex: number, width: number = 0, height: number = 0,
-        index: number = 0, leftWidth: number = 0, rightWidth: number = 0) {
+                           index: number = 0, leftWidth: number = 0, rightWidth: number = 0) {
         if (!node.viewState) {
             return;
         }
@@ -1642,8 +1642,8 @@ export class SizingVisitor implements Visitor {
     }
 
     private calculateStatementSizing(statements: STNode[], index: number, blockViewState: BlockViewState,
-        height: number, width: number, lastStatementIndex: any, leftWidth: number,
-        rightWidth: number) {
+                                     height: number, width: number, lastStatementIndex: any, leftWidth: number,
+                                     rightWidth: number) {
         const startIndex = index;
 
         blockViewState.collapsedViewStates.forEach(collapsedVS => {
