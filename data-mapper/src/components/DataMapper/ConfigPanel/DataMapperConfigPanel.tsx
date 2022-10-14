0fbd34a0d54eb55f23beb036720d9dc5c42f4452
Anjana S Porawagama <axewilledge123@gmail.com>
2022-10-14 11:06:53 +0530

    Show output input when in edit state

--- data-mapper/src/components/DataMapper/ConfigPanel/DataMapperConfigPanel.tsx
+++ data-mapper/src/components/DataMapper/ConfigPanel/DataMapperConfigPanel.tsx
@@ -4,15 +4,15 @@ import styled from "@emotion/styled";
 import Divider from "@material-ui/core/Divider/Divider";
 import FormControl from "@material-ui/core/FormControl/FormControl";
 import {
-  createFunctionSignature,
-  STModification,
-  updateFunctionSignature,
+    createFunctionSignature,
+    STModification,
+    updateFunctionSignature,
 } from "@wso2-enterprise/ballerina-low-code-edtior-commons";
 import {
-  FormActionButtons,
-  FormHeaderSection,
-  Panel,
-  useStyles as useFormStyles,
+    FormActionButtons,
+    FormHeaderSection,
+    Panel,
+    useStyles as useFormStyles,
 } from "@wso2-enterprise/ballerina-low-code-edtior-ui-components";
 import { ExpressionFunctionBody, STKindChecker } from "@wso2-enterprise/syntax-tree";
 
@@ -26,169 +26,175 @@ import { getFnNameFromST, getInputsFromST, getOutputTypeFromST } from "./utils";
 import { RecordButtonGroup } from "./RecordButtonGroup";
 
 export function DataMapperConfigPanel(props: DataMapperProps) {
-  const {
-    onClose,
-    fnST,
-    applyModifications,
-    targetPosition,
-    onSave,
-    recordPanel,
-  } = props;
-  const formClasses = useFormStyles();
+    const {
+        onClose,
+        fnST,
+        applyModifications,
+        targetPosition,
+        onSave,
+        recordPanel,
+    } = props;
+    const formClasses = useFormStyles();
 
-  const [fnName, setFnName] = useState("transform");
-  const [inputParams, setInputParams] = useState<DataMapperInputParam[]>([]);
-  const [outputType, setOutputType] = useState("");
-  const [inputType, setInputType] = useState("");
-  const [isNewRecord, setIsNewRecord] = useState(false);
-  const [isAddExistType, setAddExistType] = useState(false);
+    const [fnName, setFnName] = useState("transform");
+    const [inputParams, setInputParams] = useState<DataMapperInputParam[]>([]);
+    const [outputType, setOutputType] = useState("");
+    const [inputType, setInputType] = useState("");
+    const [isNewRecord, setIsNewRecord] = useState(false);
+    const [isAddExistType, setAddExistType] = useState(false);
 
-  const [showOutputType, setShowOutputType] = useState(false);
-  const [newRecordBy, setNewRecordBy] = useState<"input" | "output">(undefined);
+    const [showOutputType, setShowOutputType] = useState(false);
+    const [newRecordBy, setNewRecordBy] = useState<"input" | "output">(undefined);
 
-  const isValidConfig = fnName && inputParams.length > 0 && outputType !== "";
+    const isValidConfig = fnName && inputParams.length > 0 && outputType !== "";
 
-  const onSaveForm = () => {
-    const parametersStr = inputParams
-      .map((item) => `${item.type} ${item.name}`)
-      .join(",");
+    const onSaveForm = () => {
+        const parametersStr = inputParams
+            .map((item) => `${item.type} ${item.name}`)
+            .join(",");
 
-    const returnTypeStr = `returns ${outputType}`;
+        const returnTypeStr = `returns ${outputType}`;
 
-    const modifications: STModification[] = [];
-    if (fnST && STKindChecker.isFunctionDefinition(fnST)) {
-      modifications.push(
-        updateFunctionSignature(fnName, parametersStr, returnTypeStr, {
-          ...fnST?.functionSignature?.position,
-          startColumn: fnST?.functionName?.position?.startColumn,
-        })
-      );
+        const modifications: STModification[] = [];
+        if (fnST && STKindChecker.isFunctionDefinition(fnST)) {
+            modifications.push(
+                updateFunctionSignature(fnName, parametersStr, returnTypeStr, {
+                    ...fnST?.functionSignature?.position,
+                    startColumn: fnST?.functionName?.position?.startColumn,
+                })
+            );
 
-      const functionExpression = (fnST.functionBody as ExpressionFunctionBody)?.expression;
-      if (functionExpression && STKindChecker.isNilLiteral(functionExpression)) {
-        // if function returns (), replace it with {}
-        modifications.push({
-          type: "INSERT",
-          config: { "STATEMENT": "{}" },
-          ...functionExpression.position
-        })
-      }
+            const functionExpression = (fnST.functionBody as ExpressionFunctionBody)?.expression;
+            if (functionExpression && STKindChecker.isNilLiteral(functionExpression)) {
+                // if function returns (), replace it with {}
+                modifications.push({
+                    type: "INSERT",
+                    config: { "STATEMENT": "{}" },
+                    ...functionExpression.position
+                })
+            }
 
-    } else {
-      modifications.push(
-        createFunctionSignature(
-          "",
-          fnName,
-          parametersStr,
-          returnTypeStr,
-          targetPosition,
-          false,
-          true
-        )
-      );
+        } else {
+            modifications.push(
+                createFunctionSignature(
+                    "",
+                    fnName,
+                    parametersStr,
+                    returnTypeStr,
+                    targetPosition,
+                    false,
+                    true
+                )
+            );
+        }
+        onSave(fnName);
+        applyModifications(modifications);
+    };
+
+    useEffect(() => {
+        if (fnST) {
+            setFnName(getFnNameFromST(fnST));
+            setInputParams(getInputsFromST(fnST));
+            console.log(getOutputTypeFromST(fnST));
+            setOutputType(getOutputTypeFromST(fnST));
+        }
+    }, [fnST]);
+
+    useEffect(() => {
+        if (outputType) {
+            setShowOutputType(true);
+        }
+    }, [outputType]);
+
+    // For Input Value
+    const enableAddNewRecord = () => {
+        setIsNewRecord(true);
+        setNewRecordBy("input");
+    };
+
+    const closeAddNewRecord = (createdNewRecord?: string) => {
+        setIsNewRecord(false);
+        if (createdNewRecord) {
+            if (newRecordBy === "input") {
+                setInputType(createdNewRecord.split(" ")[1]);
+            }
+            if (newRecordBy === "output") {
+                setOutputType(createdNewRecord.split(" ")[1]);
+            }
+        }
+        setNewRecordBy(undefined);
+    };
+
+    const handleShowOutputType = () => {
+        setShowOutputType(true);
     }
-    onSave(fnName);
-    applyModifications(modifications);
-  };
 
-  useEffect(() => {
-    if (fnST) {
-      setFnName(getFnNameFromST(fnST));
-      setInputParams(getInputsFromST(fnST));
-      console.log(getOutputTypeFromST(fnST));
-      setOutputType(getOutputTypeFromST(fnST));
+    // For Output Value
+    const handleShowRecordEditor = () => {
+        enableAddNewRecord();
+        handleShowOutputType();
+        setNewRecordBy("output");
     }
-  }, [fnST]);
 
-  // For Input Value
-  const enableAddNewRecord = () => {
-    setIsNewRecord(true);
-    setNewRecordBy("input");
-  };
-
-  const closeAddNewRecord = (createdNewRecord?: string) => {
-    setIsNewRecord(false);
-    if (createdNewRecord) {
-      if (newRecordBy === "input") {
-        setInputType(createdNewRecord.split(" ")[1]);
-      }
-      if (newRecordBy === "output") {
-        setOutputType(createdNewRecord.split(" ")[1]);
-      }
-    }
-    setNewRecordBy(undefined);
-  };
-
-  const handleShowOutputType = () => {
-    setShowOutputType(true);
-  }
-
-  // For Output Value
-  const handleShowRecordEditor = () => {
-    enableAddNewRecord();
-    handleShowOutputType();
-    setNewRecordBy("output");
-  }
-
-  const breadCrumb = (
-    <FormHeaderSection
-      onCancel={closeAddNewRecord}
-      formTitle={"lowcode.develop.configForms.dataMapper.titleExtend"}
-      defaultMessage={"Data Mapper"}
-      formTitleSecond={"lowcode.develop.configForms.dataMapper.titleRecord"}
-      defaultMessageSecond={"Record"}
-    />
-  );
-
-  return (
-    <Panel onClose={onClose}>
-      <FormControl
-        variant="outlined"
-        data-testid="data-mapper-form"
-        className={formClasses.wizardFormControlExtended}
-      >
-        {(isNewRecord && breadCrumb) || (
-          <FormHeaderSection
-            onCancel={onClose}
-            formTitle={"lowcode.develop.configForms.dataMapper.title"}
+    const breadCrumb = (
+        <FormHeaderSection
+            onCancel={closeAddNewRecord}
+            formTitle={"lowcode.develop.configForms.dataMapper.titleExtend"}
             defaultMessage={"Data Mapper"}
-          />
-        )}
-        {isNewRecord && recordPanel({ closeAddNewRecord: closeAddNewRecord })}
-        {!isNewRecord && (
-          <>
-            <FormBody>
-              <FunctionNameEditor value={fnName} onChange={setFnName} />
-              <FormDivider />
-              <InputParamsPanel
-                newRecordParam={inputType}
-                inputParams={inputParams}
-                onUpdateParams={setInputParams}
-                enableAddNewRecord={enableAddNewRecord}
-                setAddExistType={setAddExistType}
-                isAddExistType={isAddExistType}
-              />
-              <FormDivider />
-              <OutputTypeConfigPanel>
-                <Title>Output Type</Title>
-                {showOutputType && <TypeBrowser type={outputType} onChange={setOutputType} />}
-                <RecordButtonGroup openRecordEditor={handleShowRecordEditor} showTypeList={handleShowOutputType} />
-              </OutputTypeConfigPanel>
-            </FormBody>
-            <FormActionButtons
-              isMutationInProgress={false}
-              cancelBtn={true}
-              saveBtnText="Save"
-              cancelBtnText="Cancel"
-              validForm={isValidConfig}
-              onSave={onSaveForm}
-              onCancel={onClose}
-            />
-          </>
-        )}
-      </FormControl>
-    </Panel>
-  );
+            formTitleSecond={"lowcode.develop.configForms.dataMapper.titleRecord"}
+            defaultMessageSecond={"Record"}
+        />
+    );
+
+    return (
+        <Panel onClose={onClose}>
+            <FormControl
+                variant="outlined"
+                data-testid="data-mapper-form"
+                className={formClasses.wizardFormControlExtended}
+            >
+                {(isNewRecord && breadCrumb) || (
+                    <FormHeaderSection
+                        onCancel={onClose}
+                        formTitle={"lowcode.develop.configForms.dataMapper.title"}
+                        defaultMessage={"Data Mapper"}
+                    />
+                )}
+                {isNewRecord && recordPanel({ closeAddNewRecord: closeAddNewRecord })}
+                {!isNewRecord && (
+                    <>
+                        <FormBody>
+                            <FunctionNameEditor value={fnName} onChange={setFnName} />
+                            <FormDivider />
+                            <InputParamsPanel
+                                newRecordParam={inputType}
+                                inputParams={inputParams}
+                                onUpdateParams={setInputParams}
+                                enableAddNewRecord={enableAddNewRecord}
+                                setAddExistType={setAddExistType}
+                                isAddExistType={isAddExistType}
+                            />
+                            <FormDivider />
+                            <OutputTypeConfigPanel>
+                                <Title>Output Type</Title>
+                                {showOutputType && <TypeBrowser type={outputType} onChange={setOutputType} />}
+                                <RecordButtonGroup openRecordEditor={handleShowRecordEditor} showTypeList={handleShowOutputType} />
+                            </OutputTypeConfigPanel>
+                        </FormBody>
+                        <FormActionButtons
+                            isMutationInProgress={false}
+                            cancelBtn={true}
+                            saveBtnText="Save"
+                            cancelBtnText="Cancel"
+                            validForm={isValidConfig}
+                            onSave={onSaveForm}
+                            onCancel={onClose}
+                        />
+                    </>
+                )}
+            </FormControl>
+        </Panel>
+    );
 }
 
 const FormBody = styled.div`
@@ -207,12 +213,12 @@ const OutputTypeConfigPanel = styled.div`
 `;
 
 export const Title = styled.div(() => ({
-  fontSize: "13px",
-  letterSpacing: "normal",
-  textTransform: "capitalize",
-  margin: "0 0 8px",
-  fontFamily: "Gilmer",
-  lineHeight: "1rem",
-  paddingBottom: "0.6rem",
-  fontWeight: 500,
+    fontSize: "13px",
+    letterSpacing: "normal",
+    textTransform: "capitalize",
+    margin: "0 0 8px",
+    fontFamily: "Gilmer",
+    lineHeight: "1rem",
+    paddingBottom: "0.6rem",
+    fontWeight: 500,
 }));
