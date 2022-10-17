456e1e8342c5a2bf7dcce0a4bb0bc4a81e80f967
Madusha <madushajg@gmail.com>
2022-10-17 14:33:59 +0530

    Add loader for query link deletion

--- data-mapper/src/components/Diagram/Node/QueryExpression/QueryExpressionNodeWidget.tsx
+++ data-mapper/src/components/Diagram/Node/QueryExpression/QueryExpressionNodeWidget.tsx
@@ -13,20 +13,21 @@
 // tslint:disable: jsx-no-multiline-js
 import * as React from 'react';
 
-import { IconButton } from "@material-ui/core";
-import { createStyles, Theme, withStyles, WithStyles } from "@material-ui/core/styles";
+import { CircularProgress } from "@material-ui/core";
+import { createStyles, makeStyles, Theme, withStyles } from "@material-ui/core/styles";
 import TooltipBase from '@material-ui/core/Tooltip';
 import DeleteIcon from '@material-ui/icons/DeleteOutlineOutlined';
+import ExitToApp from "@material-ui/icons/ExitToApp";
 import QueryIcon from '@material-ui/icons/StorageOutlined';
 import { DiagramEngine } from '@projectstorm/react-diagrams';
-import ExitToApp from "@material-ui/icons/ExitToApp";
+import clsx from 'clsx';
+
 import { ViewOption } from "../../../DataMapper/DataMapper";
-import { DataMapperPortWidget, RecordFieldPortModel } from '../../Port';
+import { DataMapperPortWidget } from '../../Port';
 
 import {
     QueryExpressionNode,
 } from './QueryExpressionNode';
-import clsx from 'clsx';
 
 export const tooltipBaseStyles = {
     tooltip: {
@@ -41,130 +42,150 @@ export const tooltipBaseStyles = {
     }
 };
 
-const styles = (theme: Theme) => createStyles({
-    root: {
-        width: '100%',
-        backgroundColor: theme.palette.common.white,
-        display: "flex",
-        flexDirection: "column",
-        gap: "5px",
-        color: theme.palette.grey[400],
-        boxShadow: "0px 5px 50px rgba(203, 206, 219, 0.5)",
-        borderRadius: "10px",
-		alignItems: "center",
-		overflow: "hidden",
-    },
-    element: {
-        backgroundColor: theme.palette.common.white,
-        padding: "5px",
-        cursor: "pointer",
-        transitionDuration: "0.2s",
-        userSelect: "none",
-        pointerEvents: "auto",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        "&:hover": {
-            filter: "brightness(0.95)",
+export const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            width: '100%',
+            backgroundColor: theme.palette.common.white,
+            display: "flex",
+            flexDirection: "column",
+            gap: "5px",
+            color: theme.palette.grey[400],
+            boxShadow: "0px 5px 50px rgba(203, 206, 219, 0.5)",
+            borderRadius: "10px",
+            alignItems: "center",
+            overflow: "hidden",
         },
-    },
-    iconWrapper: {
-        height: "22px",
-        width: "22px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    fromClause: {
-        padding: "5px",
-        fontFamily: "GilmerMedium",
-        marginRight: '10px'
-    },
-    mappingPane: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
-    header: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-    icons: {
-        padding: '5px'
-    },
-    openQueryIcon: {
-        color: theme.palette.grey[300],
-        padding: "5px",
-        height: "32px",
-        width: "32px"
-    },
-    editIcon: {
-        color: theme.palette.grey[300],
-    },
-    deleteIcon: {
-        color: theme.palette.error.main
-    }
-});
+        element: {
+            backgroundColor: theme.palette.common.white,
+            padding: "5px",
+            cursor: "pointer",
+            transitionDuration: "0.2s",
+            userSelect: "none",
+            pointerEvents: "auto",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            "&:hover": {
+                filter: "brightness(0.95)",
+            },
+        },
+        iconWrapper: {
+            height: "22px",
+            width: "22px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        fromClause: {
+            padding: "5px",
+            fontFamily: "GilmerMedium",
+            marginRight: '10px'
+        },
+        mappingPane: {
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between"
+        },
+        header: {
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center"
+        },
+        icons: {
+            padding: '5px'
+        },
+        openQueryIcon: {
+            color: theme.palette.grey[300],
+            padding: "5px",
+            height: "32px",
+            width: "32px"
+        },
+        editIcon: {
+            color: theme.palette.grey[300],
+        },
+        deleteIcon: {
+            color: theme.palette.error.main
+        },
+        loadingContainer: {
+            padding: "10px"
+        },
+        circularProgress: {
+            color: "#CBCEDB",
+            display: "block"
+        }
+    })
+);
 
-export interface QueryExprAsSFVNodeWidgetProps extends WithStyles<typeof styles> {
+export interface QueryExprAsSFVNodeWidgetProps {
     node: QueryExpressionNode;
     engine: DiagramEngine;
 }
 
-class QueryExprAsSFVNodeWidgetC extends React.Component<QueryExprAsSFVNodeWidgetProps> {
-    render() {
-        const node = this.props.node;
-        const classes = this.props.classes;
-        const engine = this.props.engine;
+export function QueryExpressionNodeWidget(props: QueryExprAsSFVNodeWidgetProps) {
+    const { node, engine } = props;
+    const classes = useStyles();
 
-        const onClickOnExpand = () => {
-            node.context.changeSelection(ViewOption.EXPAND,
-                {
-                    ...node.context.selection,
-                    selectedST: {
-                        stNode: node.parentNode,
-                        fieldPath: node.targetFieldFQN
-                    }
-                })
-        }
+    const [deleteInProgress, setDeleteInProgress] = React.useState(false);
 
-        const deleteQueryLink = () => {
-            node.deleteLink();
-        }
+    const onClickOnExpand = () => {
+        node.context.changeSelection(ViewOption.EXPAND,
+            {
+                ...node.context.selection,
+                selectedST: {
+                    stNode: node.parentNode,
+                    fieldPath: node.targetFieldFQN
+                }
+            })
+    }
 
-        const TooltipComponent = withStyles(tooltipBaseStyles)(TooltipBase);
+    const deleteQueryLink = () => {
+        setDeleteInProgress(true);
+        node.deleteLink();
+    }
 
-        return (!node.hidden &&(
-            <>
-                {/* TODO: Identify inner query expressions and render minimized boxes to denote those with links */}
-                {!!node.sourcePort && (
-                    <div className={classes.root} >
-                        <div className={classes.header}>
-                            <DataMapperPortWidget engine={engine} port={node.inPort} />
-                            <TooltipComponent interactive={false} arrow={true} title={"Query Expression"}>
-                                <span className={classes.openQueryIcon} >
-                                    <QueryIcon  />
-                                </span>
-                            </TooltipComponent>
-                            <div className={classes.element} onClick={onClickOnExpand}>
-                                <div className={classes.iconWrapper}>
-                                    <ExitToApp className={clsx(classes.editIcon)}/>
-                                </div>
+    const TooltipComponent = withStyles(tooltipBaseStyles)(TooltipBase);
+
+    const loadingScreen = (
+        <CircularProgress
+            size={22}
+            thickness={3}
+            className={classes.circularProgress}
+        />
+    );
+
+    return (!node.hidden && (
+        <>
+            {!!node.sourcePort && (
+                <div className={classes.root} >
+                    <div className={classes.header}>
+                        <DataMapperPortWidget engine={engine} port={node.inPort} />
+                        <TooltipComponent interactive={false} arrow={true} title={"Query Expression"}>
+                            <span className={classes.openQueryIcon} >
+                                <QueryIcon  />
+                            </span>
+                        </TooltipComponent>
+                        <div className={classes.element} onClick={onClickOnExpand}>
+                            <div className={classes.iconWrapper}>
+                                <ExitToApp className={clsx(classes.editIcon)}/>
                             </div>
+                        </div>
+                        {deleteInProgress ? (
+                            <div className={clsx(classes.element, classes.loadingContainer)}>
+                                {loadingScreen}
+                            </div>
+                        ) : (
                             <div className={classes.element} onClick={deleteQueryLink}>
                                 <div className={classes.iconWrapper}>
                                     <DeleteIcon className={clsx(classes.deleteIcon)}/>
                                 </div>
                             </div>
-                            <DataMapperPortWidget engine={engine} port={node.outPort} />
-                        </div>
+                        )}
+                        <DataMapperPortWidget engine={engine} port={node.outPort} />
                     </div>
-                )}
-            </>
-        ));
-    }
+                </div>
+            )}
+        </>
+    ));
 }
-
-export const QueryExpressionNodeWidget = withStyles(styles, { withTheme: true })(QueryExprAsSFVNodeWidgetC);
