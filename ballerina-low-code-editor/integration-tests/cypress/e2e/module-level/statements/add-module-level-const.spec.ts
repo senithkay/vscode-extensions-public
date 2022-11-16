cc22fe1a347c1673c92b53bcff7962fdf271c0b9
kaumini <kauminikg@gmail.com>
2022-11-16 19:27:24 +0530

    Update const tests to const form changes

--- ballerina-low-code-editor/integration-tests/cypress/e2e/module-level/statements/add-module-level-const.spec.ts
+++ ballerina-low-code-editor/integration-tests/cypress/e2e/module-level/statements/add-module-level-const.spec.ts
@@ -64,7 +64,7 @@ describe('Add const to module level via Low Code', () => {
     });
 
     it('Add constant to module level with type', () => {
-        Canvas.clickTopLevelPlusButton(1);
+        Canvas.clickTopLevelPlusButton(0);
 
         TopLevelPlusWidget
             .clickOption("Constant");
@@ -75,19 +75,14 @@ describe('Add const to module level via Low Code', () => {
 
         EditorPane
             .getStatementRenderer()
-            .getExpression("StringTypeDesc")
-            .doubleClickExpressionContent('string');
-
-        InputEditor
-            .typeInput('int');
-
-        EditorPane
-            .getStatementRenderer()
             .getExpression("IdentifierToken")
             .doubleClickExpressionContent('CONST_NAME');
 
         InputEditor
-            .typeInput('TWO');
+            .typeInput('int TWO');
+
+        EditorPane
+            .validateNewExpression("IntTypeDesc", 'int')
 
         EditorPane
             .getStatementRenderer()
