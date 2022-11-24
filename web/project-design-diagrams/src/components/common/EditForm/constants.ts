6c1319324163047a752d3f81c6676c61d809b3b6
SumayyaZiyad <github.sumayyaziyad@gmail.com>
2022-11-24 20:38:57 +0530

    Form UI for add service

---
+++ web/project-design-diagrams/src/components/common/EditForm/constants.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+export const PackageNameRegex: RegExp = new RegExp('^[a-zA-Z0-9\.\_]+$');
+export const OrganizationRegex: RegExp = new RegExp('^[a-zA-Z0-9\_]+$');
+export const VersionRegex: RegExp = new RegExp('^[a-zA-Z0-9\.\-]+$');
+
+export const PackageNameRules: string = 'Should only contain alphanumerics, underscore, and period.';
+export const OrganizationRules: string = 'Should only contain alphanumerics and underscore.';
+export const VersioningRules: string = 'Should follow the SemVer best practices.';
---
+++ web/project-design-diagrams/src/components/common/EditForm/styles.ts
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+import styled from '@emotion/styled';
+
+export const PrimaryContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    min-width: 370px;
+`;
+
+export const Header = styled.div`
+    align-items: center;
+    align-self: stretch;
+    box-shadow: inset 0px -1px 0px #E0E2E9;
+    color: #1D2028;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 50px;
+    padding: 12px 12px 12px 20px;
+`;
+
+export const HeaderTitle = styled.span`
+    font-family: GilmerMedium;
+    font-size: 17px;
+    text-align: center;
+`;
+
+export const InputComponent = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-family: GilmerRegular;
+    font-size: 15px;
+    padding: 10px;
+`;
+
+export const ControlsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    padding: 10px;
+`;
