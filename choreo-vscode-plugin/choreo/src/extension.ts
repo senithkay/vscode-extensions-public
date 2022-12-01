34441b7cdea1577d17455c452649cabf21c0fb5f
Kavith Lokuhewage <kaviththiranga@gmail.com>
2022-12-01 08:57:47 +0530

    Add projects and accout views to Choreo Ext

--- choreo-vscode-plugin/choreo/src/extension.ts
+++ choreo-vscode-plugin/choreo/src/extension.ts
@@ -1,12 +1,19 @@
 import * as vscode from 'vscode';
+import { window } from 'vscode';
+import { ProjectsTreeProvider } from './views/projects-tree-provider';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('choreo.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('choreo.sign.in', () => {
 		vscode.window.showInformationMessage('Hello World from WSO2 Choreo!');
 	});
-
 	context.subscriptions.push(disposable);
+
+	const choreoResourcesProvider = new ProjectsTreeProvider();
+    window.createTreeView('choreo-resources', {
+        treeDataProvider: choreoResourcesProvider, showCollapseAll: true
+    });
+	
 }
 
 export function deactivate() {}
