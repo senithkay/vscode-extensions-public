8f350dc13673d9e80063f30555a2f7c7f0384d48
Kanushka Gayan <kanushkanet@gmail.com>
2022-12-16 09:26:07 +0530

    Fix typo error

--- ballerina-low-code-editor/statement-editor/src/components/EditorOverlay/index.tsx
+++ ballerina-low-code-editor/statement-editor/src/components/EditorOverlay/index.tsx
@@ -46,7 +46,7 @@ export function EditorOverlay(props: EditorOverlayProps) {
                 <div className={overlayClasses.loadingWrapper}>
                     <PullingModuleImg />
                     <p className={classes.title}>Pulling packages</p>
-                    <p className={classes.subtitle}>It takes some tim</p>
+                    <p className={classes.subtitle}>It takes some time</p>
                 </div>
             )}
         </div>
