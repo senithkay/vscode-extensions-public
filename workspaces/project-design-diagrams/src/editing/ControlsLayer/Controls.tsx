39fa39ba65f30010682ebb4bd2f502574fdc8ba8
SumayyaZiyad <github.sumayyaziyad@gmail.com>
2023-01-15 08:38:10 +0530

    Refactor the footer controls with the prompt screen

--- workspaces/project-design-diagrams/src/editing/ControlsLayer/Controls.tsx
+++ workspaces/project-design-diagrams/src/editing/ControlsLayer/Controls.tsx
@@ -19,36 +19,62 @@
 
 import React from 'react';
 import AddIcon from '@mui/icons-material/Add';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import Fab from '@mui/material/Fab';
 import styled from '@emotion/styled';
 import { Colors } from '../../resources';
+import { SxProps } from '@mui/material';
 
 const Container = styled.div`
-    position: absolute;
+    position: ${(props: {letFloat: boolean}) => props.letFloat ? 'relative' : 'absolute'};
     bottom: 15px;
     left: 15px;
 `;
 
-export function AddButton(props: { onClick: () => void }) {
-    const { onClick } = props;
+const ButtonStyles: SxProps = {
+    backgroundColor: Colors.PRIMARY,
+    borderRadius: '2px',
+    color: 'white',
+    fontSize: '12px',
+    marginInline: '2.5px',
+    '&:hover': {
+        backgroundColor: '#4958ba'
+    }
+};
+
+interface ControlsProps {
+    onComponentAddClick: () => void;
+    isChoreoProject?: boolean;
+    float?: boolean;
+}
+
+export function ControlsLayer(props: ControlsProps) {
+    const { onComponentAddClick, isChoreoProject, float } = props;
 
     return (
-        <Container onClick={onClick}>
+        <Container letFloat={float}>
             <Fab
                 aria-label='add'
                 variant='extended'
-                sx={{
-                    backgroundColor: Colors.PRIMARY,
-                    borderRadius: '2px',
-                    color: 'white',
-                    '&:hover': {
-                        backgroundColor: '#4958ba'
-                    }
-                }}
+                size='small'
+                onClick={onComponentAddClick}
+                sx={ButtonStyles}
             >
                 <AddIcon sx={{ marginRight: '5px' }} />
                 Component
             </Fab>
+
+            {isChoreoProject &&
+                <Fab
+                    aria-label='overview'
+                    variant='extended'
+                    size='small'
+                    sx={ButtonStyles}
+                >
+                    <AccountTreeIcon sx={{ marginRight: '5px' }} />
+                    Overview
+                </Fab>
+            }
         </Container>
     )
 }
