dedd4580f547c99859ce0184ce0aa0135b1190b5
CharukaK <charukakarunanayake@gmail.com>
2023-01-31 22:09:05 +0530

    Fix lint issues

--- workspaces/low-code-editor/src/DiagramViewManager/index.tsx
+++ workspaces/low-code-editor/src/DiagramViewManager/index.tsx
@@ -187,7 +187,6 @@ export function DiagramViewManager(props: EditorProps) {
     }, [diagramFocus])
 
     const updateSelectedComponent = (componentDetails: ComponentViewInfo) => {
-        console.log('component details >>>', componentDetails)
         const { filePath, position } = componentDetails;
         (async () => {
             try {
