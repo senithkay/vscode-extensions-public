464474e4b7600aa7d3bcd60ec9af89284f47cc18
gigara <info@gigara.info>
2023-02-10 10:39:48 +0530

    Remove choreo section from ui-test

--- ui-test/util.ts
+++ ui-test/util.ts
@@ -34,9 +34,7 @@ export async function getDiagramExplorer() {
     // test tree views
     expect(await sideBar.getTitlePart().getTitle()).is.equal("BALLERINA LOW-CODE: DIAGRAM EXPLORER");
     const diagramExplorer = await sideBar.getContent().getSection('Diagram Explorer') as CustomTreeSection;
-    const choreoLogin = await sideBar.getContent().getSection('Choreo') as CustomTreeSection;
     expect(diagramExplorer).is.not.undefined;
-    expect(choreoLogin).is.not.undefined;
     await wait(5000);
     return diagramExplorer;
 }
