e843103a45cda21c2d69315acfe49b07cb5c40ef
Madusha <madushajg@gmail.com>
2023-02-16 09:40:11 +0530

    Code cleanup

--- ballerina-low-code-editor/data-mapper/src/components/Diagram/Link/model/DataMapperLink.ts
+++ ballerina-low-code-editor/data-mapper/src/components/Diagram/Link/model/DataMapperLink.ts
@@ -1,4 +1,4 @@
-import { BezierCurve, Point } from "@projectstorm/geometry";
+import { BezierCurve } from "@projectstorm/geometry";
 import { DefaultLinkModel } from "@projectstorm/react-diagrams";
 import { STNode } from "@wso2-enterprise/syntax-tree";
 import { Diagnostic } from "vscode-languageserver-protocol";
@@ -32,19 +32,16 @@ export class DataMapperLinkModel extends DefaultLinkModel {
 	getSVGPath(): string {
 		if (this.points.length === 2) {
 			const curve = new BezierCurve();
-			const firstPoint =  new Point(this.getFirstPoint().getPosition().x + 8,
-				this.getFirstPoint().getPosition().y)
-			const lastPoint =  new Point(this.getLastPoint().getPosition().x - 8, this.getLastPoint().getPosition().y)
+			curve.setSource(this.getFirstPoint().getPosition());
+			curve.setTarget(this.getLastPoint().getPosition());
 
-			curve.setSource(firstPoint);
-			curve.setTarget(lastPoint);
-			if (this.sourcePort instanceof IntermediatePortModel){
-				curve.setSourceControl(firstPoint);
-				curve.setTargetControl(lastPoint);
+			if (this.sourcePort instanceof IntermediatePortModel) {
+				curve.setSourceControl(this.getFirstPoint().getPosition());
+				curve.setTargetControl(this.getLastPoint().getPosition());
 			} else {
-				const srcControl = firstPoint.clone();
+				const srcControl = this.getFirstPoint().getPosition().clone();
 				srcControl.translate(220, 0);
-				const targetControl = lastPoint.clone();
+				const targetControl = this.getLastPoint().getPosition().clone();
 				targetControl.translate(-220, 0);
 				curve.setSourceControl(srcControl);
 				curve.setTargetControl(targetControl);
