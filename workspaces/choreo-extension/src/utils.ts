0f9fe458b4a3bb296489d5f020b09785e0c4d7e7
SumayyaZiyad <github.sumayyaziyad@gmail.com>
2023-02-17 16:31:23 +0530

    Refactor component enriching util

--- workspaces/choreo-extension/src/utils.ts
+++ workspaces/choreo-extension/src/utils.ts
@@ -13,42 +13,40 @@
 
 import { ApiVersion, Service } from "@wso2-enterprise/choreo-core";
 
-export function enrichDeploymentData(services: Map<string, Service>, apiVersions: ApiVersion[], componentLocation: string,
+export function enrichDeploymentData(pkgServices: Map<string, Service>, apiVersions: ApiVersion[], componentLocation: string,
     isLocal: boolean, accessibility?: string): boolean {
-    for (const service of services.values()) {
-        // Checks whether both Choreo and local paths are same
-        if (service.elementLocation.filePath.includes(componentLocation)) {
-            let isInternetExposed = false;
-            let isIntranetExposed = false;
-            if (!isLocal && apiVersions.length > 0) {
-                apiVersions.forEach((version: ApiVersion) => {
-                    if (version.accessibility === "internal") {
-                        isIntranetExposed = true;
-                    }
-                    if (version.accessibility === "external") {
-                        isInternetExposed = true;
-                    }
-                });
-            } else if (isLocal) {
-                if (accessibility === "internal") {
+    const services = [...pkgServices.values()];
+    const componentServices = services.filter((service) => { service.elementLocation.filePath.includes(componentLocation) });
+    for (const service of componentServices) {
+        let isInternetExposed = false;
+        let isIntranetExposed = false;
+        if (!isLocal && apiVersions.length > 0) {
+            apiVersions.forEach((version: ApiVersion) => {
+                if (version.accessibility === "internal") {
                     isIntranetExposed = true;
                 }
-                if (accessibility === "external") {
+                if (version.accessibility === "external") {
                     isInternetExposed = true;
                 }
+            });
+        } else if (isLocal) {
+            if (accessibility === "internal") {
+                isIntranetExposed = true;
             }
-            service.deploymentMetadata = {
-                gateways: {
-                    internet: {
-                        isExposed: isInternetExposed
-                    },
-                    intranet: {
-                        isExposed: isIntranetExposed
-                    }
-                }
-            };
-            return true;
+            if (accessibility === "external") {
+                isInternetExposed = true;
+            }
         }
+        service.deploymentMetadata = {
+            gateways: {
+                internet: {
+                    isExposed: isInternetExposed
+                },
+                intranet: {
+                    isExposed: isIntranetExposed
+                }
+            }
+        };
     }
-    return false;
+    return componentServices.length > 0;
 }
