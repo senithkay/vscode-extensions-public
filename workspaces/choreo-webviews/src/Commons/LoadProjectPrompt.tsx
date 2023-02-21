c2b0bfe7f99e7cc1d1e6b1f14aca23c7649744b8
SumayyaZiyad <github.sumayyaziyad@gmail.com>
2023-02-21 10:36:19 +0530

    Add warning screen for non-Choreo workspace component creation

---
+++ workspaces/choreo-webviews/src/Commons/LoadProjectPrompt.tsx
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+ * 
+ *  This software is the property of WSO2 LLC. and its suppliers, if any.
+ *  Dissemination of any information or reproduction of any material contained
+ *  herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ *  the WSO2 Commercial License available at http://wso2.com/licenses.
+ *  For specific language governing the permissions and limitations under
+ *  this license, please see the license as well as any agreement you’ve
+ *  entered into with WSO2 governing the purchase of this software and any
+ *  associated services.
+ */
+
+import { css, cx } from "@emotion/css";
+import styled from "@emotion/styled";
+
+const codiconStyles = css`
+    color: var(--vscode-editorWarning-foreground);
+    margin-right: 6px;
+    vertical-align: middle;
+    zoom: 2;
+`;
+
+const Container = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+export function LoadChoreoProjectPrompt() {
+    return (
+        <Container>
+            <i className={`codicon codicon-warning ${cx(codiconStyles)}`} />
+            <p>Load a Choreo project workspace to create components.</p>
+        </Container>
+    );
+}
