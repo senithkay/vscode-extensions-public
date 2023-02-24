eba3afaa9da4e9d083b1d1a3c74b185592590d95
gigara <info@gigara.info>
2023-02-24 16:19:21 +0530

    Add package.json

--- .github/actions/cache/restore.js
+++ .github/actions/cache/restore.js
@@ -1,8 +1,5 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
 const cache = require('@actions/cache');
-const fs = require('fs');
-const path = require('path');
 
 import { pnpmCacheKey, pnpmCacheDir, rushCacheKey, rushCacheDir, rushSysCacheDir } from "./constants";
 
