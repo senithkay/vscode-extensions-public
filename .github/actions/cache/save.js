39a0845b0981584063b0496e52a9c519cee052e5
gigara <info@gigara.info>
2023-02-26 13:00:28 +0530

    debug

--- .github/actions/cache/save.js
+++ .github/actions/cache/save.js
@@ -1,12 +1,23 @@
 const core = require('@actions/core');
+const glob = require('@actions/glob');
+const path = require('path');
 // const cache = require('@actions/cache');
-const { consts } = require("./constants");
+// const { consts } = require("./constants");
 
 async function run() {
   try {
-    const { pnpmCacheDir, rushCacheDir, rushSysCacheDir } = await consts;
-    const pnpmCacheKey = core.getState("pnpmCacheKey");
-    const rushCacheKey = core.getState("rushCacheKey");
+const projectPath = "./";
+    
+    const rushCacheKey = `rush-${process.platform}-` + (await glob.hashFiles("rush.json"));
+    // const rushCacheDir = path.join(projectPath, 'common', 'temp', 'install-run');
+    // const rushBuildCacheDir = path.join(projectPath, 'common', 'temp', 'build-cache');
+    // const rushSysCacheDir = path.join(process.env.HOME, '.rush');
+
+    const pnpmCacheDir =" path.join(projectPath, 'common', 'temp', 'pnpm-store')";
+    const pnpmCacheKey = `pnpm-${process.platform}-` + (await glob.hashFiles("**/pnpm-lock.yaml"));
+
+    // const pnpmCacheKey = core.getState("pnpmCacheKey");
+    // const rushCacheKey = core.getState("rushCacheKey");
     const pnpmCacheExists = core.getState("pnpmCacheExists");
     const rushCacheExists = core.getState("rushCacheExists");
 
