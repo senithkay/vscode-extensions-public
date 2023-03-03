08732ff5190b3120410d8b37c20169044bfe7611
gigara <info@gigara.info>
2023-03-03 14:22:44 +0530

    Add keys

--- .github/actions/env/src/index.js
+++ .github/actions/env/src/index.js
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    core.exportVariable(key, process.env["ACTIONS_CACHE_URL"]);
-    core.exportVariable(key, process.env["ACTIONS_RUNTIME_TOKEN"]);
+    core.exportVariable("ACTIONS_CACHE_URL", process.env["ACTIONS_CACHE_URL"]);
+    core.exportVariable("ACTIONS_RUNTIME_TOKEN", process.env["ACTIONS_RUNTIME_TOKEN"]);
   } catch (error) {
     core.setFailed(error.message);
   }
