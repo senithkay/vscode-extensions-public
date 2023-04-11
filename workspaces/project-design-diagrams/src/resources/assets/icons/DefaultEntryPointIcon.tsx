3a6e31de03fcca8c792b2622285d4893a6fc9560
SumayyaZiyad <github.sumayyaziyad@gmail.com>
2023-04-11 11:41:53 +0530

    UI updates for trigger differentiation

--- workspaces/project-design-diagrams/src/resources/assets/icons/DefaultEntryPointIcon.tsx
+++ workspaces/project-design-diagrams/src/resources/assets/icons/DefaultEntryPointIcon.tsx
@@ -1,20 +1,26 @@
-/*
- * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+/**
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com) All Rights Reserved.
  *
- * This software is the property of WSO2 Inc. and its suppliers, if any.
- * Dissemination of any information or reproduction of any material contained
- * herein is strictly forbidden, unless permitted by WSO2 in accordance with
- * the WSO2 Commercial License available at http://wso2.com/licenses.
- * For specific language governing the permissions and limitations under
- * this license, please see the license as well as any agreement you’ve
- * entered into with WSO2 governing the purchase of this software and any
- * associated services.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
  */
 
 import React from 'react';
 import { Colors } from '../../model';
 
-export function EntryPointIcon() {
+export function DefaultEntryPointIcon() {
     return (
         <svg width="16px" height="14px" viewBox="0 0 16 14" version="1.1" className={"sub-menu-dark-fill"}>
             <g id="Home" stroke="none" strokeWidth="1" fill={Colors.PRIMARY} fillRule="evenodd">
@@ -25,5 +31,5 @@ export function EntryPointIcon() {
                 </g>
             </g>
         </svg>
-    )
+    );
 }
