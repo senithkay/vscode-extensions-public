12bb98f2ab7c06e68bf38c439962dc6d9d1d50a2
Madusha <madushajg@gmail.com>
2023-06-02 15:35:46 +0530

    Add mocked editor props

--- workspaces/low-code-editor/src/tests/utils/test-utils.ts
+++ workspaces/low-code-editor/src/tests/utils/test-utils.ts
@@ -10,6 +10,7 @@
 * entered into with WSO2 governing the purchase of this software and any
 * associated services.
 */
+import { jest } from "@jest/globals";
 import { BalleriaLanguageClient } from "@wso2-enterprise/ballerina-languageclient";
 
 import { EditorProps } from "../../DiagramGenerator/vscode/Diagram";
@@ -19,32 +20,33 @@ export function getEditorProps(fileUri: string, langClient: BalleriaLanguageClie
         diagramFocus: undefined,
         experimentalEnabled: false,
         filePath: fileUri,
-        getAllFiles: jest.fn(),
-        getBallerinaVersion: () => Promise.resolve(process.env.BAL_VERSION || "2201.6.0"),
-        getEnv: jest.fn(),
-        getFileContent: jest.fn(),
-        getLibrariesData: jest.fn(),
-        getLibrariesList: jest.fn(),
-        getLibraryData: jest.fn(),
-        getPerfDataFromChoreo: jest.fn(),
-        getSentryConfig: jest.fn(),
-        gotoSource: jest.fn(),
         langClientPromise: Promise.resolve(langClient),
         lastUpdatedAt: "",
-        openArchitectureView: jest.fn(),
-        openExternalUrl: jest.fn(),
-        openInDiagram: undefined,
         projectPaths: [],
-        resolveMissingDependency: jest.fn(),
-        resolveMissingDependencyByCodeAction: jest.fn(),
-        runBackgroundTerminalCommand: jest.fn(),
-        runCommand: jest.fn(),
-        sendTelemetryEvent: jest.fn(),
-        showMessage: jest.fn(),
-        showPerformanceGraph: jest.fn(),
         startColumn: 0,
         startLine: 0,
-        updateFileContent: jest.fn(),
-        workspaceName: ""
+        workspaceName: "",
+        openInDiagram: undefined,
+        getAllFiles: jest.fn(() => Promise.resolve([])),
+        getBallerinaVersion: jest.fn(() => Promise.resolve(undefined)),
+        getEnv: jest.fn(() => Promise.resolve()),
+        getFileContent: jest.fn(() => Promise.resolve("")),
+        getLibrariesData: jest.fn(() => Promise.resolve(undefined)),
+        getLibrariesList: jest.fn(() => Promise.resolve(undefined)),
+        getLibraryData: jest.fn(() => Promise.resolve(undefined)),
+        getPerfDataFromChoreo: jest.fn(() => Promise.resolve(undefined)),
+        getSentryConfig: jest.fn(() => Promise.resolve(undefined)),
+        gotoSource: jest.fn(() => Promise.resolve(false)),
+        openArchitectureView: jest.fn(() => Promise.resolve(false)),
+        openExternalUrl: jest.fn(() => Promise.resolve(false)),
+        renameSymbol: jest.fn(() => Promise.resolve(false)),
+        resolveMissingDependency: jest.fn(() => Promise.resolve(undefined)),
+        resolveMissingDependencyByCodeAction: jest.fn(() => Promise.resolve(false)),
+        runBackgroundTerminalCommand: jest.fn(() => Promise.resolve(undefined)),
+        runCommand: jest.fn(() => Promise.resolve(false)),
+        sendTelemetryEvent: jest.fn(() => Promise.resolve(undefined)),
+        showMessage: jest.fn(() => Promise.resolve(false)),
+        showPerformanceGraph: jest.fn(() => Promise.resolve(false)),
+        updateFileContent: jest.fn(() => Promise.resolve(false)),
     };
 }
