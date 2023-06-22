9cd2e2f6583aad3175fee6b36664ee4a7106545c
gigara <info@gigara.info>
2023-06-22 09:55:27 +0530

    Fix lint

--- workspaces/low-code-editor/src/assets/icons/Package.tsx
+++ workspaces/low-code-editor/src/assets/icons/Package.tsx
@@ -5,7 +5,8 @@
  * Dissemination of any information or reproduction of any material contained
  * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
  * You may not alter or remove any copyright or other notice from copies of this content.
- */import React from 'react';
+ */
+import React from 'react';
 
 interface PackageIconProps {
     fillColor?: string;
