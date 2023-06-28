3c3d12b17c8c15bc6cfe052011742a9bf09cf392
Kavith Lokuhewage <kaviththiranga@gmail.com>
2023-06-28 13:05:58 +0530

    Use async loading UX for project list
    When user opens up vscode, it might take
    a couple of seconds until the project list is
    retrieved.
    
    If user quickly opens project switch quick
    pick, it will be stuck for couple of seconds.
    This will fix it.

--- workspaces/choreo-extension/src/cmds/open-project.ts
+++ workspaces/choreo-extension/src/cmds/open-project.ts
@@ -17,6 +17,7 @@ import { ext } from '../extensionVariables';
 import { ProjectRegistry } from '../registry/project-registry';
 import { cloneProject } from './clone';
 import path = require('path');
+import { Project } from '@wso2-enterprise/choreo-core';
 
 export function activateOpenProjectCmd(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand(openProjectCmdId, () => {
@@ -25,13 +26,81 @@ export function activateOpenProjectCmd(context: vscode.ExtensionContext) {
 }
 
 export async function openChoreoProject() {
+
+    const quickPickInstance = vscode.window.createQuickPick();
+    quickPickInstance.busy = true;
+    quickPickInstance.placeholder = 'Loading projects...';
+    quickPickInstance.show();
+
+    const isLoggedIn = await ext.api.waitForLogin();
     // show a message if user is not logged in
-    if (ext.api.status !== 'LoggedIn') {
+    if (!isLoggedIn) {
         vscode.window.showInformationMessage('You are not logged in. Please log in to continue.');
+        quickPickInstance.hide();
         return;
     }
-    const projects = await ProjectRegistry.getInstance().getProjects(ext.api.selectedOrg?.id!);
+
     const currentProject = await ext.api.getChoreoProject();
+    const { quickPicks, projects } = await getProjectQuickPicks(currentProject);
+
+    quickPickInstance.busy = false;
+    quickPickInstance.placeholder = 'Select a project to continue';
+    quickPickInstance.items = quickPicks;
+
+    quickPickInstance.onDidAccept(async () => {
+        quickPickInstance.hide();
+        const selection = quickPickInstance.selectedItems[0];
+        // show project creation wizard if user selects the last item
+        if (selection?.label === 'Create new') {
+            vscode.commands.executeCommand(createNewProjectCmdId);
+            return;
+        }
+        // if the selected project is already opened, show a message and return
+        if (selection?.label === currentProject?.name) {
+            vscode.window.showInformationMessage('The project is already opened in current window.');
+            return;
+        }
+
+        const selectedProject = projects.find(project => project.name === selection?.label);
+        if (selectedProject) {
+            const projectLocation = ProjectRegistry.getInstance().getProjectLocation(selectedProject.id);
+            if (projectLocation) {
+                // ask user where to open the project, current window or a new window
+                const openInCurrentWorkspace = await vscode.window.showInformationMessage(
+                    'Where do you want to open the project?',
+                    {
+                        modal: true,
+                    },
+                    'Current Window',
+                    'New Window',
+                );
+                if (openInCurrentWorkspace === 'Current Window') {
+                    await vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(projectLocation), {
+                        forceNewWindow: false,
+                    });
+                } else if (openInCurrentWorkspace === 'New Window') {
+                    await vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(projectLocation), {
+                        forceNewWindow: true,
+                    });
+                }
+            } else {
+                // Project is not cloned yet, clone the project and open it
+                // show a quick pick to ask user whether to clone the project or not
+                const cloneSelection = await vscode.window.showQuickPick([
+                    { label: 'Select folder to clone the project' },
+                    { label: 'Cancel' },
+                ], { title: 'The project is not cloned yet. Do you want to clone and open it?' });
+                if (cloneSelection?.label === 'Select folder to clone the project') {
+                    cloneProject(selectedProject);
+                }
+            }
+        }
+
+    });
+}
+
+async function getProjectQuickPicks(currentProject: Project | undefined) {
+    const projects = await ProjectRegistry.getInstance().getProjects(ext.api.selectedOrg?.id!);
     const quickPicks: vscode.QuickPickItem[] = projects.map(project => {
         const currentlyOpened = currentProject?.id === project.id;
         const location = ProjectRegistry.getInstance().getProjectLocation(project.id);
@@ -57,58 +126,5 @@ export async function openChoreoProject() {
         label: '$(add) Create new',
         detail: 'Create and open a new Choreo project',
     });
-
-    // show a popup to select a project
-    const selection = await vscode.window.showQuickPick(quickPicks, {
-        title: 'Select a project to continue',
-        canPickMany: false,
-        matchOnDescription: true,
-        matchOnDetail: false,
-    });
-
-    // show project creation wizard if user selects the last item
-    if (selection?.label === 'Create new') {
-        vscode.commands.executeCommand(createNewProjectCmdId);
-        return;
-    }
-    // if the selected project is already opened, show a message and return
-    if (selection?.label === currentProject?.name) {
-        vscode.window.showInformationMessage('The project is already opened in current window.');
-        return;
-    }
-
-    const selectedProject = projects.find(project => project.name === selection?.label);
-    if (selectedProject) {
-        const projectLocation = ProjectRegistry.getInstance().getProjectLocation(selectedProject.id);
-        if (projectLocation) {
-            // ask user where to open the project, current window or a new window
-            const openInCurrentWorkspace = await vscode.window.showInformationMessage(
-                'Where do you want to open the project?',
-                {
-                    modal: true,
-                },
-                'Current Window',
-                'New Window',
-            );
-            if (openInCurrentWorkspace === 'Current Window') {
-                await vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(projectLocation), {
-                    forceNewWindow: false,
-                });
-            } else if (openInCurrentWorkspace === 'New Window') {
-                await vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(projectLocation), {
-                    forceNewWindow: true,
-                });
-            }
-        } else {
-            // Project is not cloned yet, clone the project and open it
-            // show a quick pick to ask user whether to clone the project or not
-            const cloneSelection = await vscode.window.showQuickPick([
-                {  label: 'Select folder to clone the project' },
-                {  label: 'Cancel' },
-            ], { title: 'The project is not cloned yet. Do you want to clone and open it?' });
-            if (cloneSelection?.label === 'Select folder to clone the project') {
-                cloneProject(selectedProject);
-            }
-        }
-    }
+    return { quickPicks, projects };
 }
