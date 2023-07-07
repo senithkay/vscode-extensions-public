f4b44aa38752e30593f438dffbf6bb1c3dd07f43
tharindulak <tharindu.jayathilake1@gmail.com>
2023-07-07 15:33:13 +0530

    Remove cell view navigation from Arch diagram

--- workspaces/project-design-diagrams/src/components/common/Header/TypeSelector/DiagramTypeSelector.tsx
+++ workspaces/project-design-diagrams/src/components/common/Header/TypeSelector/DiagramTypeSelector.tsx
@@ -23,7 +23,7 @@ import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import { DiagramContext } from '../../DiagramContext/DiagramContext';
 import { useStyles } from './styles';
-import { CellViewIcon, L1ServicesIcon, L2ServicesIcon, TypesDiagramIcon, Views } from '../../../../resources';
+import { L1ServicesIcon, L2ServicesIcon, TypesDiagramIcon, Views } from '../../../../resources';
 
 const ViewTypePanel = styled.div`
     display: flex;
@@ -57,11 +57,6 @@ const switchables: ViewTypes[] = [
         type: Views.L2_SERVICES,
         label: 'Resources',
         icon: <L2ServicesIcon />
-    },
-    {
-        type: Views.CELL_VIEW,
-        label: 'Cell View',
-        icon: <CellViewIcon />
     },
     {
         type: Views.TYPE,
