abf593f4db429660d4eef990ba17905338ce15a9
kaumini <kauminikg@gmail.com>
2023-07-19 17:19:47 +0530

    Fix repo selector issue

--- workspaces/choreo-webviews/src/GithubRepoSelector/GithubRepoSelector.tsx
+++ workspaces/choreo-webviews/src/GithubRepoSelector/GithubRepoSelector.tsx
@@ -87,7 +87,7 @@ export function GithubRepoSelector(props: GithubRepoSelectorProps) {
     const handleGhOrgChange = (value: string) => {
         const org = filteredOrgs.find(org => org.orgName === value);
         if (org && org.repositories.length > 0) {
-            onRepoSelect(org.orgName, !!selectedRepo?.repo ? selectedRepo?.repo : org.repositories[0]?.name, selectedRepo?.branch);
+            onRepoSelect(org.orgName, org.repositories[0]?.name, selectedRepo?.branch);
         } else {
             onRepoSelect(org?.orgName);
         }
@@ -101,7 +101,7 @@ export function GithubRepoSelector(props: GithubRepoSelectorProps) {
         onRepoSelect(selectedOrg?.orgName, selectedRepo?.repo, value);
     };
 
-    if (selectedOrg) {
+    if (!selectedRepo.org) {
         handleGhOrgChange(selectedOrg.orgName);
     }
 
