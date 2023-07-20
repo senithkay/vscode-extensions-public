3c8087a702dc1edc9c5cdfcb17567c2d249d5f94
Anjana S Porawagama <axewilledge123@gmail.com>
2023-07-20 11:33:43 +0530

    Removed org restrictions

--- workspaces/ballerina-extension/src/utils/file-utils.ts
+++ workspaces/ballerina-extension/src/utils/file-utils.ts
@@ -34,19 +34,13 @@ export async function handleOpenFile(ballerinaExtInstance: BallerinaExtension, g
 
         try {
             progress.report({ message: "Verifying the gist file." });
-            const response = await axios.get(`https://api.github.com/gists/${gist}`);
-
-            const gistDetails = response.data;
-            const responseOwner = gistDetails.owner.login;
-            const validGist = gistOwner === responseOwner;
-
-            if (validGist && fileName.endsWith('.bal')) {
+            if (fileName.endsWith('.bal')) {
                 const rawFileLink = `https://gist.githubusercontent.com/${gistOwner}/${gist}/raw/${fileName}`;
                 const defaultDownloadsPath = path.join(os.homedir(), 'Downloads'); // Construct the default downloads path
                 const selectedPath = ballerinaExtInstance.getFileDownloadPath() || defaultDownloadsPath;
                 await updateDirectoryPath(selectedPath);
                 const filePath = path.join(selectedPath, fileName);
-                handleDownloadFile(rawFileLink, filePath, progress, cancelled);
+                await handleDownloadFile(rawFileLink, filePath, progress, cancelled);
             } else {
                 window.showErrorMessage(`Gist or the file is not valid.`);
                 return;
@@ -61,11 +55,6 @@ export async function handleOpenFile(ballerinaExtInstance: BallerinaExtension, g
 
 export async function handleOpenRepo(ballerinaExtInstance: BallerinaExtension, repoUrl: string) {
     try {
-        if (!repoUrl.includes("wso2")) {
-            const errorMsg = `Not a valid repository.`;
-            await window.showWarningMessage(errorMsg);
-            return;
-        }
         const defaultDownloadsPath = path.join(os.homedir(), 'Downloads'); // Construct the default downloads path
         const selectedPath = ballerinaExtInstance.getFileDownloadPath() || defaultDownloadsPath;
         await commands.executeCommand('git.clone', repoUrl, selectedPath);
@@ -158,20 +147,4 @@ async function openFileInVSCode(filePath: string): Promise<void> {
     } catch (error) {
         window.showErrorMessage(`Failed to open file: ${error}`);
     }
-}
-
-function getProjectFolderName(repoUrl) {
-    // Remove protocol, domain, and ".git" suffix
-    const path = repoUrl.replace(/^https?:\/\/[^/]+\/|\.git$/g, '');
-
-    // Extract the project folder
-    const folders = path.split('/');
-    const projectFolder = folders[folders.length - 1];
-
-    return projectFolder;
-}
-
-async function openDirectory(directoryPath) {
-    const uri = Uri.file(directoryPath);
-    await commands.executeCommand('vscode.openFolder', uri, false);
 }
