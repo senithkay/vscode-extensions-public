266f4dcd0d6a76844abadcd349cd2266f1528286
Joseph Fonseka <hevayo@gmail.com>
2023-08-04 14:17:07 +0530

    Implement xml import

--- workspaces/ballerina-extension/src/project/cmds/cmd-runner.ts
+++ workspaces/ballerina-extension/src/project/cmds/cmd-runner.ts
@@ -24,6 +24,7 @@ export enum PALETTE_COMMANDS {
     SAVE_ALL = 'workbench.action.files.saveFiles',
     TEST = 'ballerina.project.test',
     PASTE_JSON_AS_RECORD = 'ballerina.pasteAsRecord',
+    PASTE_XML_AS_RECORD = 'ballerina.pasteXMLAsRecord',
     CHOREO_SIGNIN = 'ballerina.choreo.signin',
     CHOREO_ANON_SIGNIN = 'ballerina.choreo.anonymous.signin',
     CHOREO_SIGNOUT = 'ballerina.choreo.signout',
