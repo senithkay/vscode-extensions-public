b94fd04d713263a70867f3508be3f03be4a89cb5
Anjana S Porawagama <axewilledge123@gmail.com>
2023-08-09 14:05:01 +0530

    Enabled all tests

--- workspaces/ballerina-extension/ui-test/sampleFileOpen.test.ts
+++ workspaces/ballerina-extension/ui-test/sampleFileOpen.test.ts
@@ -14,7 +14,7 @@ import { join } from 'path';
 import { By, EditorView, Key, VSBrowser, WebDriver } from 'vscode-extension-tester';
 import { clickDialogButton, wait } from './util';
 
-describe.only('Open ballerina samples in VSCode from URL', () => {
+describe('Open ballerina samples in VSCode from URL', () => {
     const PROJECT_ROOT = join(__dirname, '..', '..', 'ui-test', 'data');
     let browser: VSBrowser;
     let driver: WebDriver;
