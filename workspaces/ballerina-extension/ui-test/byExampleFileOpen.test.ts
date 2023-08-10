62838b760ba7cfd02799c2f86a5fb4fde0b0c488
Anjana S Porawagama <axewilledge123@gmail.com>
2023-08-10 17:03:00 +0530

    Updated tests for vscode file open

--- workspaces/ballerina-extension/ui-test/byExampleFileOpen.test.ts
+++ workspaces/ballerina-extension/ui-test/byExampleFileOpen.test.ts
@@ -11,9 +11,8 @@ import { expect } from 'chai';
 import { existsSync, mkdirSync, rmSync } from 'fs';
 import { before, describe, it } from 'mocha';
 import { join } from 'path';
-import { By, EditorView, Key, VSBrowser, WebDriver } from 'vscode-extension-tester';
-import { wait } from './util';
-import os from 'os';
+import { By, EditorView, InputBox, Key, VSBrowser, WebDriver } from 'vscode-extension-tester';
+import { waitUntil } from './util';
 
 describe('Open ballerina samples in VSCode from URL', () => {
     const PROJECT_ROOT = join(__dirname, '..', '..', 'ui-test', 'data');
@@ -25,57 +24,51 @@ describe('Open ballerina samples in VSCode from URL', () => {
     before(async () => {
         // Delete files and folders in this folder
         rmSync(samplesDownloadDirectory, { recursive: true, force: true });
-        await wait(2000);
         // Create folder if not present
         if (!existsSync(samplesDownloadDirectory)) {
             mkdirSync(samplesDownloadDirectory);
         }
-
         browser = VSBrowser.instance;
         driver = browser.driver;
-        // Wait till vscode instance starts
-        await wait(8000);
-        // Close all open tabs
+        await browser.openResources(samplesDownloadDirectory, samplesDownloadDirectory);
         await new EditorView().closeAllEditors();
     });
 
-    it('Open URL to download first example on first time and change directory', async () => {
-        // Use Developer URL to excecute a URL
+    it('Open URL to download first sample on first time and change directory', async () => {
+
+        // Use Developer URL to execute a URL
         const url = 'vscode://wso2.ballerina/open-file?gist=18e6c62b7ef307d7064ed4ef39e4d0d8&file=functions.bal';
         await executeURLdownload(driver, url);
 
-        // Find the information message boxes for change direcotory
-        const changePathBtns = await driver.findElements(By.linkText('Change Directory'));
-        // Iterate over the information message boxes
-        for (const button of changePathBtns) {
-            await button.click();
-        }
-        await wait(3000);
+        // Open Downloaded file
+        const openFile = await waitUntil(By.linkText('Open'));
+        await openFile.click();
 
-        await driver.actions()
-            .keyDown(os.platform() === 'darwin' ? Key.COMMAND : Key.CONTROL)
-            .sendKeys('a')
-            .keyUp(os.platform() === 'darwin' ? Key.COMMAND : Key.CONTROL)
-            .sendKeys(Key.DELETE)
-            .sendKeys(samplesDownloadDirectory)
-            .perform();
+        // Click on change directory
+        const changePathBtn = await waitUntil(By.linkText('Change Directory'));
+        await changePathBtn.click();
 
-        await wait(1000);
+        // Wait for OK button to be appeared
+        const okButton = await waitUntil(By.linkText('OK'));
 
-        await driver.actions().sendKeys(Key.ENTER).perform();
+        const input = await InputBox.create();
 
-        await wait(3000);
+        // Set the new downloads path
+        await input.clear();
 
-        // Find the path input boxes
-        const inputs = await driver.findElements(By.linkText('OK'));
-        // Iterate over the path input boxes
-        for (const input of inputs) {
-            await input.click();
-        }
-        await wait(3000);
+        await input.setText(samplesDownloadDirectory);
+
+        // Save the new download path
+        await okButton.click();
 
         // Check if the file has been downloaded to the new location
         await executeURLdownload(driver, url);
+
+        // Open Downloaded file
+        const openFileSecond = await waitUntil(By.linkText('Open'));
+        await openFileSecond.click();
+        await waitUntil(By.linkText('Change Directory'));
+
         expect(existsSync(`${samplesDownloadDirectory}/functions.bal`), "Second assert with functions.bal").to.be.true;
 
     });
@@ -84,21 +77,52 @@ describe('Open ballerina samples in VSCode from URL', () => {
         // Use Developer URL to excecute a URL
         const url = 'vscode://wso2.ballerina/open-file?gist=8ada14df03d5d8841d03ce4b92819b2b&file=hello_world.bal';
         await executeURLdownload(driver, url);
+        // Open Downloaded file
+        const openFile = await waitUntil(By.linkText('Open'));
+        await openFile.click();
+        await waitUntil(By.linkText('Change Directory'));
+
         expect(existsSync(`${samplesDownloadDirectory}/hello_world.bal`)).to.be.true;
+    });
+
+    it('Open URL to download a not valid sample file', async () => {
+        // Use Developer URL to excecute a URL
+        const url = 'vscode://wso2.ballerina/open-file?gist=1b94f48ad579969bc7c6a79549684dca&file=PeopleManagementService.bal';
+        await executeURLdownload(driver, url);
+        expect(existsSync(`${samplesDownloadDirectory}/PeopleManagementService.bal`)).to.be.not.true;
+    });
+
+    it('Open URL to download github sample file', async () => {
+        // Use Developer URL to excecute a URL
+        const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/wso2/choreo-sample-apps/blob/main/ballerina/greeter/service.bal';
+        await executeURLdownload(driver, url);
+
+        // Open Downloaded file
+        const openFile = await waitUntil(By.linkText('Open'));
+        await openFile.click();
+        await waitUntil(By.linkText('Change Directory'));
+
+        expect(existsSync(`${samplesDownloadDirectory}/service.bal`)).to.be.true;
+    });
+
+    it('Open URL to download not valid github sample file', async () => {
+        // Use Developer URL to excecute a URL
+        const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/jclark/semtype/blob/master/main.bal';
+        await executeURLdownload(driver, url);
+        expect(existsSync(`${samplesDownloadDirectory}/main.bal`)).to.be.not.true;
     });
 
     it('Open URL to download git repo', async () => {
         // Use Developer URL to excecute a URL
         const url = 'vscode://wso2.ballerina/open-repo?repoUrl=https://github.com/wso2/choreo-sample-apps';
         await executeURLdownload(driver, url);
-        await wait(8000);
 
-        // Find the information message boxes for the file download verification
-        const vscodeVerify = await driver.findElement(By.className('monaco-dialog-box')).findElements(By.linkText('Cancel'));
-        // Iterate over the information message boxes
-        for (const infoNotification of vscodeVerify) {
-            await infoNotification.click();
-        }
+        // Confirm Clone
+        const openFile = await waitUntil(By.linkText('Clone Now'));
+        await openFile.click();
+
+        const cloneDialog = await waitUntil(By.className('monaco-dialog-box'), 30000).findElement(By.linkText('Cancel'));
+        await cloneDialog.click();
 
         expect(existsSync(`${samplesDownloadDirectory}/choreo-sample-apps/README.md`)).to.be.true;
     });
@@ -109,17 +133,12 @@ describe('Open ballerina samples in VSCode from URL', () => {
 const executeURLdownload = async (driver, url: string) => {
     // Send keyboard shortcut to open the command palette
     await driver.actions().sendKeys(Key.F1).perform();
-    await wait(2000);
     // Simulate entering the search query in the command palette
     const searchQuery = 'Open URL';
-    await driver.actions().sendKeys(searchQuery, Key.ENTER).sendKeys(url, Key.ENTER).perform();
-
-    await wait(2000);
-    // Find the information message boxes for the file download verification
-    const vscodeVerify = await driver.findElement(By.className('monaco-dialog-box')).findElements(By.linkText('Open'));
-    // Iterate over the information message boxes
-    for (const infoNotification of vscodeVerify) {
-        await infoNotification.click();
-    }
-    await wait(6000);
+    const commandInput = await InputBox.create();
+    await commandInput.sendKeys(searchQuery, Key.ENTER);
+    await commandInput.sendKeys(url, Key.ENTER);
+    // URL Open verification
+    const vscodeVerify = await waitUntil(By.className('monaco-dialog-box')).findElement(By.linkText('Open'));
+    await vscodeVerify.click();
 }
