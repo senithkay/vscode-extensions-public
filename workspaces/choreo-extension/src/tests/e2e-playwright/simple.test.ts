7835bf2e8ed42f3c6f7021960cb6c04c084df339
Kavith Lokuhewage <kaviththiranga@gmail.com>
2023-08-11 10:02:59 +0530

    Implement playwright based tests for Choreo Ext

--- workspaces/choreo-extension/src/tests/e2e-playwright/simple.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, BrowserContext } from '@playwright/test';
-
-export const wait = async (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-test('has title', async ({ browser, page }) => {
-    wait(100000);
-    const contexts = browser.contexts();
-    console.log(contexts);
-});
