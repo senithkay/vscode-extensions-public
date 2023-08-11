9c70d174578647950104c3c185402c39e041a1c4
Kavith Lokuhewage <kaviththiranga@gmail.com>
2023-08-11 14:30:12 +0530

    Integrate mocha runner with playwright tests

--- workspaces/choreo-extension/src/tests/e2e-playwright/launch.ts
+++ workspaces/choreo-extension/src/tests/e2e-playwright/launch.ts
@@ -1,12 +1,11 @@
-    import { _electron } from 'playwright';
+import { _electron } from 'playwright';
 import { getCypressBrowser, getCypressBrowserOptions } from "@wso2-enterprise/cypress-vscode-runner";
 
 import path = require('path');
 import { ReleaseQuality } from '@wso2-enterprise/cypress-vscode-runner/out/codeUtil';
-import { ChoreoActivity } from './components/ActivityBar';
 
 const resourcesFolder = path.join(__dirname, '..', '..', '..', 'test-resources');
-const vscodeVersion = '1.81.0';
+const vscodeVersion = '1.81.1';
 
 export const startVSCode = async () => {
     const browser = await getCypressBrowser(resourcesFolder, vscodeVersion, ReleaseQuality.Stable);
@@ -16,7 +15,6 @@ export const startVSCode = async () => {
         executablePath: browser.path,
         args: browserOptions.args,
         env: browserOptions.env,
-
     });
 
     // Get the first window that the app opens, wait if necessary.
@@ -31,14 +29,3 @@ export const startVSCode = async () => {
     await window.locator('.monaco-workbench').waitFor({ state: 'visible' });
     return vscode;
 };
-
-const firstTest = async () => {
-    const vscode = await startVSCode();
-    const page = await vscode.firstWindow();
-
-    const choreoActivity = new ChoreoActivity(page);
-    await choreoActivity.activate();
-};
-
-// TODO use mocha for implementing tests
-firstTest();
