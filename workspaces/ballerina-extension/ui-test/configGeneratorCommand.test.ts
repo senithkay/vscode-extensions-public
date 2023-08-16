0ee4b34b7053a999b4117ad1c656db9d149a5236
Anjana S Porawagama <axewilledge123@gmail.com>
2023-08-16 08:50:00 +0530

    Update config generate command

--- workspaces/ballerina-extension/ui-test/configGeneratorCommand.test.ts
+++ workspaces/ballerina-extension/ui-test/configGeneratorCommand.test.ts
@@ -11,8 +11,8 @@ import { expect } from 'chai';
 import { existsSync, readFileSync, unlinkSync } from 'fs';
 import { before, describe, it } from 'mocha';
 import { join } from 'path';
-import { By, EditorView, Key, VSBrowser, WebDriver, Window } from 'vscode-extension-tester';
-import { areVariablesIncludedInString, wait } from './util';
+import { By, EditorView, Key, VSBrowser, WebDriver, Window, Workbench } from 'vscode-extension-tester';
+import { areVariablesIncludedInString, wait, waitUntil } from './util';
 
 
 const expectedConfigs = [
@@ -43,11 +43,12 @@ const expectedConfigs = [
     'authConfig'
 ];
 
-describe('VSCode Config Creation Using Command UI Tests', () => {
+describe.only('VSCode Config Creation Using Command UI Tests', () => {
     const PROJECT_ROOT = join(__dirname, '..', '..', 'ui-test', 'data');
     let browser: VSBrowser;
     let driver: WebDriver;
     let window: Window;
+    let workbench: Workbench;
 
 
     const configFilePath = `${PROJECT_ROOT}/configServicePackage/Config.toml`;
@@ -63,20 +64,15 @@ describe('VSCode Config Creation Using Command UI Tests', () => {
         driver = browser.driver;
         // Close all open tabs
         await new EditorView().closeAllEditors();
+        workbench = new Workbench();
         await browser.openResources(PROJECT_ROOT, `${PROJECT_ROOT}/configServicePackage/service.bal`);
     });
 
     it('Open command palette to select config create command', async () => {
-        // Send keyboard shortcut to open the command palette
-        await driver.actions().sendKeys(Key.F1).perform();
+       
+        await workbench.executeCommand("Ballerina: Create Config.toml");
 
-        await wait(3000);
-
-        // Simulate entering the search query in the command palette
-        const searchQuery = 'Create Config.toml';
-        await driver.actions().sendKeys(searchQuery, Key.ENTER).perform();
-
-        await wait(5000);
+        await waitUntil(By.xpath("//*[contains(text(), 'Successfully updated')]"));
 
         // Check if the config file has been generated
         expect(existsSync(configFilePath)).to.be.true;
@@ -87,6 +83,14 @@ describe('VSCode Config Creation Using Command UI Tests', () => {
         // Compare the generated config file with the expected config file
         expect(areVariablesIncludedInString(expectedConfigs, generatedConfigContent)).to.true;
 
+    });
+
+    after(async () => {
+        // Check if the file exists
+        if (existsSync(configFilePath)) {
+            // If the file exists, delete it
+            unlinkSync(configFilePath);
+        }
     });
 
 });
