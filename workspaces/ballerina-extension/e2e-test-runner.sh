b70efe8369227e3e47ecc61a037684b20e42be0f
gigara <info@gigara.info>
2023-08-18 08:21:32 +0530

    set the log out to git output

--- workspaces/ballerina-extension/e2e-test-runner.sh
+++ workspaces/ballerina-extension/e2e-test-runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start xvfb
-xvfb-run --listen-tcp --server-num 98.0 -s "-ac -screen 0 1920x1080x24" pnpm run e2e-test > test-resources/output.txt 2>&1 &
+xvfb-run --listen-tcp --server-num 98.0 -s "-ac -screen 0 1920x1080x24" pnpm run e2e-test > $GITHUB_OUTPUT 2>&1 &
 XVFB_RUN_PID=$!
 
 # Wait for xvfb to start (adjust the sleep time as needed)
