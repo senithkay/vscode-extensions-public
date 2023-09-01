1fe1fff0ea9ce945d98631cb8d73287f42c4d19e
gigara <info@gigara.info>
2023-09-01 15:51:16 +0530

    Init mi extension

---
+++ workspaces/mi/mi-extension/src/extension.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "integration-studio" is now active!');
+
+	let disposable = vscode.commands.registerCommand('integration-studio.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello Worldd from Integration Studio!');
+	});
+
+	context.subscriptions.push(disposable);
+}
