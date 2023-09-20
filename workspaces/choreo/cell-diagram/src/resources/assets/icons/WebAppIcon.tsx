143a4d3807751c13749442f49bf62bfc58dd1eb6
Kanushka Gayan <kanushkanet@gmail.com>
2023-09-20 17:11:55 +0530

    Update webapp icon svg attributes

--- workspaces/choreo/cell-diagram/src/resources/assets/icons/WebAppIcon.tsx
+++ workspaces/choreo/cell-diagram/src/resources/assets/icons/WebAppIcon.tsx
@@ -13,13 +13,13 @@ export function WebAppIcon(props: { styles?: CSSProperties }) {
     return (
         <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" style={{ ...props.styles }}>
             <g fill="none">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M42 18v22a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V18" />
-                <path stroke="currentColor" stroke-linejoin="round" stroke-width="4" d="M6 8a2 2 0 0 1 2-2h32a2 2 0 0 1 2 2v10H6V8Z" />
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M42 18v22a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V18" />
+                <path stroke="currentColor" strokeLinejoin="round" strokeWidth="4" d="M6 8a2 2 0 0 1 2-2h32a2 2 0 0 1 2 2v10H6V8Z" />
                 <path
                     fill="currentColor"
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M12 14a2 2 0 1 0 0-4a2 2 0 0 0 0 4Zm6 0a2 2 0 1 0 0-4a2 2 0 0 0 0 4Zm6 0a2 2 0 1 0 0-4a2 2 0 0 0 0 4Z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                 />
             </g>
         </svg>
