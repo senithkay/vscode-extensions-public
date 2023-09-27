374783d280fae1bdd4957129a72c5f81be58b993
tharindulak <tharindu.jayathilake1@gmail.com>
2023-09-27 10:17:59 +0530

    Add side panel implementation

---
+++ workspaces/common-libs/ui-toolkit/src/components/SidePanel/SidePanel.tsx
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+import styled from '@emotion/styled';
+import React from 'react';
+import { Overlay } from './../Commons/Overlay';
+import { colors } from '../Commons/Colors';
+
+export interface SidePanelProps {
+	isOpen?: boolean;
+    alignmanet?: "left" | "right";
+	width?: number;
+	children?: React.ReactNode;
+	sx?: any;
+}
+
+const SidePanelContainer = styled.div<SidePanelProps>`
+	position: fixed;
+	top: 0;
+	left: ${(props: SidePanelProps) => props.alignmanet === "left" ? 0 : "auto"};
+	right: ${(props: SidePanelProps) => props.alignmanet === "right" ? 0 : "auto"};
+	width: ${(props: SidePanelProps) => `${props.width}px`};
+	height: 100%;
+	background-color: var(--vscode-editor-background);
+	color: var(--vscode-editor-foreground);
+	box-shadow: 0 5px 10px 0 var(--vscode-badge-background);
+	z-index: 200;
+	overflow-y: auto;
+	opacity: ${(props: SidePanelProps) => props.isOpen ? 1 : 0};
+	${(props: SidePanelProps) => props.sx};
+`;
+    
+export const SidePanel: React.FC<SidePanelProps> = (props: SidePanelProps) => {
+    const { isOpen = false, alignmanet = "right", width = 312, children, sx } = props;
+    console.log("isOpen", alignmanet);
+    return (
+        <>
+            {isOpen && (<Overlay sx={{background: colors.vscodeEditorInactiveSelectionBackground, opacity: 0.4}}/>)}
+            <SidePanelContainer isOpen={isOpen} width={width} alignmanet={alignmanet} sx={sx}>
+                {children}
+            </SidePanelContainer>
+        </>
+    );
+};
---
+++ workspaces/common-libs/ui-toolkit/src/styles/SidePanel.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const SidePanelBody = styled.div`
+	padding: 16px;
+`;
+
+export const SidePanelTitleContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+	padding: 16px;
+    border-bottom: 1px solid var(--vscode-panel-border);
+    font: inherit;
+    font-weight: bold;
+    color: var(--vscode-editor-foreground);
+`;
