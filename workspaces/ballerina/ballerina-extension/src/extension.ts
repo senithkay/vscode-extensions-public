9622a2f74198b6f796f0c3381887fc67c221073a
Anjana S Porawagama <axewilledge123@gmail.com>
2023-09-27 18:01:59 +0530

    Add missing change

--- workspaces/ballerina/ballerina-extension/src/extension.ts
+++ workspaces/ballerina/ballerina-extension/src/extension.ts
@@ -28,7 +28,7 @@ import { activate as activateNotebook } from './notebook';
 import { activate as activateLibraryBrowser } from './library-browser';
 import { activate as activateERDiagram } from './persist-layer-diagram';
 import { activate as activateDesignDiagramView } from './project-design-diagrams';
-import { debug, log } from './utils';
+import { debug, handleResolveMissingDependencies, log } from './utils';
 import { activateUriHandlers } from './uri-handlers';
 
 let langClient: ExtendedLangClient;
@@ -147,6 +147,8 @@ export async function activate(context: ExtensionContext): Promise<BallerinaExte
                 });
             });
         }
+    }).finally(() => {
+        handleResolveMissingDependencies(ballerinaExtInstance);
     });
     return ballerinaExtInstance;
 }
