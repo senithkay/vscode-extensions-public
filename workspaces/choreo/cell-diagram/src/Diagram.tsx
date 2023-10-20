7c647fd5b79b7dfba8e0a0342d5c529709a9d647
Kanushka Gayan <kanushkanet@gmail.com>
2023-10-20 10:05:10 +0530

    Remove heading tool bar

--- workspaces/choreo/cell-diagram/src/Diagram.tsx
+++ workspaces/choreo/cell-diagram/src/Diagram.tsx
@@ -11,7 +11,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { DiagramEngine, DiagramModel } from "@projectstorm/react-diagrams";
 import CircularProgress from "@mui/material/CircularProgress";
 import { generateEngine, getComponentDiagramWidth, getNodesNLinks, manualDistribute, calculateCellWidth, isRenderInsideCell } from "./utils";
-import { DiagramControls, HeaderWidget, OverlayLayerModel, CellDiagramContext, PromptScreen, ConnectionModel, MenuItem } from "./components";
+import { DiagramControls, OverlayLayerModel, CellDiagramContext, PromptScreen, ConnectionModel, MenuItem } from "./components";
 import { Colors, MAIN_CELL, NO_CELL_NODE } from "./resources";
 import { Container, DiagramContainer, useStyles } from "./utils/CanvasStyles";
 
@@ -181,7 +181,6 @@ export function CellDiagram(props: CellDiagramProps) {
     return (
         <Container>
             <CellDiagramContext {...ctx}>
-                <HeaderWidget />
                 <DiagramContainer onClick={handleCanvasClick}>
                     {diagramEngine?.getModel() && diagramModel ? (
                         <>
