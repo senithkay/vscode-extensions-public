7fc2f2661a7bf2b763992d5ebe2e8e8083ee98a7
Anjana S Porawagama <axewilledge123@gmail.com>
2023-10-27 16:39:34 +0530

    Add ballerina-client

---
+++ workspaces/ballerina/ballerina-client/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: "build",
+        entryFileNames: '[name].js'
+      },
+      input: 'src/index.ts', // Replace with the path to your entry file
+    },
+  },
+})
