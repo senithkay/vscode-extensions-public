2e2c7732e49e53f5e39189088fdaa50ced01cd5e
gigara <info@gigara.info>
2023-11-03 01:05:42 +0530

    debug

--- workspaces/ballerina/ballerina-extension/ui-test/button.test.ts
+++ workspaces/ballerina/ballerina-extension/ui-test/button.test.ts
@@ -1,38 +1,38 @@
-/**
- * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
- *
- * This software is the property of WSO2 LLC. and its suppliers, if any.
- * Dissemination of any information or reproduction of any material contained
- * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
- * You may not alter or remove any copyright or other notice from copies of this content.
- */
+// /**
+//  * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+//  *
+//  * This software is the property of WSO2 LLC. and its suppliers, if any.
+//  * Dissemination of any information or reproduction of any material contained
+//  * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+//  * You may not alter or remove any copyright or other notice from copies of this content.
+//  */
 
-import { expect } from 'chai';
-import { before, describe, it } from 'mocha';
-import { join } from 'path';
-import { EditorView, VSBrowser } from 'vscode-extension-tester';
-import { wait } from './util';
-import { ExtendedEditorView } from './utils/ExtendedEditorView';
+// import { expect } from 'chai';
+// import { before, describe, it } from 'mocha';
+// import { join } from 'path';
+// import { EditorView, VSBrowser } from 'vscode-extension-tester';
+// import { wait } from './util';
+// import { ExtendedEditorView } from './utils/ExtendedEditorView';
 
-describe('VSCode UI Tests', () => {
-    const PROJECT_ROOT = join(__dirname, '..', '..', 'ui-test', 'data');
+// describe('VSCode UI Tests', () => {
+//     const PROJECT_ROOT = join(__dirname, '..', '..', 'ui-test', 'data');
 
-    before(async () => {
-        await VSBrowser.instance.openResources(PROJECT_ROOT, `${PROJECT_ROOT}/hello_world.bal`);
-        await VSBrowser.instance.waitForWorkbench();
-    });
+//     before(async () => {
+//         await VSBrowser.instance.openResources(PROJECT_ROOT, `${PROJECT_ROOT}/hello_world.bal`);
+//         await VSBrowser.instance.waitForWorkbench();
+//     });
 
-    it('Test Title bar', async () => {
-        const editorView = new ExtendedEditorView(new EditorView());
-        expect(await editorView.getAction("Run")).is.not.undefined;
-        expect(await editorView.getAction("Debug")).is.not.undefined;
+//     it('Test Title bar', async () => {
+//         const editorView = new ExtendedEditorView(new EditorView());
+//         expect(await editorView.getAction("Run")).is.not.undefined;
+//         expect(await editorView.getAction("Debug")).is.not.undefined;
 
-        await wait(5000);
-        expect(await editorView.getAction("Show Diagram")).is.not.undefined;
-        (await editorView.getAction("Show Diagram"))!.click();
+//         await wait(5000);
+//         expect(await editorView.getAction("Show Diagram")).is.not.undefined;
+//         (await editorView.getAction("Show Diagram"))!.click();
 
-        expect(await editorView.getAction("Show Source")).is.not.undefined;
+//         expect(await editorView.getAction("Show Source")).is.not.undefined;
 
-    });
+//     });
 
-});
+// });
--- workspaces/ballerina/ballerina-extension/ui-test/byExampleFileOpen.test.ts
+++ workspaces/ballerina/ballerina-extension/ui-test/byExampleFileOpen.test.ts
@@ -1,197 +1,197 @@
-/**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
- *
- * This software is the property of WSO2 LLC. and its suppliers, if any.
- * Dissemination of any information or reproduction of any material contained
- * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
- * You may not alter or remove any copyright or other notice from copies of this content.
- */
+// /**
+//  * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+//  *
+//  * This software is the property of WSO2 LLC. and its suppliers, if any.
+//  * Dissemination of any information or reproduction of any material contained
+//  * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+//  * You may not alter or remove any copyright or other notice from copies of this content.
+//  */
 
-import { expect } from 'chai';
-import { existsSync, mkdirSync, rmSync } from 'fs';
-import { before, describe, it } from 'mocha';
-import { join } from 'path';
-import { By, EditorView, InputBox, VSBrowser, WebDriver, Workbench, until } from 'vscode-extension-tester';
-import { waitUntil } from './util';
+// import { expect } from 'chai';
+// import { existsSync, mkdirSync, rmSync } from 'fs';
+// import { before, describe, it } from 'mocha';
+// import { join } from 'path';
+// import { By, EditorView, InputBox, VSBrowser, WebDriver, Workbench, until } from 'vscode-extension-tester';
+// import { waitUntil } from './util';
 
-describe('Open ballerina samples in VSCode from URL', () => {
-    const PROJECT_ROOT = join(__dirname, '..', '..', 'ui-test', 'data');
-    let browser: VSBrowser;
-    let driver: WebDriver;
-    let workbench: Workbench;
+// describe('Open ballerina samples in VSCode from URL', () => {
+//     const PROJECT_ROOT = join(__dirname, '..', '..', 'ui-test', 'data');
+//     let browser: VSBrowser;
+//     let driver: WebDriver;
+//     let workbench: Workbench;
 
-    const samplesDownloadDirectory = `${PROJECT_ROOT}/sampleDownloadFolder`;
+//     const samplesDownloadDirectory = `${PROJECT_ROOT}/sampleDownloadFolder`;
 
-    before(async () => {
-        // Delete files and folders in this folder
-        rmSync(samplesDownloadDirectory, { recursive: true, force: true });
-        // Create folder if not present
-        if (!existsSync(samplesDownloadDirectory)) {
-            mkdirSync(samplesDownloadDirectory);
-        }
-        browser = VSBrowser.instance;
-        driver = browser.driver;
-        workbench = new Workbench();
-        await new EditorView().closeAllEditors();
-        await browser.openResources(samplesDownloadDirectory);
-        await browser.waitForWorkbench();
-        // Re-locate the editor group container element
-        await driver.wait(until.elementLocated(By.css('.editor-group-container')), 30000);
-    });
+//     before(async () => {
+//         // Delete files and folders in this folder
+//         rmSync(samplesDownloadDirectory, { recursive: true, force: true });
+//         // Create folder if not present
+//         if (!existsSync(samplesDownloadDirectory)) {
+//             mkdirSync(samplesDownloadDirectory);
+//         }
+//         browser = VSBrowser.instance;
+//         driver = browser.driver;
+//         workbench = new Workbench();
+//         await new EditorView().closeAllEditors();
+//         await browser.openResources(samplesDownloadDirectory);
+//         await browser.waitForWorkbench();
+//         // Re-locate the editor group container element
+//         await driver.wait(until.elementLocated(By.css('.editor-group-container')), 30000);
+//     });
 
-    it('Open URL to download first sample on first time and change directory', async () => {
+//     it('Open URL to download first sample on first time and change directory', async () => {
 
-        // Use Developer URL to execute a URL
-        const url = 'vscode://wso2.ballerina/open-file?gist=18e6c62b7ef307d7064ed4ef39e4d0d8&file=functions.bal';
-        await executeURLdownload(workbench, url);
+//         // Use Developer URL to execute a URL
+//         const url = 'vscode://wso2.ballerina/open-file?gist=18e6c62b7ef307d7064ed4ef39e4d0d8&file=functions.bal';
+//         await executeURLdownload(workbench, url);
 
-        // Open Downloaded file
-        const openFile = await waitUntil(By.linkText('Open'));
-        await openFile.click();
+//         // Open Downloaded file
+//         const openFile = await waitUntil(By.linkText('Open'));
+//         await openFile.click();
 
-        // Click on change directory
-        const changePathBtn = await waitUntil(By.linkText('Change Directory'));
-        await changePathBtn.click();
+//         // Click on change directory
+//         const changePathBtn = await waitUntil(By.linkText('Change Directory'));
+//         await changePathBtn.click();
 
-        // Wait for OK button to be appeared
-        const okButton = await waitUntil(By.linkText('OK'));
+//         // Wait for OK button to be appeared
+//         const okButton = await waitUntil(By.linkText('OK'));
 
-        const input = await InputBox.create();
+//         const input = await InputBox.create();
 
-        // Set the new downloads path
-        await input.clear();
+//         // Set the new downloads path
+//         await input.clear();
 
-        await input.setText(samplesDownloadDirectory);
+//         await input.setText(samplesDownloadDirectory);
 
-        // Save the new download path
-        await okButton.click();
+//         // Save the new download path
+//         await okButton.click();
 
-        // Check if the file has been downloaded to the new location
-        await executeURLdownload(workbench, url);
+//         // Check if the file has been downloaded to the new location
+//         await executeURLdownload(workbench, url);
 
-        // Open Downloaded file
-        const openFileSecond = await waitUntil(By.linkText('Open'));
-        await openFileSecond.click();
-        await waitUntil(By.linkText('Change Directory'));
+//         // Open Downloaded file
+//         const openFileSecond = await waitUntil(By.linkText('Open'));
+//         await openFileSecond.click();
+//         await waitUntil(By.linkText('Change Directory'));
 
-        expect(existsSync(`${samplesDownloadDirectory}/functions.bal`), "Second assert with functions.bal").to.be.true;
-        await new EditorView().closeAllEditors();
-    });
+//         expect(existsSync(`${samplesDownloadDirectory}/functions.bal`), "Second assert with functions.bal").to.be.true;
+//         await new EditorView().closeAllEditors();
+//     });
 
-    it('Open URL to download second sample file', async () => {
-        // Use Developer URL to excecute a URL
-        const url = 'vscode://wso2.ballerina/open-file?gist=8ada14df03d5d8841d03ce4b92819b2b&file=hello_world.bal';
-        await executeURLdownload(workbench, url);
-        // Open Downloaded file
-        const openFile = await waitUntil(By.linkText('Open'));
-        await openFile.click();
-        await waitUntil(By.linkText('Change Directory'));
+//     it('Open URL to download second sample file', async () => {
+//         // Use Developer URL to excecute a URL
+//         const url = 'vscode://wso2.ballerina/open-file?gist=8ada14df03d5d8841d03ce4b92819b2b&file=hello_world.bal';
+//         await executeURLdownload(workbench, url);
+//         // Open Downloaded file
+//         const openFile = await waitUntil(By.linkText('Open'));
+//         await openFile.click();
+//         await waitUntil(By.linkText('Change Directory'));
 
-        expect(existsSync(`${samplesDownloadDirectory}/hello_world.bal`)).to.be.true;
-        await new EditorView().closeAllEditors();
-    });
+//         expect(existsSync(`${samplesDownloadDirectory}/hello_world.bal`)).to.be.true;
+//         await new EditorView().closeAllEditors();
+//     });
 
-    it('Open URL to download a not valid sample file', async () => {
-        // Use Developer URL to excecute a URL
-        const url = 'vscode://wso2.ballerina/open-file?gist=1b94f48ad579969bc7c6a79549684dca&file=PeopleManagementService.bal';
-        await executeURLdownload(workbench, url);
-        expect(existsSync(`${samplesDownloadDirectory}/PeopleManagementService.bal`)).to.be.not.true;
-    });
+//     it('Open URL to download a not valid sample file', async () => {
+//         // Use Developer URL to excecute a URL
+//         const url = 'vscode://wso2.ballerina/open-file?gist=1b94f48ad579969bc7c6a79549684dca&file=PeopleManagementService.bal';
+//         await executeURLdownload(workbench, url);
+//         expect(existsSync(`${samplesDownloadDirectory}/PeopleManagementService.bal`)).to.be.not.true;
+//     });
 
-    it('Open URL to download github sample file', async () => {
-        // Use Developer URL to excecute a URL
-        const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/wso2/choreo-sample-apps/blob/main/ballerina/greeter/service.bal';
-        await executeURLdownload(workbench, url);
+//     it('Open URL to download github sample file', async () => {
+//         // Use Developer URL to excecute a URL
+//         const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/wso2/choreo-sample-apps/blob/main/ballerina/greeter/service.bal';
+//         await executeURLdownload(workbench, url);
 
-        // Open Downloaded file
-        const openFile = await waitUntil(By.linkText('Open'));
-        await openFile.click();
-        await waitUntil(By.linkText('Change Directory'));
+//         // Open Downloaded file
+//         const openFile = await waitUntil(By.linkText('Open'));
+//         await openFile.click();
+//         await waitUntil(By.linkText('Change Directory'));
 
-        expect(existsSync(`${samplesDownloadDirectory}/service.bal`)).to.be.true;
-        await new EditorView().closeAllEditors();
-    });
+//         expect(existsSync(`${samplesDownloadDirectory}/service.bal`)).to.be.true;
+//         await new EditorView().closeAllEditors();
+//     });
 
-    it('Open URL to download not valid github sample file', async () => {
-        // Use Developer URL to excecute a URL
-        const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/jclark/semtype/blob/master/main.bal';
-        await executeURLdownload(workbench, url);
-        expect(existsSync(`${samplesDownloadDirectory}/main.bal`)).to.be.not.true;
-    });
+//     it('Open URL to download not valid github sample file', async () => {
+//         // Use Developer URL to excecute a URL
+//         const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/jclark/semtype/blob/master/main.bal';
+//         await executeURLdownload(workbench, url);
+//         expect(existsSync(`${samplesDownloadDirectory}/main.bal`)).to.be.not.true;
+//     });
 
-    it('Open URL to download a not valid sample file', async () => {
-        // Use Developer URL to excecute a URL
-        const url = 'vscode://wso2.ballerina/open-file?gist=1b94f48ad579969bc7c6a79549684dca&file=PeopleManagementService.bal';
-        await executeURLdownload(workbench, url);
-        expect(existsSync(`${samplesDownloadDirectory}/PeopleManagementService.bal`)).to.be.not.true;
-    });
+//     it('Open URL to download a not valid sample file', async () => {
+//         // Use Developer URL to excecute a URL
+//         const url = 'vscode://wso2.ballerina/open-file?gist=1b94f48ad579969bc7c6a79549684dca&file=PeopleManagementService.bal';
+//         await executeURLdownload(workbench, url);
+//         expect(existsSync(`${samplesDownloadDirectory}/PeopleManagementService.bal`)).to.be.not.true;
+//     });
 
-    it('Open URL to download github sample file', async () => {
-        // Use Developer URL to excecute a URL
-        const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/wso2/choreo-sample-apps/blob/main/ballerina/greeter/service.bal';
-        await executeURLdownload(workbench, url);
+//     it('Open URL to download github sample file', async () => {
+//         // Use Developer URL to excecute a URL
+//         const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/wso2/choreo-sample-apps/blob/main/ballerina/greeter/service.bal';
+//         await executeURLdownload(workbench, url);
 
-        // Open Downloaded file
-        const openFile = await waitUntil(By.linkText('Open'));
-        await openFile.click();
-        await waitUntil(By.linkText('Change Directory'));
+//         // Open Downloaded file
+//         const openFile = await waitUntil(By.linkText('Open'));
+//         await openFile.click();
+//         await waitUntil(By.linkText('Change Directory'));
 
-        expect(existsSync(`${samplesDownloadDirectory}/service.bal`)).to.be.true;
-    });
+//         expect(existsSync(`${samplesDownloadDirectory}/service.bal`)).to.be.true;
+//     });
 
-    it('Open URL to download not valid github sample file', async () => {
-        // Use Developer URL to excecute a URL
-        const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/jclark/semtype/blob/master/main.bal';
-        await executeURLdownload(workbench, url);
-        expect(existsSync(`${samplesDownloadDirectory}/main.bal`)).to.be.not.true;
-    });
+//     it('Open URL to download not valid github sample file', async () => {
+//         // Use Developer URL to excecute a URL
+//         const url = 'vscode://wso2.ballerina/open-file?repoFileUrl=https://github.com/jclark/semtype/blob/master/main.bal';
+//         await executeURLdownload(workbench, url);
+//         expect(existsSync(`${samplesDownloadDirectory}/main.bal`)).to.be.not.true;
+//     });
 
-    it('Open URL to download git repo', async () => {
-        // Use Developer URL to excecute a URL
-        const url = 'vscode://wso2.ballerina/open-repo?repoUrl=https://github.com/wso2/choreo-sample-apps';
-        await executeURLdownload(workbench, url);
+//     it('Open URL to download git repo', async () => {
+//         // Use Developer URL to excecute a URL
+//         const url = 'vscode://wso2.ballerina/open-repo?repoUrl=https://github.com/wso2/choreo-sample-apps';
+//         await executeURLdownload(workbench, url);
 
-        // Confirm Clone
-        const openFile = await waitUntil(By.linkText('Clone Now'));
-        await openFile.click();
+//         // Confirm Clone
+//         const openFile = await waitUntil(By.linkText('Clone Now'));
+//         await openFile.click();
 
-        const cloneDialog = await waitUntil(By.className('monaco-dialog-box'), 30000).findElement(By.linkText('Cancel'));
-        await cloneDialog.click();
+//         const cloneDialog = await waitUntil(By.className('monaco-dialog-box'), 30000).findElement(By.linkText('Cancel'));
+//         await cloneDialog.click();
 
-        expect(existsSync(`${samplesDownloadDirectory}/choreo-sample-apps/README.md`)).to.be.true;
-        await new EditorView().closeAllEditors();
-    });
+//         expect(existsSync(`${samplesDownloadDirectory}/choreo-sample-apps/README.md`)).to.be.true;
+//         await new EditorView().closeAllEditors();
+//     });
 
-    it('Open URL to download existing git repo and open file', async () => {
-        // Use Developer URL to excecute a URL
-        const url = 'vscode://wso2.ballerina/open-repo?repoUrl=https://github.com/wso2/choreo-sample-apps&openFile=ballerina/greeter/service.bal';
-        await executeURLdownload(workbench, url);
+//     it('Open URL to download existing git repo and open file', async () => {
+//         // Use Developer URL to excecute a URL
+//         const url = 'vscode://wso2.ballerina/open-repo?repoUrl=https://github.com/wso2/choreo-sample-apps&openFile=ballerina/greeter/service.bal';
+//         await executeURLdownload(workbench, url);
 
-        // Confirm Existing
-        const openFile = await waitUntil(By.linkText('Open'));
-        await openFile.click();
+//         // Confirm Existing
+//         const openFile = await waitUntil(By.linkText('Open'));
+//         await openFile.click();
 
-        const editorView = new EditorView();
-        const editorTitle = await editorView.getOpenEditorTitles();
-        expect(editorTitle).to.includes('service.bal');        
-        await new EditorView().closeAllEditors();
-    });
+//         const editorView = new EditorView();
+//         const editorTitle = await editorView.getOpenEditorTitles();
+//         expect(editorTitle).to.includes('service.bal');        
+//         await new EditorView().closeAllEditors();
+//     });
 
-    after(async () => {
-        // Delete files and folders in this folder
-        rmSync(samplesDownloadDirectory, { recursive: true, force: true });
-    });
+//     after(async () => {
+//         // Delete files and folders in this folder
+//         rmSync(samplesDownloadDirectory, { recursive: true, force: true });
+//     });
 
-});
+// });
 
 
-const executeURLdownload = async (workbench, url: string) => {
-    await workbench.executeCommand("Developer: Open URL");
-    const commandInput = await InputBox.create();
-    await commandInput.setText(url);
-    await commandInput.confirm();
-    // URL Open verification
-    const vscodeVerify = await waitUntil(By.className('monaco-dialog-box')).findElement(By.linkText('Open'));
-    await vscodeVerify.click();
-}
+// const executeURLdownload = async (workbench, url: string) => {
+//     await workbench.executeCommand("Developer: Open URL");
+//     const commandInput = await InputBox.create();
+//     await commandInput.setText(url);
+//     await commandInput.confirm();
+//     // URL Open verification
+//     const vscodeVerify = await waitUntil(By.className('monaco-dialog-box')).findElement(By.linkText('Open'));
+//     await vscodeVerify.click();
+// }
 
