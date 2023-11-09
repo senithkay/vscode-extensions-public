33b5aac29609dc00d3d2e01fd1842718610dc88c
tharindulak <tharindu.jayathilake1@gmail.com>
2023-11-09 10:46:04 +0530

    Add popover

--- workspaces/common-libs/ui-toolkit/src/index.ts
+++ workspaces/common-libs/ui-toolkit/src/index.ts
@@ -19,6 +19,7 @@ export * from './components/ComponentCard/ComponentCard';
 export * from './components/Typography/Typography';
 export * from './components/Button/Button';
 export * from './components/ActionButtons/ActionButtons';
+export * from './components/Popover/Popover';
 export * from './components/SidePanel/SidePanel';
 export * from './components/TextArea/TextArea';
 export * from './components/Divider/Divider';
