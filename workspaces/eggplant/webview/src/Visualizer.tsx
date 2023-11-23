c196fcffb5bd46c31d46b3fd9ffdfe594cb6365b
Joseph Fonseka <hevayo@gmail.com>
2023-11-23 11:13:13 +0530

    Add eggplant webview

---
+++ workspaces/eggplant/webview/src/Visualizer.tsx
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+import { Overview } from "@wso2-enterprise/overview-view";
+import { ServiceDesigner } from "@wso2-enterprise/service-designer-view";
+import React, { useEffect } from "react";
+import { ViewLocation, useVisualizerContext } from "@wso2-enterprise/ballerina-rpc-client";
+
+export function Webview() {
+    const { viewLocation, setViewLocation, ballerinaRpcClient } = useVisualizerContext();
+
+    useEffect(() => {
+        setViewLocationState();
+        ballerinaRpcClient.onStateChanged((state: { viewContext: ViewLocation }) => {
+            setViewLocation(state.viewContext);
+        });
+    }, []);
+
+    const setViewLocationState = async () => {
+        const state = await ballerinaRpcClient.getVisualizerClient().getVisualizerState();
+        if (state) {
+            setViewLocation(state);
+        }
+    }
+    return (
+        <>
+            <h2>Hello Nav</h2>
+            {viewLocation.view === "Overview" && <Overview />}
+            {viewLocation.view === "ServiceDesigner" && <ServiceDesigner />}
+            {viewLocation.view === "Architecture" && <h2>Hello Arch</h2>}
+        </>
+    );
+};
