bede615e721c34eee9bebd57fa313ad01bfa82ff
YasithD <yasith.18@cse.mrt.ac.lk>
2023-11-29 15:10:19 +0530

    Add story for breadcrumb

---
+++ workspaces/common-libs/ui-toolkit/src/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ComponentStory } from "@storybook/react";
+import Breadcrumbs, { BreadcrumbProps } from "./Breadcrumb";
+
+const Template: ComponentStory<typeof Breadcrumbs> = (args: BreadcrumbProps) => (
+    <Breadcrumbs {...args}>
+        <div key={1}>Home</div>
+        <div key={2}>Products</div>
+        <div key={3}>Category</div>
+        <div key={4}>Sub Category</div>
+        <div key={5}>Product</div>
+    </Breadcrumbs>
+);
+
+export const Default = Template.bind();
+Default.args = {
+    maxItems: 3,
+};
+
+export default { component: Breadcrumbs, title: "Breadcrumb" };
