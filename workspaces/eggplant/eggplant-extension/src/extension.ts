2e957490b7d305b5864c420c416d94efe4856b98
Joseph Fonseka <hevayo@gmail.com>
2023-12-05 09:36:21 +0530

    Add state machine to eggplant

--- workspaces/eggplant/eggplant-extension/src/extension.ts
+++ workspaces/eggplant/eggplant-extension/src/extension.ts
@@ -4,6 +4,8 @@ import * as path from 'path';
 import { activateActivityBarWebViews } from './ActivityBar/activate';
 import { ext } from './eggplantExtentionContext';
 import { activateLowCodeWebViews } from './LowCode/activate';
+import { api as stateMachineApi } from './stateMachine';
+
 
 
 // 1. Render the low code diagram in the webview
@@ -19,46 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
 	activateActivityBarWebViews(context);
 	activateLowCodeWebViews(context);
 
-	// Get Ballerina extension
-	const ballerinaExt = vscode.extensions.getExtension('wso2.ballerina');
-	if (!ballerinaExt) {
-		console.error('Cannot find Ballerina extension');
-		return;
-	}
-
-	// Activate Ballerina extension if not activated
-	if (!ballerinaExt.isActive) {
-		ballerinaExt.activate().then(() => {
-			let workspaceUri = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri : null;
-			// @ts-ignore
-			ballerinaExt.exports.langClient.getBallerinaProjectComponents({
-				documentIdentifiers: [{ uri: workspaceUri?.path + "/main.bal" }]
-				// @ts-ignore
-			}).then((componentResponse: BallerinaProjectComponents) => {
-				console.log(componentResponse);
-			});
-			console.log('Ballerina extension activated');
-		}, error => {
-			console.error('Failed to activate Ballerina extension:', error);
-		});
-	}
-
-	vscode.workspace.findFiles('**/Ballerina.toml', '**/node_modules/**', 1).then(files => {
-		if (files.length > 0) {
-			fs.readFile(files[0].fsPath, 'utf8', (err, data) => {
-				if (err) {
-					console.error(err);
-					return;
-				}
-				if (data.includes('eggplant')) {
-					vscode.commands.executeCommand('workbench.view.extension.eggplant');
-					vscode.commands.executeCommand('eggplant.openLowCode');
-				}
-			});
-		}
-	});
-
-
+	stateMachineApi.initialize();
 }
 
 
