69e4aa26dad0ad4a88783ddd7cf5b55292ac4d82
Madusha <madushajg@gmail.com>
2023-12-07 11:28:32 +0530

    Remove old DM

--- workspaces/ballerina/data-mapper/src/components/DataMapper/Context/current-file-context.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-import { CurrentFile } from "@wso2-enterprise/ballerina-low-code-edtior-commons";
-
-export const CurrentFileContext = React.createContext<CurrentFile>(undefined);
--- workspaces/ballerina/data-mapper/src/components/Diagram/OverriddenLinkLayer/LinkOverlayPortal.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-
-export const LinkOverayContainerID = "data-mapper-link-overlay-container";
-
-export const LinkOveryPortal: React.FC<{}> = (props) => {
-    const container = document.getElementById(LinkOverayContainerID);
-    return container !== null ? ReactDOM.createPortal(props.children, container) : <></>;
-}
