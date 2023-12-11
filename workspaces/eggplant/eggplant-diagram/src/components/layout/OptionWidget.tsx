a18e9974c41454323005765c8d4e8edd76de2bce
Kanushka Gayan <kanushkanet@gmail.com>
2023-12-11 23:00:17 +0530

    Add support to add multiple cases in switch nodes

--- workspaces/eggplant/eggplant-diagram/src/components/layout/OptionWidget.tsx
+++ workspaces/eggplant/eggplant-diagram/src/components/layout/OptionWidget.tsx
@@ -50,6 +50,37 @@ export function OptionWidget(props: OptionWidgetProps) {
     // clone the node
     let node = JSON.parse(JSON.stringify(selectedNode.getOptions().node)) as Node;
 
+    const handleAddCase = () => {
+        const caseCount = node.properties.cases.length + 1;
+        const portId = `out-${caseCount + 1}`;
+        const defaultPortId = node.properties?.defaultCase?.nodes.length > 0 ? node.properties.defaultCase.nodes[0] : null;
+        if (defaultPortId) {
+            // switch the default case to the new case
+            node.properties.defaultCase.nodes = [portId];
+            node.properties.cases.push({
+                expression: "true",
+                nodes: [defaultPortId],
+            });
+        } else {
+            node.properties.cases.push({
+                expression: "true",
+                nodes: [portId],
+            });
+        }
+        selectedNode.addOutPort(portId, {
+            id: portId,
+            type: "any",
+        });
+        selectedNode.setNode(node);
+        updateFlowModel(node);
+    };
+
+    const handleOnSave = () => {
+        selectedNode.setNode(node);
+        updateFlowModel(node);
+        setSelectedNode(null);
+    };
+
     return (
         <S.Tray>
             <S.TitleContainer>
@@ -82,32 +113,31 @@ export function OptionWidget(props: OptionWidgetProps) {
                     }}
                 />
             )}
-            {selectedNode.getKind() === NODE_TYPE.SWITCH &&
-                node.properties?.cases?.map((caseBlock: SwitchCaseBlock, index: number) => {
-                    return (
-                        <div key={index}>
-                            <TextArea
-                                label={`Case ${index + 1}`}
-                                value={caseBlock.expression.toString() || ""}
-                                rows={2}
-                                resize="vertical"
-                                onChange={(value: string) => {
-                                    if (node) {
-                                        node.properties.cases[index].expression = value;
-                                    }
-                                }}
-                            />
-                        </div>
-                    );
-                })}
-            <Button
-                onClick={() => {
-                    selectedNode.setNode(node);
-                    updateFlowModel(node);
-                }}
-            >
-                Save
-            </Button>
+            {selectedNode.getKind() === NODE_TYPE.SWITCH && (
+                <>
+                    {node.properties?.cases?.map((caseBlock: SwitchCaseBlock, index: number) => {
+                        return (
+                            <div key={index}>
+                                <TextArea
+                                    label={`Case ${index + 1}`}
+                                    value={caseBlock.expression.toString() || ""}
+                                    rows={2}
+                                    resize="vertical"
+                                    onChange={(value: string) => {
+                                        if (node) {
+                                            node.properties.cases[index].expression = value;
+                                        }
+                                    }}
+                                />
+                            </div>
+                        );
+                    })}
+                    <Button appearance="secondary" onClick={handleAddCase}>
+                        Add Case
+                    </Button>
+                </>
+            )}
+            <Button onClick={handleOnSave}>Save</Button>
             {children}
         </S.Tray>
     );
