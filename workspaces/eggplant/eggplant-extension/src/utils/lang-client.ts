2e14f7735f70ccbdbc7f3d85886696c659906fa1
Anjana S Porawagama <axewilledge123@gmail.com>
2023-12-13 17:36:34 +0530

    Add new method to ls client

--- workspaces/eggplant/eggplant-extension/src/utils/lang-client.ts
+++ workspaces/eggplant/eggplant-extension/src/utils/lang-client.ts
@@ -1,14 +1,19 @@
 import { BallerinaFunctionSTRequest, BallerinaSTModifyResponse } from "@wso2-enterprise/ballerina-core";
-import { LangClientInterface } from "@wso2-enterprise/eggplant-core";
+import { EggplantModelRequest, LangClientInterface } from "@wso2-enterprise/eggplant-core";
 
 enum EXTENDED_APIS {
     DOCUMENT_ST_BY_RANGE = 'ballerinaDocument/syntaxTreeByRange',
+    EGGPLANT_MODEL = 'workerDesignService/getWorkerDesignModel'
 }
 
+// YOU CAN ADD EGGPLANT SPECIFIC LANG CLIENT METHODS HERE
 export function registerNewLSMethods(langClient: LangClientInterface): LangClientInterface {
-    // YOU CAN ADD EGGPLANT SPECIFIC LANG CLIENT METHODS HERE
     langClient.getSTByRange = async (params: BallerinaFunctionSTRequest) => {
         return langClient.sendRequest<BallerinaSTModifyResponse>(EXTENDED_APIS.DOCUMENT_ST_BY_RANGE, params);
+    };
+
+    langClient.getEggplantModel = async (params: EggplantModelRequest) => {
+        return langClient.sendRequest<any>(EXTENDED_APIS.EGGPLANT_MODEL, params);
     };
 
     return langClient;
