bbcc584b4e6d07f383cfbe451ac2c3c215b11294
Madusha <madushajg@gmail.com>
2023-12-18 09:48:20 +0530

    Fix using emotion Global within visualizer

--- workspaces/ballerina/visualizer-view/src/Visualizer.tsx
+++ workspaces/ballerina/visualizer-view/src/Visualizer.tsx
@@ -26,8 +26,6 @@ const globalStyles = css`
   }
 `;
 
-const GlobalStyles = () => <Global styles={globalStyles} />;
-
 const VisualizerContainer = styled.div`
     width: 100%;
     height: 100%;
@@ -56,7 +54,7 @@ export function Webview() {
 
     return (
         <>
-            <GlobalStyles />
+            <Global styles={globalStyles} />
             <VisualizerContainer>
                 <NavigationBar />
                 {viewLocation.view === "Overview" && <Overview />}
