3aae6508c76b405b7e4e5a16947c7db5036ba1b0
Anjana S Porawagama <axewilledge123@gmail.com>
2024-01-05 15:36:06 +0530

    Add missing prop

--- workspaces/ballerina/visualizer-view/src/Visualizer.tsx
+++ workspaces/ballerina/visualizer-view/src/Visualizer.tsx
@@ -58,7 +58,7 @@ export function Webview() {
             <VisualizerContainer>
                 <NavigationBar />
                 {viewLocation.view === "Overview" && <Overview />}
-                {viewLocation.view === "ServiceDesigner" && <ServiceDesigner model={null} rpcClient={null} />}
+                {viewLocation.view === "ServiceDesigner" && <ServiceDesigner model={null} rpcClient={null} showDiagram={null}/>}
                 {viewLocation.view === "DataMapper" && <DataMapperOverlay />}
                 {viewLocation.view === "Architecture" && <h2>Hello Arch</h2>}
             </VisualizerContainer>
