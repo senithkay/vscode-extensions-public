8cddfca07bf4bfb55f9e89069c6cd75fbd904868
CharukaK <charukakarunanayake@gmail.com>
2024-01-05 06:30:05 +0530

    Add event handlers for tree view

--- workspaces/mi/mi-extension/src/activity-panel/project-explorer-provider.ts
+++ workspaces/mi/mi-extension/src/activity-panel/project-explorer-provider.ts
@@ -4,13 +4,16 @@ import { GetProjectStructureResponse, ProjectStructureEntry } from '@wso2-enterp
 
 export class ProjectExplorerEntry extends vscode.TreeItem {
 	children: ProjectExplorerEntry[] | undefined
+	info: ProjectStructureEntry | undefined
 
 	constructor(
 		public readonly label: string,
-		public readonly collapsibleState: vscode.TreeItemCollapsibleState
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		info: ProjectStructureEntry | undefined = undefined
 	) {
 		super(label, collapsibleState);
 		this.tooltip = `${this.label}`;
+		this.info = info;
 	}
 }
 
@@ -48,6 +51,8 @@ export class ProjectExplorerEntryProvider implements vscode.TreeDataProvider<Pro
 		return element.children;
 	}
 
+	
+
 }
 
 async function getProjectStructureData(context: vscode.ExtensionContext): Promise<ProjectExplorerEntry[]> {
@@ -63,299 +68,7 @@ async function getProjectStructureData(context: vscode.ExtensionContext): Promis
 
 	if (!!langClient) {
 		const resp = await langClient.getProjectStructure(rootPath)
-
-		const object: GetProjectStructureResponse =
-		{
-			"directoryMap": {
-				"esbConfigs": {
-					"message-stores": [
-						{
-							"type": "message-stores",
-							"name": "messageStore.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/message-stores/messageStore.xml"
-						}
-					],
-					"endpoints": [
-						{
-							"type": "endpoints",
-							"name": "address.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/address.xml"
-						},
-						{
-							"type": "endpoints",
-							"name": "http.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/http.xml"
-						},
-						{
-							"type": "endpoints",
-							"name": "wsdl.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/wsdl.xml"
-						},
-						{
-							"type": "endpoints",
-							"name": "failover.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/failover.xml"
-						},
-						{
-							"type": "endpoints",
-							"name": "template.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/template.xml"
-						},
-						{
-							"type": "endpoints",
-							"name": "recipientlist.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/recipientlist.xml"
-						},
-						{
-							"type": "endpoints",
-							"name": "wsdl1.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/wsdl1.xml"
-						},
-						{
-							"type": "endpoints",
-							"name": "zc.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/zc.xml"
-						},
-						{
-							"type": "endpoints",
-							"name": "default.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/default.xml"
-						},
-						{
-							"type": "endpoints",
-							"name": "loadbalance.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/loadbalance.xml"
-						}
-					],
-					"proxy-services": [
-						{
-							"sequences": [
-
-							],
-							"endpoints": [
-
-							],
-							"type": "proxy-services",
-							"name": "proxy.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/proxy-services/proxy.xml"
-						},
-						{
-							"sequences": [
-
-							],
-							"endpoints": [
-
-							],
-							"type": "proxy-services",
-							"name": "wsdlproxy.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/proxy-services/wsdlproxy.xml"
-						},
-						{
-							"sequences": [
-
-							],
-							"endpoints": [
-
-							],
-							"type": "proxy-services",
-							"name": "wsdlfile.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/proxy-services/wsdlfile.xml"
-						}
-					],
-					"local-entries": [
-						{
-							"type": "local-entries",
-							"name": "localEntry.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/local-entries/localEntry.xml"
-						}
-					],
-					"templates": [
-
-					],
-					"message-processors": [
-
-					],
-					"api": [
-						{
-							"sequences": [
-
-							],
-							"endpoints": [
-
-							],
-							"type": "api",
-							"name": "HelloWorld.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/api/HelloWorld.xml"
-						},
-						{
-							"sequences": [
-
-							],
-							"endpoints": [
-
-							],
-							"type": "api",
-							"name": "newapi.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/api/newapi.xml"
-						},
-						{
-							"sequences": [
-
-							],
-							"endpoints": [
-
-							],
-							"type": "api",
-							"name": "hh.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/api/hh.xml"
-						},
-						{
-							"sequences": [
-								{
-									"type": "sequence",
-									"name": "defseq",
-									"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/sequences/defseq.xml"
-								}
-							],
-							"endpoints": [
-								{
-									"type": "endpoint",
-									"name": "loadbalance",
-									"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/loadbalance.xml"
-								},
-								{
-									"type": "endpoint",
-									"name": "address",
-									"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/endpoints/address.xml"
-								}
-							],
-							"type": "api",
-							"name": "HelloWorld copy.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/api/HelloWorld copy.xml"
-						}
-					],
-					"sequences": [
-						{
-							"sequences": [
-
-							],
-							"endpoints": [
-
-							],
-							"type": "sequences",
-							"name": "defseq.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/sequences/defseq.xml"
-						}
-					],
-					"inbound-endpoints": [
-						{
-							"sequences": [
-
-							],
-							"endpoints": [
-
-							],
-							"type": "inbound-endpoints",
-							"name": "ibe.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/inbound-endpoints/ibe.xml"
-						}
-					],
-					"tasks": [
-						{
-							"type": "tasks",
-							"name": "tem.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/tasks/tem.xml"
-						},
-						{
-							"type": "tasks",
-							"name": "adfa.xml",
-							"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloConfigs/src/main/synapse-config/tasks/adfa.xml"
-						}
-					]
-				},
-				"dataServiceConfigs": [
-					{
-						"type": "DATA_SERVICE_CONFIGS",
-						"name": "dssconfig",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/dssconfig"
-					}
-				],
-				"dataSourceConfigs": [
-					{
-						"type": "DATA_SOURCE_CONFIGS",
-						"name": "datasource",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/datasource"
-					},
-					{
-						"type": "DATA_SOURCE_CONFIGS",
-						"name": "test-datasource",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/test-datasource"
-					}
-				],
-				"mediatorProjects": [
-					{
-						"type": "MEDIATOR_PROJECT",
-						"name": "mediatorProject",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/mediatorProject"
-					}
-				],
-				"registryResources": [
-					{
-						"type": "REGISTRY_RESOURCE",
-						"name": "reg",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/reg"
-					},
-					{
-						"type": "REGISTRY_RESOURCE",
-						"name": "sampleregres",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/sampleregres"
-					}
-				],
-				"javaLibraryProjects": [
-
-				],
-				"compositeExporters": [
-					{
-						"type": "COMPOSITE_EXPORTER",
-						"name": "helloCompositeExporter",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/helloCompositeExporter"
-					}
-				],
-				"connectorExporters": [
-					{
-						"type": "CONNECTOR_EXPORTER",
-						"name": "utilityconnectorexpoertr",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/utilityconnectorexpoertr"
-					}
-				],
-				"dockerExporters": [
-					{
-						"type": "DOCKER_EXPORTER",
-						"name": "docker",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/docker"
-					},
-					{
-						"type": "DOCKER_EXPORTER",
-						"name": "dss",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/dss"
-					}
-				],
-				"kubernetesExporters": [
-					{
-						"type": "KUBERNETES_EXPORTER",
-						"name": "k8sexporter",
-						"path": "/Users/charukakarunanayake/test-projects/mi-tests/hello/k8sexporter"
-					}
-				]
-			}
-		};
-
-
-		console.log(resp)
-
-
 		return generateTreeData(resp);
-
 	}
 
 	return []
@@ -423,7 +136,7 @@ function genProjectStructureEntry(data: ProjectStructureEntry[]): ProjectExplore
 
 	for (const entry of data) {
 		const children = genProjectStructureEntry(entry.sequences ?? []).concat(genProjectStructureEntry(entry.endpoints ?? []))
-		result.push(new ProjectExplorerEntry(entry.name, isCollapsibleState(children.length > 0)));
+		result.push(new ProjectExplorerEntry(entry.name, isCollapsibleState(children.length > 0), entry),);
 	}
 
 	return result;
--- workspaces/mi/mi-extension/src/extension.ts
+++ workspaces/mi/mi-extension/src/extension.ts
@@ -17,10 +17,20 @@ import { ProjectExplorerEntryProvider } from './activity-panel/project-explorer-
 export async function activate(context: vscode.ExtensionContext) {
 
 	const projectExplorerDataProvider = new ProjectExplorerEntryProvider(context)
-	vscode.window.registerTreeDataProvider('project-explorer', projectExplorerDataProvider)
+	// vscode.window.registerTreeDataProvider('project-explorer', projectExplorerDataProvider)
+	const projectTree = vscode.window.createTreeView('project-explorer', { treeDataProvider: projectExplorerDataProvider })
 	vscode.commands.registerCommand('project-explorer.refreshEntry', () => { projectExplorerDataProvider.refresh() })
-	vscode.commands.registerCommand('project-explorer.add', () => { 
+	vscode.commands.registerCommand('project-explorer.add', () => {
 		// TODO: Form opening logic should go here
+
+	})
+
+	projectTree.onDidChangeSelection(e => {
+		if (e.selection.length > 0 && e.selection[0].info) {
+			const info = e.selection[0].info;
+			console.log(info);
+			// TODO: Open file logic should go here
+		}
 	})
 
 	let disposable = vscode.commands.registerCommand('integrationStudio.showDiagram', () => {
