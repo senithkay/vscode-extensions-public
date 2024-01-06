1c7561249798662ca96d81955d897e6e078d4db9
Madusha <madushajg@gmail.com>
2024-01-06 13:03:09 +0530

    Update intermediate node positions

--- workspaces/ballerina/data-mapper-view/src/components/Diagram/Node/QueryExpression/QueryExpressionNode.ts
+++ workspaces/ballerina/data-mapper-view/src/components/Diagram/Node/QueryExpression/QueryExpressionNode.ts
@@ -323,7 +323,7 @@ export class QueryExpressionNode extends DataMapperNodeModel {
     public updatePosition() {
         if (this.targetPort){
             const position = this.targetPort.getPosition()
-            this.setPosition(OFFSETS.QUERY_EXPRESSION_NODE.X, position.y - 5.5)
+            this.setPosition(OFFSETS.QUERY_EXPRESSION_NODE.X, position.y - 2)
         }
     }
 
