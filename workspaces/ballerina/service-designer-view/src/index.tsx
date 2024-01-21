a4292f67114aa6fdf715046dd8f3e6468953ad52
tharindulak <tharindu.jayathilake1@gmail.com>
2024-01-21 10:11:47 +0530

    Fix theming and font issues in storybook

--- workspaces/ballerina/service-designer-view/src/index.tsx
+++ workspaces/ballerina/service-designer-view/src/index.tsx
@@ -6,6 +6,5 @@
  * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
  * You may not alter or remove any copyright or other notice from copies of this content.
  */
-
 export * from "./ServiceDesigner/definitions";
 export { ServiceDesigner } from "./ServiceDesigner";
