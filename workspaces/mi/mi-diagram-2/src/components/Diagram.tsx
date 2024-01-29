13e6052d908cf13a55e5eeca3ab5655e440ede79
Joseph Fonseka <hevayo@gmail.com>
2024-01-29 18:14:26 +0530

    Add new diagram flow

--- workspaces/mi/mi-diagram-2/src/components/Diagram.tsx
+++ workspaces/mi/mi-diagram-2/src/components/Diagram.tsx
@@ -1,31 +1,14 @@
 import * as React from "react";
 import { DiagramEngine, DiagramModel, DefaultNodeModel, DiagramWidget } from "@projectstorm/react-diagrams";
-import { APIResource, Sequence } from "@wso2-enterprise/mi-syntax-tree/lib/src";
+import { APIResource, Sequence } from "@wso2-enterprise/mi-syntax-tree/src";
 
 interface DiagramProps {
     model: APIResource | Sequence;
 }
 
-export const Diagram: React.FC = () => {
-    //1. Create the diagram engine
-    var engine = new DiagramEngine();
+export const Diagram: React.FC<DiagramProps> = (props: DiagramProps) => {
 
-    // engine.installDefaultFactories();
-
-    //2. Create the diagram model
-    var model = new DiagramModel();
-
-    //3. Create a default node
-    var node1 = new DefaultNodeModel("Node 1", "rgb(0,192,255)");
-    let port = node1.addOutPort("Out");
-    node1.setPosition(100, 100);
-
-    //4. Add the node to the diagram model
-    model.addAll(node1);
-
-    //5. Load the model into the diagram engine
-    engine.setModel(model);
 
     //6. Render the diagram!
-    return <DiagramWidget className="diagram-container" diagramEngine={engine} />;
+    return <h1>Diagram: {props.model}</h1>;
 };
