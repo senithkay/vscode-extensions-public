cfe97f17f00c2f7c35dfcc6207b74106b7d56410
Kanushka Gayan <kanushkanet@gmail.com>
2024-01-30 07:30:47 +0530

    Add initial diagram setup

--- workspaces/mi/mi-diagram-2/src/components/Diagram.tsx
+++ workspaces/mi/mi-diagram-2/src/components/Diagram.tsx
@@ -1,14 +1,70 @@
-import * as React from "react";
-import { DiagramEngine, DiagramModel, DefaultNodeModel, DiagramWidget } from "@projectstorm/react-diagrams";
-import { APIResource, Sequence } from "@wso2-enterprise/mi-syntax-tree/src";
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
 
-interface DiagramProps {
+import React, { useState, useEffect } from "react";
+import { DiagramEngine, DiagramModel } from "@projectstorm/react-diagrams";
+import { CanvasWidget } from "@projectstorm/react-canvas-core";
+import { APIResource, Sequence, traversNode } from "@wso2-enterprise/mi-syntax-tree/src";
+import { SizingVisitor } from "../visitors/SizingVisitor";
+import { PositionVisitor } from "../visitors/PositionVisitor";
+import { NodeVisitor } from "../visitors/NodeVisitor";
+import { generateEngine } from "../utils/diagram";
+import { DiagramCanvas } from "./DiagramCanvas";
+
+export interface DiagramProps {
     model: APIResource | Sequence;
 }
 
-export const Diagram: React.FC<DiagramProps> = (props: DiagramProps) => {
+export function Diagram(props: DiagramProps) {
+    const { model } = props;
 
+    const [diagramEngine] = useState<DiagramEngine>(generateEngine());
+    const [diagramModel, setDiagramModel] = useState<DiagramModel | null>(null);
 
-    //6. Render the diagram!
-    return <h1>Diagram: {props.model}</h1>;
-};
+    useEffect(() => {
+        if (diagramEngine) {
+            drawDiagram();
+        }
+    }, []);
+
+    // run sizing visitor
+    const sizingVisitor = new SizingVisitor();
+    traversNode(model, sizingVisitor);
+    // run position visitor
+    const positionVisitor = new PositionVisitor();
+    traversNode(model, positionVisitor);
+    // run node visitor
+    const nodeVisitor = new NodeVisitor();
+    traversNode(model, nodeVisitor);
+    const nodes = nodeVisitor.getNodes();
+    console.log("nodes", nodes);
+
+    // add node edges
+    // setup react diagram engine
+    // render diagram
+
+    const drawDiagram = () => {
+        const model = new DiagramModel();
+
+        //TODO: add nodes to diagram model
+
+        diagramEngine.setModel(model);
+        setDiagramModel(model);
+    };
+
+    return (
+        <>
+            {diagramEngine && diagramModel && (
+                <DiagramCanvas>
+                    <CanvasWidget engine={diagramEngine} />
+                </DiagramCanvas>
+            )}
+        </>
+    );
+}
