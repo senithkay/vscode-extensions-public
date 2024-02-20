bccaf7bedf61eb7f398c6fbada20969257fbbfe3
Ravindu Wegiriya <ravinduwegiriya@gmail.com>
2024-02-20 15:22:05 +0530

    Add suggestive questions

--- workspaces/mi/mi-visualizer/src/constants/index.ts
+++ workspaces/mi/mi-visualizer/src/constants/index.ts
@@ -1,2 +1,3 @@
 export const SAMPLE_ICONS_GITHUB_URL = "https://raw.githubusercontent.com/wso2/integration-studio/main/SamplesForVSCode/icons/";
-export const MI_COPILOT_BACKEND_URL = "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/awwr/mi-copilot-backend/mi-copilot-backend-5de/v1.0";
+// export const MI_COPILOT_BACKEND_URL = "https://cf3a4176-54c9-4547-bcd6-c6fe400ad0d8-dev.e1-us-east-azure.choreoapis.dev/awwr/mi-copilot-backend/mi-copilot-backend-5de/v1.0";
+export const MI_COPILOT_BACKEND_URL = "http://127.0.0.1:8000/generate-synapse";
