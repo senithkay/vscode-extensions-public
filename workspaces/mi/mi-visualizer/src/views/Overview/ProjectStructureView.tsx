f554e7996a91c2093fc8ea9bbf96fcb8f641cf52
gigara <info@gigara.info>
2024-02-26 13:29:06 +0530

    Add license header

--- workspaces/mi/mi-visualizer/src/views/Overview/ProjectStructureView.tsx
+++ workspaces/mi/mi-visualizer/src/views/Overview/ProjectStructureView.tsx
@@ -1,6 +1,14 @@
-import React, { useState } from 'react';
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
 import { ProjectStructureEntry, ProjectStructureResponse, ProjectDirectoryMap, EsbDirectoryMap } from '@wso2-enterprise/mi-core';
-import { ComponentCard, Typography } from '@wso2-enterprise/ui-toolkit';
+import { ComponentCard } from '@wso2-enterprise/ui-toolkit';
 import styled from '@emotion/styled';
 import { useVisualizerContext } from '@wso2-enterprise/mi-rpc-client';
 
