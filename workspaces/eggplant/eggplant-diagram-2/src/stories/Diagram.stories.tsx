7ff5a0b66079ce1837fce10c1e272e2368a9be82
Kanushka Gayan <kanushkanet@gmail.com>
2024-02-27 18:44:18 +0530

    Add advanced stories for testing

--- workspaces/eggplant/eggplant-diagram-2/src/stories/Diagram.stories.tsx
+++ workspaces/eggplant/eggplant-diagram-2/src/stories/Diagram.stories.tsx
@@ -4,7 +4,8 @@ import { Diagram, DiagramProps } from "../components/Diagram";
 
 import basicModel from "./basic.json";
 import advancedModel from "./advanced.json";
-
+import advanced2Model from "./advanced-2.json";
+import advanced3Model from "./advanced-3.json";
 
 export default {
     title: "Example/Diagram",
@@ -21,4 +22,14 @@ Basic.args = {
 export const Advanced = Template.bind({});
 Advanced.args = {
     model: advancedModel,
+};
+
+export const Advanced2 = Template.bind({});
+Advanced2.args = {
+    model: advanced2Model,
+};
+
+export const Advanced3 = Template.bind({});
+Advanced3.args = {
+    model: advanced3Model,
 };
