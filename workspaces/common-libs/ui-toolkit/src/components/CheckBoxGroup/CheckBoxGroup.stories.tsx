909bd4c0b1c53b03e75314da0450359cd5286fa4
YasithD <yasith.18@cse.mrt.ac.lk>
2024-02-29 09:59:21 +0530

    Create CheckBoxGroup component

---
+++ workspaces/common-libs/ui-toolkit/src/components/CheckBoxGroup/CheckBoxGroup.stories.tsx
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+import React from "react";
+import { ComponentStory } from "@storybook/react";
+import { CheckBox, CheckBoxGroup, CheckBoxGroupProps } from "./CheckBoxGroup";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+    display: flex;
+    gap: 20px;
+`;
+
+const Template: ComponentStory<typeof CheckBoxGroup> = (args: CheckBoxGroupProps) => {
+    return (
+        <Container>
+            <CheckBoxGroup {...args}>
+                <CheckBox label="Option 1" value="option1" />
+                <CheckBox label="Option 2" value="option2" />
+                <CheckBox label="Option 3" value="option3" />
+                <CheckBox label="Option 4" value="option4" />
+                <CheckBox label="Option 5" value="option5" />
+            </CheckBoxGroup>
+        </Container>
+    );
+};
+
+export const Default = Template.bind();
+Default.args = {
+    onLabelChange: (selected: string[]) => {
+        console.log(selected);
+    },
+};
+
+export default { component: CheckBoxGroup, title: "CheckBoxGroup" };
---
+++ workspaces/common-libs/ui-toolkit/src/components/CheckBoxGroup/CheckBoxGroup.tsx
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+import styled from "@emotion/styled";
+import { VSCodeCheckbox } from "@vscode/webview-ui-toolkit/react";
+import React from "react";
+
+const CheckBoxContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+`;
+
+export type CheckBoxProps = {
+    label: string;
+    value: string | number;
+    checked?: boolean;
+    onClick?: (e: React.SyntheticEvent) => void;
+};
+
+export type CheckBoxGroupProps = React.ComponentPropsWithoutRef<"div"> & {
+    onLabelChange: (selected: string[]) => void;
+};
+
+export const CheckBox = ({ label, value, checked, onClick }: CheckBoxProps) => {
+    return (
+        <VSCodeCheckbox value={value} checked={checked} onClick={onClick}>
+            {label}
+        </VSCodeCheckbox>
+    );
+};
+
+export const CheckBoxGroup = ({ onLabelChange, children }: CheckBoxGroupProps) => {
+    const selected = React.useRef<string[]>([]);
+
+    return (
+        <CheckBoxContainer>
+            {React.Children.toArray(children).map(child => {
+                const checkBox = child as React.ReactElement<CheckBoxProps>;
+                const label = checkBox.props.label;
+                return React.cloneElement(checkBox, {
+                    onClick: (e: React.SyntheticEvent) => {
+                        const target = e.target as HTMLInputElement;
+                        if (target.checked) {
+                            selected.current = [...selected.current, label];
+                        } else {
+                            selected.current = selected.current.filter(item => item !== label);
+                        }
+                        onLabelChange(selected.current);
+                    },
+                    checked: selected.current.indexOf(label) > -1,
+                });
+            })}
+        </CheckBoxContainer>
+    );
+};
