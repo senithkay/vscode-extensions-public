e1d5939f6f7d899e7df383bb265e271b15332f42
YasithD <yasith.18@cse.mrt.ac.lk>
2024-03-07 10:50:39 +0530

    Update service edit template

--- workspaces/mi/mi-visualizer/src/utils/template-engine/mustache-templates/core/api.ts
+++ workspaces/mi/mi-visualizer/src/utils/template-engine/mustache-templates/core/api.ts
@@ -7,7 +7,7 @@
  * You may not alter or remove any copyright or other notice from copies of this content.
  */
 export function getEditServiceTemplate() {
-    return `<api name="{{name}}" context="{{context}}" version="{{version}}" version-type="{{version_type}}">`
+    return `<api name="{{{name}}}" context="{{{context}}}" version="{{{version}}}" version-type="{{version_type}}">`
 }
 
 export function getAddApiResourceTemplate() {
