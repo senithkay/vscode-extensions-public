42efec500a64acd81d04cd587c25cbe9fd363c63
tharindulak <tharindu.jayathilake1@gmail.com>
2024-03-12 11:59:52 +0530

    Add error boundary component

---
+++ workspaces/common-libs/ui-toolkit/src/components/ErrorBoundary/Error/Error.tsx
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+import * as React from "react";
+
+import { useStyles } from "./style";
+import { Typography } from "../../Typography/Typography";
+import { Codicon } from "../../Codicon/Codicon";
+
+export interface ErrorProps {
+    errorMsg?: string;
+}
+
+export default function Default(props: ErrorProps) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Codicon name="error" sx={{height: "100px", width: "100px"}} iconSx={{ fontSize: 100, color: "var(--vscode-errorForeground)" }} />
+            <Typography variant="h4" className={classes.errorTitle}>
+                {props.errorMsg ? props.errorMsg : "A problem occurred."}
+            </Typography>
+            <Typography variant="body2" className={classes.errorMsg}>
+                Please raise an issue in our <a href="https://github.com/wso2/ballerina-plugin-vscode/issues">issue tracker</a>
+            </Typography>
+        </div>
+    );
+}
--- workspaces/common-libs/ui-toolkit/src/index.ts
+++ workspaces/common-libs/ui-toolkit/src/index.ts
@@ -41,6 +41,7 @@ export * from './components/Swich/Swich';
 export * from './components/ClickAwayListener/ClickAwayListener';
 export * from './components/Confirm/Confirm';
 export * from './components/CheckBoxGroup/CheckBoxGroup';
+export * from './components/ErrorBoundary/ErrorBoundary';
 
 export * from './components/SampleForm/Header/Header';
 export * from './components/SampleForm/HorizontalIcons/HorizontalIcons';
