15016c638d5b3766e2c2ba69f4ded3c06bd26e75
Anjana S Porawagama <axewilledge123@gmail.com>
2024-03-14 12:50:46 +0530

    Add license headers

--- workspaces/mi/mi-visualizer/src/AiPanel.tsx
+++ workspaces/mi/mi-visualizer/src/AiPanel.tsx
@@ -1,8 +1,15 @@
+/**
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
 import React, { useEffect, useState } from 'react';
 import { AI_MACHINE_VIEW } from '@wso2-enterprise/mi-core';
 import { useVisualizerContext } from '@wso2-enterprise/mi-rpc-client';
-import { APIWizard, APIWizardProps } from './views/Forms/APIform';
-import { EndpointWizard } from './views/Forms/EndpointForm';
 import { VSCodeProgressRing } from '@vscode/webview-ui-toolkit/react';
 import styled from '@emotion/styled';
 import { AIOverviewWindow } from './views/AIOverviewWindow';
@@ -38,9 +45,6 @@ const AiPanel = () => {
             switch (machineView?.view) {
                 case AI_MACHINE_VIEW.AIOverview:
                     setViewComponent(<AIOverviewWindow />);
-                    break;
-                case AI_MACHINE_VIEW.AIChat:
-                    setViewComponent(<AIChat/>);
                     break;
                 case AI_MACHINE_VIEW.AIArtifact:
                     setViewComponent(<AIArtifactWindow/>);
