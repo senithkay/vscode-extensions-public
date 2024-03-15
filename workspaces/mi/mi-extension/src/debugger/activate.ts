8e5403665e9c679c0045cee85c7bcf12a661a884
sachiniSam <sachiniprof@gmail.com>
2024-03-15 13:53:28 +0530

    Refactor code

--- workspaces/mi/mi-extension/src/debugger/activate.ts
+++ workspaces/mi/mi-extension/src/debugger/activate.ts
@@ -15,31 +15,31 @@ import { extension } from '../MIExtensionContext';
 
 
 class MiConfigurationProvider implements vscode.DebugConfigurationProvider {
-	
-	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
-		// if launch.json is missing or empty
-		if (!config.type && !config.request && !config.name) {
-			config.type = 'mi';
-			config.name = 'MI: Run';
-			config.request = 'launch';
-		}
 
-		return config;
-	}
+    resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+        // if launch.json is missing or empty
+        if (!config.type && !config.request && !config.name) {
+            config.type = 'mi';
+            config.name = 'MI: Run';
+            config.request = 'launch';
+        }
+
+        return config;
+    }
 }
 
 export function activateDebugger(context: vscode.ExtensionContext) {
 
-	vscode.commands.registerCommand(COMMANDS.CHANGE_SERVER_PATH, async () => { 
+    vscode.commands.registerCommand(COMMANDS.CHANGE_SERVER_PATH, async () => {
         const addServer: string = "Add MI Server";
         const currentServerPath: string | undefined = extension.context.globalState.get(SELECTED_SERVER_PATH);
         const quickPicks: vscode.QuickPickItem[] = [];
         if (!currentServerPath) {
-            quickPicks.push({label: addServer});
+            quickPicks.push({ label: addServer });
         } else {
             quickPicks.push({ kind: vscode.QuickPickItemKind.Separator, label: "current server path" });
-            quickPicks.push({label: currentServerPath, },
-            {label: addServer});
+            quickPicks.push({ label: currentServerPath, },
+                { label: addServer });
         }
         const options: vscode.QuickPickOptions = { canPickMany: false, title: "Select Server Path" };
         const selected = await vscode.window.showQuickPick(quickPicks, options);
@@ -57,37 +57,37 @@ export function activateDebugger(context: vscode.ExtensionContext) {
                     return undefined;
                 }
 
-                await extension.context.globalState.update(SELECTED_SERVER_PATH,folders[0].fsPath);
+                await extension.context.globalState.update(SELECTED_SERVER_PATH, folders[0].fsPath);
             } else {
                 await extension.context.globalState.update(SELECTED_SERVER_PATH, selected.label);
             }
         }
-     });
+    });
 
     // register a configuration provider for 'mi' debug type
     const provider = new MiConfigurationProvider();
-	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('mi', provider));
+    context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('mi', provider));
 
-	// register a dynamic configuration provider for 'mi' debug type
-	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('mi', {
-		provideDebugConfigurations(folder: WorkspaceFolder | undefined): ProviderResult<DebugConfiguration[]> {
-			return [
-				{
-					name: "MI: Run",
-					request: "launch",
-					type: "mi"
-				}
-			];
-		}
-	}, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
+    // register a dynamic configuration provider for 'mi' debug type
+    context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('mi', {
+        provideDebugConfigurations(folder: WorkspaceFolder | undefined): ProviderResult<DebugConfiguration[]> {
+            return [
+                {
+                    name: "MI: Run",
+                    request: "launch",
+                    type: "mi"
+                }
+            ];
+        }
+    }, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
 
     const factory = new InlineDebugAdapterFactory();
     context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('mi', factory));
 }
 
 class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
-	
-	createDebugAdapterDescriptor(_session: vscode.DebugSession): ProviderResult<vscode.DebugAdapterDescriptor> {
-		return new vscode.DebugAdapterInlineImplementation(new MiDebugAdapter());
-	}
+
+    createDebugAdapterDescriptor(_session: vscode.DebugSession): ProviderResult<vscode.DebugAdapterDescriptor> {
+        return new vscode.DebugAdapterInlineImplementation(new MiDebugAdapter());
+    }
 }
