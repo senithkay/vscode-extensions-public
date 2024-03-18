6ce6bc731d560ebdc49620091ab944e516e59c75
Kanushka Gayan <kanushkanet@gmail.com>
2024-03-18 11:37:38 +0530

    Fix control offset calculation in AdvancedLinkModel and lock ProjectModel

--- workspaces/choreo/cell-diagram/src/components/Project/AdvancedLink/AdvancedLinkModel.ts
+++ workspaces/choreo/cell-diagram/src/components/Project/AdvancedLink/AdvancedLinkModel.ts
@@ -63,22 +63,20 @@ export class AdvancedLinkModel extends LinkModel<DefaultLinkModelGenerics> {
     }
 
     getSVGPath(): string {
-        if (this.points.length == 2 || true) {
-            const curve = new BezierCurve();
-            curve.setSource(this.getFirstPoint().getPosition());
-            curve.setTarget(this.getLastPoint().getPosition());
-            curve.setSourceControl(this.getFirstPoint().getPosition().clone());
-            curve.setTargetControl(this.getLastPoint().getPosition().clone());
+        const curve = new BezierCurve();
+        curve.setSource(this.getFirstPoint().getPosition());
+        curve.setTarget(this.getLastPoint().getPosition());
+        curve.setSourceControl(this.getFirstPoint().getPosition().clone());
+        curve.setTargetControl(this.getLastPoint().getPosition().clone());
 
-            if (this.sourcePort) {
-                curve.getSourceControl().translate(...this.calculateControlOffset(this.getSourcePort()));
-            }
-
-            if (this.targetPort) {
-                curve.getTargetControl().translate(...this.calculateControlOffset(this.getTargetPort()));
-            }
-            return curve.getSVGCurve();
+        if (this.sourcePort) {
+            curve.getSourceControl().translate(...this.calculateControlOffset(this.getSourcePort()));
         }
+
+        if (this.targetPort) {
+            curve.getTargetControl().translate(...this.calculateControlOffset(this.getTargetPort()));
+        }
+        return curve.getSVGCurve();
     }
 
     getSVGPathSegment(firstPoint: PointModel, lastPoint: PointModel): string {
@@ -178,7 +176,7 @@ export class AdvancedLinkModel extends LinkModel<DefaultLinkModelGenerics> {
         return path;
     }
 
-    calculateControlOffsetWithPosition(rightTraget:boolean, bottomTarget:boolean): [number, number] {
+    calculateControlOffsetWithPosition(rightTraget: boolean, bottomTarget: boolean): [number, number] {
         if (rightTraget) {
             return [this.options.curvyness, 0];
         } else if (!rightTraget) {
