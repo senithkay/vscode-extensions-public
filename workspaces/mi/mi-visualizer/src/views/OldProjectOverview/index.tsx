7b53b06332a922cc12a133f794664f5b6b1fcc2d
YasithD <yasith.18@cse.mrt.ac.lk>
2024-03-25 10:55:26 +0530

    Update old project overview content

--- workspaces/mi/mi-visualizer/src/views/OldProjectOverview/index.tsx
+++ workspaces/mi/mi-visualizer/src/views/OldProjectOverview/index.tsx
@@ -7,61 +7,129 @@
  * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
-import React, { useEffect } from "react";
-import { WorkspaceFolder } from "@wso2-enterprise/mi-core";
-import { useVisualizerContext } from "@wso2-enterprise/mi-rpc-client";
-import { Button, Icon, Codicon } from "@wso2-enterprise/ui-toolkit";
-import styled from "@emotion/styled";
-import { View, ViewContent, ViewHeader } from "../../components/View";
+import React, { useEffect } from 'react';
+import { WorkspaceFolder } from '@wso2-enterprise/mi-core';
+import { useVisualizerContext } from '@wso2-enterprise/mi-rpc-client';
+import { Button, Typography } from '@wso2-enterprise/ui-toolkit';
+import styled from '@emotion/styled';
+import { View, ViewContent, ViewHeader } from '../../components/View';
 
-const ProjectActions = styled.div({
-    display: 'flex',
-    alignItems: 'center',
-    gap: '5px'
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 36px;
+  padding: 85px 120px;
+`;
 
-});
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const Headline = styled.div`
+  font-size: 2.7em;
+  font-weight: 400;
+  font-size: 2.7em;
+  white-space: nowrap;
+  padding-bottom: 10px;
+`;
+
+const SubTitle = styled.div`
+  font-weight: 400;
+  margin-top: 0;
+  margin-bottom: 5px;
+  font-size: 1.5em;
+  line-height: normal;
+`;
+
+const Card = styled.div`
+  border: 1px solid var(--vscode-activityBar-dropBorder);
+  border-radius: 8px;
+  padding: 16px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
 
 export function OldProjectOverview() {
-    const { rpcClient } = useVisualizerContext();
-    const [activeWorkspaces, setActiveWorkspaces] = React.useState<WorkspaceFolder>(undefined);
+  const { rpcClient } = useVisualizerContext();
+  const [activeWorkspaces, setActiveWorkspaces] = React.useState<WorkspaceFolder>(undefined);
 
-    useEffect(() => {
-        rpcClient.getMiVisualizerRpcClient().getWorkspaces().then((response) => {
-            setActiveWorkspaces(response.workspaces[0]);
-            console.log(response.workspaces[0]);
-        });
-    }, []);
+  const handleMigrateProject = async () => {
+    rpcClient
+      .getMiDiagramRpcClient()
+      .migrateProject({ source: activeWorkspaces?.fsPath })
+      .then(response => {
+        console.log(response);
+      });
+  };
 
-    return (
-        <View>
-            <ViewHeader title={"Project: " + activeWorkspaces?.name} codicon="project">
-                <ProjectActions>
-                    <Button
-                        appearance="icon"
-                        onClick={() => { }}
-                        tooltip="Icon Button"
-                    >
-                        <Codicon
-                            name="combine"
-                        />
-                        &nbsp;&nbsp;Build
+  useEffect(() => {
+    rpcClient
+      .getMiVisualizerRpcClient()
+      .getWorkspaces()
+      .then(response => {
+        setActiveWorkspaces(response.workspaces[0]);
+        console.log(response.workspaces[0]);
+      });
+  }, []);
 
-                    </Button>
-                    <Button
-                        appearance="icon"
-                        tooltip="Deploy your integration on the Cloud with Choreo">
-                        <Icon
-                            name="choreo"
-                            sx={{ marginTop: '2px', color: '#5567d5' }}
-                        />
-                        &nbsp;Deploy in Choreo
-                    </Button>
-                </ProjectActions>
-            </ViewHeader>
-            <ViewContent padding>
+  return (
+    <View>
+      <ViewHeader title={'Project: ' + activeWorkspaces?.name} codicon='project' />
+      <ViewContent padding>
+        <Container>
+          <Block>
+            <Headline>MI VSCode Extension</Headline>
+            <Typography variant='body3'>
+              Elevate your digital transformation journey with the MI VSCode Extension, your comprehensive integration
+              solution. This tool streamlines the connectivity between applications, services, data, and the cloud
+              through a user-friendly, low-code graphical interface. For further details, please consult the{' '}
+              <a href='https://ei.docs.wso2.com' target='_blank' rel='noopener noreferrer'>
+                official documentation
+              </a>
+              .
+            </Typography>
+          </Block>
+          <Block>
+            <SubTitle>Integration Studio Projects</SubTitle>
+            <Typography variant='body3'>
+              Although Integration Studio is now a legacy platform for integration design and development, it retains
+              limited compatibility with the MI VSCode extension. Users can still modify existing artifacts and view
+              project diagrams. Creation of new artifacts within the VSCode extension is not supported, requiring the
+              use of Integration Studio for these activities.
+            </Typography>
+            <Card>
+              <Block>
+                <Typography variant='body1'>Utilizing the Extension with Integration Studio Projects</Typography>
+                <Typography variant='body3'>
+                  To access the diagram view of your project, simply click the "Open Diagram" button within your synapse
+                  file.
+                </Typography>
+                {/* TODO: Import image */}
+              </Block>
+            </Card>
+            <Typography variant='body3'>
+              For an enhanced experience, migrating your project to a fully supported format by the VSCode extension is
+              advisable. The migration tool facilitates the conversion of your Integration Studio project into a
+              compatible format with the MI VSCode extension, ensuring full access to all features offered by the
+              extension without restrictions.
+            </Typography>
+            <Typography variant='body3'>
+              Begin the migration process by selecting the "Migrate Project" option below.
+            </Typography>
+            <ButtonContainer>
+              <Button appearance='primary' onClick={handleMigrateProject}>
                 Migrate
-            </ViewContent>
-        </View>
-    );
+              </Button>
+            </ButtonContainer>
+          </Block>
+        </Container>
+      </ViewContent>
+    </View>
+  );
 }
-
