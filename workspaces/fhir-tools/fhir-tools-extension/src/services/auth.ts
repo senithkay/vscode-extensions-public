c2aeb3cc8efaee6b06d07d460bcc685441335992
joelsathi <joelsathiyendra@gmail.com>
2024-03-25 11:27:49 +0530

    update configurations

--- workspaces/fhir-tools/fhir-tools-extension/src/services/auth.ts
+++ workspaces/fhir-tools/fhir-tools-extension/src/services/auth.ts
@@ -9,12 +9,13 @@
 
 import axios from 'axios';
 import { ERROR_MESSAGES } from '../constants/messages';
-import { TOKEN_ENDPOINT, APIKEY, ENABLE_AUTHORIZATION } from '../constants/config';
+import { CDA_ENABLE_AUTHORIZATION, HL7v2_ENABLE_AUTHORIZATION, HL7v2_TOKEN_ENDPOINT, HL7v2_API_KEY, CDA_API_KEY, CDA_TOKEN_ENDPOINT } from '../constants/config';
 
 let cachedToken: string | undefined = undefined;
+let cachedToken_hl7v2: string | undefined = undefined;
+let cachedToken_cda: string | undefined = undefined;
 
 const auth = axios.create({
-    baseURL: TOKEN_ENDPOINT,
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     },
@@ -24,8 +25,13 @@ const auth = axios.create({
  * @description Clear the cached token
  * @returns - void
  */
-export function clearCachedToken() {
-    cachedToken = undefined;
+export function clearCachedToken(type: string) {
+    if (type === 'hl7v2') {
+        cachedToken_hl7v2 = undefined;
+    }
+    if (type === 'cda') {
+        cachedToken_cda = undefined;
+    }
 }
 
 /**
@@ -34,27 +40,35 @@ export function clearCachedToken() {
  * @param clientSecret - The client secret
  * @returns - The auth token
  */
-export async function getToken(refresh = false): Promise<string | Error> {
-    if (!ENABLE_AUTHORIZATION) {
+export async function getToken(convType:string, refresh = false): Promise<string | Error> {
+    const enableAuth = convType === 'HL7v2' ? HL7v2_ENABLE_AUTHORIZATION : CDA_ENABLE_AUTHORIZATION;
+    if (!enableAuth) {
         return '';
     }
     
     if (refresh) {
+        cachedToken_hl7v2 = undefined;
+        cachedToken_cda = undefined;
         cachedToken = undefined;
     }
 
+    cachedToken = convType === 'HL7v2' ? cachedToken_hl7v2 : cachedToken_cda;
     if (cachedToken) {
         return cachedToken;
     }
 
-    if (APIKEY === '' || APIKEY === undefined) {
+    const apikey = convType === 'HL7v2' ? HL7v2_API_KEY : CDA_API_KEY;
+    if (apikey === '' || apikey === undefined) {
         return new Error(
             ERROR_MESSAGES.API_KEY_NOT_FOUND +
                 '::LOG::' +
                 ERROR_MESSAGES.API_KEY_NOT_FOUND,
         );
     }
-    if (TOKEN_ENDPOINT === '' || TOKEN_ENDPOINT === undefined) {
+
+    const tokenEndpoint = convType === 'HL7v2' ? HL7v2_TOKEN_ENDPOINT: CDA_TOKEN_ENDPOINT;
+    auth.defaults.baseURL = tokenEndpoint;
+    if (tokenEndpoint === '' || tokenEndpoint === undefined) {
         return new Error(
             ERROR_MESSAGES.TOKEN_ENDPOINT_NOT_FOUND +
                 '::LOG::' +
@@ -62,16 +76,26 @@ export async function getToken(refresh = false): Promise<string | Error> {
         );
     }
 
-    try{
+    try {
         const response = await auth.post('/', "grant_type=client_credentials", {
             headers: {
-                Authorization: `Basic ${Buffer.from(APIKEY).toString('base64')}`
+                Authorization: `Basic ${Buffer.from(apikey).toString('base64')}`
             }
         });
-        cachedToken = response.data.access_token;
-        return cachedToken;
-    }
-    catch (error) {
+        if (convType === 'HL7v2') {
+            cachedToken_hl7v2 = response.data.access_token;
+            return cachedToken_hl7v2;
+        }
+        cachedToken_cda = response.data.access_token;
+        return cachedToken_cda;
+    } catch (error) {
+        if ((error as any).code === 'ENOTFOUND') {
+            throw new Error(
+                ERROR_MESSAGES.INTERNET_CONNECTION_ERROR +
+                    '::LOG::' +
+                    ERROR_MESSAGES.INTERNET_CONNECTION_ERROR,
+            );
+        }
         throw new Error(
             ERROR_MESSAGES.INVALID_AUTH_CREDETIALS +
                 '::LOG::' +
