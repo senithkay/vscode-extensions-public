49d26344461c8f6bc318424cfc2d63c1b614d7df
Kanushka Gayan <kanushkanet@gmail.com>
2024-04-01 18:50:15 +0530

    Refactor Template function in Diagram.stories.tsx

--- workspaces/ballerina/sequence-diagram/src/stories/Diagram.stories.tsx
+++ workspaces/ballerina/sequence-diagram/src/stories/Diagram.stories.tsx
@@ -9,9 +9,7 @@ export default {
     component: Diagram,
 } as Meta;
 
-const Template: Story<DiagramProps> = (
-    args: React.JSX.IntrinsicAttributes & DiagramProps,
-) => <Diagram {...args} />;
+const Template: Story<DiagramProps> = (args: React.JSX.IntrinsicAttributes & DiagramProps) => <Diagram {...args} />;
 
 export const Empty = Template.bind({});
 Empty.args = {
