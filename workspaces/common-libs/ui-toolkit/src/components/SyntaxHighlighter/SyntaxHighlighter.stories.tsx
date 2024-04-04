ca4702bcdb16c7ee842735a08cab7bd47ecdd340
tharindulak <tharindu.jayathilake1@gmail.com>
2024-04-04 14:24:20 +0530

    Add Syntax highlighter

---
+++ workspaces/common-libs/ui-toolkit/src/components/SyntaxHighlighter/SyntaxHighlighter.stories.tsx
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+import React from "react";
+import { ComponentStory } from "@storybook/react";
+import { SyntaxHighlighter, SyntaxHighlighterProps } from "./SyntaxHighlighter";
+
+const xmlCode = `<note>
+<to city="Colombo">User</to>
+<from>WSO2</from>
+<heading>Reminder</heading>
+<body>Don't forget to subscribe!</body>
+</note>`;
+
+const jsonCode = '{"name":"John", "age":30, "car":null}';
+const javaScriptCode = `function add(a, b) {
+    return a + b;
+}`;
+
+const Template: ComponentStory<typeof SyntaxHighlighter> = (args: SyntaxHighlighterProps) => <SyntaxHighlighter {...args} />;
+
+export const SampleXML = Template.bind();
+SampleXML.args = { code: xmlCode, language: "xml" };
+
+export const SampleJSON = Template.bind();
+SampleJSON.args = { code: jsonCode, language: "json" };
+
+export const SampleJS = Template.bind();
+SampleJS.args = { code: javaScriptCode, language: "javascript" };
+
+export default { component: SyntaxHighlighter, title: "Syntax Highlighter" };
---
+++ workspaces/common-libs/ui-toolkit/src/components/SyntaxHighlighter/SyntaxHighlighter.tsx
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+import React, { useEffect } from "react";
+import Prism from 'prismjs';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-csharp';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-sql';
+import "@wso2-enterprise/font-wso2-vscode/dist/wso2-vscode.css";
+import "./theme.css";
+
+import styled from "@emotion/styled";
+
+interface ContainerProps {
+    sx?: any;
+}
+
+const Container = styled.div<ContainerProps>`
+    ${(props: ContainerProps) => props.sx};
+`;
+
+export interface SyntaxHighlighterProps {
+    id?: string;
+    code: string;
+    language?: string;
+    className?: string;
+    sx?: any;
+}
+
+export const SyntaxHighlighter: React.FC<SyntaxHighlighterProps> = (props: SyntaxHighlighterProps) => {
+    const { id, className, sx, code, language } = props;
+
+    useEffect(() => {
+        Prism.highlightAll();
+    }, [code]);
+    
+    return (
+        <Container id={id} className={className} sx={sx}>
+            <pre>
+                <code className={`language-${language}`}>
+                    {code}
+                </code>
+            </pre>
+        </Container>
+    );
+};
