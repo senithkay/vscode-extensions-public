a6f9f7f106ef5ca19d4235012f02dc83331ef0f8
kaje94 <a.kajendran@gmail.com>
2024-04-12 05:59:35 +0530

    Add minor fix to invalid workspace component

--- workspaces/choreo/choreo-webviews/src/views/ComponentListView/InvalidWorkspaceView.tsx
+++ workspaces/choreo/choreo-webviews/src/views/ComponentListView/InvalidWorkspaceView.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { ChoreoWebViewAPI } from "../../utilities/WebViewRpc";
+import { VSCodeLink } from "@vscode/webview-ui-toolkit/react";
 
 export const InvalidWorkspaceView = () => {
+    const openFolder = () => {
+        ChoreoWebViewAPI.getInstance().triggerCmd("vscode.openFolder");
+    };
+
     return (
         <div className="w-full flex flex-col px-6 py-2 gap-[10px]">
-            <p>To use the Choreo extension, please open a directory in VS Code.</p>
+            <p>
+                To use the Choreo extension, please <VSCodeLink onClick={openFolder}>open a folder</VSCodeLink> in VS
+                Code.
+            </p>
         </div>
     );
 };
