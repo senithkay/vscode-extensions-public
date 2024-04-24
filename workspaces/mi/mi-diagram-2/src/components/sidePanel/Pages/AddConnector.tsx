11fb7c2ade259de2cafd3ae5202f8a63bc3cb3ee
kaumini <kauminikg@gmail.com>
2024-04-24 10:53:17 +0530

    Fix issue of connectionName not getting added

--- workspaces/mi/mi-diagram-2/src/components/sidePanel/Pages/AddConnector.tsx
+++ workspaces/mi/mi-diagram-2/src/components/sidePanel/Pages/AddConnector.tsx
@@ -189,15 +189,17 @@ const AddConnector = (props: AddConnectorProps) => {
 
             const operationName = props.formData?.operationName ?? props.operationName ??
                 sidePanelContext.formValues.operationName;
-                
-            const root = template.ele(`${connectorName}${operationName ? `.${operationName}` : ''}`);
-            root.att('configKey', formValues['configKey']);
+
+            const root = template.ele(`${connectorName}${operationName ? `.${operationName}` : ''}`)
+                .att('configKey', formValues['configKey']);
+
             // Fill the values
             Object.keys(formValues).forEach((key) => {
                 if (key !== 'configRef' && key !== 'configKey') {
                     root.ele(key).txt(formValues[key]);
                 }
             });
+
             const modifiedXml = template.end({ prettyPrint: true, headless: true });
 
             rpcClient.getMiDiagramRpcClient().applyEdit({
