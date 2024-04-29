f9b5cbbd52011c12624927eb193c3597704920db
YasithD <yasith.18@cse.mrt.ac.lk>
2024-04-29 11:48:47 +0530

    Create an output channel for MI

---
+++ workspaces/mi/mi-extension/src/util/logger.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+import * as vscode from 'vscode';
+
+export const outputChannel = vscode.window.createOutputChannel("Micro Integrator");
+
+function withNewLine(value: string) {
+    if (typeof value === 'string' && !value.endsWith('\n')) {
+        return value += '\n';
+    }
+    return value;
+}
+
+// This function will log the value to the MI output channel
+export function log(value: string): void {
+    const output = withNewLine(value);
+    console.log(output);
+    outputChannel.append(output);
+}
+
+export function getOutputChannel() {
+    return outputChannel;
+}
