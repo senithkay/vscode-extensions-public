22201fe388302c4c5ee9472497a032ee6c4ddfb4
Lahiru Madushanka <gdlmadushanka@gmail.com>
2024-06-17 11:06:21 +0530

    Revert "Change samples location to CDN."

--- workspaces/mi/mi-visualizer/src/views/SamplesView.tsx
+++ workspaces/mi/mi-visualizer/src/views/SamplesView.tsx
@@ -111,6 +111,7 @@ export function SamplesView() {
     }
 
     const handleSearch = (searchText: string) => {
+        console.log("searchText", searchText);
         setSearch(searchText);
         if (searchText !== "") {
             let filteredData = filteredSampleDataCopy.filter(sample => sample.title.toLowerCase().includes(searchText.toLowerCase()));
