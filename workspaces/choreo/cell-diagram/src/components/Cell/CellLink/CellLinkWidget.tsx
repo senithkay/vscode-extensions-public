7293bb58bc6d94c1ffe8f3ad1e4cf9acf1b5e7ab
Kanushka Gayan <kanushkanet@gmail.com>
2024-06-24 11:45:42 +0530

    Update link popovers with mui popper component

--- workspaces/choreo/cell-diagram/src/components/Cell/CellLink/CellLinkWidget.tsx
+++ workspaces/choreo/cell-diagram/src/components/Cell/CellLink/CellLinkWidget.tsx
@@ -4,10 +4,11 @@ import { CellLinkModel } from "./CellLinkModel";
 import { CELL_LINK, Colors, WarningIcon } from "../../../resources";
 import { ObservationLabel } from "../../ObservationLabel/ObservationLabel";
 import { TooltipLabel } from "../../TooltipLabel/TooltipLabel";
-import { Popover } from "@wso2-enterprise/ui-toolkit";
 import { DiagramContext } from "../../DiagramContext/DiagramContext";
 import { DiagramLayer } from "../../../types";
 import { SharedLink } from "../../SharedLink/SharedLink";
+import Popper from "@mui/material/Popper";
+import Box from "@mui/material/Box";
 
 interface WidgetProps {
     engine: DiagramEngine;
@@ -165,15 +166,12 @@ export function CellLinkWidget(props: WidgetProps) {
                 {hasDiffLayer && link.observationOnly && <WarningIcon x={midPoint.x - 10} y={midPoint.y - 10} width="20" height="20" />}
             </g>
             {(hasObservabilityLayer || link.tooltip) && (
-                <Popover
-                    id={link.getID()}
-                    open={open}
-                    anchorEl={anchorEl}
-                    sx={link.observations?.length > 0 && !link.tooltip ? observabilityPopOverStyle : tooltipPopOverStyle}
-                >
-                    {link.tooltip && <TooltipLabel tooltip={link.tooltip} />}
-                    {link.observations?.length > 0 && !link.tooltip && <ObservationLabel observations={link.observations} />}
-                </Popover>
+                <Popper id={link.getID()} open={open} anchorEl={anchorEl}>
+                    <Box sx={link.observations?.length > 0 && !link.tooltip ? observabilityPopOverStyle : tooltipPopOverStyle}>
+                        {link.tooltip && <TooltipLabel tooltip={link.tooltip} />}
+                        {link.observations?.length > 0 && !link.tooltip && <ObservationLabel observations={link.observations} />}
+                    </Box>
+                </Popper>
             )}
         </>
     );
