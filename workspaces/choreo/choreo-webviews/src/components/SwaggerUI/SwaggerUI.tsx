93c5ab09c0aceb6f41040e9b975eb42e8544ebe8
kaje94 <a.kajendran@gmail.com>
2024-06-27 12:09:44 +0530

    Move swage ui styles to ui toolkit

--- workspaces/choreo/choreo-webviews/src/components/SwaggerUI/SwaggerUI.tsx
+++ workspaces/choreo/choreo-webviews/src/components/SwaggerUI/SwaggerUI.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import SwaggerUIReact, { SwaggerUIProps } from "swagger-ui-react";
-import "./styles/main.scss";
+import '@wso2-enterprise/ui-toolkit/src/styles/swagger/main.scss';
 
 export const SwaggerUI: FC<SwaggerUIProps> = (props) => {
     return <SwaggerUIReact {...props} />;
