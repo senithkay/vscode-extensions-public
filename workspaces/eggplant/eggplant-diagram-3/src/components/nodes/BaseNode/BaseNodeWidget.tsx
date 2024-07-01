900bc27aee3996e9127101d32a6acede32f4f105
Kanushka Gayan <kanushkanet@gmail.com>
2024-07-01 20:25:27 +0530

    Add initial eggplant diagram package

---
+++ workspaces/eggplant/eggplant-diagram-3/src/components/nodes/BaseNode/BaseNodeWidget.tsx
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+import React from "react";
+import styled from "@emotion/styled";
+import { DiagramEngine, PortWidget } from "@projectstorm/react-diagrams-core";
+import { BaseNodeModel } from "./BaseNodeModel";
+import { Colors, NODE_HEIGHT, NODE_WIDTH } from "../../../resources/constants";
+import { Button } from "@wso2-enterprise/ui-toolkit";
+import { CodeIcon, MoreVertIcon } from "../../../resources";
+import { Node } from "../../../utils/types";
+import NodeIcon from "../../NodeIcon";
+
+export namespace NodeStyles {
+    export type NodeStyleProp = {
+        selected: boolean;
+        hovered: boolean;
+    };
+    export const Node = styled.div<NodeStyleProp>`
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        min-width: ${NODE_WIDTH}px;
+        min-height: ${NODE_HEIGHT}px;
+        padding: 0 8px;
+        border: 1.5px solid
+            ${(props: NodeStyleProp) =>
+                props.selected ? Colors.PRIMARY : props.hovered ? Colors.PRIMARY : Colors.OUTLINE_VARIANT};
+        border-radius: 10px;
+        background-color: ${Colors.SURFACE_DIM};
+        color: ${Colors.ON_SURFACE};
+        /* cursor: pointer; */
+    `;
+
+    export const Header = styled.div<{}>`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        width: 100%;
+        padding: 8px;
+    `;
+
+    export const StyledButton = styled(Button)`
+        border-radius: 5px;
+        position: absolute;
+        right: 6px;
+    `;
+
+    export const TopPortWidget = styled(PortWidget)`
+        margin-top: -3px;
+    `;
+
+    export const BottomPortWidget = styled(PortWidget)`
+        margin-bottom: -2px;
+    `;
+
+    export const StyledText = styled.div`
+        font-size: 14px;
+    `;
+
+    export const Icon = styled.div`
+        padding: 4px;
+        svg {
+            fill: ${Colors.ON_SURFACE};
+        }
+    `;
+
+    export const Title = styled(StyledText)`
+        max-width: ${NODE_WIDTH - 50}px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-family: "GilmerMedium";
+    `;
+
+    export const Description = styled(StyledText)`
+        font-size: 12px;
+        max-width: ${NODE_WIDTH - 50}px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-family: monospace;
+    `;
+
+    export const Row = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+    `;
+
+    export const Hr = styled.hr`
+        width: 100%;
+    `;
+}
+
+interface BaseNodeWidgetProps {
+    model: BaseNodeModel;
+    engine: DiagramEngine;
+    onClick?: (node: Node) => void;
+}
+
+export interface NodeWidgetProps extends Omit<BaseNodeWidgetProps, "children"> {}
+
+export function BaseNodeWidget(props: BaseNodeWidgetProps) {
+    const { model, engine, onClick } = props;
+    const [isHovered, setIsHovered] = React.useState(false);
+
+    const handleOnClick = () => {
+        onClick && onClick(model.node);
+    };
+
+    return (
+        <NodeStyles.Node
+            selected={model.isSelected()}
+            hovered={isHovered}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <NodeStyles.TopPortWidget port={model.getPort("in")!} engine={engine} />
+            <NodeStyles.Row>
+                <NodeStyles.Icon>
+                    <NodeIcon type={model.node.kind} />
+                </NodeStyles.Icon>
+                <NodeStyles.Header>
+                    <NodeStyles.Title>{model.node.label || model.node.kind}</NodeStyles.Title>
+                    <NodeStyles.Description>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </NodeStyles.Description>
+                </NodeStyles.Header>
+                <NodeStyles.StyledButton appearance="icon" onClick={handleOnClick}>
+                    <MoreVertIcon />
+                </NodeStyles.StyledButton>
+            </NodeStyles.Row>
+            <NodeStyles.BottomPortWidget port={model.getPort("out")!} engine={engine} />
+        </NodeStyles.Node>
+    );
+}
