0e1d698a1e90c7e401486d982990fc6790f0fb80
kaumini <kauminikg@gmail.com>
2024-07-08 16:10:28 +0530

    Fix height issue of popup panel

--- workspaces/mi/mi-visualizer/src/views/Forms/ConnectionForm/index.tsx
+++ workspaces/mi/mi-visualizer/src/views/Forms/ConnectionForm/index.tsx
@@ -393,7 +393,7 @@ export function ConnectorStore(props: ConnectionStoreProps) {
                 <AddConnection
                     allowedConnectionTypes={Object.keys(selectedConnector.connectionUiSchema)}
                     connector={selectedConnector}
-                    isPopup={false}
+                    isPopup={props.isPopup}
                     changeConnector={changeConnector} />
             ) : (
                 <FormView title={`Add New Connection`} onClose={handleOnClose} hideClose={props.isPopup}>
