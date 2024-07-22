23b57f7a9db074796907f21ab64c8937e9154d53
DedunuKarunarathne <46235093+DedunuKarunarathne@users.noreply.github.com>
2024-07-22 11:09:00 +0530

    Move root url to constants

--- workspaces/mi/mi-extension/src/extension.ts
+++ workspaces/mi/mi-extension/src/extension.ts
@@ -18,19 +18,16 @@ import { RPCLayer } from './RPCLayer';
 import { activateDebugger } from './debugger/activate';
 import { activateMigrationSupport } from './migration';
 import { activateRuntimeService } from './runtime-services-panel/activate';
+import { latestMICopilotRootUrl } from "./constants";
 
 export async function activate(context: vscode.ExtensionContext) {
 	extension.context = context;
-	
-	// Latest root URL for MI Copilot backend
-	const latestCopilotRootUrl = "https://e95488c8-8511-4882-967f-ec3ae2a0f86f-prod.e1-us-east-azure.choreoapis.dev/miaideployments/micopilot/mi-copilot-backend-be2/v1.0";
-	
+			
+	// Update the MI copilot root URL if it is not the latest
 	const config = vscode.workspace.getConfiguration('MI');
-	
-	// Update the root URL if it is not the latest
 	const currentRootUrl = config.get<string>('rootUrl');
-	if (currentRootUrl !== latestCopilotRootUrl) {
-		await config.update('rootUrl', latestCopilotRootUrl, vscode.ConfigurationTarget.Global);
+	if (currentRootUrl !== latestMICopilotRootUrl) {
+		await config.update('rootUrl', latestMICopilotRootUrl, vscode.ConfigurationTarget.Global);
 	}
 
 	RPCLayer.init();
