666de8e406bc8b806d7ab1b975bcdf068cd3a522
kaumini <kauminikg@gmail.com>
2024-07-25 11:12:38 +0530

    update show inbound endpoint

--- workspaces/mi/mi-extension/src/project-explorer/activate.ts
+++ workspaces/mi/mi-extension/src/project-explorer/activate.ts
@@ -214,7 +214,10 @@ export async function activateProjectExplorer(context: ExtensionContext, lsClien
 	});
 	commands.registerCommand(COMMANDS.SHOW_INBOUND_ENDPOINT, (documentUri: Uri, resourceIndex: string, beside: boolean = true) => {
 		revealWebviewPanel(beside);
-		openView(EVENT_TYPE.OPEN_VIEW, { view: MACHINE_VIEW.InboundEPForm, documentUri: documentUri?.fsPath, identifier: resourceIndex });
+		const uri = Uri.file(documentUri?.fsPath);
+		workspace.openTextDocument(uri).then((document) => {
+			window.showTextDocument(document);
+		});
 	});
 	commands.registerCommand(COMMANDS.SHOW_SOURCE, (e: any) => {
 		const documentUri = StateMachine.context().documentUri;
