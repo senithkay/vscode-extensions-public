b5582d9cc77e7d59eb015947d15d319480596a1b
sachiniSam <sachiniprof@gmail.com>
2024-07-30 14:00:34 +0530

    FIx output console to open on runWithoutDebug

--- workspaces/mi/mi-extension/src/debugger/activate.ts
+++ workspaces/mi/mi-extension/src/debugger/activate.ts
@@ -28,6 +28,7 @@ class MiConfigurationProvider implements vscode.DebugConfigurationProvider {
             config.type = 'mi';
             config.name = 'MI: Run and Debug';
             config.request = 'launch';
+            config.internalConsoleOptions =  config.noDebug ? 'neverOpen' : 'openOnSessionStart';
         }
 
         return config;
@@ -115,10 +116,12 @@ export function activateDebugger(context: vscode.ExtensionContext) {
                     name: 'MI: Run',
                     request: 'launch',
                     noDebug: true,
+                    internalConsoleOptions:'neverOpen'
                 };
             } else {
                 config.name = 'MI: Run';
                 config.noDebug = true;
+                config.internalConsoleOptions = 'neverOpen';
             }
 
             try {
