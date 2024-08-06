8f1b3c1f1766062a05404426f4f2bb874477c0c8
Thuvarakan Sritharan <sthuva98@gmail.com>
2024-08-06 10:59:53 +0530

    Fix call mediator template

--- workspaces/mi/mi-diagram/src/utils/template-engine/mustach-templates/core/call.ts
+++ workspaces/mi/mi-diagram/src/utils/template-engine/mustach-templates/core/call.ts
@@ -14,7 +14,6 @@ import { RpcClient } from "@wso2-enterprise/mi-rpc-client";
 
 export function getCallMustacheTemplate() {
   return `
-  {{#isNewMediator}}
   {{^sourceOrTargetOrEndpoint}}
   <call {{#enableBlockingCalls}}blocking="{{enableBlockingCalls}}" {{^initAxis2ClientOptions}}initAxis2ClientOptions="false" {{/initAxis2ClientOptions}}{{/enableBlockingCalls}}{{#description}}description="{{description}}" {{/description}} ></call>
   {{/sourceOrTargetOrEndpoint}}
@@ -42,43 +41,6 @@ export function getCallMustacheTemplate() {
     {{/propertyTarget}}
 </call>
 {{/sourceOrTargetOrEndpoint}}
-{{/isNewMediator}}
-{{^isNewMediator}}
-{{#editCall}}
-{{^sourceOrTargetOrEndpoint}}
-<call {{#enableBlockingCalls}}blocking="{{enableBlockingCalls}}" {{^initAxis2ClientOptions}}initAxis2ClientOptions="false" {{/initAxis2ClientOptions}}{{/enableBlockingCalls}}{{#description}}description="{{description}}" {{/description}} ></call>
-{{/sourceOrTargetOrEndpoint}}
-{{#sourceOrTargetOrEndpoint}}
-<call {{#enableBlockingCalls}}blocking="{{enableBlockingCalls}}" {{^initAxis2ClientOptions}}initAxis2ClientOptions="false" {{/initAxis2ClientOptions}}{{/enableBlockingCalls}}{{#description}}description="{{description}}" {{/description}}>
-{{/sourceOrTargetOrEndpoint}}
-{{/editCall}}
-{{#editEndpoint}} {{#inlineEndpoint}}
-{{{inlineEndpoint}}}{{/inlineEndpoint}}
-{{#registryOrXpathEndpoint}}  <endpoint {{#endpointXpath}}key-expression="{{value}}" {{#namespaces}}xmlns:{{prefix}}="{{uri}}" {{/namespaces}}{{/endpointXpath}}{{#endpointRegistryKey}}key="{{endpointRegistryKey}}" {{/endpointRegistryKey}}/>{{/registryOrXpathEndpoint}}
-{{/editEndpoint}}
-{{#editSource}}
-{{#bodySource}}
-  <source type="body"/>
-{{/bodySource}}
-{{#propertySource}}
-  <source {{#contentType}}contentType="{{contentType}}"{{/contentType}} type="property">{{sourceProperty}}</source>
-{{/propertySource}}
-{{#inlineSource}}
-  <source {{#contentType}}contentType="{{contentType}}"{{/contentType}} type="inline">{{{sourcePayload}}}</source>
-{{/inlineSource}}
-{{#customSource}}
-  <source {{#contentType}}contentType="{{contentType}}"{{/contentType}} type="custom">{{#sourceXPath}}{{value}}{{/sourceXPath}}</source>
-{{/customSource}}
-{{/editSource}}
-{{#editTarget}}
-{{#bodyTarget}}
-  <target type="{{targetType}}"/>
-{{/bodyTarget}}
-{{#propertyTarget}}
-  <target type="{{targetType}}">{{targetProperty}}</target>
-{{/propertyTarget}}
-{{/editTarget}}
-{{/isNewMediator}}
 `
 }
 
@@ -122,81 +84,23 @@ export function getCallXml(data: { [key: string]: any }, dirtyFields?: any, defa
     delete data.contentType;
   }
 
-  if (defaultValues == undefined || Object.keys(defaultValues).length == 0) {
-    return getNewMediator(data);
-  }
-
-  return getEdits(data, dirtyFields, defaultValues);
-}
-
-function getNewMediator(data: { [key: string]: any }) {
-  let newData = { ...data };
-  newData.isNewMediator = true;
-  let output = Mustache.render(getCallMustacheTemplate(), newData).trim();
+  let output = Mustache.render(getCallMustacheTemplate(), data).trim();
   return output;
-}
-
-function getEdits(data: { [key: string]: any }, dirtyFields: any, defaultValues: any) {
-
-  let callTagAttributes = ["enableBlockingCalls", "initAxis2ClientOptions", "description"];
-  let endpointTagAttributes = ["endpointType", "endpointXpath", "endpointRegistryKey", "endpoint", "inlineEndpoint"];
-  let sourceTagAttributes = ["sourceType", "contentType", "sourceProperty", "sourcePayload", "sourceXPath"];
-  let targetTagAttributes = ["targetType", "targetProperty"];
-  let dirtyKeys = Object.keys(dirtyFields);
-  let edits: { [key: string]: any }[] = [];
-
-  if (checkAttributesExist(dirtyKeys, callTagAttributes)) {
-    edits.push(getEdit("Call", data, defaultValues, true));
-  }
-  if (checkAttributesExist(dirtyKeys, endpointTagAttributes)) {
-    edits.push(getEdit("Endpoint", data, defaultValues, false));
-  }
-  if (checkAttributesExist(dirtyKeys, sourceTagAttributes)) {
-    edits.push(getEdit("Source", data, defaultValues, false));
-  }
-  if (checkAttributesExist(dirtyKeys, targetTagAttributes)) {
-    edits.push(getEdit("Target", data, defaultValues, false));
-  }
-  edits.sort((a, b) => b.range.start.line - a.range.start.line);
-  return edits;
-}
-
-function getEdit(key: string, data: { [key: string]: any }, defaultValues: any, editStartTagOnly: boolean) {
-
-  let newData = { ...data };
-  newData["edit" + key] = true;
-  let output = Mustache.render(getCallMustacheTemplate(), newData).trim();
-  let range = defaultValues.ranges[key.toLowerCase()];
-  let editRange;
-  if (range) {
-    editRange = {
-      start: range.startTagRange.start,
-      end: editStartTagOnly ? range.startTagRange.end : (range?.endTagRange?.end ? range.endTagRange.end : range.startTagRange.end)
-    }
-  } else {
-    let callRange = defaultValues.ranges.call;
-    editRange = {
-      start: callRange.endTagRange.start,
-      end: callRange.endTagRange.start
-    }
-  }
-  let edit = {
-    text: output,
-    range: editRange
-  }
-  return edit;
 }
 
 export async function getCallFormDataFromSTNode(data: { [key: string]: any }, node: Call, documentUri: string, rpcClient: RpcClient) {
   const endpoint = node.endpoint;
   if (endpoint && (endpoint?.key == undefined && endpoint?.keyExpression == undefined)) {
     data.endpoint = { isExpression: false, value: "INLINE" };
-
+    let endpointClosePosition = endpoint.selfClosed ? endpoint.range.startTagRange.end : endpoint.range.endTagRange?.end;
+    if (!endpointClosePosition) {
+      endpointClosePosition = node.range.endTagRange.start;
+    }
     const xml = await rpcClient.getMiDiagramRpcClient().getTextAtRange({
       documentUri,
       range: {
         start: endpoint.spaces.startingTagSpace.leadingSpace.range.start,
-        end: endpoint.selfClosed ? endpoint.range.startTagRange.end : endpoint.range.endTagRange.end
+        end: endpointClosePosition
       }
     });
     const leadingSpaces = xml.text.match(/^(\s+)<endpoint/)[1].replace(/\n/g, '').replaceAll('\t', '    ');
@@ -206,7 +110,7 @@ export async function getCallFormDataFromSTNode(data: { [key: string]: any }, no
       .join('\n')
       .trim();
   } else {
-    data.endpoint = { isExpression: endpoint?.keyExpression ? true : false, value: endpoint?.key ?? endpoint?.keyExpression, namespaces: transformNamespaces(endpoint?.namespaces) }
+    data.endpoint = { isExpression: endpoint?.keyExpression ? true : false, value: endpoint?.key ?? endpoint?.keyExpression ?? "INLINE", namespaces: transformNamespaces(endpoint?.namespaces) }
   }
 
   data.enableBlockingCalls = node.blocking;
