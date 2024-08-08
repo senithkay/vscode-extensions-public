142d819cbb94d921568f107aef163fbf1725a90d
Joseph Fonseka <hevayo@gmail.com>
2024-08-08 07:21:33 +0530

    Add AI Panel files

---
+++ workspaces/ballerina/ballerina-extension/src/rpc-managers/ai-panel/rpc-manager.ts
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ * 
+ * THIS FILE INCLUDES AUTO GENERATED CODE
+ */
+import {
+    AIPanelAPI,
+    AIVisualizerState
+} from "@wso2-enterprise/ballerina-core";
+import { StateMachineAI } from '../../views/ai-panel/aiMachine';
+
+export class AiPanelRpcManager implements AIPanelAPI {
+    async getBackendURL(): Promise<string> {
+        // ADD YOUR IMPLEMENTATION HERE
+        throw new Error('Not implemented');
+    }
+
+    async updateProject(): Promise<void> {
+        // ADD YOUR IMPLEMENTATION HERE
+        throw new Error('Not implemented');
+    }
+
+    async getToken(): Promise<string> {
+        // ADD YOUR IMPLEMENTATION HERE
+        throw new Error('Not implemented');
+    }
+
+    async login(): Promise<void> {
+        // ADD YOUR IMPLEMENTATION HERE
+        throw new Error('Not implemented');
+    }
+
+    async logout(): Promise<void> {
+        // ADD YOUR IMPLEMENTATION HERE
+        throw new Error('Not implemented');
+    }
+
+    async getAIVisualizerState(): Promise<AIVisualizerState> {
+        // ADD YOUR IMPLEMENTATION HERE
+        throw new Error('Not implemented');
+    }
+
+    async getAiPanelState(): Promise<AIVisualizerState> {
+        const state = { state: StateMachineAI.state() };
+        return state;
+    }
+
+    async getAccessToken(): Promise<string> {
+        // ADD YOUR IMPLEMENTATION HERE
+        throw new Error('Not implemented');
+    }
+
+    async refreshAccessToken(): Promise<void> {
+        // ADD YOUR IMPLEMENTATION HERE
+        throw new Error('Not implemented');
+    }
+}
---
+++ workspaces/ballerina/ballerina-extension/src/views/ai-panel/constants.ts
@@ -0,0 +1,9 @@
+
+
+
+
+export const AI_BE_URL = "https://e95488c8-8511-4882-967f-ec3ae2a0f86f-dev.e1-us-east-azure.choreoapis.dev/ballerina-copilot/ballerina-copilot/ai-be2/v1/";
+export const AI_AUTH_URL = "https://api.asgardeo.io/t/ballerinacopilotdev/oauth2/authorize";
+export const AI_TOKEN_URL = "https://api.asgardeo.io/t/ballerinacopilotdev/oauth2/token";
+export const AI_CLIENT_ID = "XpQ6lphi7kjKkWzumYyqqNf7CjIa";
+export const AI_CLIENT_SECRET = "q8M81q2xmMqjK_3GBcshF6B7Le1JmgOtj4wclWDzfjsa";
