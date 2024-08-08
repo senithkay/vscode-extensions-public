39f99fa3067e403e2e773f66ec56d9a2827229f9
kaumini <kauminikg@gmail.com>
2024-08-08 20:38:07 +0530

    Add check for sequence list

--- workspaces/mi/mi-visualizer/src/views/Forms/InboundEPform/inboundConnectorForm.tsx
+++ workspaces/mi/mi-visualizer/src/views/Forms/InboundEPform/inboundConnectorForm.tsx
@@ -66,7 +66,7 @@ export function AddInboundConnector(props: AddInboundConnectorProps) {
     const fetchSequences = async () => {
         try {
             const sequenceList = await rpcClient.getMiDiagramRpcClient().getEndpointsAndSequences();
-            const sequenceNames: string[] = sequenceList ? (sequenceList.data[1].map((seq: string) => {
+            const sequenceNames: string[] = sequenceList.data[1]?. length > 0 ? (sequenceList.data[1].map((seq: string) => {
                 seq = seq.replace(".xml", "");
                 return seq.replace(".xml", "")
             })) : [];
