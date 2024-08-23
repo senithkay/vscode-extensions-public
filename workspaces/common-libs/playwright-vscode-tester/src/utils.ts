492af1d4ec1f9eba12f5a72a54a7870834230b12
gigara <info@gigara.info>
2024-08-23 12:37:48 +0530

    Add support to open resource folder at startup

--- workspaces/common-libs/playwright-vscode-tester/src/utils.ts
+++ workspaces/common-libs/playwright-vscode-tester/src/utils.ts
@@ -1,5 +1,7 @@
+import path from "path";
 import { CodeUtil, ReleaseQuality } from "./codeUtil";
 import { BrowserLaunchOptions, Browser } from "./types";
+import fs from "fs";
 
 export async function getBrowser(folder: string, version: string, quality: ReleaseQuality): Promise<Browser> {
     const codeUtil = new CodeUtil(folder, quality);
@@ -8,8 +10,23 @@ export async function getBrowser(folder: string, version: string, quality: Relea
     return browser;
 }
 
-export async function getBrowserLaunchOptions(folder: string, version: string, quality: ReleaseQuality): Promise<BrowserLaunchOptions> {
-    const codeUtil = new CodeUtil(folder, quality);
-    const options = await codeUtil.getCypressBrowserOptions({ vscodeVersion: version, resources: [] });
+export async function getBrowserLaunchOptions(folder: string, version: string, quality: ReleaseQuality, projectPath?: string, extensionsFolder?: string): Promise<BrowserLaunchOptions> {
+    const codeUtil = new CodeUtil(folder, quality, extensionsFolder);
+    const resources = []
+    if (projectPath) {
+        resources.push(projectPath);
+    }
+    const options = await codeUtil.getCypressBrowserOptions({ vscodeVersion: version, resources });
+
+    // install all vsix extensions in the folder
+    if (extensionsFolder) {
+        const files = path.resolve(extensionsFolder);
+        const vsixFiles = fs.readdirSync(files).filter(file => file.endsWith('.vsix'));
+        for (const vsix of vsixFiles) {
+            const vsixPath = path.join(files, vsix);
+            codeUtil.installExtension(vsixPath);
+        }
+    }
+    
     return options;
 }
