d410401e6950d74f9bcbb82e9987c5350cd24b09
Thuvarakan Sritharan <sthuva98@gmail.com>
2024-08-23 14:36:11 +0530

    Add missing java folder when importing capp

--- workspaces/mi/mi-extension/src/util/importCapp.ts
+++ workspaces/mi/mi-extension/src/util/importCapp.ts
@@ -102,6 +102,7 @@ export async function importCapp(params: ImportProjectRequest): Promise<ImportPr
                             'connectors': '',
                         },
                     },
+                    'java': ''
                 },
                 'test': {
                     'wso2mi': '',
