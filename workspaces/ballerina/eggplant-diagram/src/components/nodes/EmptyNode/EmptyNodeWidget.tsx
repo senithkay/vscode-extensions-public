7492d6262a2d0ba6a0175ce41cdd25e9fda89f42
Kanushka Gayan <kanushkanet@gmail.com>
2024-08-25 03:24:20 +0530

    Fix draft node appearing issue with comment popup box

--- workspaces/ballerina/eggplant-diagram/src/components/nodes/EmptyNode/EmptyNodeWidget.tsx
+++ workspaces/ballerina/eggplant-diagram/src/components/nodes/EmptyNode/EmptyNodeWidget.tsx
@@ -134,7 +134,6 @@ export function EmptyNodeWidget(props: EmptyNodeWidgetProps) {
                             gap: 5px;
                             cursor: pointer;
                         `}
-                        onClick={handleAddNode}
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -169,7 +168,7 @@ export function EmptyNodeWidget(props: EmptyNodeWidgetProps) {
                                 d="M12 0C5 0 0 5 0 12s5 12 12 12 12-5 12-12S19 0 12 0z"
                             />
                             <path
-                                fill={Colors.PRIMARY}
+                                fill={isHovered ? Colors.PRIMARY : Colors.ON_SURFACE}
                                 d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2m0 18a8 8 0 1 1 8-8a8 8 0 0 1-8 8m4-9h-3V8a1 1 0 0 0-2 0v3H8a1 1 0 0 0 0 2h3v3a1 1 0 0 0 2 0v-3h3a1 1 0 0 0 0-2"
                             />
                         </svg>
--- workspaces/ballerina/eggplant-diagram/src/resources/constants.ts
+++ workspaces/ballerina/eggplant-diagram/src/resources/constants.ts
@@ -111,7 +111,7 @@ export const DRAFT_NODE_HEIGHT= NODE_HEIGHT;
 export const DRAFT_NODE_BORDER_WIDTH = 2;
 
 // popup box
-export const POPUP_BOX_WIDTH = NODE_WIDTH + NODE_GAP_X;
+export const POPUP_BOX_WIDTH = NODE_WIDTH + NODE_GAP_X + 20;
 export const POPUP_BOX_HEIGHT = 40;
 
 // HACK
