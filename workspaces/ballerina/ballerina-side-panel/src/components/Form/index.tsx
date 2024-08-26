3da030af04f27f8c766d87c1ec5ff429d5f1712c
Kanushka Gayan <kanushkanet@gmail.com>
2024-08-26 13:56:46 +0530

    Hide optional properties from form

--- workspaces/ballerina/ballerina-side-panel/src/components/Form/index.tsx
+++ workspaces/ballerina/ballerina-side-panel/src/components/Form/index.tsx
@@ -7,12 +7,13 @@
  * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Button, SidePanelBody } from "@wso2-enterprise/ui-toolkit";
+import { Button, Codicon, LinkButton, SidePanelBody } from "@wso2-enterprise/ui-toolkit";
 import { FormField, FormValues } from "./types";
 import styled from "@emotion/styled";
 import { FormFieldEditor } from "../editors/FormFieldEditor";
+import { Colors } from "../../resources/constants";
 
 namespace S {
     export const Container = styled(SidePanelBody)`
@@ -53,6 +54,13 @@ namespace S {
     export const DrawerContainer = styled.div<{}>`
         width: 400px;
     `;
+
+    export const ButtonContainer = styled.div<{}>`
+        display: flex;
+        flex-direction: row;
+        flex-grow: 1;
+        justify-content: flex-end;
+    `;
 }
 
 interface FormProps {
@@ -65,24 +73,87 @@ export function Form(props: FormProps) {
     const { formFields, onSubmit, openRecordEditor } = props;
     const { getValues, register } = useForm<FormValues>();
 
+    const [showAdvancedOptions, setShowAdvancedOptions] = useState(false);
+
     console.log(">>> form fields", { formFields, values: getValues() });
 
     const handleOnSave = () => {
         onSubmit(getValues());
     };
 
+    const handleOnShowAdvancedOptions = () => {
+        setShowAdvancedOptions(true);
+    };
+
+    const handleOnHideAdvancedOptions = () => {
+        setShowAdvancedOptions(false);
+    };
+
+    // HACK: hide some fields if the form. need to fix from LS
+    formFields.forEach((field) => {
+        // hide http scope
+        if (field.key === "scope") {
+            field.optional = true;
+        }
+    });
+
+    // has optional fields
+    const hasOptionalFields = formFields.some((field) => field.optional);
+
     // TODO: support multiple type fields
     return (
         <S.Container>
-            {formFields.map((field) => (
-                <S.Row key={field.key}>
-                    <FormFieldEditor
-                        field={field}
-                        register={register}
-                        openRecordEditor={openRecordEditor}
-                    />
+            {formFields.map((field) => {
+                if (!field.optional) {
+                    return (
+                        <S.Row key={field.key}>
+                            <FormFieldEditor field={field} register={register} openRecordEditor={openRecordEditor} />
+                        </S.Row>
+                    );
+                }
+            })}
+
+            {hasOptionalFields && (
+                <S.Row>
+                    Advanced Properties
+                    <S.ButtonContainer>
+                        {!showAdvancedOptions && (
+                            <LinkButton
+                                onClick={handleOnShowAdvancedOptions}
+                                sx={{ fontSize: 12, padding: 8, color: Colors.PRIMARY, gap: 4 }}
+                            >
+                                <Codicon name={"chevron-down"} iconSx={{ fontSize: 12 }} sx={{ height: 12 }} />
+                                Expand
+                            </LinkButton>
+                        )}
+                        {showAdvancedOptions && (
+                            <LinkButton
+                                onClick={handleOnHideAdvancedOptions}
+                                sx={{ fontSize: 12, padding: 8, color: Colors.PRIMARY, gap: 4 }}
+                            >
+                                <Codicon name={"chevron-up"} iconSx={{ fontSize: 12 }} sx={{ height: 12 }} />
+                                Collapsed
+                            </LinkButton>
+                        )}
+                    </S.ButtonContainer>
                 </S.Row>
-            ))}
+            )}
+
+            {hasOptionalFields &&
+                showAdvancedOptions &&
+                formFields.map((field) => {
+                    if (field.optional) {
+                        return (
+                            <S.Row key={field.key}>
+                                <FormFieldEditor
+                                    field={field}
+                                    register={register}
+                                    openRecordEditor={openRecordEditor}
+                                />
+                            </S.Row>
+                        );
+                    }
+                })}
 
             <S.Footer>
                 <Button appearance="primary" onClick={handleOnSave}>
