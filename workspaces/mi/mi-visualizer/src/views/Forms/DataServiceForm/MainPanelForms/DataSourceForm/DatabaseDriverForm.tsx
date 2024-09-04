75a179f07d3d6e3223be65e1c442425fe4446952
Kaumini Gunasinghe <kauminikg@gmail.com>
2024-09-04 14:29:46 +0530

    Update DatabaseDriverForm.tsx
    Co-authored-by: Chinthaka Jayatilake <37581983+ChinthakaJ98@users.noreply.github.com>

--- workspaces/mi/mi-visualizer/src/views/Forms/DataServiceForm/MainPanelForms/DataSourceForm/DatabaseDriverForm.tsx
+++ workspaces/mi/mi-visualizer/src/views/Forms/DataServiceForm/MainPanelForms/DataSourceForm/DatabaseDriverForm.tsx
@@ -110,7 +110,7 @@ export function DatabaseDriverForm(props: DatabaseDriverFormProps) {
                     <span>Note</span>
                     <span>
                         You will not be able to use test database connection and perform resource
-                        generation for data services and such inbuilt features as the relevant drivers
+                        generation for data services such inbuilt features as the relevant drivers
                         does not exist in the developer environment.
                     </span>
                 </>
