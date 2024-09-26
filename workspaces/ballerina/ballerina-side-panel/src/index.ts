e0d9ad59f98231143c127062537de53fe2729e7b
Kanushka Gayan <kanushkanet@gmail.com>
2024-09-26 09:13:09 +0530

    Refactor side panel editor factory

---
+++ workspaces/ballerina/ballerina-side-panel/src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+export * from "./components/Panel";
+export * from "./components/NodeList";
+export * from "./components/NodeList/types";
+export * from "./components/Form";
+export * from "./components/Form/types";
+export * from "./components/editors";
