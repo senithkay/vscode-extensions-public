ea6cbc7e6d78b2a0960df1bf574e43d22ab6713c
gigara <info@gigara.info>
2024-10-01 12:43:39 +0530

    Fix go to source in datamapper

--- workspaces/mi/mi-extension/src/visualizer/activate.ts
+++ workspaces/mi/mi-extension/src/visualizer/activate.ts
@@ -136,7 +136,8 @@ export function activateVisualizer(context: vscode.ExtensionContext) {
 
     StateMachine.service().onTransition((state) => {
         if (state.event.viewLocation?.view) {
-            commands.executeCommand('setContext', 'showGoToSource', state.event.viewLocation?.documentUri?.toLowerCase().endsWith('.xml'));
+            const documentUri = state.event.viewLocation?.documentUri;
+            commands.executeCommand('setContext', 'showGoToSource', documentUri.toLowerCase().endsWith('.xml') || documentUri.toLowerCase().endsWith('.ts'));
         }
     });
 }
