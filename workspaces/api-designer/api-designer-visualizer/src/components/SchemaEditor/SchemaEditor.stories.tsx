184646c73da959db1379362791b259aaaf40dde1
tharindulak <tharindu.jayathilake1@gmail.com>
2024-10-17 11:51:47 +0530

    Fix Storybook

---
+++ workspaces/api-designer/api-designer-visualizer/src/components/SchemaEditor/SchemaEditor.stories.tsx
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
+ */
+
+import React from 'react';
+import { Schema, SchemaEditor } from './SchemaEditor';
+import { useState } from 'react';
+
+export default {
+    component: SchemaEditor,
+    title: 'SchemaEditor',
+};
+
+const schema: Schema = {
+    type: "object",
+    properties: {
+        name: {
+            type: "string"
+        },
+        age: {
+            type: "number"
+        },
+        address: {
+            type: "object",
+            properties: {
+                street: {
+                    type: "string"
+                },
+                city: {
+                    type: "string"
+                }
+            }
+        }
+    }
+};
+
+export const SchemaEditorStory = () => {
+    const [selectedId, setSelectedId] = useState<string | null>("1");
+    const handleClick = (id: string) => {
+        setSelectedId(id);
+    };
+
+    return (
+        <SchemaEditor schema={schema} schemaName="Person" />
+    );
+};
