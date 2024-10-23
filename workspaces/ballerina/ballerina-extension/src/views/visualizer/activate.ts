93575a14386a050784de81c88f2cb77a92722bee
Kanushka Gayan <kanushkanet@gmail.com>
2024-10-23 23:59:45 +0530

    Update initial setup page styles

--- workspaces/ballerina/ballerina-extension/src/views/visualizer/activate.ts
+++ workspaces/ballerina/ballerina-extension/src/views/visualizer/activate.ts
@@ -13,6 +13,8 @@ import { StateMachine, openView } from '../../stateMachine';
 import { extension } from '../../BalExtensionContext';
 import { BI_COMMANDS, EVENT_TYPE, MACHINE_VIEW, SHARED_COMMANDS } from '@wso2-enterprise/ballerina-core';
 import { ViewColumn } from 'vscode';
+import { ballerinaExtInstance } from '../../core';
+import { isSupportedSLVersion } from '../../utils';
 
 export function activateSubscriptions() {
     const context = extension.context;
@@ -42,7 +44,7 @@ export function activateSubscriptions() {
 
     context.subscriptions.push(
         vscode.commands.registerCommand(SHARED_COMMANDS.OPEN_BI_WELCOME, () => {
-            if (StateMachine.langClient()) {
+            if (StateMachine.langClient() && ballerinaExtInstance && isSupportedSLVersion(ballerinaExtInstance, 2201100)) {
                 openView(EVENT_TYPE.OPEN_VIEW, { view: MACHINE_VIEW.BIWelcome });
             } else {
                 openView(EVENT_TYPE.OPEN_VIEW, { view: MACHINE_VIEW.SetupView });
--- workspaces/ballerina/ballerina-visualizer/src/views/BI/SetupView/index.tsx
+++ workspaces/ballerina/ballerina-visualizer/src/views/BI/SetupView/index.tsx
@@ -11,41 +11,14 @@ import React from "react";
 import { DownloadProgress } from "@wso2-enterprise/ballerina-core";
 import { useRpcContext } from "@wso2-enterprise/ballerina-rpc-client";
 import styled from "@emotion/styled";
-import { Button, Codicon } from "@wso2-enterprise/ui-toolkit";
-import { VSCodeLink } from "@vscode/webview-ui-toolkit/react";
-
-const TextWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
+import { Button, Icon, ProgressRing } from "@wso2-enterprise/ui-toolkit";
+import { Colors } from "../../../resources/constants";
 
 const Wrapper = styled.div`
     height: calc(100vh - 100px);
     max-width: 800px;
     margin: 80px 120px;
     overflow: auto;
-`;
-
-const TitlePanel = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding-bottom: 40px;
-`;
-
-const Pane = styled.div`
-    display: flex;
-    padding: 0px !important;
-    flex-direction: column;
-    width: 100%;
-`;
-
-const Tab = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 20px 0px;
-    gap: 5px;
 `;
 
 const Headline = styled.div`
@@ -56,21 +29,7 @@ const Headline = styled.div`
     margin-bottom: 20px;
 `;
 
-const SubTitle = styled.div`
-    font-weight: 400;
-    margin-top: 0;
-    margin-bottom: 5px;
-    font-size: 1.5em;
-    line-height: normal;
-`;
-
-const Grid = styled.div({
-    display: "flex",
-    flexDirection: "row",
-    gap: 20,
-});
-
-const AddButton = styled(Button)`
+const StyledButton = styled(Button)`
     margin-top: 10px;
     width: 100%;
 `;
@@ -83,6 +42,65 @@ const ButtonContent = styled.div`
     height: 28px;
 `;
 
+//
+
+const TitleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    margin-bottom: 20px;
+`;
+
+const Caption = styled.div`
+    font-size: 1.1em;
+    line-height: 1.5em;
+    font-weight: 400;
+    margin-top: 0;
+    margin-bottom: 5px;
+`;
+
+const StepContainer = styled.div`
+    margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 20px;
+`;
+
+const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    gap: 10px;
+`;
+
+const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 4px;
+`;
+
+const StepTitle = styled.div<{ color?: string }>`
+    font-size: 1.2em;
+    font-weight: 400;
+    margin-top: 0;
+    margin-bottom: 5px;
+    color: ${(props: { color?: string }) => props.color || "inherit"};
+`;
+
+const StepDescription = styled.div<{ color?: string }>`
+    font-size: 1em;
+    font-weight: 400;
+    margin-top: 0;
+    margin-bottom: 5px;
+    color: ${(props: { color?: string }) => props.color || "inherit"};
+`;
+
+const IconContainer = styled.div`
+    margin-top: 4px;
+`;
+
 export interface SetupViewProps {
     haveLS: boolean;
 }
@@ -93,59 +111,122 @@ export function SetupView(props: SetupViewProps) {
 
     const [progress, setProgress] = React.useState<DownloadProgress>(null);
 
-
     const downloadLS = () => {
-        rpcClient.getCommonRpcClient().executeCommand({ commands: ["kolab-setup.setupKola"] })
+        rpcClient.getCommonRpcClient().executeCommand({ commands: ["kolab-setup.setupKola"] });
     };
 
     const reloadVscode = () => {
-        rpcClient.getCommonRpcClient().executeCommand({ commands: ["workbench.action.reloadWindow"] })
+        rpcClient.getCommonRpcClient().executeCommand({ commands: ["workbench.action.reloadWindow"] });
     };
-
 
     rpcClient?.onDownloadProgress((response: DownloadProgress) => {
         setProgress(response);
     });
 
-    return (
-        <>
-            <Wrapper>
-                <TitlePanel>
-                    <Headline>Kola Setup for VS Code</Headline>
-                </TitlePanel>
-                <Grid>
-                    <Pane>
-                        <Tab>
-                            <SubTitle>Have LS {haveLS}</SubTitle>
-                        </Tab>
-                        <Tab>
-                            <SubTitle>Set up the Kola Distribution</SubTitle>
-                            <AddButton appearance="primary" onClick={() => downloadLS()}>
-                                <ButtonContent>
-                                    <Codicon name="add" iconSx={{ fontSize: 16 }} />
-                                    <TextWrapper>Download LS</TextWrapper>
-                                </ButtonContent>
-                            </AddButton>
-                        </Tab>
-                        {progress && <Tab>
-                            Progress Information:
-                            <SubTitle>Total Size: {progress.totalSize}MB</SubTitle>
-                            <SubTitle>Downloaded: {progress.downloadedSize}MB</SubTitle>
-                            <SubTitle>Message: {progress.message}</SubTitle>
-                            <SubTitle>Percentage: {progress.percentage}%</SubTitle>
+    const getIcon = (complete: boolean, loading: boolean) => {
+        if (complete) {
+            return <Icon name="enable-inverse" iconSx={{ fontSize: "15px", color: Colors.PRIMARY }} />;
+        } else if (loading) {
+            return <ProgressRing sx={{ height: "16px", width: "16px" }} color={Colors.PRIMARY} />;
+        } else {
+            return <Icon name="radio-button-unchecked" iconSx={{ fontSize: "16px" }} />;
+        }
+    };
 
-                            {progress.success &&
-                                <AddButton appearance="primary" onClick={() => reloadVscode()}>
-                                    <ButtonContent>
-                                        <Codicon name="add" iconSx={{ fontSize: 16 }} />
-                                        <TextWrapper>Reload vscode</TextWrapper>
-                                    </ButtonContent>
-                                </AddButton>
-                            }
-                        </Tab>}
-                    </Pane>
-                </Grid>
-            </Wrapper>
-        </>
+    return (
+        <Wrapper>
+            <TitleContainer>
+                <Headline>Let's Set Up Kola for VSCode</Headline>
+                <Caption>
+                    Welcome to Kola! Let's quickly set up your environment to get started. Just click the button below,
+                    and we’ll take care of everything step by step.
+                </Caption>
+            </TitleContainer>
+            <StyledButton appearance="primary" onClick={() => downloadLS()} disabled={progress !== null}>
+                <ButtonContent>Set Up</ButtonContent>
+            </StyledButton>
+            <StepContainer>
+                <Row>
+                    <IconContainer>
+                        {getIcon(progress?.step > 1 || progress?.success, progress?.step === 1)}
+                    </IconContainer>
+
+                    <Column>
+                        <StepTitle>Check Latest Version</StepTitle>
+                        <StepDescription>Ensuring you have the latest version of the Kola runtime.</StepDescription>
+                    </Column>
+                </Row>
+                <Row>
+                    <IconContainer>
+                        {getIcon(progress?.step > 2 || progress?.success, progress?.step === 2)}
+                    </IconContainer>
+                    <Column>
+                        <StepTitle>
+                            Download Kola Runtime {progress?.percentage ? "( " + progress.percentage + "% )" : ""}
+                        </StepTitle>
+                        <StepDescription>Fetching the runtime required to run Kola.</StepDescription>
+                    </Column>
+                </Row>
+                <Row>
+                    <IconContainer>
+                        {getIcon(progress?.step > 3 || progress?.success, progress?.step === 3)}
+                    </IconContainer>
+                    <Column>
+                        <StepTitle>Unzip Files</StepTitle>
+                        <StepDescription>Unpacking the downloaded files to prepare for installation.</StepDescription>
+                    </Column>
+                </Row>
+                <Row>
+                    <IconContainer>
+                        {getIcon(progress?.step > 4 || progress?.success, progress?.step === 4)}
+                    </IconContainer>
+                    <Column>
+                        <StepTitle>Install Kola Runtime</StepTitle>
+                        <StepDescription>Integrating the Kola runtime with your VSCode setup.</StepDescription>
+                    </Column>
+                </Row>
+                <Row>
+                    <IconContainer>
+                        {getIcon(progress?.step > 5 || progress?.success, progress?.step === 5)}
+                    </IconContainer>
+                    <Column>
+                        <StepTitle>Clean Up</StepTitle>
+                        <StepDescription>Cleaning up temporary files and finalizing the setup.</StepDescription>
+                    </Column>
+                </Row>
+            </StepContainer>
+            {progress && progress.step && progress.step === -1 && (
+                <StepContainer>
+                    <Row>
+                        <Column>
+                            <StepTitle color={Colors.ERROR}>Something went wrong while setting up Kola</StepTitle>
+                            <StepDescription>{progress.message}</StepDescription>
+                            <StepDescription>
+                                Please check your internet connection or permissions and try again.
+                            </StepDescription>
+                            <StyledButton appearance="primary" onClick={() => downloadLS()}>
+                                <ButtonContent>Retry Setup</ButtonContent>
+                            </StyledButton>
+                        </Column>
+                    </Row>
+                </StepContainer>
+            )}
+            {progress && progress.success && (
+                <StepContainer>
+                    <Row>
+                        <Column>
+                            <StepTitle>Restart to Apply Changes</StepTitle>
+                            <StepDescription>
+                                To finish the setup, please restart the Kola extension. This ensures everything is
+                                configured correctly and ready to use.
+                            </StepDescription>
+                            <StyledButton appearance="primary" onClick={() => reloadVscode()}>
+                                <ButtonContent>Restart VSCode</ButtonContent>
+                            </StyledButton>
+                        </Column>
+                    </Row>
+                </StepContainer>
+            )}
+        </Wrapper>
     );
 }
