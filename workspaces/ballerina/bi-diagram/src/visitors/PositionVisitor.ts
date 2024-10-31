c2a400f31b695e835858d54d74d3903043dd8aa7
Kanushka Gayan <kanushkanet@gmail.com>
2024-10-31 20:03:51 +0530

    Update WHILE and FOREACH nodes rendering and sizing

--- workspaces/ballerina/bi-diagram/src/visitors/PositionVisitor.ts
+++ workspaces/ballerina/bi-diagram/src/visitors/PositionVisitor.ts
@@ -151,20 +151,11 @@ export class PositionVisitor implements BaseVisitor {
     }
 
     beginVisitForeach(node: FlowNode, parent?: FlowNode): void {
-        node.viewState.y = this.lastNodeY;
-        this.lastNodeY += node.viewState.h + NODE_GAP_Y;
-
-        const centerX = getTopNodeCenter(node, parent, this.diagramCenterX);
-        node.viewState.x = centerX - node.viewState.w / 2;
-
-        const bodyBranch = node.branches.find((branch) => branch.label === "Body");
-        bodyBranch.viewState.y = this.lastNodeY;
-
-        bodyBranch.viewState.x = centerX -  bodyBranch.viewState.cw / 2
+        this.beginVisitWhile(node, parent);
     }
 
     endVisitForeach(node: FlowNode, parent?: FlowNode): void {
-        this.lastNodeY = node.viewState.y + node.viewState.ch + NODE_GAP_Y;
+        this.endVisitWhile(node, parent);
     }
 
     skipChildren(): boolean {
