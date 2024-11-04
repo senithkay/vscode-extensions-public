ea22d51836c4a2644e121619e6cf4e25551c5a05
ChamodA <chamoda@wso2.com>
2024-11-04 15:06:05 +0530

    Revert "use set textFieldValues instead of ref.current"
    This reverts commit 684bee1fcb3cfad7d98ebf6ee80ac2bce6d994fc.

--- workspaces/mi/mi-data-mapper/src/components/DataMapper/Header/ExpressionBar.tsx
+++ workspaces/mi/mi-data-mapper/src/components/DataMapper/Header/ExpressionBar.tsx
@@ -109,16 +109,16 @@ export default function ExpressionBarWrapper(props: ExpressionBarProps) {
             
             const relativeCursorPosition = textFieldRef.current.shadowRoot.querySelector('textarea').selectionStart;
 
-            if (!shouldCompletionsAppear(textFieldValue, relativeCursorPosition)){
+            if (!shouldCompletionsAppear(textFieldValueRef.current, relativeCursorPosition)){
                 return [];
             }
 
-            const partialTextMatcher = textFieldValue.substring(0, relativeCursorPosition).trimStart().match('([a-zA-Z0-9_$]+)$');
+            const partialTextMatcher = textFieldValueRef.current.substring(0, relativeCursorPosition).trimStart().match('([a-zA-Z0-9_$]+)$');
             const partialText = (partialTextMatcher && partialTextMatcher.length) ? partialTextMatcher[partialTextMatcher.length-1] : undefined;
 
             let fileContent = nodeForSuggestions.getSourceFile().getFullText();
 
-            fileContent = fileContent.slice(0, completionReqPosStart.current) + ' ' + textFieldValue + fileContent.slice(completionReqPosEnd.current);
+            fileContent = fileContent.slice(0, completionReqPosStart.current) + ' ' + textFieldValueRef.current + fileContent.slice(completionReqPosEnd.current);
             const cursorPosition = completionReqPosStart.current + 1 + relativeCursorPosition;
             const response = await rpcClient.getMiDataMapperRpcClient().getCompletions({
                 filePath,
