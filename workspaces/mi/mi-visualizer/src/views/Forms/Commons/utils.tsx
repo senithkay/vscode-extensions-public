b10c879cf6289f3d68754f73a21e5eebfe5ae607
chathurangaj <chathuranga.jayanath.1999@gmail.com>
2024-11-24 23:19:51 +0530

    add utils

---
+++ workspaces/mi/mi-visualizer/src/views/Forms/Commons/utils.tsx
@@ -0,0 +1,21 @@
+export function formatForConfigurable(value: string): string {
+    return `$config:${value}`;
+}
+  
+export function removeConfigurableFormat(formattedValue: string): string {
+    const prefix = "$config:";
+    if (formattedValue.startsWith(prefix)) {
+        return formattedValue.slice(prefix.length);
+    }
+    return formattedValue;
+}
+
+export function isConfigurable(value: string): boolean {
+    const pattern = /^\$config:/;
+    return pattern.test(value);
+}
+
+export function isCertificateFileName(value: string): boolean {
+    const certificateExtension = ".crt";
+    return value.endsWith(certificateExtension);
+}
