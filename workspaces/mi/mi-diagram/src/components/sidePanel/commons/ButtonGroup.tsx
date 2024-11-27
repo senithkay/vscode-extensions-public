a6740449a6f9e00950913cfb6927c58cdd6dedd8
kaumini <kaumini@gmail.com>
2024-11-27 08:16:54 +0530

    Update button group styles

--- workspaces/mi/mi-diagram/src/components/sidePanel/commons/ButtonGroup.tsx
+++ workspaces/mi/mi-diagram/src/components/sidePanel/commons/ButtonGroup.tsx
@@ -19,19 +19,40 @@ const ButtonGrid = styled.div`
     grid-template-columns: 1fr 1fr;
     gap: 5px 5px;
     padding: 10px 10px;
-    border: 2px solid;
-    border-right: none;
-    border-top: none;
     border-radius: 10px;
-    border-color: var(--vscode-editorIndentGuide-background);
 `;
+
+const VersionTag = styled.div`
+    color: #808080;
+    font-size: 10px;
+    padding-left: 2px;
+`;
+
+const CardContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+`;
+
+const CardLabel = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-self: flex-start;
+    width: 100%;
+    gap: 10px;
+`;
+
 
 interface ButtonroupProps {
     title: string;
     children?: React.ReactNode;
     isCollapsed?: boolean;
+    iconUri?: string;
+    versionTag?: string;
 }
-export const ButtonGroup: React.FC<ButtonroupProps> = ({ title, children, isCollapsed = true }) => {
+export const ButtonGroup: React.FC<ButtonroupProps> = ({ title, children, isCollapsed = true, iconUri, versionTag }) => {
     const [collapsed, setCollapsed] = useState(isCollapsed);
 
     useEffect(() => {
@@ -41,14 +62,78 @@ export const ButtonGroup: React.FC<ButtonroupProps> = ({ title, children, isColl
     const toggleCollapse = () => setCollapsed(!collapsed);
 
     return (
-        <div>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', cursor: 'pointer' }} onClick={toggleCollapse}>
+        <div style={{
+            backgroundColor: 'var(--vscode-editorWidget-background)',
+            border: '0px',
+            borderRadius: 2,
+            cursor: 'pointer',
+            display: 'flex',
+            justifyContent: 'left',
+            transition: '0.3s',
+            flexDirection: 'column',
+            marginBottom: '15px'
+        }}>
+            {/* <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', cursor: 'pointer' }} onClick={toggleCollapse}>
                 <Typography variant="h3" sx={{ margin: '0px' }}>{title}</Typography>
                 <hr style={{ flexGrow: 1, margin: '0 10px', borderColor: 'var(--vscode-panel-border)' }} />
                 <Button appearance="icon" tooltip={collapsed ? 'Expand' : 'Collapse'}>
                     <Codicon name={collapsed ? 'chevron-down' : 'chevron-up'} />
                 </Button>
-            </div>
+            </div> */}
+            <ComponentCard
+                id={title}
+                key={title}
+                onClick={toggleCollapse}
+                sx={{
+                    border: '0px',
+                    borderRadius: 2,
+                    padding: '6px 10px',
+                    width: 'auto',
+                    height: '32px'
+                }}
+            >
+                <CardContent>
+                    <CardLabel>
+                        {iconUri && (
+                            <IconContainer>
+                                <img
+                                    src={iconUri}
+                                    alt="Icon"
+                                    onError={(e) => {
+                                        const target = e.target as HTMLImageElement;
+                                        target.src = 'https://mi-connectors.wso2.com/icons/wordpress.gif'
+                                    }}
+                                />
+                            </IconContainer>
+                        )}
+                        <div style={{
+                            width: '100%',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                            textAlign: 'left',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center'
+                        }}>
+                            <IconLabel>
+                                {title}
+                            </IconLabel>
+                            <VersionTag>
+                                {versionTag}
+                            </VersionTag>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                            {/* {expandedConnections.includes(connection) ?
+                                <Codicon name={"chevron-up"} /> : <Codicon name={"chevron-down"} />
+                            } */}
+                            <Button appearance="icon" tooltip={collapsed ? 'Expand' : 'Collapse'}>
+                                <Codicon name={collapsed ? 'chevron-down' : 'chevron-up'} />
+                            </Button>
+                        </div>
+                    </CardLabel>
+                </CardContent>
+            </ComponentCard>
             {!collapsed &&
                 <ButtonGrid>
                     {children}
@@ -98,7 +183,7 @@ export const GridButton: React.FC<GridButtonProps> = ({ title, description, icon
                     marginBottom: 10,
                     padding: 10,
                     transition: '0.3s',
-                    width: 165
+                    width: 168
                 }}
             >
                 <IconContainer>
