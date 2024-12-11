a02afa3f359fcc8d4d0ebcc7cdd5136106f8dbd8
ChamodA <chamoda@wso2.com>
2024-12-11 15:34:35 +0530

    Fix sub mapping node overlap with input node

--- workspaces/mi/mi-data-mapper/src/components/Hooks/index.tsx
+++ workspaces/mi/mi-data-mapper/src/components/Hooks/index.tsx
@@ -60,7 +60,11 @@ export const useRepositionedNodes = (
             const x = OFFSETS.SOURCE_NODE.X;
             const computedY = prevBottomY + (prevBottomY ? GAP_BETWEEN_INPUT_NODES : 0);
             const hasArrayFilterNode = nodesClone.some(node => node instanceof ArrayFilterNode);
-            let y = exisitingNode && sameView && (!hasArrayFilterNode || filtersUnchanged) && exisitingNode.getY() !== 0 ? exisitingNode.getY() : computedY;
+            const utilizeExistingY = exisitingNode && sameView
+                && (!hasArrayFilterNode || filtersUnchanged)
+                && exisitingNode.getY() !== 0
+                && !(node instanceof SubMappingNode);
+            let y = utilizeExistingY ? exisitingNode.getY() : computedY;
             node.setPosition(x, y);
             if (node instanceof InputNode) {
                 const nodeHeight = getIONodeHeight(node.numberOfFields);
