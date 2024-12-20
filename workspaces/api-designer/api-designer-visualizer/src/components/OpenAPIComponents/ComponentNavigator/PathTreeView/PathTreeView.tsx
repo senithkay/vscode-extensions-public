b2dc6ad2c8be129b555906d5053a186a7261b0d6
tharindulak <tharindu.jayathilake1@gmail.com>
2024-12-20 11:17:13 +0530

    Fix issues

--- workspaces/api-designer/api-designer-visualizer/src/components/OpenAPIComponents/ComponentNavigator/PathTreeView/PathTreeView.tsx
+++ workspaces/api-designer/api-designer-visualizer/src/components/OpenAPIComponents/ComponentNavigator/PathTreeView/PathTreeView.tsx
@@ -11,7 +11,6 @@ import { LeftPathContainer, PathContainer, RightPathContainerButtons } from '../
 import { OpenAPI } from '../../../../Definitions/ServiceDefinitions';
 import { PathTreeViewItem } from '../PathTreeViewItem/PathTreeViewItem';
 import { useVisualizerContext } from '@wso2-enterprise/api-designer-rpc-client';
-import { APIResources } from '../../../../constants';
 import { useContext } from 'react';
 import { APIDesignerContext } from '../../../../APIDesignerContext';
 
@@ -50,51 +49,6 @@ export function PathTreeView(props: PathTreeViewProps) {
             }
         });
     };
-    const onPathRename = (newPath: string, oldPath: string) => {
-        const { paths } = openAPI;
-        const updatedPaths = { ...paths };
-        updatedPaths[newPath] = updatedPaths[oldPath];
-        delete updatedPaths[oldPath];
-        handlePathTreeViewChange({ ...openAPI, paths: updatedPaths });
-    };
-    const onAddResources = (path: string, methods: string[]) => {
-        const { paths } = openAPI;
-        const updatedPaths = { ...paths };
-        const currentPathItem = updatedPaths[path];
-        // Add methods to the currentPathItem
-        updatedPaths[path] = { ...currentPathItem, ...methods.reduce((acc, method) => ({ ...acc, [method]: {} }), {}) };
-        handlePathTreeViewChange({ ...openAPI, paths: updatedPaths });
-    };
-    const modifyPathClick = (evt: React.MouseEvent, path: string, currentOperations: string[]) => {
-        evt.stopPropagation();
-        rpcClient.selectQuickPickItem({
-            title: `Select an option`, items: [
-                { label: "Edit Route path", detail: `Edit the route '${path}'` },
-                { label: "Select Methods", detail: `Select the methods belonging to the path '${path}'` }
-            ]
-        }).then(res => {
-            if (res?.label === "Edit Route path") {
-                rpcClient.showInputBox({ title: "Edit Route path", value: path }).then(newPath => {
-                    if (onPathRename && newPath) {
-                        onPathRename(newPath, path)
-                    }
-                })
-            } else if (res?.label === "Select Methods") {
-                rpcClient.selectQuickPickItems({
-                    title: "Select the methods of the path", items: APIResources.map(method => ({
-                        label: method,
-                        picked: currentOperations.includes(method)
-                    }))
-                }).then(methodSelection => {
-                    if (!methodSelection || methodSelection.length < 1) {
-                        rpcClient.showErrorNotification("Need to select at least one method for the path")
-                    } else {
-                        onAddResources(path, methodSelection.map(item => item.label))
-                    }
-                })
-            }
-        })
-    };
 
     return (
         <TreeView
@@ -115,7 +69,6 @@ export function PathTreeView(props: PathTreeViewProps) {
                     </LeftPathContainer>
 
                     <RightPathContainerButtons className="buttons-container">
-                        <Button tooltip="Modify Path" appearance="icon" onClick={(e) => modifyPathClick(e, String(path), operations)}><Codicon name="gear" /></Button>
                         <Button tooltip="Delete Path" appearance="icon" onClick={(e) => handleDeletePath(e, String(path))}><Codicon name="trash" /></Button>
                     </RightPathContainerButtons>
                 </PathContainer>
