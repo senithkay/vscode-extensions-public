3080eda130b73e074cd8fb5ee3af8659858ea1f7
YasithD <yasith.18@cse.mrt.ac.lk>
2024-12-20 14:21:43 +0530

    Fix category page loading issue

--- workspaces/ballerina/ballerina-side-panel/src/components/editors/ExpressionEditor/HelperPane/CategoryPage.tsx
+++ workspaces/ballerina/ballerina-side-panel/src/components/editors/ExpressionEditor/HelperPane/CategoryPage.tsx
@@ -19,7 +19,7 @@ export const CategoryPage = ({ setCurrentPage, onClose }: CategoryPageProps) => 
     return (
         <>
             <HelperPane.Header title="Select Category" onClose={onClose} />
-            <HelperPane.Body>
+            <HelperPane.Body isLoading={false}>
                 <HelperPane.CategoryItem label="Variables" onClick={() => setCurrentPage(1)} />
                 <HelperPane.CategoryItem label="Functions" onClick={() => setCurrentPage(2)} />
                 <HelperPane.CategoryItem label="Configurables" onClick={() => setCurrentPage(3)} />
