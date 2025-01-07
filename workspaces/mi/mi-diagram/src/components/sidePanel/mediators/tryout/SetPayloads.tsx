3fd8d57f62a0f17955d97e1cc83204f4473cc56d
gigara <info@gigara.info>
2025-01-07 11:01:27 +0530

    Add info message

--- workspaces/mi/mi-diagram/src/components/sidePanel/mediators/tryout/SetPayloads.tsx
+++ workspaces/mi/mi-diagram/src/components/sidePanel/mediators/tryout/SetPayloads.tsx
@@ -7,7 +7,7 @@
  * You may not alter or remove any copyright or other notice from copies of this content.
 */
 
-import { Button, ProgressRing } from "@wso2-enterprise/ui-toolkit";
+import { Button, Icon, ProgressRing } from "@wso2-enterprise/ui-toolkit";
 
 import { Typography } from "@wso2-enterprise/ui-toolkit";
 import SidePanelContext, { clearSidePanelState } from "../../SidePanelContexProvider";
@@ -105,6 +105,10 @@ export function SetPayloads(props: SetPayloadsProps) {
                 sx={{ padding: "10px", marginBottom: "10px", borderBottom: "1px solid var(--vscode-editorWidget-border)" }}
                 variant="body3">
                 {`Save the payload to try out the mediators`}
+                <Typography variant="body2" sx={{ display: 'flex', gap: '10px', alignItems: 'center', marginTop: '10px' }}>
+                    <Icon name="info" isCodicon />
+                    <Typography variant="body3">Only JSON request types are supported</Typography>
+                </Typography>
             </Typography>
 
             <ParameterManager
