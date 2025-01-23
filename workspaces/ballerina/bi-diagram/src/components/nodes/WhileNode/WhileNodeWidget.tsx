f2e72214272f8d86008fda48768565d9422b23b3
Kanushka Gayan <kanushkanet@gmail.com>
2025-01-24 00:59:37 +0530

    Update error handler and while node icons with new font-wso2 icons

--- workspaces/ballerina/bi-diagram/src/components/nodes/WhileNode/WhileNodeWidget.tsx
+++ workspaces/ballerina/bi-diagram/src/components/nodes/WhileNode/WhileNodeWidget.tsx
@@ -19,7 +19,7 @@ import {
     NODE_GAP_X,
     CONTAINER_PADDING,
 } from "../../../resources/constants";
-import { Button, Item, Menu, MenuItem, Popover } from "@wso2-enterprise/ui-toolkit";
+import { Button, Item, Menu, MenuItem, Popover, Icon } from "@wso2-enterprise/ui-toolkit";
 import { FlowNode } from "../../../utils/types";
 import { useDiagramContext } from "../../DiagramContext";
 import { MoreVertIcon } from "../../../resources";
@@ -266,18 +266,7 @@ export function WhileNodeWidget(props: WhileNodeWidgetProps) {
                             />
                         )}
                         <NodeStyles.TopPortWidget port={model.getPort("in")!} engine={engine} />
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                            <g
-                                fill="none"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2.5"
-                                stroke={Colors.ON_SURFACE}
-                            >
-                                <path d="M12 3a9 9 0 1 1-5.657 2" />
-                                <path d="M3 4.5h4v4" />
-                            </g>
-                        </svg>
+                        <Icon name="bi-loop" sx={{ stroke: Colors.ON_SURFACE, fontSize: 24, width: 24, height: 24 }} />
                         <NodeStyles.BottomPortWidget port={model.getPort("out")!} engine={engine} />
                     </NodeStyles.Box>
                 </NodeStyles.Column>
