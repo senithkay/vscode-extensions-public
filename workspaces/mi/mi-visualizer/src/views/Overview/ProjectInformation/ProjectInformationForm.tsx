7f5baa24835a40d59551f7fc3adacf17bb71dc9b
tharindulak <tharindu.jayathilake1@gmail.com>
2025-01-26 13:40:58 +0530

    Init project setting revamp

--- workspaces/mi/mi-visualizer/src/views/Overview/ProjectInformation/ProjectInformationForm.tsx
+++ workspaces/mi/mi-visualizer/src/views/Overview/ProjectInformation/ProjectInformationForm.tsx
@@ -9,21 +9,30 @@
 
 import { DependencyDetails, ProjectDetailsResponse } from "@wso2-enterprise/mi-core";
 import { useVisualizerContext } from "@wso2-enterprise/mi-rpc-client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import { Button, Dropdown, Banner, FormActions, FormGroup, FormView, OptionProps, ProgressIndicator, TextField, Codicon } from "@wso2-enterprise/ui-toolkit";
+import { Button, Dropdown, Banner, FormActions, FormGroup, FormView, OptionProps, ProgressIndicator, TextField, Codicon, SplitView, TreeView, TreeViewItem, Typography } from "@wso2-enterprise/ui-toolkit";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Controller, useForm } from "react-hook-form";
+import styled from "@emotion/styled";
 
 interface ProjectInformationFormProps {
     onClose: () => void;
 }
+
+const TitleBoxShadow = styled.div`
+    box-shadow: var(--vscode-scrollbar-shadow) 0 6px 6px -6px inset;
+    height: 3px;
+`;
+
 export function ProjectInformationForm(props: ProjectInformationFormProps) {
     const { rpcClient } = useVisualizerContext();
     const [projectDetails, setProjectDetails] = useState<ProjectDetailsResponse>();
     const [runtimeVersions, setRuntimeVersions] = useState<OptionProps[]>([]);
     const [initialRuntimeVersion, setInitialRuntimeVersion] = useState<string>("");
+
+    const [selectedId, setSelectedId] = useState<string | null>("Project Information");
 
     const schema = yup.object({
         "primaryDetails.projectName": yup.string().required("Project Name is required"),
@@ -53,6 +62,13 @@ export function ProjectInformationForm(props: ProjectInformationFormProps) {
 
     const currentRuntimeVersion = watch("primaryDetails.runtimeVersion");
     const isRuntimeVersionChanged = currentRuntimeVersion && currentRuntimeVersion !== initialRuntimeVersion;
+
+    const divRefs: Record<string, React.RefObject<HTMLDivElement>> = {
+        "Project Information": useRef<HTMLDivElement | null>(null),
+        "Build Details": useRef<HTMLDivElement | null>(null),
+        "Unit Test": useRef<HTMLDivElement | null>(null),
+    };
+
     useEffect(() => {
         async function fetchData() {
             try {
@@ -83,43 +99,27 @@ export function ProjectInformationForm(props: ProjectInformationFormProps) {
         fetchData();
     }, [rpcClient, reset]);
 
-    const setDependencies = (dependencies: DependencyDetails[]) => {
-        return {
-            paramValues: dependencies.map((dep, index) => ({
-                id: index,
-                key: index.toString(),
-                value: dep.groupId,
-                icon: 'package',
-                paramValues: [
-                    { value: dep.groupId },
-                    { value: dep.artifact },
-                ]
-            })) || [],
-            paramFields: [
-                {
-                    "type": "TextField" as "TextField",
-                    "label": "Group ID",
-                    "defaultValue": "",
-                    "isRequired": true,
-                    "canChange": false
-                },
-                {
-                    "type": "TextField" as "TextField",
-                    "label": "Artifact ID",
-                    "defaultValue": "",
-                    "isRequired": true,
-                    "canChange": false
-                },
-                {
-                    "type": "TextField" as "TextField",
-                    "label": "Version",
-                    "defaultValue": "",
-                    "isRequired": true,
-                    "canChange": false
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setSelectedId(entry.target.id); // Update selectedId based on the visible div
                 }
-            ]
+            });
+        }, { threshold: 0.8 }); // Adjust threshold as needed
+
+        // Observe each div
+        Object.keys(divRefs).forEach(key => {
+            if (divRefs[key].current) {
+                observer.observe(divRefs[key].current);
+            }
+        });
+
+        return () => {
+            // Cleanup observer on unmount
+            observer.disconnect();
         };
-    };
+    }, [divRefs]);
 
     const handleFormSubmit = async () => {
         try {
@@ -160,96 +160,158 @@ export function ProjectInformationForm(props: ProjectInformationFormProps) {
         props.onClose();
     };
 
+    const handleClick = (id: string) => {
+        setSelectedId(id);
+        if (divRefs[id].current) {
+            divRefs[id].current.scrollIntoView({ behavior: "smooth", block: "start" }); // Scroll to the selected div
+        }
+    };
+
     if (!projectDetails) {
         return <ProgressIndicator />;
     }
 
+    console.log("Selected ID: ", selectedId);
+
     return (
-        <FormView title="Project Settings" onClose={handleCancel}>
-            <FormGroup title="Primary Details" isCollapsed={false}>
-                <TextField
-                    label="Project Name"
-                    required
-                    errorMsg={errors["primaryDetails.projectName"]?.message?.toString()}
-                    {...register("primaryDetails.projectName")}
-                />
-                <TextField
-                    label="Description"
-                    {...register("primaryDetails.projectDescription")}
-                />
-                <TextField
-                    label="Version"
-                    required
-                    errorMsg={errors["primaryDetails.projectVersion"]?.message?.toString()}
-                    {...register("primaryDetails.projectVersion")}
-                />
-                <div>
-                    <Dropdown
-                        id='runtimeVersion'
-                        label="Runtime Version"
-                        required
-                        errorMsg={errors["primaryDetails.runtimeVersion"]?.message?.toString()}
-                        items={runtimeVersions}
-                        {...register("primaryDetails.runtimeVersion")}
-                    />
-                    {isRuntimeVersionChanged && (
-                        <Banner 
-                        icon={<Codicon name="warning" sx={{fontSize:12}}/>} 
-                        type="warning" 
-                        message="Extension will restart when submitting" />
-                    )}
+        <SplitView sx={{ width: "auto", height: "auto", padding: 60, maxWidth: 1200 }} defaultWidths={[20, 80]} dynamicContainerSx={{ overflow: "visible" }}>
+            {/* Left side view */}
+            <div style={{ padding: "50px 0 50px 30px;" }}>
+                <TreeView rootTreeView id="Project Information" content={<Typography sx={{ margin: 0 }} variant="h4">Project Information</Typography>} selectedId={selectedId} onSelect={handleClick} />
+                <TreeView rootTreeView id="Build Details" content={<Typography sx={{ margin: 0 }} variant="h4">Build Details</Typography>} selectedId={selectedId} onSelect={handleClick} />
+                <TreeView rootTreeView id="Unit Test" content={<Typography sx={{ margin: 0 }} variant="h4">Unit Test</Typography>} selectedId={selectedId} onSelect={handleClick} />
+            </div>
+            {/* Right side view */}
+            <div style={{ paddingLeft: 40 }}>
+                {/* Title and subtitle */}
+                <div style={{ position: "sticky", top: 0, zIndex: 20005, height: 60, color: "var(--vscode-editor-foreground)", backgroundColor: "var(--vscode-editor-background)" }}>
+                    <Typography variant="h1" sx={{ marginTop: 0, paddingTop: 8 }} >{selectedId}</Typography>
+                    <TitleBoxShadow />
                 </div>
-            </FormGroup>
+                {/* Item 1 */}
+                <div style={{ maxHeight: '70vh', overflowY: 'auto' }}>
+                    {/* Body 1.1 */}
+                    <div ref={divRefs["Project Information"]} id="Project Information" style={{ display: "flex", flexDirection: "column", gap: 40, padding: "0 0 30px", marginTop: "20px" }}>
+                        <TextField
+                            label="Project Name"
+                            required
+                            description="The name of the project"
+                            descriptionSx={{ margin: "15px 0" }}
+                            errorMsg={errors["primaryDetails.projectName"]?.message?.toString()}
+                            {...register("primaryDetails.projectName")}
+                        />
+                        <TextField
+                            label="Description"
+                            description="The description of the project"
+                            descriptionSx={{ margin: "15px 0" }}
+                            {...register("primaryDetails.projectDescription")}
+                        />
+                        <TextField
+                            label="Version"
+                            required
+                            description="The version of the project"
+                            descriptionSx={{ margin: "15px 0" }}
+                            errorMsg={errors["primaryDetails.projectVersion"]?.message?.toString()}
+                            {...register("primaryDetails.projectVersion")}
+                        />
 
-            <FormGroup title="Build Details" isCollapsed={false}>
-                <TextField
-                    label="Base Image"
-                    required
-                    errorMsg={errors["dockerDetails.dockerFileBaseImage"]?.message?.toString()}
-                    {...register("dockerDetails.dockerFileBaseImage")}
-                />
-                <TextField
-                    label="Docker Name"
-                    required
-                    errorMsg={errors["dockerDetails.dockerName"]?.message?.toString()}
-                    {...register("dockerDetails.dockerName")}
-                />
-            </FormGroup>
-
-            <FormGroup title="Unit Tests Configuration" isCollapsed={false}>
-                <TextField
-                    label="Server Host"
-                    {...register("unitTest.serverHost")}
-                />
-                <TextField
-                    label="Server Port"
-                    {...register("unitTest.serverPort")}
-                />
-                <TextField
-                    label="Server Path"
-                    {...register("unitTest.serverPath")}
-                />
-                <TextField
-                    label="Server Type"
-                    {...register("unitTest.serverType")}
-                />
-            </FormGroup>
-
-            <FormActions>
-                <Button
-                    appearance="secondary"
-                    onClick={handleCancel}
-                >
-                    Cancel
-                </Button>
-                <Button
-                    appearance="primary"
-                    onClick={handleSubmit(handleFormSubmit)}
-                    disabled={!Object.keys(dirtyFields).length || isSubmitting}
-                >
-                    Save Changes
-                </Button>
-            </FormActions>
-        </FormView>
+                        <Dropdown
+                            id='runtimeVersion'
+                            label="Runtime Version"
+                            required
+                            description="The runtime version of the project"
+                            descriptionSx={{ margin: "13px 0 15px" }}
+                            errorMsg={errors["primaryDetails.runtimeVersion"]?.message?.toString()}
+                            items={runtimeVersions}
+                            {...register("primaryDetails.runtimeVersion")}
+                        />
+                        {isRuntimeVersionChanged && (
+                            <Banner
+                                icon={<Codicon name="warning" sx={{ fontSize: 12 }} />}
+                                type="warning"
+                                message="Extension will restart when submitting"
+                            />
+                        )}
+                    </div>
+                    <Typography variant="h1" sx={{ margin: 0 }} > Build Details </Typography>
+                    <div ref={divRefs["Build Details"]} id="Build Details" style={{ display: "flex", flexDirection: "column", gap: 40, padding: "0 0 30px", marginTop: "20px" }}>
+                        <TextField
+                            label="Base Image"
+                            required
+                            errorMsg={errors["dockerDetails.dockerFileBaseImage"]?.message?.toString()}
+                            description="The base image of the project"
+                            descriptionSx={{ margin: "15px 0" }}
+                            {...register("dockerDetails.dockerFileBaseImage")}
+                        />
+                        <TextField
+                            label="Docker Name"
+                            required
+                            errorMsg={errors["dockerDetails.dockerName"]?.message?.toString()}
+                            description="The name of the docker"
+                            descriptionSx={{ margin: "15px 0" }}
+                            {...register("dockerDetails.dockerName")}
+                        />
+                        <TextField
+                            label="Docker Namennnn"
+                            required
+                            errorMsg={errors["dockerDetails.dockerName"]?.message?.toString()}
+                            description="The name of the docker"
+                            descriptionSx={{ margin: "15px 0" }}
+                            {...register("dockerDetails.dockerName")}
+                        />
+                        <TextField
+                            label="Docker Name"
+                            required
+                            errorMsg={errors["dockerDetails.dockerName"]?.message?.toString()}
+                            description="The name of the docker"
+                            descriptionSx={{ margin: "15px 0" }}
+                            {...register("dockerDetails.dockerName")}
+                        />
+                    </div>
+                    <Typography variant="h1" sx={{ margin: 0 }} > Unit Test </Typography>
+                    <div ref={divRefs["Unit Test"]} id="Unit Test" style={{ display: "flex", flexDirection: "column", gap: 40, padding: "0 0 30px", marginTop: "20px" }}>
+                        <TextField
+                            label="Server Host"
+                            description="The host of the server"
+                            descriptionSx={{ margin: "15px 0" }}
+                            {...register("unitTest.serverHost")}
+                        />
+                        <TextField
+                            label="Server Port"
+                            description="The port of the server"
+                            {...register("unitTest.serverPort")}
+                        />
+                        <TextField
+                            label="Server Path"
+                            description="The path of the server"
+                            {...register("unitTest.serverPath")}
+                        />
+                        <TextField
+                            label="Server Type"
+                            description="The type of the server"
+                            {...register("unitTest.serverType")}
+                        />
+                    </div>
+                </div>
+                <div style={{ position: "sticky", bottom: 0, zIndex: 20005, height: 40, backgroundColor: "var(--vscode-editor-background)"}}>
+                    {/* <TitleBoxShadow/> */}
+                    <FormActions>
+                        <Button
+                            appearance="secondary"
+                            onClick={handleCancel}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            appearance="primary"
+                            onClick={handleSubmit(handleFormSubmit)}
+                            disabled={!Object.keys(dirtyFields).length || isSubmitting}
+                        >
+                            Save Changes
+                        </Button>
+                    </FormActions>
+                </div>
+            </div>
+        </SplitView>
     );
 }
