036d44db77af139c27e3f5c862385dc1ef36063b
ChamodA <chamoda@wso2.com>
2025-01-31 10:06:10 +0530

    Fix falsy recursive identification

--- workspaces/mi/mi-extension/src/util/dataMapper.ts
+++ workspaces/mi/mi-extension/src/util/dataMapper.ts
@@ -224,7 +224,7 @@ function getTypeInfoForInterface(typeNode: Type, allInterfaces: string[]): DMTyp
             isRecursive: true
         };
     } else {
-        allInterfaces.push(typeName);
+        allInterfaces = [...allInterfaces, typeName];
     }
 
     const fields = interfaceNode.getMembers().map(member => {
