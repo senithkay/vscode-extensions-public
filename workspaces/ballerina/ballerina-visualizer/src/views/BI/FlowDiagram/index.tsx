e1e5d7b3265adaac76f9800f8ff7edbe97130e60
Kanushka Gayan <kanushkanet@gmail.com>
2025-02-07 07:26:07 +0530

    Hide unsupported nodes from node pallete

--- workspaces/ballerina/ballerina-visualizer/src/views/BI/FlowDiagram/index.tsx
+++ workspaces/ballerina/ballerina-visualizer/src/views/BI/FlowDiagram/index.tsx
@@ -194,7 +194,24 @@ export function BIFlowDiagram(props: BIFlowDiagramProps) {
                     console.error(">>> Error getting available nodes", response);
                     return;
                 }
-                const convertedCategories = convertBICategoriesToSidePanelCategories(response.categories as Category[]);
+                // filter out some categories that are not supported in the diagram
+                // TODO: these categories should be supported in the future
+                const notSupportedCategories = [
+                    "PARALLEL_FLOW",
+                    "LOCK",
+                    "START",
+                    "TRANSACTION",
+                    "COMMIT",
+                    "ROLLBACK",
+                    "RETRY",
+                ];
+                const filteredCategories = response.categories.map(category => ({
+                    ...category,
+                    items: category?.items?.filter(
+                        (item) => item && 'codedata' in item && !notSupportedCategories.includes((item as AvailableNode).codedata?.node)
+                    )
+                })).filter(category => category.items && category.items.length > 0) as Category[];
+                const convertedCategories = convertBICategoriesToSidePanelCategories(filteredCategories);
                 setCategories(convertedCategories);
                 initialCategoriesRef.current = convertedCategories; // Store initial categories
                 // add draft node to model
