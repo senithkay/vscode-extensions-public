04932d734ba05475ca2af040fe4e719dc8d9c212
sachiniSam <sachiniprof@gmail.com>
2025-02-10 11:53:42 +0530

    Pass projectUri for view

--- workspaces/ballerina/ballerina-extension/src/utils/state-machine-utils.ts
+++ workspaces/ballerina/ballerina-extension/src/utils/state-machine-utils.ts
@@ -18,7 +18,7 @@ import { FindConstructByIndexVisitor } from "./history/find-construct-by-index-v
 import { getConstructBodyString } from "./history/util";
 import { ballerinaExtInstance } from "../core";
 
-export async function getView(documentUri: string, position: NodePosition): Promise<HistoryEntry> {
+export async function getView(documentUri: string, position: NodePosition, projectUri?: string): Promise<HistoryEntry> {
 
     const req = getSTByRangeReq(documentUri, position);
     const node = await StateMachine.langClient().getSTByRange(req) as SyntaxTreeResponse;
@@ -87,7 +87,8 @@ export async function getView(documentUri: string, position: NodePosition): Prom
                         view: MACHINE_VIEW.GraphQLDiagram,
                         identifier: node.syntaxTree.absoluteResourcePath.map((path) => path.value).join(''),
                         documentUri: documentUri,
-                        position: position
+                        position: position,
+                        projectUri: projectUri
                     }
                 };
             } else {
