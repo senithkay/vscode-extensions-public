24437267698f023240ea817e85ee6086b2b53009
YasithD <yasith.18@cse.mrt.ac.lk>
2025-03-05 11:37:01 +0530

    Fix debounce time in suggestions page

--- workspaces/ballerina/ballerina-visualizer/src/views/BI/HelperPane/SuggestionsPage.tsx
+++ workspaces/ballerina/ballerina-visualizer/src/views/BI/HelperPane/SuggestionsPage.tsx
@@ -64,7 +64,7 @@ export const SuggestionsPage = ({ fileName, targetLineRange, defaultValue, onCha
         debounce((searchText: string) => {
             setFilteredVariableInfo(filterHelperPaneVariables(variableInfo, searchText));
             setIsLoading(false);
-        }, 5000),
+        }, 150),
         [variableInfo, setFilteredVariableInfo, setIsLoading, filterHelperPaneVariables]
     );
 
