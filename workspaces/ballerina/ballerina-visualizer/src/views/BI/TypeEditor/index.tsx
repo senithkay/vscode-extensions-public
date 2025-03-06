7b80e2bb02b55deef426f833db88f84a6d5462e4
YasithD <yasith.18@cse.mrt.ac.lk>
2025-03-06 11:58:12 +0530

    Update TypeEditor for BI forms

--- workspaces/ballerina/ballerina-visualizer/src/views/BI/TypeEditor/index.tsx
+++ workspaces/ballerina/ballerina-visualizer/src/views/BI/TypeEditor/index.tsx
@@ -7,12 +7,13 @@
  * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
-import { useEffect, useState } from 'react';
-import { Type } from '@wso2-enterprise/ballerina-core';
+import { useCallback, useEffect, useState } from 'react';
+import { debounce } from 'lodash';
+import { LineRange, Type } from '@wso2-enterprise/ballerina-core';
 import { useRpcContext } from '@wso2-enterprise/ballerina-rpc-client';
-import { TypeEditor, TypeHelperCategory } from '@wso2-enterprise/type-editor';
+import { TypeEditor, TypeHelperCategory, TypeHelperItem, TypeHelperOperator } from '@wso2-enterprise/type-editor';
 import { TYPE_HELPER_OPERATORS } from './constants';
-import { getCategories } from './utils';
+import { filterOperators, filterTypes, getTypeBrowserTypes, getTypes } from './utils';
 
 type FormTypeEditorProps = {
     type?: Type;
@@ -24,36 +25,139 @@ type FormTypeEditorProps = {
 export const FormTypeEditor = (props: FormTypeEditorProps) => {
     const { type, onTypeChange, newType, isGraphql } = props;
     const { rpcClient } = useRpcContext();
-    const [categories, setCategories] = useState<TypeHelperCategory[]>([]);
+
+    const [filePath, setFilePath] = useState<string | undefined>(undefined);
+    const [targetLineRange, setTargetLineRange] = useState<LineRange | undefined>(undefined);
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [loadingTypeBrowser, setLoadingTypeBrowser] = useState<boolean>(false);
+
+    const [basicTypes, setBasicTypes] = useState<TypeHelperCategory[] | undefined>(undefined);
+    const [filteredBasicTypes, setFilteredBasicTypes] = useState<TypeHelperCategory[]>([]);
+    const [filteredOperators, setFilteredOperators] = useState<TypeHelperOperator[]>([]);
+    const [filteredTypeBrowserTypes, setFilteredTypeBrowserTypes] = useState<TypeHelperCategory[]>([]);
 
     useEffect(() => {
         if (rpcClient) {
             rpcClient.getVisualizerLocation().then((machineView) => {
+                setFilePath(machineView.metadata.recordFilePath);
                 rpcClient
                     .getBIDiagramRpcClient()
-                    .getVisibleTypes({
-                        filePath: machineView.metadata.recordFilePath,
-                        position: {
-                            line: 0,
-                            offset: 0
-                        }
+                    .getEndOfFile({
+                        filePath: machineView.metadata.recordFilePath
                     })
-                    .then((types) => {
-                        setCategories(getCategories(types));
-                    })
+                    .then((linePosition) => {
+                        setTargetLineRange({
+                            startLine: linePosition,
+                            endLine: linePosition
+                        });
+                    });
             });
         }
-    }, []);
+    }, [rpcClient]);
+
+    const debouncedSearchTypeHelper = useCallback(
+        debounce((searchText: string, isType: boolean) => {
+            if (isType && basicTypes === undefined) {
+                if (rpcClient) {
+                    rpcClient
+                        .getBIDiagramRpcClient()
+                        .getVisibleTypes({
+                            filePath: filePath,
+                            position: {
+                                line: targetLineRange.startLine.line,
+                                offset: targetLineRange.startLine.offset
+                            }
+                        })
+                        .then((types) => {
+                            setBasicTypes(getTypes(types));
+                            setFilteredBasicTypes(getTypes(types));
+                        })
+                        .finally(() => {
+                            setLoading(false);
+                        });
+                }
+            } else if (isType) {
+                setFilteredBasicTypes(filterTypes(basicTypes, searchText));
+            } else {
+                setFilteredOperators(filterOperators(TYPE_HELPER_OPERATORS, searchText));
+            }
+
+            setLoading(false);
+        }, 150),
+        [basicTypes, filePath, targetLineRange]
+    );
+
+    const handleSearchTypeHelper = useCallback(
+        (searchText: string, isType: boolean) => {
+            setLoading(true);
+            debouncedSearchTypeHelper(searchText, isType);
+        },
+        [debouncedSearchTypeHelper, basicTypes]
+    );
+
+    const debouncedSearchTypeBrowser = useCallback(
+        debounce((searchText: string) => {
+            if (rpcClient) {
+                setLoadingTypeBrowser(true);
+                rpcClient.getVisualizerLocation().then((machineView) => {
+                    rpcClient
+                        .getBIDiagramRpcClient()
+                        .search({
+                            filePath: machineView.metadata.recordFilePath,
+                            position: targetLineRange,
+                            queryMap: {
+                                q: searchText,
+                                offset: 0,
+                                limit: 60
+                            },
+                            searchKind: 'TYPE'
+                        })
+                        .then((response) => {
+                            setFilteredTypeBrowserTypes(getTypeBrowserTypes(response.categories));
+                        })
+                        .finally(() => {
+                            setLoadingTypeBrowser(false);
+                        });
+                });
+            }
+        }, 150),
+        [filteredTypeBrowserTypes]
+    );
+
+    const handleSearchTypeBrowser = useCallback(
+        (searchText: string) => {
+            setLoadingTypeBrowser(true);
+            debouncedSearchTypeBrowser(searchText);
+        },
+        [debouncedSearchTypeBrowser, filteredTypeBrowserTypes]
+    );
+
+    const handleTypeItemClick = (item: TypeHelperItem) => {
+        console.log(item);
+    };
 
     return (
-        <TypeEditor
-            type={type}
-            rpcClient={rpcClient}
-            onTypeChange={onTypeChange}
-            newType={newType}
-            isGraphql={isGraphql}
-            categories={categories}
-            operators={TYPE_HELPER_OPERATORS}
-        />
+        <>
+            {filePath && targetLineRange && (
+                <TypeEditor
+                    type={type}
+                    rpcClient={rpcClient}
+                    onTypeChange={onTypeChange}
+                    newType={newType}
+                    isGraphql={isGraphql}
+                    typeHelper={{
+                        loading,
+                        loadingTypeBrowser,
+                        basicTypes: filteredBasicTypes,
+                        operators: filteredOperators,
+                        typeBrowserTypes: filteredTypeBrowserTypes,
+                        onSearchTypeHelper: handleSearchTypeHelper,
+                        onSearchTypeBrowser: handleSearchTypeBrowser,
+                        onTypeItemClick: handleTypeItemClick
+                    }}
+                />
+            )}
+        </>
     );
 };
