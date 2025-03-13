5e47a519ddb301ce1f19db6c5a9a35eb4b48baa6
Nipuna Fernando <nipunafe@gmail.com>
2025-03-13 12:11:01 +0530

    Update the function forms

--- workspaces/ballerina/ballerina-visualizer/src/views/BI/FunctionForm/index.tsx
+++ workspaces/ballerina/ballerina-visualizer/src/views/BI/FunctionForm/index.tsx
@@ -8,7 +8,7 @@
  */
 
 import { useEffect, useRef, useState } from "react";
-import { FunctionNode, LineRange, NodeProperties } from "@wso2-enterprise/ballerina-core";
+import { FunctionNode, LineRange, NodeKind, NodeProperties } from "@wso2-enterprise/ballerina-core";
 import { View, ViewContent } from "@wso2-enterprise/ui-toolkit";
 import styled from "@emotion/styled";
 import { useRpcContext } from "@wso2-enterprise/ballerina-rpc-client";
@@ -49,53 +49,63 @@ export function FunctionForm(props: FunctionFormProps) {
     const [functionFields, setFunctionFields] = useState<FormField[]>([]);
     const [functionNode, setFunctionNode] = useState<FunctionNode>(undefined);
     const [targetLineRange, setTargetLineRange] = useState<LineRange>();
+    const [titleSubtitle, setTitleSubtitle] = useState<string>("");
+    const [formSubtitle, setFormSubtitle] = useState<string>("");
 
     const fileName = filePath.split(/[\\/]/).pop();
-    const formType = useRef(
-        isAutomation ? "Automation" : 
-        isDataMapper ? "Data Mapper" : 
-        isNpFunction ? "Natural Function" : 
-        "Function"
-    );
+    const formType = useRef("Function");
 
     useEffect(() => {
+        let nodeKind: NodeKind;
+        if (isAutomation || functionName === "main") {
+            nodeKind = 'AUTOMATION';
+            formType.current = "Automation";
+            setTitleSubtitle('An automation that can be invoked periodically or manually');
+            setFormSubtitle('Periodic invocation should be scheduled in an external system such as cronjob, k8s, or Devant');
+        } else if (isDataMapper) {
+            nodeKind = 'DATA_MAPPER_DEFINITION';
+            formType.current = 'Data Mapper';
+            setTitleSubtitle('Transform data between different data types');
+            setFormSubtitle('Create mappings on how to convert the inputs to a single record output');
+        } else if (isNpFunction) {
+            nodeKind = 'NP_FUNCTION_DEFINITION';
+            formType.current = 'Natural Programming Function';
+            setTitleSubtitle('Build a flow using a natural language description');
+            setFormSubtitle('Describe what you need in a prompt and let AI handle the implementation');
+        } else {
+            nodeKind = 'FUNCTION_DEFINITION';
+            formType.current = 'Function';
+            setTitleSubtitle('Build reusable custom flows');
+            setFormSubtitle('Define a flow that can be used within your integration');
+        }
+
         if (functionName) {
             getExistingFunctionNode();
         } else {
-            getFunctionNode();
+            getFunctionNode(nodeKind);
         }
-    }, [filePath, functionName, isDataMapper, isAutomation]);
+    }, [isDataMapper, isNpFunction, isAutomation, functionName]);
 
     useEffect(() => {
         let fields = functionNode ? convertConfig(functionNode.properties) : [];
         
+        // TODO: Remove this once the hidden flag is implemented 
         if (isAutomation || functionName === "main") {
             formType.current = "Automation";
             const automationFields = fields.filter(field => field.key !== "functionName" && field.key !== "type");
             fields = automationFields;
         }
-        
-        if (isDataMapper && fields.length > 0) {
-            fields.forEach((field) => {
-                field.optional = false;
-            });
-        }
 
         setFunctionFields(fields);
-    }, [functionNode, isAutomation, isDataMapper]);
+    }, [functionNode]);
 
-    const getFunctionNode = async () => {
+    const getFunctionNode = async (kind: NodeKind) => {
         const res = await rpcClient
             .getBIDiagramRpcClient()
             .getNodeTemplate({
                 position: { line: 0, offset: 0 },
                 filePath: Utils.joinPath(URI.file(projectPath), fileName).fsPath,
-                id: { 
-                    node: isAutomation ? 'AUTOMATION' :
-                          isDataMapper ? 'DATA_MAPPER_DEFINITION' : 
-                          isNpFunction ? 'NP_FUNCTION_DEFINITION' : 
-                          'FUNCTION_DEFINITION' 
-                },
+                id: { node: kind },
             });
         const flowNode = res.flowNode;
         setFunctionNode(flowNode);
@@ -160,37 +170,19 @@ export function FunctionForm(props: FunctionFormProps) {
         }
     }, [filePath, rpcClient]);
 
-    const getTitleSubtitle = () => {
-        if (isAutomation || functionName === "main") {
-            return "Periodic invocation should be scheduled in an external system such as cronjob, k8s, or Devant";
-        } 
-        let keyword = isDataMapper ? "data mappers" : isNpFunction ? "natural functions" : "functions";
-        return `Manage ${keyword} in your integration`;  
-    };
-
-    const getFormSubtitle = () => {
-        if (isAutomation || functionName === "main") {
-            return 'Create an automation that can be invoked periodically or manually';
-        }
-        // TOOD: Define the subtitle for data mappers and natural functions
-        return '';
-    };
-
     return (
         <View>
             <TopNavigationBar />
             <TitleBar 
                 title={formType.current} 
-                subtitle={getFormSubtitle()} 
+                subtitle={titleSubtitle} 
             />
             <ViewContent padding>
                 <Container>
-                    {functionName && (
-                        <FormHeader title={`Edit ${formType.current}`} subtitle={getTitleSubtitle()} />
-                    )}
-                    {!functionName && (
-                        <FormHeader title={`Create New ${formType.current}`} subtitle={getFormSubtitle()} />
-                    )}
+                    <FormHeader 
+                        title={`${functionName ? 'Edit' : 'Create New'} ${formType.current}`}
+                        subtitle={formSubtitle} 
+                    />
                     <FormContainer>
                         {filePath && targetLineRange && functionFields.length > 0 &&
                             <FormGeneratorNew
