67c334296bb99acf102cbed55e12e14c8f3a2ede
Anjana S Porawagama <axewilledge123@gmail.com>
2025-03-13 15:31:36 +0530

    Update workspaces/mi/mi-extension/src/constants/index.ts
    Co-authored-by: Chamupathi Gigara Hettige <info@gigara.info>

--- workspaces/mi/mi-extension/src/constants/index.ts
+++ workspaces/mi/mi-extension/src/constants/index.ts
@@ -108,7 +108,7 @@ export const COMMANDS = {
     DEVAN_DEPLOY: 'wso2.wso2-platform.create.component',
     DEVAN_OPEN_COMPONENT: 'wso2.wso2-platform.open.component.src',
 
-    BI_EXTENSION: 'ballerina-integrator',
+    BI_EXTENSION: 'WSO2.ballerina-integrator',
     BI_OPEN_COMMAND: 'ballerina.open.bi.welcome',
     INSTALL_EXTENSION_COMMAND: 'workbench.extensions.installExtension'
 };
