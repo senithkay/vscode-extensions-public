b192906ff4040c1db479ceefeaa2519ed8713f21
sachiniSam <sachiniprof@gmail.com>
2025-03-13 20:05:23 +0530

    Revert all listener deletion changes

--- workspaces/ballerina/ballerina-core/src/interfaces/bi.ts
+++ workspaces/ballerina/ballerina-core/src/interfaces/bi.ts
@@ -204,7 +204,6 @@ export enum DIRECTORY_SUB_TYPE {
     SERVICE = "service",
     AUTOMATION = "automation",
     TRIGGER = "trigger",
-    LISTENER = "listener",
     DATA_MAPPER = "dataMapper",
     NATURAL_FUNCTIONS = "naturalFunction",
     AGENTS = "agents",
