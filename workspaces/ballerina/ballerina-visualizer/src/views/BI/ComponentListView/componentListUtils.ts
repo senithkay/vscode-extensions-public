4e84e37266ec92c52efd7f9fe611bdfb7a351ce5
madushajg <madushajg@gmail.com>
2025-03-14 15:02:46 +0530

    Drop 'beta' flag from features

--- workspaces/ballerina/ballerina-visualizer/src/views/BI/ComponentListView/componentListUtils.ts
+++ workspaces/ballerina/ballerina-visualizer/src/views/BI/ComponentListView/componentListUtils.ts
@@ -6,12 +6,5 @@
  * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
  * You may not alter or remove any copyright or other notice from copies of this content.
  */
-
-const BetaModules = ["graphql", "tcp", "salesforce", "trigger.github"];
-
 export const OutOfScopeComponentTooltip = "This feature is not available in the current scope.";
 export const AutomationAlreadyExistsTooltip = "An integration can only have one automation. You have already created an automation.";
-
-export function isBetaModule(moduleName: string) {
-    return BetaModules.includes(moduleName);
-}
