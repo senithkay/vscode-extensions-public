270f971b4ed6efc3040b9d35d31e2fff1a038a57
Kanushka Gayan <kanushkanet@gmail.com>
2025-03-28 09:37:38 +0530

    Update ReadonlyField component styles and add bi-lock icon

--- workspaces/ballerina/ballerina-side-panel/src/components/editors/ReadonlyField.tsx
+++ workspaces/ballerina/ballerina-side-panel/src/components/editors/ReadonlyField.tsx
@@ -9,7 +9,7 @@
 
 import React from "react";
 import { FormField } from "../Form/types";
-import { Codicon, Button } from "@wso2-enterprise/ui-toolkit";
+import { Button, Icon } from "@wso2-enterprise/ui-toolkit";
 import { capitalize } from "./utils";
 import styled from "@emotion/styled";
 
@@ -19,12 +19,11 @@ interface ReadonlyFieldProps {
 
 const Container = styled.div`
     width: 100%;
+    cursor: not-allowed;
 `;
 
 const Label = styled.div`
-    font: GlimmerRegular;
-    font-size: 12px;
-    color: var(--vscode-foreground);
+    color: var(--vscode-editor-foreground);
     margin-bottom: 4px;
 `;
 
@@ -33,24 +32,21 @@ const RequiredMark = styled.span`
 `;
 
 const Description = styled.div`
-    font-size: 12px;
-    color: var(--vscode-descriptionForeground);
+    color: var(--vscode-list-deemphasizedForeground);
     margin-top: 4px;
 `;
 
 const InputContainer = styled.div`
     display: flex;
     align-items: center;
-    height: 24px;
-    background-color: var(--vscode-input-background);
-    border: 1px solid var(--vscode-input-border);
-    border-radius: 5px;
-    padding: 0 8px;
-    font-size: 12px;
-    color: var(--vscode-input-foreground);
-    opacity: 0.8;
-    min-width: 120px;
-    width: fit-content;
+    color: var(--input-foreground);
+    background: var(--input-background);
+    border-radius: calc(var(--corner-radius)* 1px);
+    border: calc(var(--border-width)* 1px) solid var(--dropdown-border);
+    height: calc(var(--input-height)* 1px);
+    min-width: var(--input-min-width);
+    width: calc(100% - 34px);
+    padding: 0 calc(var(--design-unit) * 2px + 1px);
     margin-top: 10px;
 `;
 
@@ -59,8 +55,9 @@ const Value = styled.span`
 `;
 
 const StyledButton = styled(Button)`
-    cursor: default !important;
     padding: 0;
+    margin-right: -6px;
+    cursor: not-allowed;
 
     :host([disabled]) {
         opacity: 1 !important;
@@ -85,22 +82,11 @@ export function ReadonlyField(props: ReadonlyFieldProps) {
                 {capitalize(field.label)}
                 {!field.optional && <RequiredMark>*</RequiredMark>}
             </Label>
-            {field.documentation && (
-                <Description>
-                    {field.documentation}
-                </Description>
-            )}
+            {field.documentation && <Description>{field.documentation}</Description>}
             <InputContainer>
                 <Value>{field.value}</Value>
-                <StyledButton
-                    appearance="icon"
-                    tooltip="Read-only"
-                    disabled
-                >
-                    <Codicon
-                        name="lock-small"
-                        sx={{ cursor: 'default', paddingLeft: '4px' }}
-                    />
+                <StyledButton appearance="icon" tooltip="Read only" disabled>
+                    <Icon name="bi-lock" sx={{ fontSize: 16, width: 16, height: 16}} />
                 </StyledButton>
             </InputContainer>
         </Container>
