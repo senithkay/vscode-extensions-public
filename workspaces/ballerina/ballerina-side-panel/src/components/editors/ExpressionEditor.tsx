43e9c2c264db116497aa06d8fee2becd3971f35e
YasithD <yasith.18@cse.mrt.ac.lk>
2025-04-01 10:30:16 +0530

    Change default expression editor height

--- workspaces/ballerina/ballerina-side-panel/src/components/editors/ExpressionEditor.tsx
+++ workspaces/ballerina/ballerina-side-panel/src/components/editors/ExpressionEditor.tsx
@@ -217,7 +217,7 @@ export const ExpressionEditor = forwardRef<FormExpressionEditorRef, ExpressionEd
         helperPaneOrigin,
         helperPaneHeight,
         recordTypeField,
-        growRange = { start: 4, offset: 4 },
+        growRange = { start: 1, offset: 4 },
         rawExpression, // original expression
         sanitizedExpression // sanitized expression that will be rendered in the editor
     } = props as ExpressionEditorProps;
