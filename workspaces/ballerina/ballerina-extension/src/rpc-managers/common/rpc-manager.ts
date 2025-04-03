d9eb41372c760c86753d343eb0ac4a8a5f6f501d
madushajg <madushajg@gmail.com>
2025-04-03 23:47:01 +0530

    Show error notification for function creation failures

--- workspaces/ballerina/ballerina-extension/src/rpc-managers/common/rpc-manager.ts
+++ workspaces/ballerina/ballerina-extension/src/rpc-managers/common/rpc-manager.ts
@@ -32,12 +32,12 @@ import {
     WorkspacesFileResponse,
 } from "@wso2-enterprise/ballerina-core";
 import child_process from 'child_process';
-import { Uri, commands, env, window, workspace } from "vscode";
+import { Uri, commands, env, window, workspace, MarkdownString } from "vscode";
 import { URI } from "vscode-uri";
 import { ballerinaExtInstance } from "../../core";
 import { StateMachine } from "../../stateMachine";
 import { goToSource } from "../../utils";
-import { askFilePath, askProjectPath, getUpdatedSource } from "./utils";
+import { askFilePath, askProjectPath, BALLERINA_INTEGRATOR_ISSUES_URL, getUpdatedSource } from "./utils";
 import path from 'path';
 
 export class CommonRpcManager implements CommonRPCAPI {
@@ -212,6 +212,8 @@ export class CommonRpcManager implements CommonRPCAPI {
     }
 
     async showErrorMessage(params: ShowErrorMessageRequest): Promise<void> {
-        window.showErrorMessage(params.message);
+        const messageWithLink = new MarkdownString(params.message);
+        messageWithLink.appendMarkdown(`\n\nPlease [create an issue](${BALLERINA_INTEGRATOR_ISSUES_URL}) if the issue persists.`);
+        window.showErrorMessage(messageWithLink.value);
     }
 }
