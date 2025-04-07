299bcd4d01bea35d197419aedf59fcd34f39318f
ChamodA <chamoda@wso2.com>
2025-04-07 12:09:48 +0530

    Use ProjectExplorer to open data mapper for arrays

--- workspaces/mi/mi-extension/src/test/e2e-playwright-tests/dataMapper.spec.ts
+++ workspaces/mi/mi-extension/src/test/e2e-playwright-tests/dataMapper.spec.ts
@@ -15,18 +15,21 @@ import { ServiceDesigner } from './components/ServiceDesigner';
 import { Diagram } from './components/Diagram';
 import { clearNotificationAlerts, clearNotificationsByCloseButton, initTest, newProjectPath, page, resourcesFolder, vscode } from './Utils';
 import { DataMapper } from './components/DataMapper';
+import { ProjectExplorer } from './components/ProjectExplorer';
 
 const fs = require('fs');
 
 export default function createTests() {
 
   test.describe('Data Mapper Tests', () => {
-    const NEED_INITIAL_SETUP = true;
+    const NEED_INITIAL_SETUP = false;
 
-    initTest(true);
+    initTest();
 
     if (NEED_INITIAL_SETUP) setupProject();
-    testBasicMappings();
+    // testBasicMappings();
+    testArrayMappings();
+    
    
     function setupProject() {
       test('Setup project for Data Mapper', async () => {
@@ -159,6 +162,196 @@ export default function createTests() {
         await dm.mapFields('input.opmI.op2', 'objectOutput.ompO.p2');
         await dmWebView.getByTestId('link-from-input.opmI.op2.OUT-to-objectOutput.ompO.p2.IN').waitFor({ state: 'attached' });
 
+
+        expect(dm.verifyTsFileContent('map.ts.cmp')).toBeTruthy();
+
+        if (NEED_INITIAL_SETUP) {
+          await dmWebView.locator('vscode-button[title="Go Back"]').click();
+        }
+
+      });
+    }
+
+    function testArrayMappings() {
+      test('Test Array Mappings', async () => {
+
+        console.log('Testing Array Mappings');
+
+        let dm: DataMapper;
+        const DM_NAME = 'array';
+
+       
+
+        if (NEED_INITIAL_SETUP) {
+          const diagram = new Diagram(page.page, 'Resource');
+          await diagram.init();
+          await diagram.addDataMapper(DM_NAME);
+          dm = new DataMapper(page.page, DM_NAME);
+          await dm.init();
+          await dm.loadJsonFromCompFolder();
+          expect(dm.verifyTsFileContent('init.ts.cmp')).toBeTruthy();
+        } else {
+          const projectExplorer = new ProjectExplorer(page.page);
+          await projectExplorer.findItem(['Project testProject', 'Other Artifacts', 'Data Mappers', DM_NAME], true);
+          dm = new DataMapper(page.page, DM_NAME);
+          await dm.init();
+        }
+
+        const dmWebView = dm.getWebView();
+
+        // primitive direct array mapping
+        await dmWebView.locator('[id="recordfield-input\\.d1I"]').click();
+        await dmWebView.getByTestId('array-type-editable-record-field-objectOutput.d1O.IN').locator('i').click();
+        await dmWebView.locator('#menu-item-a2a-direct').click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-input.d1I.OUT-to-objectOutput.d1O.IN').waitFor({ state: 'attached' });
+
+        // primitive array mapping with mapping function
+        await dmWebView.locator('[id="recordfield-input\\.m1I"]').click();
+        await dmWebView.getByTestId('array-type-editable-record-field-objectOutput.m1O.IN').locator('i').click();
+        await dmWebView.locator('#menu-item-a2a-inner').click();
+        // await dm.waitForProgressEnd();
+        // await dmWebView.getByTestId('link-from-input.m1I.OUT-to-datamapper-intermediate-port').waitFor({ state: 'attached' });
+        // await dmWebView.getByTestId('link-from-datamapper-intermediate-port-to-objectOutput.m1O.IN').waitFor({ state: 'attached' });
+
+        // await dmWebView.getByTitle('Map array elements').locator('i').click();
+        await dmWebView.getByTestId('focusedInput.m1IItem-node').locator('i').click();
+        await dmWebView.getByTestId('primitiveOutput-node').locator('i').nth(1).click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-focusedInput.m1IItem.OUT-to-primitiveOutput.number.IN').waitFor({ state: 'attached' });
+
+        await dmWebView.getByTestId('dm-header-breadcrumb-0').click();
+        await clearNotificationsByCloseButton(page);
+
+        // object array mapping with mapping function
+        await dmWebView.locator('[id="recordfield-input\\.m1objI"]').click();
+        await dmWebView.getByTestId('array-type-editable-record-field-objectOutput.m1objO.IN').locator('i').click();
+        await dmWebView.locator('#menu-item-a2a-inner').click();
+        // await dm.waitForProgressEnd();
+        // await dmWebView.getByTestId('link-from-input.m1objI.OUT-to-datamapper-intermediate-port').waitFor({ state: 'attached' });
+        // await dmWebView.getByTestId('link-from-datamapper-intermediate-port-to-objectOutput.m1objO.IN').waitFor({ state: 'attached' });
+
+        // await dmWebView.getByTestId('array-connector-node-objectOutput.m1objO.IN').getByTitle('Map array elements').locator('i').click();
+        await dmWebView.locator('[id="recordfield-focusedInput\\.m1objIItem\\.p1"]').click();
+        await dmWebView.locator('[id="recordfield-objectOutput\\.q1"]').first().click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-focusedInput.m1objIItem.p1.OUT-to-objectOutput.q1.IN').waitFor({ state: 'attached' });
+
+        await dmWebView.locator('[id="recordfield-focusedInput\\.m1objIItem\\.p2"]').click();
+        await page.page.waitForTimeout(1000);
+
+
+        await dmWebView.getByTestId('array-type-editable-record-field-objectOutput.q2.IN').locator('i').click();
+        await dmWebView.locator('#menu-item-a2a-inner').click();
+        // await dm.waitForProgressEnd();
+        // await dmWebView.getByTestId('link-from-focusedInput.m1objIItem.p2.OUT-to-datamapper-intermediate-port').waitFor({ state: 'attached' });
+        // await dmWebView.getByTestId('link-from-datamapper-intermediate-port-to-objectOutput.q2.IN').waitFor({ state: 'attached' });
+
+        // await dmWebView.getByTitle('Map array elements').locator('i').click();
+        await dmWebView.getByTestId('focusedInput.p2Item-node').locator('i').click();
+        await dmWebView.getByTestId('primitiveOutput-node').locator('i').nth(1).click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-focusedInput.p2Item.OUT-to-primitiveOutput.string.IN').waitFor({ state: 'attached' });
+
+        await dm.gotoPreviousView();
+        await dm.gotoPreviousView();
+
+        // Initialize 1d array and map
+        await dmWebView.locator('[id="recordfield-objectOutput\\.i1O"] #component-list-menu-btn i').click();
+        await dmWebView.getByRole('gridcell', { name: 'Initialize Array' }).click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('array-widget-objectOutput.i1O.IN-add-element').click();
+        await dmWebView.locator('[id="recordfield-input\\.i1I"]').click();
+        await dmWebView.getByTestId('array-widget-objectOutput.i1O.IN-values').locator('i').first().click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-input.i1I.OUT-to-objectOutput.i1O.0.IN').waitFor({ state: 'attached' });
+
+
+        // Initialize 2d array and map
+        await dmWebView.locator('[id="recordfield-objectOutput\\.i2O"] #component-list-menu-btn i').click();
+        await dmWebView.getByText('Initialize Array').click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('array-widget-objectOutput.i2O.IN-add-element').click();
+        await dmWebView.getByTestId('array-widget-objectOutput.i2O.0.IN-add-element').click();
+        await dmWebView.locator('[id="recordfield-input\\.i1I"]').click();
+        await dmWebView.getByTestId('array-widget-objectOutput.i2O.0.IN-values').locator('i').first().click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-input.i1I.OUT-to-objectOutput.i2O.0.0.IN').waitFor({ state: 'attached' });
+
+
+        // Init array object and map scroll 
+
+        await dmWebView.locator('[id="recordfield-objectOutput\\.iobjO"] #component-list-menu-btn i').click();
+        await dmWebView.getByText('Initialize Array').click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('array-widget-objectOutput.iobjO.IN-add-element').click();
+        await dmWebView.locator('[id="recordfield-input\\.i1I"]').click();
+        await dm.scrollClickOutput(dmWebView.locator('[id="recordfield-objectOutput\\.iobjO\\.0\\.p1"]'));
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-input.i1I.OUT-to-objectOutput.iobjO.0.p1.IN').waitFor({ state: 'attached' });
+
+        await dmWebView.locator('[id="recordfield-objectOutput\\.iobjO\\.0\\.p2"] #component-list-menu-btn i').click();
+        await dmWebView.getByText('Initialize Array').click();
+        await dmWebView.getByTestId('array-widget-objectOutput.iobjO.0.p2.IN-add-element').click();
+        await dmWebView.locator('[id="recordfield-input\\.i1I"]').click();
+        await dmWebView.getByTestId('array-widget-objectOutput.iobjO.0.p2.IN-values').locator('i').first().click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-input.i1I.OUT-to-objectOutput.iobjO.0.p2.0.IN').waitFor({ state: 'attached' });
+
+        await dm.scrollClickOutput(dmWebView.getByTestId('array-widget-objectOutput.iobjO.IN-add-element'));
+        await dmWebView.locator('[id="recordfield-input\\.iobjI"]').click();
+        await dmWebView.locator('[id="recordfield-objectOutput\\.iobjO\\.1"]').click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-input.iobjI.OUT-to-objectOutput.iobjO.1.IN').waitFor({ state: 'attached' });
+
+
+        // 2D array direct mapping
+        await dmWebView.locator('[id="recordfield-input\\.d2I"]').click();
+        await dm.scrollClickOutput(dmWebView.getByTestId('array-type-editable-record-field-objectOutput.d2O.IN').locator('i'));
+        await dmWebView.locator('#menu-item-a2a-direct').click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-input.d2I.OUT-to-objectOutput.d2O.IN').waitFor({ state: 'attached' });
+
+        // 1D - 0D array direct mapping (singleton access)
+        await dmWebView.locator('[id="recordfield-input\\.s10O"]').click();
+        await dm.scrollClickOutput(dmWebView.locator('[id="recordfield-objectOutput\\.s10O"]').first());
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-input.s10O.OUT-to-datamapper-intermediate-port').waitFor({ state: 'attached' });
+        await dmWebView.getByTestId('link-from-datamapper-intermediate-port-to-objectOutput.s10O.IN').waitFor({ state: 'attached' });
+        await dmWebView.getByTestId('link-connector-node-objectOutput.s10O.IN').waitFor();
+
+        // 2D - 1D array direct mapping (singleton access) 30-38
+        await dmWebView.locator('[id="recordfield-input\\.s21I"]').click();
+        await dm.scrollClickOutput(dmWebView.getByTestId('array-type-editable-record-field-objectOutput.s21O.IN').locator('i'));
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-input.s21I.OUT-to-datamapper-intermediate-port').waitFor({ state: 'attached' });
+        await dmWebView.getByTestId('link-from-datamapper-intermediate-port-to-objectOutput.s21O.IN').waitFor({ state: 'attached' });
+        await dmWebView.getByTestId('link-connector-node-objectOutput.s21O.IN').waitFor({ state: 'attached' });
+
+        // 2D array mapping with mapping function 11-24
+        await dmWebView.locator('[id="recordfield-input\\.m2I"]').click();
+        await dmWebView.getByTestId('array-type-editable-record-field-objectOutput.m2O.IN').locator('i').click();
+        await dmWebView.locator('#menu-item-a2a-inner').click();
+        // await dm.waitForProgressEnd();
+        // await dmWebView.getByTestId('link-from-input.m2I.OUT-to-datamapper-intermediate-port').waitFor({ state: 'attached' });
+        // await dmWebView.getByTestId('link-from-datamapper-intermediate-port-to-objectOutput.m2O.IN').waitFor({ state: 'attached' });
+
+        // await dmWebView.getByTestId('expand-array-fn-m2O').locator('i').click();
+        await dmWebView.getByTestId('focusedInput.m2IItem-node').locator('i').click();
+        await dmWebView.getByTestId('arrayOutput-node').locator('i').first().click();
+        // await dmWebView.locator('#menu-item-a2a-inner').click();
+        // await dm.waitForProgressEnd();
+        // await dmWebView.getByTestId('link-from-focusedInput.m2IItem.OUT-to-datamapper-intermediate-port').waitFor({ state: 'attached' });
+        // await dmWebView.getByTestId('link-from-datamapper-intermediate-port-to-arrayOutput.IN').waitFor({ state: 'attached' });
+
+        // await dmWebView.getByTitle('Map array elements').locator('i').click();
+        await dmWebView.getByTestId('focusedInput.m2IItemItem-node').locator('i').click();
+        await dmWebView.getByTestId('primitiveOutput-node').locator('i').nth(1).click();
+        await dm.waitForProgressEnd();
+        await dmWebView.getByTestId('link-from-focusedInput.m2IItemItem.OUT-to-primitiveOutput.number.IN').waitFor({ state: 'attached' });
+
+        await dmWebView.getByTestId('dm-header-breadcrumb-0').click();
+        await dmWebView.getByTestId('dm-header-breadcrumb-0').waitFor({ state: 'detached' });
 
         expect(dm.verifyTsFileContent('map.ts.cmp')).toBeTruthy();
 
