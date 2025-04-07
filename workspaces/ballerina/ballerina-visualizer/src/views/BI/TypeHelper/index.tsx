c1fded9604c2dc3db703f60e97ae6d43b0a58ad6
madushajg <madushajg@gmail.com>
2025-04-07 20:56:58 +0530

    Add loader on type browser item select

--- workspaces/ballerina/ballerina-visualizer/src/views/BI/TypeHelper/index.tsx
+++ workspaces/ballerina/ballerina-visualizer/src/views/BI/TypeHelper/index.tsx
@@ -7,7 +7,7 @@
  * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
-import { HelperPaneHeight } from "@wso2-enterprise/ui-toolkit";
+import { HelperPaneHeight, Overlay, ThemeColors } from "@wso2-enterprise/ui-toolkit";
 
 import { RefObject } from 'react';
 
@@ -23,6 +23,19 @@ import {
 import { useRpcContext } from '@wso2-enterprise/ballerina-rpc-client';
 import { filterOperators, filterTypes, getImportedTypes, getTypeBrowserTypes, getTypes } from '../TypeEditor/utils';
 import { TYPE_HELPER_OPERATORS } from '../TypeEditor/constants';
+import { useMutation } from "@tanstack/react-query";
+import { createPortal } from "react-dom";
+import { LoadingRing } from "../../../components/Loader";
+import styled from "@emotion/styled";
+
+const LoadingContainer = styled.div`
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    z-index: 5000;
+`;
 
 type TypeHelperProps = {
     fieldKey: string;
@@ -180,13 +193,18 @@ const TypeHelperEl = (props: TypeHelperProps) => {
         [debouncedSearchTypeBrowser]
     );
 
+    const { mutateAsync: addFunction, isLoading: isAddingType  } = useMutation(
+        (item: TypeHelperItem) => 
+            rpcClient.getBIDiagramRpcClient().addFunction({
+                filePath: filePath,
+                codedata: item.codedata,
+                kind: item.kind,
+                searchKind: 'TYPE'
+            })
+    );
+
     const handleTypeItemClick = async (item: TypeHelperItem) => {
-        const response = await rpcClient.getBIDiagramRpcClient().addFunction({
-            filePath: filePath,
-            codedata: item.codedata,
-            kind: item.kind,
-            searchKind: 'TYPE'
-        });
+        const response = await addFunction(item);
 
         if (response) {
             const importStatement = {
@@ -204,24 +222,33 @@ const TypeHelperEl = (props: TypeHelperProps) => {
     };
 
     return (
-        <TypeHelperComponent
-            open={typeHelperState}
-            currentType={currentType}
-            currentCursorPosition={currentCursorPosition}
-            loading={loading}
-            loadingTypeBrowser={loadingTypeBrowser}
-            basicTypes={filteredBasicTypes}
-            importedTypes={importedTypes}
-            operators={filteredOperators}
-            typeBrowserTypes={filteredTypeBrowserTypes}
-            typeBrowserRef={typeBrowserRef}
-            typeHelperHeight={helperPaneHeight}
-            onChange={onChange}
-            onSearchTypeHelper={handleSearchTypeHelper}
-            onSearchTypeBrowser={handleSearchTypeBrowser}
-            onTypeItemClick={handleTypeItemClick}
-            onClose={handleTypeHelperClose}
-        />
+        <>
+            <TypeHelperComponent
+                open={typeHelperState}
+                currentType={currentType}
+                currentCursorPosition={currentCursorPosition}
+                loading={loading}
+                loadingTypeBrowser={loadingTypeBrowser}
+                basicTypes={filteredBasicTypes}
+                importedTypes={importedTypes}
+                operators={filteredOperators}
+                typeBrowserTypes={filteredTypeBrowserTypes}
+                typeBrowserRef={typeBrowserRef}
+                typeHelperHeight={helperPaneHeight}
+                onChange={onChange}
+                onSearchTypeHelper={handleSearchTypeHelper}
+                onSearchTypeBrowser={handleSearchTypeBrowser}
+                onTypeItemClick={handleTypeItemClick}
+                onClose={handleTypeHelperClose}
+            />
+            {isAddingType && createPortal(
+                <>
+                    <Overlay sx={{ background: `${ThemeColors.SURFACE_CONTAINER}`, opacity: `0.7`, zIndex: 5000 }} />
+                    <LoadingContainer> <LoadingRing /> </LoadingContainer>
+                </>
+                , document.body
+            )}
+        </>
     );
 };
 
