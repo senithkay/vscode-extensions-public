1e0e0e607eb70312f381c98efc527fc7ad77a30d
Chinthaka Jayatilake <37581983+ChinthakaJ98@users.noreply.github.com>
2025-04-11 14:57:11 +0530

    Add E2E tests for Ballerina modules

--- workspaces/mi/mi-extension/src/test/e2e-playwright-tests/Utils.ts
+++ workspaces/mi/mi-extension/src/test/e2e-playwright-tests/Utils.ts
@@ -91,6 +91,10 @@ export async function toggleNotifications(disable: boolean) {
 
 }
 
+export async function showNotifications() {
+    await page.executePaletteCommand("Notifications: Show Notifications");
+}
+
 export function initTest(newProject: boolean = false, cleanupAfter?: boolean) {
     test.beforeAll(async ({ }, testInfo) => {
         console.log(`>>> Starting tests. Title: ${testInfo.title}, Attempt: ${testInfo.retry + 1}`);
