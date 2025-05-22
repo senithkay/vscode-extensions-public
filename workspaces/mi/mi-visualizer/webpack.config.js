e4a492009d5623ab29e98ce10caa1e26785336e6
Senith Uthsara <senithkarunarathneu@gmail.com>
2025-05-22 14:46:52 +0530

    mi-visualizer upgrade | stable

--- workspaces/mi/mi-visualizer/webpack.config.js
+++ workspaces/mi/mi-visualizer/webpack.config.js
@@ -1,39 +1,54 @@
 const path = require("path");
-const webpack = require('webpack');
+const webpack = require("webpack");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
-  target: "web",
-  devtool: "eval-cheap-module-source-map",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "Visualizer.js",
-    library: "visualizerWebview",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
-    alias: {
-      'react': path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-      'vscode': path.resolve(__dirname, 'node_modules/vscode-uri'),
-      "crypto": false,
-      "net": false,
-      "os": false,
-      "path": false,
-      "fs": false,
-      "child_process": false,
-    }
-  },
-  module: {
-    rules: [
+    entry: "./src/index.tsx",
+    target: "web",
+    devtool: !process.env.CI ? "eval-source-map" : undefined,
+    mode: !process.env.CI ? "development" : "production",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "Visualizer.js",
+        library: "visualizerWebview",
+        devtoolModuleFilenameTemplate: function (info) {
+            return "file:///" + encodeURI(info.absoluteResourcePath);
+        },
+        publicPath: "http://localhost:9000/",
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+        alias: {
+            'react': path.resolve(__dirname, 'node_modules/react'),
+            'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+             'vscode': path.resolve(__dirname, 'node_modules/vscode-uri'),
+            "crypto": false,
+            "net": false,
+            "os": false,
+            "fs": false,
+            "child_process": false,
+        },
+        fallback: { 'process/browser': require.resolve('process/browser'), }
+    },
+    module: {
+        rules: [
             {
                 test: /\.(ts|tsx)$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
+                loader: "ts-loader",
+                exclude: '/node_modules/',
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules\/(?!typescript)/, // Exclude all node_modules except typescript,
+                loader: "source-map-loader"
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             },
             {
                 test: /\.s[ac]ss$/i,
@@ -41,36 +56,33 @@ module.exports = {
             },
             {
                 test: /\.(woff|woff2|ttf|eot)$/,
-                type: "asset/inline",
+                type: 'asset/inline',
             },
             {
                 test: /\.(svg|png)$/,
-                type: "asset/resource",
+                type: 'asset/resource',
                 generator: {
-                    filename: "./images/[name][ext]",
+                    filename: './images/[name][ext]',
                 },
-            },
+            }
         ],
-  },
-  devServer: {
-    allowedHosts: 'all',
-    port: 9000,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
+        noParse: [require.resolve("@ts-morph/common/dist/typescript.js")],
     },
-    devMiddleware: {
-      mimeTypes: {
-        'text/css': ['css']
-      },
+    devServer: {
+        allowedHosts: 'all',
+        port: 9000,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        devMiddleware: {
+            mimeTypes: { 'text/css': ['css'] },
+        },
+        hot: true,
     },
-    static: path.join(__dirname, "build"),
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-    }),
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+        }),
+        !process.env.CI & new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean),
 };
