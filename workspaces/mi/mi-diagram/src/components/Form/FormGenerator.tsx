c16cdefa5b6c0e9f5bf180c067652adb61ae4a53
Senith Uthsara <senithkarunarathneu@gmail.com>
2025-06-16 11:29:24 +0530

    fix form generator

--- workspaces/mi/mi-diagram/src/components/Form/FormGenerator.tsx
+++ workspaces/mi/mi-diagram/src/components/Form/FormGenerator.tsx
@@ -488,7 +488,7 @@ export function FormGenerator(props: FormGeneratorProps) {
     };
 
     function ParamManagerComponent(element: Element, isRequired: boolean, helpTipElement: JSX.Element, field: any) {
-        return <ComponentCard sx={cardStyle} disbaleHoverEffect>
+        return <ComponentCard id={'parameterManager-' + element.name} sx={cardStyle} disbaleHoverEffect>
             <div style={{ display: 'flex', alignItems: 'center' }}>
                 <Typography variant="h3">{element.displayName}</Typography>
                 {isRequired && (<RequiredFormInput />)}
