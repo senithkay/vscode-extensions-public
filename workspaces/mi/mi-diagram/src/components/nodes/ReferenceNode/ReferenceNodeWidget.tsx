49881cc9d64e42f40efd0acc7cf44f4b8768a375
Thuvarakan Sritharan <sthuva98@gmail.com>
2025-06-19 17:25:05 +0530

    Add reference support for call dataservice

--- workspaces/mi/mi-diagram/src/components/nodes/ReferenceNode/ReferenceNodeWidget.tsx
+++ workspaces/mi/mi-diagram/src/components/nodes/ReferenceNode/ReferenceNodeWidget.tsx
@@ -108,7 +108,7 @@ export function ReferenceNodeWidget(props: ReferenceNodeWidgetProps) {
 
     const getDefinition = async () => {
         let range;
-        if (node.mediatorName === MEDIATORS.SEQUENCE) {
+        if (node.mediatorName === MEDIATORS.SEQUENCE || node.mediatorName === MEDIATORS.DATASERVICECALL) {
             range = node.stNode?.range?.startTagRange ?? node.getParentStNode()?.range?.startTagRange;
         } else if (node.mediatorName === MEDIATORS.AGGREGATE) {
             range = (node.stNode as Aggregate)?.correlateOnOrCompleteConditionOrOnComplete?.onComplete?.range?.startTagRange;
